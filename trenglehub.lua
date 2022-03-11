f5d7cc3a7b7b71f5f0b7b9c2afa3befb8a356932
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-03-11 13:21:00 +0100

    Updated links

--- trenglehub.lua
+++ trenglehub.lua
@@ -14,11 +14,11 @@ if game.PlaceId == 192800 then
     local script = main:NewSection("---Scripts---")
     
     script:NewButton("FE Gui with Autofarm", "Made by ameicaa, distributed through TurkOyuncu99", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/TurkOyuncu99.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Work%20at%20a%20Pizza%20Place/TurkOyuncu99.lua"))()
     end)
 
     script:NewButton("Normal Autofarm", "Made by unknown", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Work%20at%20a%20Pizza%20Place%20Autofarm.lua"))()     
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Work%20at%20a%20Pizza%20Place/Work%20at%20a%20Pizza%20Place%20Autofarm.lua"))()     
     end)
 end
 
