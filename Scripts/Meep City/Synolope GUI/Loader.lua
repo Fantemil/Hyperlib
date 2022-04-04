822364cd30c1f57e057caf87a27d8e38b4266c5c
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-04-04 17:32:08 +0200

    Update Loader.lua

--- Scripts/Meep City/Synolope GUI/Loader.lua
+++ Scripts/Meep City/Synolope GUI/Loader.lua
@@ -1,7 +1,7 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 if game.CreatorId == 123247 then
-local loaderurl = "https://raw.githubusercontent.com/synolope/mpcity/main/loader.lua"
-local scripturl = "https://raw.githubusercontent.com/synolope/mpcity/main/script.lua"
+local loaderurl = "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Meep%20City/Synolope%20GUI/Loader.lua"
+local scripturl = "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Meep%20City/Synolope%20GUI/Script.lua"
 loadstring(game:HttpGet(scripturl,true))()
 syn.queue_on_teleport([[
     wait(3)
