71c7fd4603e68c2660e0d033e68921b627eae061
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-21 22:30:18 +0200

    added UniversalBypasserNwords.lua automatically!

---
+++ Scripts/Autoadd/UniversalBypasserNwords.lua
@@ -0,0 +1,3 @@
+-- Delivered by Synergy. Make sure to join the server at dsc.gg/legoscripts or dsc.gg/synergyyy.
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/daddysyn/synergy/additional/betterbypasser",true))()
