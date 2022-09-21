3112e8f32dc0a880bc91f5d29b776c048f6fb74f
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-21 22:21:38 +0200

    Updated trenglehub.lua automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -533,3 +533,6 @@ addscript(7991339063, "RainbowFriendsAutoCollectItemsFinishAreaTeleportWalkspeed
         
 addscript(4997792989, "HomelessSimulatorInRussiaPetAutofarmInfMoney", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/HomelessSimulatorInRussiaPetAutofarmInfMoney.lua")
                         
+        
+addscript(9993529229, "CounterBloxRemasteredSilentAimEspAimbot", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/CounterBloxRemasteredSilentAimEspAimbot.lua")
+                        
