98098ba6e39ca4c93a30c92fefebee7dbf3e1098
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-21 19:01:28 +0200

    added trenglehub.lua automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -511,3 +511,6 @@ addscriptexist("BloxFruitsGUIAutoFarmAutoLevelMore", "Terni", "https://raw.githu
 addscriptexist("DaHoodFlyPickupKnifeAutoBuy", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DaHoodFlyPickupKnifeAutoBuy.lua")
                 
 addscriptexist("RushPointSkinChanger", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RushPointSkinChanger.lua")
+
+addscript("https://www.roblox.com/games/9431770682/Lost-Rooms-New-Map", "LostRoomsESPFlyFarmandMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LostRoomsESPFlyFarmandMore.lua")
+                
