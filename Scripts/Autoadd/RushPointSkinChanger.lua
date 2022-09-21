8881dbcde43277db92947b3763ac2eeecda52f54
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-21 19:03:23 +0200

    del

--- Scripts/Autoadd/RushPointSkinChanger.lua
+++
@@ -1,3 +0,0 @@
-game:GetService("ReplicatedStorage").Assets.Weapons.Knife.Default:destroy()
-wait(0.5)
-game:GetService("ReplicatedStorage").Assets.Weapons.Knife["name of the knife you want"].Name = "Default"
