d6a7a784b47d19a04275f7ea694e70ea017f5976
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-21 18:59:14 +0200

    added trenglehub.lua automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -496,26 +496,6 @@ end
 
 
 
-                
-                addscriptexist("BloxFruitsGUIAutoFarmAutoLevelMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BloxFruitsGUIAutoFarmAutoLevelMore.lua")
 
-                addscript("https://www.roblox.com/games/2788229376/Da-Hood", "DaHoodFlyPickupKnifeAutoBuy", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DaHoodFlyPickupKnifeAutoBuy.lua")
                 
-
-                addscript("https://www.roblox.com/games/5993942214/ETHEREAL-SKIN-Rush-Point", "RushPointSkinChanger", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RushPointSkinChanger.lua")
-                
-
-                addscript("https://www.roblox.com/games/9431770682/Lost-Rooms-New-Map", "LostRoomsESPFlyFarmandMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LostRoomsESPFlyFarmandMore.lua")
-                
-
-                addscript("https://www.roblox.com/games/21532277/Notoriety", "NotorietyWalkspeedFlyTpBypass", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NotorietyWalkspeedFlyTpBypass.lua")
-                
-
-                addscript("https://www.roblox.com/games/9072679513/UPDATE-6-Anime-Impact-Simulator#ropro-quick-play", "AnimeImpactSimulatorAutoTrainAutoRankAutoEnergy", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AnimeImpactSimulatorAutoTrainAutoRankAutoEnergy.lua")
-                
-
-                addscript("https://www.roblox.com/games/6461766546/COSMIC-5X-STATS-A-Heros-Destiny#ropro-quick-play", "AHerosDestinyAutoFarmAutoTrainAutoPunch", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AHerosDestinyAutoFarmAutoTrainAutoPunch.lua")
-                
-
-                addscript("https://www.roblox.com/games/183364845/Speed-Run-4", "SpeedRun4AutoFarmCompleteDimensionnMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SpeedRun4AutoFarmCompleteDimensionnMore.lua")
-                
+addscriptexist("BloxFruitsGUIAutoFarmAutoLevelMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BloxFruitsGUIAutoFarmAutoLevelMore.lua")
