7e7e5b6cdccb2cc8014e81e83342908c51e3ac2a
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-21 22:31:56 +0200

    Updated trenglehub.lua automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -670,3 +670,5 @@ addscript(1428232912, "MurderMystery2XRayChamsEspAutoFarms", "astrofile", "https
                         
                         
 addscriptexist(3177438863, "DragonBloxAutoMultiplierZoneAutoAttackAutoRebirth", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DragonBloxAutoMultiplierZoneAutoAttackAutoRebirth.lua")
+                        
+addscriptexist(3177438863, "DragonBloxAutoAttackAutoRebirth", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DragonBloxAutoAttackAutoRebirth.lua")
