e002a9b9efc5bea3996eccee21ea88c79a9d8e68
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-21 22:22:29 +0200

    Updated trenglehub.lua automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -545,3 +545,6 @@ addscript(10192063645, "ThyHoodToolCollector", "Terni", "https://raw.githubuserc
         
 addscript(5956785391, "ProjectSlayersStunEffectServerBreaker", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ProjectSlayersStunEffectServerBreaker.lua")
                         
+        
+addscript(488667523, "ThoseWhoRemainItemESP", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ThoseWhoRemainItemESP.lua")
+                        
