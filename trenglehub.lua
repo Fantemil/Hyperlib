68b8fa0eda4e0c13ee24a0f36b0c3e991653cd7c
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-22 21:28:48 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -704,3 +704,5 @@ addscript(621129760, "KnifeAbilityTestSilentAimAimTorsoFov", "astrofile", "https
         
 addscript(2860904296719118471981788929662860904290111958650, "ArsenalAimlockEspAimKey", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ArsenalAimlockEspAimKey.lua")
                         
+                        
+addscriptexist(2788229376, "DaHoodSilentAimanotherversion", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DaHoodSilentAimanotherversion.lua")
