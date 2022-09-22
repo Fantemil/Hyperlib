10d9f6449c65eb155bf80d165a00b6a160c2b00c
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-22 21:54:14 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -847,3 +847,5 @@ addscript(62845830303, "PetSimulatorXAutofarmTroll", "rbxscript", "https://raw.g
                         
                         
 addscriptexist(6284583030, "PetSimulatorXAutoFarmAutoOpenEggStatTacker", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PetSimulatorXAutoFarmAutoOpenEggStatTacker.lua")
+                        
+addscriptexist(62845830303, "PetSimulatorXEggOfManyGiftsAutoBuy", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PetSimulatorXEggOfManyGiftsAutoBuy.lua")
