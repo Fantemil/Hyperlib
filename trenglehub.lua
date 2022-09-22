93818f55af213c7b77ae2c00250be7214007f5b7
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-22 21:25:20 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -660,3 +660,5 @@ addscript(106311815871102, "StoneMinerSimulator2ClaimAllCodesClaimsAllloginRewar
                         
                         
 addscriptexist(2788229376, "DaHoodCashCollectFlyNoclip", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DaHoodCashCollectFlyNoclip.lua")
+                        
+addscriptexist(106311815871102, "StoneMinerSimulator2AutoRebirth", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/StoneMinerSimulator2AutoRebirth.lua")
