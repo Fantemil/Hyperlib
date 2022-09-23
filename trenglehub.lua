f6f70e6b396726c83541a733bf181933fc024b91
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 00:08:14 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1133,3 +1133,5 @@ addscript(5505830188,"Arcane Oblivion RPG ", "ArcaneOblivionRPGAutoBossAutoFarm"
                     
                     
 addscriptexist(2753915549, "RobloxFEChainsaw", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RobloxFEChainsaw.lua")
+                    
+addscriptexist(621129760, "KnifeAbilityTestGodmodeSeverBreak", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/KnifeAbilityTestGodmodeSeverBreak.lua")
