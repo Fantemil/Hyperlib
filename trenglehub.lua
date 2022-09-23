60fecb8efdc3615ed576a06f6473971850178533
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 00:04:04 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -998,3 +998,5 @@ addscript(6874129651,"Vynixus Teleport Tool ", "VynixusTeleportToolWorksForJailb
 addscriptexist(537413528, "BuildABoatForTreasureGUIAutoFarmUtilitesMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BuildABoatForTreasureGUIAutoFarmUtilitesMore.lua")
                     
 addscriptexist(1428232912, "MurderMystery2GUIGodModeKillAuraMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MurderMystery2GUIGodModeKillAuraMore.lua")
+                    
+addscriptexist(87509976474, "TappingLegendsXGUIAutoClickAutoRebirthMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TappingLegendsXGUIAutoClickAutoRebirthMore.lua")
