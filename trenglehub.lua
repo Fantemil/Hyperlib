ac34d75ff1a6bf1ed990d900af98b381ad3d2e67
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 00:13:49 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1316,3 +1316,6 @@ addscriptexist(6753004901, "SwingCityUnlockAllSkinsMore", "Terni", "https://raw.
 addscriptexist(736920834117, "SuperHeroUniverse17InfiniteCoins", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SuperHeroUniverse17InfiniteCoins.lua")
                     
 addscriptexist(2066381781, "DeadlySinsRetributionTeleportToIstar", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DeadlySinsRetributionTeleportToIstar.lua")
+    
+addscript(8704181738,"Tien Tien Piece [Auto Farm Gems In Second Sea]", "TienTienPieceAutoFarmGemsInSecondSea", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TienTienPieceAutoFarmGemsInSecondSea.lua")
+                    
