508999dbc2afb72771932134e8d4feabb50a7014
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-23 23:57:29 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -823,3 +823,6 @@ addscriptexist(155615604, "PrisonLifeGunModeGiveGunWalkspeed", "astrofile", "htt
     
 addscript(8508303900162,"Tower of Flood Escape 2 ", "TowerofFloodEscape2FreeOpenCase", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TowerofFloodEscape2FreeOpenCase.lua")
                     
+    
+addscript(87509976474,"Tapping legends X ", "TappinglegendsXAutoClickAutoRebirthAutoHatch", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TappinglegendsXAutoClickAutoRebirthAutoHatch.lua")
+                    
