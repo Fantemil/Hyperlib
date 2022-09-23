f8511b9f9b910f53186edd8f686e3963c39e3f90
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-23 23:55:33 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/KnifeAbilityTestWalkspeedJumppowerEsp.lua
@@ -0,0 +1,2 @@
+-- Current Key DGCommunity_858
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Margman/whynot/main/Knife%20Ability%20Test%202022"))() 
--- trenglehub.lua
+++ trenglehub.lua
@@ -775,3 +775,5 @@ addscript(1067652383411,"Racing Rocket ", "RacingRocketAutoClickPowerAutoHatchAu
 addscriptexist(2788229376, "DaHoodAimLock", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DaHoodAimLock.lua")
                     
 addscriptexist(10320240572, "EasyRaceClickerFastWinAutoFarm", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/EasyRaceClickerFastWinAutoFarm.lua")
+                    
+addscriptexist(621129760, "KnifeAbilityTestWalkspeedJumppowerEsp", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/KnifeAbilityTestWalkspeedJumppowerEsp.lua")
