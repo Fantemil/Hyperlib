a4fdcca4b6c948f257bca43f16c8629561c728f1
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 00:08:43 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1146,3 +1146,5 @@ addscript(10647156264,"Become a Nextbot ", "BecomeaNextbotTeleportsWalkspeedGetR
                     
                     
 addscriptexist(94171973341, "AnimeStoryAutoFarmSemiInstantKill", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AnimeStoryAutoFarmSemiInstantKill.lua")
+                    
+addscriptexist(9662949425, "PunchabrickwallsimulatorInfiniteBonesAutoRebirth", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PunchabrickwallsimulatorInfiniteBonesAutoRebirth.lua")
