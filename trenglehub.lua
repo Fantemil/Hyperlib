7d02e2dc2175b11b5c47c5bc8eb3e54aa18e93ee
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 00:15:36 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1381,3 +1381,6 @@ addscript(30178705742,"2 Player Evolution Tycoon [Auto Farm]", "2PlayerEvolution
 addscriptexist(10676523834, "RacingRocketFarmStarFast", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RacingRocketFarmStarFast.lua")
                     
 addscriptexist(1833648454, "SpeedRun4InfGemsAutoStars", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SpeedRun4InfGemsAutoStars.lua")
+    
+addscript(8260276694,"Ability Wars [Combat Bot]", "AbilityWarsCombatBot", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AbilityWarsCombatBot.lua")
+                    
