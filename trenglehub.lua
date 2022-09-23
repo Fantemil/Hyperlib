cab6b222e22e060ab97ed42dbed0c8de48e95232
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-23 21:40:03 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -501,3 +501,6 @@ end
         
 addscript(6403373529,"Slap Battles: AutoFarm Slapples, Trap AutoFarm", "SlapBattlesAutoFarmSlapplesTrapAutoFarm", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SlapBattlesAutoFarmSlapplesTrapAutoFarm.lua")
                         
+        
+addscript(185655149,"Bloxburg: Automatic Pizza Delivery", "BloxburgAutomaticPizzaDelivery", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BloxburgAutomaticPizzaDelivery.lua")
+                        
