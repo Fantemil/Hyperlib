e93b358ee37ba47ac3551d4a6b2b5c39dbc8384e
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-23 23:15:15 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -575,3 +575,6 @@ addscript(9304358188104,"Find The Memes ", "FindTheMemesGetAllMemes", "Terni", "
     
 addscript(9585537847,"SHADOVIS RPG ", "SHADOVISRPGGUIMultiFarmKillAuraMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SHADOVISRPGGUIMultiFarmKillAuraMore.lua")
                     
+    
+addscript(5796917097,"Road to Grambys ", "RoadtoGrambysGodModeFlyMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RoadtoGrambysGodModeFlyMore.lua")
+                    
