f75680b961cf2ba13db734eed9d817fd1412c1ab
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 00:00:51 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -915,3 +915,5 @@ addscript(2686040248,"Math Obby ", "MathObbyShowCorrectDoors", "astrofile", "htt
     
 addscript(8540168650,"Stand Upright", "StandUprightRebootedAutoFarmItemsFarmAutoBuy", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/StandUprightRebootedAutoFarmItemsFarmAutoBuy.lua")
                     
+                    
+addscriptexist(50630210801, "MyModdedDayWalkspeedGraballItemsTeleports", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MyModdedDayWalkspeedGraballItemsTeleports.lua")
