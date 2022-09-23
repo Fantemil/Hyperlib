d9dc1518133e9dd504396e4b5d7ecebb48e8af8b
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-23 23:58:56 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -863,3 +863,5 @@ addscript(9498006165,"Tapping Simulator! ", "TappingSimulatorRemoveEggHatchandCl
                     
                     
 addscriptexist(2248408710, "DestructionSimulatorAddCoinsResetRemoveCooldown", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DestructionSimulatorAddCoinsResetRemoveCooldown.lua")
+                    
+addscriptexist(2788229376, "DaHoodSmoothAimLock", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DaHoodSmoothAimLock.lua")
