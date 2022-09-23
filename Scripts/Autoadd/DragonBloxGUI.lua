c72674ae892bb64accb595232e2249236c66dd2c
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-23 23:44:22 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/DragonBloxGUI.lua
@@ -0,0 +1,10 @@
+getgenv().AutoFarm = false
+getgenv().AutoPunch = false
+getgenv().FarmDistance = 5
+getgenv().FarmMeth = "Below" -- Below or Above
+getgenv().npcName = "Tigreal Tribe" -- check npcs with dex
+
+getgenv().questNPC = workspace.Misc.NPC.Quests.QuestNPCMain2_ForestArea -- check quest npcs with dex
+getgenv().questNumber = 2
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LioK251/RbScripts/main/dragon_blox.lua"))()
