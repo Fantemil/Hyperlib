a4d26db8aa3f93fb34440349f91730b63f7f2073
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 18:05:05 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1965,3 +1965,6 @@ addscript(9796315265,"Streetz War [Auto Farm]", "StreetzWarAutoFarm", "Qalnik", 
 addscriptexist(9285238704, "RaceClickerAutoClickAutoWin", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RaceClickerAutoClickAutoWin.lua")
                     
 addscriptexist(286090429, "ArsenalKillAllInvisible", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ArsenalKillAllInvisible.lua")
+    
+addscript(6766156863,"Strongman Simulator [Anti Afk, AutoRebirth]", "StrongmanSimulatorAntiAfkAutoRebirth", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/StrongmanSimulatorAntiAfkAutoRebirth.lua")
+                    
