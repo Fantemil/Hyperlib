e6d5cf19a58348d07d9f5a56a2c881fc60ff706e
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:22:25 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1268,3 +1268,5 @@ addscript(7075737729,"Hoop Central 6 ", "HoopCentral6AntiCheatBypasssBypassItemB
 addscriptexist(6753004901, "SwingCityUnlockAllSkillsFarmMoneyExpFAST", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SwingCityUnlockAllSkillsFarmMoneyExpFAST.lua")
                     
 addscriptexist(4639625707, "WarfareTycoonKillPlayersKillitemKillAll", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/WarfareTycoonKillPlayersKillitemKillAll.lua")
+                    
+addscriptexist(2788229376, "DaHoodNoClipAutoFarmFly", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DaHoodNoClipAutoFarmFly.lua")
