a09e61e854db3d0cbf1f89c1b2d9bc404af613f8
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:00:16 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/iHeartLandMusicTycoonGetFreeItem.lua
@@ -0,0 +1,5 @@
+for i = 1,100 do
+wait(0.3)
+ game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = 
+ game:GetService("Workspace").ScavengerHunt.IheartScavengerHunt["iHeart_"..i].Collider.CFrame
+ end
