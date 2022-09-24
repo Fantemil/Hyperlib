ee1200c654a65303a803ca8f497561b6bf643b27
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 20:30:10 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -4327,3 +4327,5 @@ addscriptexist(9568982793, "DaHoodBattlegroundsSilentAimbot", "Terni", "https://
 addscriptexist(4459593483, "FORCETeleportToTrinkets", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FORCETeleportToTrinkets.lua")
                     
 addscriptexist(4459593483, "FORCESellTrinkets", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FORCESellTrinkets.lua")
+                    
+addscriptexist(9772878203, "RaiseAFloppa2GUIAutoClickFloppaAutoCollectMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RaiseAFloppa2GUIAutoClickFloppaAutoCollectMore.lua")
