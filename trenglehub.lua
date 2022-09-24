ebb0148b531cdb2f2baa9e87314d33bcf4efed8e
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:06:32 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -974,3 +974,5 @@ addscriptexist(2788229376, "DaHoodTeleportsEspAimlock", "astrofile", "https://ra
 addscriptexist(142823291, "MurderMystery2SilentAimSpeedHaxKillAll", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MurderMystery2SilentAimSpeedHaxKillAll.lua")
                     
 addscriptexist(4924922222, "BrookhavenRPTeleports", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BrookhavenRPTeleports.lua")
+                    
+addscriptexist(3956818381, "NinjaLegendsAutoSwingAutoSellUnlockAllElements", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NinjaLegendsAutoSwingAutoSellUnlockAllElements.lua")
