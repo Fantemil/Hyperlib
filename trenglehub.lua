1c5dac1394660074df7594e8bc79af28a777e2df
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:09:48 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1030,3 +1030,5 @@ addscriptexist(8750997647, "TappingLegendsXGUIAutoClickAutoRebirthMore", "Terni"
 addscriptexist(8884433153, "CollectAllPetsAutoClaimDailyEggAutoFarmTPEgg", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/CollectAllPetsAutoClaimDailyEggAutoFarmTPEgg.lua")
                     
 addscriptexist(2788229376, "DaHoodSimpleAimLock", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DaHoodSimpleAimLock.lua")
+                    
+addscriptexist(9264596435, "IdleHeroesSimulatorAutoFarmAutoUpgrade", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/IdleHeroesSimulatorAutoFarmAutoUpgrade.lua")
