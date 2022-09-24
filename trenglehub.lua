30a87a1462e47185ae33ccd2b20bf6e67e84314a
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 22:04:12 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -5644,3 +5644,5 @@ addscriptexist(9049840490, "SonicSpeedSimulatorNewFasterAutoFarm", "Terni", "htt
 addscriptexist(9049840490, "SonicSpeedSimulatorAutoOpenChests", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SonicSpeedSimulatorAutoOpenChests.lua")
                     
 addscriptexist(9530846958, "Epsilon2GUIV1AutoTrinketMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Epsilon2GUIV1AutoTrinketMore.lua")
+                    
+addscriptexist(155382109, "SurviveandkillthekillersinArea51GUIGiveAllGuns", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SurviveandkillthekillersinArea51GUIGiveAllGuns.lua")
