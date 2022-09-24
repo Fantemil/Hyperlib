b0d03bacce622c5a7c9b6b35164db269724d7251
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 00:31:56 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -7526,3 +7526,6 @@ addscript(6879598157,"Roblox [Animation Logger]", "RobloxAnimationLogger", "Qaln
 addscriptexist(8592863835, "MagnetSimulator218XAutoSellScript", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MagnetSimulator218XAutoSellScript.lua")
                     
 addscriptexist(574407221, "SuperHeroTycoonStealMoneyOnceStealMoneyCratesDeletelaserDoors", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SuperHeroTycoonStealMoneyOnceStealMoneyCratesDeletelaserDoors.lua")
+    
+addscript(6484864709,"Gym Tycoon [Auto WorkOut, Redeem Codes, Collect Drops]", "GymTycoonAutoWorkOutRedeemCodesCollectDrops", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/GymTycoonAutoWorkOutRedeemCodesCollectDrops.lua")
+                    
