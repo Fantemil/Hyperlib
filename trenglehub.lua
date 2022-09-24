4614c6e948780ae5188d2dce6b2476ac57e6300f
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:44:27 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1631,3 +1631,5 @@ addscript(6278885452,"Roblox [WalkSpeed GUI]", "RobloxWalkSpeedGUI", "Qalnik", "
                     
                     
 addscriptexist(2537430692, "JengaNoKillBrickFLy", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/JengaNoKillBrickFLy.lua")
+                    
+addscriptexist(2537430692, "JengaEndGameMoai", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/JengaEndGameMoai.lua")
