6b3a0c3f66b7c574113f94f2ffc0d5306f8b5c98
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 23:07:52 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -6399,3 +6399,5 @@ addscriptexist(2414851778, "DungeonQuestAutoFarmAutoSkillWalkSpeed", "Qalnik", "
 addscriptexist(5116869569, "DoodleWorldAttackWildDoodlesAnywhere", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DoodleWorldAttackWildDoodlesAnywhere.lua")
                     
 addscriptexist(8492553855, "ProjectUprightNewAutoFarmGUI", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ProjectUprightNewAutoFarmGUI.lua")
+                    
+addscriptexist(8781849572, "StarSimulatorBetaCompleteAutoFarmGUI", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/StarSimulatorBetaCompleteAutoFarmGUI.lua")
