7dc8c1085448405163107c9d7602dc7e243657de
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 23:01:10 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -6312,3 +6312,5 @@ addscriptexist(662417684, "LUCKYBLOCKSBattlegroundsLuckyBlockclicktpinfitejump",
 addscriptexist(9049840490, "SonicSpeedSimulatorGet15KCoinsEvery6Hours", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SonicSpeedSimulatorGet15KCoinsEvery6Hours.lua")
                     
 addscriptexist(6878438976, "DeveloperInc10KCashIn30Seconds", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DeveloperInc10KCashIn30Seconds.lua")
+                    
+addscriptexist(386329050, "TheBlackMesaIncidentFreeGamepassGun", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TheBlackMesaIncidentFreeGamepassGun.lua")
