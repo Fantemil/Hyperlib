5e4bb9117524c0f386ab1eeee65f1f18b962d6c2
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:54:37 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1813,3 +1813,5 @@ addscript(5901548022,"Try To Die [Give All Tools]", "TryToDieGiveAllTools", "ast
     
 addscript(7165194252,"Ranger Piece [Get Spawned Items]", "RangerPieceGetSpawnedItems", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RangerPieceGetSpawnedItems.lua")
                     
+                    
+addscriptexist(662417684, "LUCKYBLOCKSBattlegroundsOpenLuckyBlockSpeed", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LUCKYBLOCKSBattlegroundsOpenLuckyBlockSpeed.lua")
