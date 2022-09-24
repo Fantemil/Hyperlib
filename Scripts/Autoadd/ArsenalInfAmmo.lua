8292b92bb0ca2f8f680f395da7dc4c79a0630486
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:39:38 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/ArsenalInfAmmo.lua
@@ -0,0 +1,4 @@
+while wait() do
+ game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
+ game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
+end
