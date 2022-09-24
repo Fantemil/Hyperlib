1eb57985774d0e73ca94590cb9c9640a487d8c5f
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:42:04 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1590,3 +1590,5 @@ addscript(3260590327,"Tower Defense Simulator [Auto Farm]", "TowerDefenseSimulat
 addscriptexist(4042427666, "AnimeFightingSimulatorInfYenInfChikara", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AnimeFightingSimulatorInfYenInfChikara.lua")
                     
 addscriptexist(6299805723, "AnimeFightersSimulatorAutoFarmAutoClick", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AnimeFightersSimulatorAutoFarmAutoClick.lua")
+                    
+addscriptexist(8737602449, "PLSDONATEServerHopAutoBeg", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PLSDONATEServerHopAutoBeg.lua")
