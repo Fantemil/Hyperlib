fe363c35077bb09583ec1048a975641c9868c6f8
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 22:19:43 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -5901,3 +5901,5 @@ addscript(8395560237,"Kick Off [Free GUI – OP Features]", "KickOffFreeGUI–OP
                     
                     
 addscriptexist(8069117419, "DemonSoulSimulatorAutoAttackCollectSoursAutoPower", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DemonSoulSimulatorAutoAttackCollectSoursAutoPower.lua")
+                    
+addscriptexist(6677985923, "MillionaireEmpireTycoonInfiniteCashAutoBuyAutoRebirth", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MillionaireEmpireTycoonInfiniteCashAutoBuyAutoRebirth.lua")
