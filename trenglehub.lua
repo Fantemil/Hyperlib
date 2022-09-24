dc08602983263de618e9f3f71ff6f925ec1ea9e9
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 23:13:52 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -6490,3 +6490,5 @@ addscript(6551386772,"Battle Royale Simulator [Hit Box, AIMBOT, ESP]", "BattleRo
                     
                     
 addscriptexist(5670218884, "ItemasylumGiveweapons", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ItemasylumGiveweapons.lua")
+                    
+addscriptexist(8737602449, "PLSDONATERainbowModdedGoal", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PLSDONATERainbowModdedGoal.lua")
