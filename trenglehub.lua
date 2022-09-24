ae15cc6d42503d772f2d6a6b7fb6fd8b575015df
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:29:49 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1371,3 +1371,6 @@ addscript(8130299583,"Trident Survival [GUI - Esp, Aimbot & More!]", "TridentSur
     
 addscript(10106105124,"Wheat Farming Simulator [Spawn Any Mythical Pet]", "WheatFarmingSimulatorSpawnAnyMythicalPet", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/WheatFarmingSimulatorSpawnAnyMythicalPet.lua")
                     
+    
+addscript(9744954014,"THA HOOD!!! [Walkspeed, Godmode]", "THAHOODWalkspeedGodmode", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/THAHOODWalkspeedGodmode.lua")
+                    
