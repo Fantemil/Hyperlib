d23d5f17c3fc19874ef918f87fc8aa1a39b31f12
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 01:04:43 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -8076,3 +8076,5 @@ addscript(6830111083,"Sandy Shores [Kill All Players]", "SandyShoresKillAllPlaye
 addscriptexist(583507031, "PapersPleaseGunMode", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PapersPleaseGunMode.lua")
                     
 addscriptexist(5112591059, "NinjamanSimulatorInfiniteStrength", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NinjamanSimulatorInfiniteStrength.lua")
+                    
+addscriptexist(7666393362, "TowerofGuessingInfiniteSkips", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TowerofGuessingInfiniteSkips.lua")
