a49f7c346080f31e2786ff5ad7ebb8e8e0d7e639
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 00:34:00 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -7560,3 +7560,5 @@ addscript(7499189111,"Encounters [Kill Player, Kick Player, inf Mana]", "Encount
                     
                     
 addscriptexist(3956818381, "NinjaLegendsAutoSwingAutoEquipSwordAutoSell", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NinjaLegendsAutoSwingAutoEquipSwordAutoSell.lua")
+                    
+addscriptexist(6872265039, "BedWarsAimBotinvisibilityinstantBreak", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BedWarsAimBotinvisibilityinstantBreak.lua")
