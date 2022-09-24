7cf0ad40b79a757b0ba23b9ff1f6fe1ff9267cb9
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 22:23:38 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -5977,3 +5977,6 @@ addscript(9346039031,"My Hello Kitty Cafe [Roblox Event Auto Farm]", "MyHelloKit
 addscriptexist(9346039031, "MyHelloKittyCafeAutoServeCoffeeAutoCleanSeatsMore", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MyHelloKittyCafeAutoServeCoffeeAutoCleanSeatsMore.lua")
                     
 addscriptexist(3467049496, "MARVELBattlegroundsGetAllGamepasses", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MARVELBattlegroundsGetAllGamepasses.lua")
+    
+addscript(171391948,"Vehicle Simulator [Auto Farm GUI]", "VehicleSimulatorAutoFarmGUI", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/VehicleSimulatorAutoFarmGUI.lua")
+                    
