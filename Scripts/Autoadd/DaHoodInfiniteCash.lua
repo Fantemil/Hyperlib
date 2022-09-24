597a12521cdb1be5b58829c4934c70316eba22fb
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 21:28:36 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/DaHoodInfiniteCash.lua
@@ -0,0 +1,10 @@
+ 
+
+
+-- Made by FinestAngel On https://forum.robloxscripts.com
+
+_G.Cash = "$999,777,771" --make sure to add the "," and the "$" at the front or it wont work. EX : $9,000
+
+game:GetService("Players").LocalPlayer.PlayerGui.MainScreenGui.MoneyText.Text = _G.Cash
+game:GetService("Players").LocalPlayer.Backpack.Wallet.Handle.BillboardGui.TextLabel.Text = _G.Cash
+game:GetService("Players").LocalPlayer.Backpack.Wallet.Handle.BillboardGui.TextLabel.Value = _G.Cash
