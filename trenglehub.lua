44efa87b640a76c7629a9a23c6b9563adf0324ec
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 16:59:29 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -818,3 +818,6 @@ addscript(9414511685,"Drive Cars Down A Hill! ", "DriveCarsDownAHillCrashServer"
     
 addscript(4042427666,"Anime Fighting Simulator ", "AnimeFightingSimulatorFlyAutoFarmAutoQuests", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AnimeFightingSimulatorFlyAutoFarmAutoQuests.lua")
                     
+    
+addscript(2474168535,"Westbound ", "WestboundAutoFarm", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/WestboundAutoFarm.lua")
+                    
