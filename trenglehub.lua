26a5ea49d0f9c50f0452207b5548e73229259c69
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:19:28 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1213,3 +1213,6 @@ addscript(9112775301,"Da Hood Fights l Auto Steal", "DaHoodFightslAutoSteal", "a
                     
                     
 addscriptexist(286090429, "ArsenalESP", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ArsenalESP.lua")
+    
+addscript(1343871267,"Fredbear's Mega Roleplay ", "FredbearsMegaRoleplayTransformAllMutatePlayerorallAnimatronic", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FredbearsMegaRoleplayTransformAllMutatePlayerorallAnimatronic.lua")
+                    
