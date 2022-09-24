ebd7477dac7ca7f57e890dd81e9d8da951f3ad59
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:15:58 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1150,3 +1150,6 @@ addscriptexist(2753915549, "RobloxGPUSaver", "Qalnik", "https://raw.githubuserco
     
 addscript(298400657,"Dragon Ball Final Remastered ", "DragonBallFinalRemasteredGUIInfiniteKiInfiniteStamina", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DragonBallFinalRemasteredGUIInfiniteKiInfiniteStamina.lua")
                     
+    
+addscript(7560156054,"Clicker Simulator ", "ClickerSimulatorAutoRebirthAutoClick", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ClickerSimulatorAutoRebirthAutoClick.lua")
+                    
