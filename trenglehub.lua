082d6de2aae2ae80a64737a1d88e741c618dcb1f
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:34:43 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1451,3 +1451,5 @@ addscript(9845705613,"Blood Hood [Godmode, KillAll]", "BloodHoodGodmodeKillAll",
                     
                     
 addscriptexist(7138009149, "PHIGHTINGNPCesp", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PHIGHTINGNPCesp.lua")
+                    
+addscriptexist(9744954014, "THAHOODGodmodeInfAmmo", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/THAHOODGodmodeInfAmmo.lua")
