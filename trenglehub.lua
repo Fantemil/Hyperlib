8cfec9c358e22f1ebb44ffdfcc2c229147233494
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 20:21:34 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -4215,3 +4215,5 @@ addscript(6155643742,"Fall Block [Click To Activate Tiles Tool]", "FallBlockClic
 addscriptexist(6155643742, "FallBlockRuinGame", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FallBlockRuinGame.lua")
                     
 addscriptexist(8554378337, "SwordSimulatorAutoFarm", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SwordSimulatorAutoFarm.lua")
+                    
+addscriptexist(4459593483, "FORCEPREALPHAGUITeleports", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FORCEPREALPHAGUITeleports.lua")
