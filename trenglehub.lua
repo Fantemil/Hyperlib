a442a17b874254c9049fcda8963a1c47650e9be0
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 18:24:41 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -2260,3 +2260,5 @@ addscript(10351639671,"Anime Legends Simulator [Auto Complete Event]", "AnimeLeg
 addscriptexist(9585537847, "SHADOVISRPGProjectiveSpam", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SHADOVISRPGProjectiveSpam.lua")
                     
 addscriptexist(7009799230, "PressureWashSimulatorInfEvrthInfMoney", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PressureWashSimulatorInfEvrthInfMoney.lua")
+                    
+addscriptexist(10524502174, "OnePunchFightersAutoFarmAutoClick", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/OnePunchFightersAutoFarmAutoClick.lua")
