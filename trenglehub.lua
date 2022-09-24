96b0ae25965596643da06ed5da52902246bc4a11
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 00:12:30 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -7263,3 +7263,5 @@ addscript(8288069630,"Stone Miner Simulator [Infinite Gems - Free Gold - Auto Se
                     
                     
 addscriptexist(6422372837, "SnipersSniperMod", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SnipersSniperMod.lua")
+                    
+addscriptexist(7167319176, "ChicagoRemasteredMoneyFarm", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ChicagoRemasteredMoneyFarm.lua")
