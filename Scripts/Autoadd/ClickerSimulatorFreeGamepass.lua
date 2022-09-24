091802085d4eed15b270f1c9ade048958058bfd7
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 20:05:09 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/ClickerSimulatorFreeGamepass.lua
@@ -0,0 +1,4 @@
+local gamepasses = ";autoclicker;;autorebirth;;doubleclicks;"
+
+game.Players.LocalPlayer.Data.gamepasses.Value = game.Players.LocalPlayer.Data.gamepasses.Value .. gamepasses
+game.Players.LocalPlayer.Data.unlockedZones.Value = game.Players.LocalPlayer.Data.unlockedZones.Value .. ";Lava;"
--- trenglehub.lua
+++ trenglehub.lua
@@ -3923,3 +3923,5 @@ addscript(9884452497,"Bloodlust [Infinite Money]", "BloodlustInfiniteMoney", "Te
 addscriptexist(185655149, "BloxburgAutoBuild", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BloxburgAutoBuild.lua")
                     
 addscriptexist(6677985923, "MillionaireEmpireTycoonAutoFarm", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MillionaireEmpireTycoonAutoFarm.lua")
+                    
+addscriptexist(7560156054, "ClickerSimulatorFreeGamepass", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ClickerSimulatorFreeGamepass.lua")
