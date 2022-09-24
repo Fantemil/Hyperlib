514c32705ec1ec3c2524bf22f0f0eb90e83e980c
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 21:00:34 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -4808,3 +4808,5 @@ addscript(5272267802,"Be a Toy! [Auto Code Farm]", "BeaToyAutoCodeFarm", "Terni"
 addscriptexist(6938803436, "AnimeDimensionsSimulatorAutoAttackAutoSkills", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AnimeDimensionsSimulatorAutoAttackAutoSkills.lua")
                     
 addscriptexist(9598746251, "HomeRunSimulatorAutoHomeRun", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/HomeRunSimulatorAutoHomeRun.lua")
+                    
+addscriptexist(8508161757, "RaiseanAlbertInfMoney", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RaiseanAlbertInfMoney.lua")
