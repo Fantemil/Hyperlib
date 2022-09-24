dcadcf27b238831f1a23cb5544598022df6de98a
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 16:46:05 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -550,3 +550,6 @@ addscriptexist(2788229376, "DaHoodSilentAim", "astrofile", "https://raw.githubus
     
 addscript(6516141723,"DOORS", "DOORSWalkspeedFullBrightFOV", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORSWalkspeedFullBrightFOV.lua")
                     
+    
+addscript(6403373529,"Slap Battles", "SlapBattlesAutoFarmSlapplesTrapAutoFarm", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SlapBattlesAutoFarmSlapplesTrapAutoFarm.lua")
+                    
