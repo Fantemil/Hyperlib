2d6af6d0ec8aa6e74b1a78b0b4f2d679ffb4e042
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 21:42:15 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -5322,3 +5322,5 @@ addscript(8006400183,"Roblox [UIBlox Toast onto your screen]", "RobloxUIBloxToas
                     
                     
 addscriptexist(534701013, "TheLabyrinthAntigrassAntinameXray", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TheLabyrinthAntigrassAntinameXray.lua")
+                    
+addscriptexist(7940932746, "RobloxFullBright", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RobloxFullBright.lua")
