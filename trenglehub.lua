b6f8f2c98a02894e5aad7034e0bd615d06c61813
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:31:14 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1400,3 +1400,5 @@ addscriptexist(10676523834, "RacingRocketAutobuyEgg", "Qalnik", "https://raw.git
     
 addscript(3017870574,"2 Player Evolution Tycoon [Auto Farm]", "2PlayerEvolutionTycoonAutoFarm", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/2PlayerEvolutionTycoonAutoFarm.lua")
                     
+                    
+addscriptexist(10676523834, "RacingRocketFarmStarFast", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RacingRocketFarmStarFast.lua")
