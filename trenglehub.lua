1b9903f32aa5d252d0084e13371e3e377a5b44c9
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:26:59 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1350,3 +1350,6 @@ addscript(6884476776,"Friend Hangout [Crash - Get All Tools & More!]", "FriendHa
                     
                     
 addscriptexist(10676523834, "RacingRocketInfiniteSpeed", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RacingRocketInfiniteSpeed.lua")
+    
+addscript(113690124,"Noob Onslaught [Inf Money]", "NoobOnslaughtInfMoney", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NoobOnslaughtInfMoney.lua")
+                    
