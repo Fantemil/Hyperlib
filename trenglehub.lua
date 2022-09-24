119b15b17af8ba10b3b258855e65d8f78bd88053
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:35:12 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1457,3 +1457,6 @@ addscriptexist(9744954014, "THAHOODGodmodeInfAmmo", "astrofile", "https://raw.gi
 addscriptexist(10676523834, "RacingRocketInfiniteStar", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RacingRocketInfiniteStar.lua")
                     
 addscriptexist(9648883891, "FestivalTycoonAutoBuyAll", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FestivalTycoonAutoBuyAll.lua")
+    
+addscript(8293257677,"Surreal RPG [100 damage]", "SurrealRPG100damage", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SurrealRPG100damage.lua")
+                    
