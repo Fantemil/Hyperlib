da81bffd705697cf3920b097314aa1c510840990
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 19:03:50 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -2904,3 +2904,5 @@ addscript(5714258552,"Bizarre Heritage [Free Money And Xp]", "BizarreHeritageFre
                     
                     
 addscriptexist(10184360875, "PetFishingSimulatorInfMoneyInfFish", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PetFishingSimulatorInfMoneyInfFish.lua")
+                    
+addscriptexist(9157870347, "PetAdventuresSimulatorAutoCollectAutoFarmAutoHatch", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PetAdventuresSimulatorAutoCollectAutoFarmAutoHatch.lua")
