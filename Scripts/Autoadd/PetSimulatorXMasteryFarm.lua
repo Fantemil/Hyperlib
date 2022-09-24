bcf4ba07efebf5137fb2f9410d0300c1789f056c
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 23:17:55 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/PetSimulatorXMasteryFarm.lua
@@ -0,0 +1,6 @@
+_G.Enabled = true --// set to "false" to disable the script
+
+_G.useGolden = true --// Set to "false" if you don't want to open GOLDEN eggs
+_G.useNormal = true --// Set to "false" if you don't want to open NORMAL eggs
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iiBlackLotus/Roblox_Scripts/main/Mastery%20Auto%20Farm%20PsX.lua"))()
--- trenglehub.lua
+++ trenglehub.lua
@@ -6552,3 +6552,5 @@ addscriptexist(621129760, "KATIIRadioBypass", "Terni", "https://raw.githubuserco
 addscriptexist(1458767429, "ABARankedCoinsFarm", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ABARankedCoinsFarm.lua")
                     
 addscriptexist(2693023319, "FindTheBaconsCollectAllTheBaconsInTheServer", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FindTheBaconsCollectAllTheBaconsInTheServer.lua")
+                    
+addscriptexist(6284583030, "PetSimulatorXMasteryFarm", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PetSimulatorXMasteryFarm.lua")
