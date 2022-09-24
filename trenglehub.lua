14509e4f7bc2334ded964973e58b15b5c5b1a17a
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 00:25:26 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -7441,3 +7441,5 @@ addscript(7603193259,"Friday Night Bloxxin [Auto Player - Insta Solo]", "FridayN
                     
                     
 addscriptexist(8448735476, "dbzadventuresunleashedAutoAttackAutoSkillsAutoQuest", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/dbzadventuresunleashedAutoAttackAutoSkillsAutoQuest.lua")
+                    
+addscriptexist(2866967438, "FishingSimulatorKillMonsterKillMobbyAutoSell", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FishingSimulatorKillMonsterKillMobbyAutoSell.lua")
