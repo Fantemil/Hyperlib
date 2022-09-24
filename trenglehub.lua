1e7aeadf408a8745d75fe530f46857059c546442
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 19:54:15 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -3725,3 +3725,5 @@ addscript(7663817366,"Prison Roleplay Escape Plan [Auto Farm]", "PrisonRoleplayE
                     
                     
 addscriptexist(9377039667, "RobloxInnovationAwardsVotingHubGetAllPuzzlePieces", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RobloxInnovationAwardsVotingHubGetAllPuzzlePieces.lua")
+                    
+addscriptexist(679715583, "StarscapeAutoMarket", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/StarscapeAutoMarket.lua")
