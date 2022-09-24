e0421906b7ae94988f703797f69cef80aa38349e
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 00:24:52 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -7434,3 +7434,5 @@ addscript(5229802390,"Anime Cross World [God mode]", "AnimeCrossWorldGodmode", "
 addscriptexist(8592863835, "MagnetSimulator2AutoFarm", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MagnetSimulator2AutoFarm.lua")
                     
 addscriptexist(1962086868, "TowerOfHellGodMode", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TowerOfHellGodMode.lua")
+                    
+addscriptexist(6604417568, "Minerscave13XrayNoFullArmorup", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Minerscave13XrayNoFullArmorup.lua")
