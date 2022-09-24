64acc53831d444c1780794871d1da8d7e5227d4f
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 20:03:09 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -3891,3 +3891,5 @@ addscript(8619263259,"Critical Legends [Collect all chests, Tp to every statue]"
                     
                     
 addscriptexist(2788229376, "DaHoodAutoFarmGodmodeKllBot", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DaHoodAutoFarmGodmodeKllBot.lua")
+                    
+addscriptexist(8750997647, "TappingLegendsXUPDATEDAUTOFARMGUI", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TappingLegendsXUPDATEDAUTOFARMGUI.lua")
