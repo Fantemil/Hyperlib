496625f07d1c25fd315c04aca6797088cb088ca0
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 01:54:01 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -8744,3 +8744,5 @@ addscriptexist(189707, "NaturalDisasterSurvivalAUTOFARMLOCALSCRIPTS", "Terni", "
 addscriptexist(185655149, "WelcometoBloxburgUPDATEAUTOFARMING", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/WelcometoBloxburgUPDATEAUTOFARMING.lua")
                     
 addscriptexist(142823291, "MurderMystery2ESPEMOTESTROLL", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MurderMystery2ESPEMOTESTROLL.lua")
+                    
+addscriptexist(3823781113, "SaberSimulatorNEWGUIAutoFarmAutoBuyAutoSell", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SaberSimulatorNEWGUIAutoFarmAutoBuyAutoSell.lua")
