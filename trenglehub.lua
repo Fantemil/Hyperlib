f1769c37cc651eaf25bf026a4c361a8fae3da640
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-24 17:11:04 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1056,3 +1056,5 @@ addscript(8227619186,"Murder Mystery A ", "MurderMysteryAGemsAutoFarmWalkspeedIn
     
 addscript(5490351219,"Clicker Madness! ", "ClickerMadnessAutoFarmBossFarmClaimFlags", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ClickerMadnessAutoFarmBossFarmClaimFlags.lua")
                     
+                    
+addscriptexist(9648883891, "FestivalTycoonAutoEndTycoon", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FestivalTycoonAutoEndTycoon.lua")
