6a87996d7523c00db8f5b2cbfc06efdbefaa3001
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 03:30:52 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -10090,3 +10090,5 @@ addscript(5346447287,"Tapping Gods", "TappingGods", "Terni", "https://raw.github
                     
                     
 addscriptexist(574407221, "SuperHeroTycoon", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SuperHeroTycoon.lua")
+                    
+addscriptexist(8288069630, "PlanetMinignSimulator", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PlanetMinignSimulator.lua")
