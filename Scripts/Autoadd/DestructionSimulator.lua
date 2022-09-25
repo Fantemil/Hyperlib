32b9aaf56e8e8ff2de2a7c74b8d3e16140d16dfa
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 03:42:28 +0200

    Updated automatically!

--- Scripts/Autoadd/DestructionSimulator.lua
+++ Scripts/Autoadd/DestructionSimulator.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/EnMqnDZX"))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/AquaModz/DestructionSIMModded/main/DestructionSimAqua.lua'))()
