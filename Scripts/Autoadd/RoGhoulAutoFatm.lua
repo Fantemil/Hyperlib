b02b61468f3ac699bd8755b9652fbb5b698ffd90
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 02:56:09 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/RoGhoulAutoFatm.lua
@@ -0,0 +1,1 @@
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/JRL-lav/Scripts/main/Ro-ghoul"))()
--- trenglehub.lua
+++ trenglehub.lua
@@ -9571,3 +9571,5 @@ addscript(6808489605,"Aimblox BETA [Aim / Esp / Character]", "AimbloxBETAAimEspC
 addscriptexist(171391948, "VehicleSimulatorAutoFarmx2FlySuperCar", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/VehicleSimulatorAutoFarmx2FlySuperCar.lua")
                     
 addscriptexist(189707, "NaturalDisasterSurvivalAutosafeTpSpeed", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NaturalDisasterSurvivalAutosafeTpSpeed.lua")
+                    
+addscriptexist(914010731, "RoGhoulAutoFatm", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RoGhoulAutoFatm.lua")
