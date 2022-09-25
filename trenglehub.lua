5990b4be228c83c6861fa6d23160508e769b7c65
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 04:46:07 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -10893,3 +10893,5 @@ addscript(5083731857,"Saitama Simulator AutoFarm", "SaitamaSimulatorAutoFarm", "
                     
                     
 addscriptexist(566399244, "ElementalBattlegrounds", "Acc", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ElementalBattlegrounds.lua")
+                    
+addscriptexist(301549746, "CounterBloxSkinChanger", "Acc", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/CounterBloxSkinChanger.lua")
