c9fa49cb9e0d561faf822daaedd4f61e0e2ab0a4
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 03:33:02 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -10113,3 +10113,5 @@ addscript(8851865158,"Aethelbeorn", "Aethelbeorn", "Terni", "https://raw.githubu
                     
                     
 addscriptexist(6234390472, "Cookingintherain", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cookingintherain.lua")
+                    
+addscriptexist(2961297129, "DEATHZONE", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DEATHZONE.lua")
