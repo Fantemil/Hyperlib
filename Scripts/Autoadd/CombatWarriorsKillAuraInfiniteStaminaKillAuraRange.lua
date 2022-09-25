60be31df0f85d2266882ecf10706e7f8223d3490
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 02:15:42 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/CombatWarriorsKillAuraInfiniteStaminaKillAuraRange.lua
@@ -0,0 +1,5 @@
+_G.killAuraEnabled = true
+_G.infiniteStamina = true
+_G.killAuraRange = 12 -- Max Magnitude (Range) you can go is 12
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/samuelLovesPie/verm_releases/main/combat_warriors.lua", true))()
