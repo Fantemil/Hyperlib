df7ceb3df02d81e27f2bdd3409df6b4e0c8c9a4a
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 02:18:59 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -9076,3 +9076,6 @@ addscript(6186867282,"Project XL [Auto Attack/Auto Skills/Auto rob]", "ProjectXL
 addscriptexist(4282985734, "CombatWarriorsKillAuraAutoParryKillAll", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/CombatWarriorsKillAuraAutoParryKillAll.lua")
                     
 addscriptexist(6720959312, "BuildABoatForTreasureGodAutoFarmTransform", "Qalnik", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BuildABoatForTreasureGodAutoFarmTransform.lua")
+    
+addscript(4545436299,"Pet Clicks Simulator [AUTO-FARM, DEV TOOL]", "PetClicksSimulatorAUTOFARMDEVTOOL", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PetClicksSimulatorAUTOFARMDEVTOOL.lua")
+                    
