0bc89f7cf1e2f73726096e5868beb2a751dac3d7
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 02:13:37 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -9012,3 +9012,5 @@ addscript(6706152946,"Jojo", "JojoCrusadersHeavenKillAura", "Qalnik", "https://r
 addscriptexist(4520749081, "KingLegacyAUTOFARMAUTOQUESTMORE", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/KingLegacyAUTOFARMAUTOQUESTMORE.lua")
                     
 addscriptexist(5865858426, "RetailTycoon2AutoPumpkinSmasher", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RetailTycoon2AutoPumpkinSmasher.lua")
+                    
+addscriptexist(4566572536, "VehicleLegendsNEWAUTOFARMSERVERHOP", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/VehicleLegendsNEWAUTOFARMSERVERHOP.lua")
