5e9b581c00c7f168cfa69e7259eb84235b50dfbb
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-25 04:39:56 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/OnePieceMillennium3.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/J5zdzkk8", true))()
--- trenglehub.lua
+++ trenglehub.lua
@@ -10837,3 +10837,6 @@ addscript(755776245,"Restaurant Tycoon", "RestaurantTycoon", "Acc", "https://raw
 addscriptexist(4623386862, "Piggy", "Acc", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Piggy.lua")
                     
 addscriptexist(5535087806, "TappingMania", "Acc", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TappingMania.lua")
+    
+addscript(5835263912,"One Piece", "OnePieceMillennium3", "Acc", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/OnePieceMillennium3.lua")
+                    
