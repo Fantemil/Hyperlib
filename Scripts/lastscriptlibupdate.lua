4455f49c36b19dcb30454ce13ddbf39d1fb4f4bc
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-26 15:20:49 +0200

    test removal

--- Scripts/lastscriptlibupdate.lua
+++
@@ -1,7 +0,0 @@
-
-local Lastupdate = Window:NewTab("Last Library Update")
-local lasttime = 26/09/2022 14:51:29
-wait()
-local LastupdateSection = Lastupdate:NewSection("Last Library Update: ",  lasttime)
-local DisclaimerSection = LastupdateSection:NewLabel("Data according to CET (GMT+1)")
-            
