bb4571c8cbb83d6071a1d370abf2d8c050aa94b7
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-29 14:24:38 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -11734,3 +11734,6 @@ addscriptuniversal("UniversalFEChatSlower", "astrofile", "https://raw.githubuser
         
     addscript(4983006874,"NGU CLICKER SIMULATOR Gui", "NGU CLICKER SIMULATOR Gui: Auto Click, Auto Upgrade, Auto Pet Evolve", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NGU CLICKER SIMULATOR Gui: Auto Click, Auto Upgrade, Auto Pet Evolve.lua")
                         
+
+                        
+    addscriptexist(2753915549, "Blox Fruits: Auto Set Spawn Points, Teleports & More", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox Fruits: Auto Set Spawn Points, Teleports & More.lua")
