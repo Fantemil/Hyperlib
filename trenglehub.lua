380b8d51204e350af85de12311517c9bb6dcff57
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-09-30 06:24:32 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -11941,3 +11941,6 @@ addscriptuniversal("UniversalFEChatSlower", "astrofile", "https://raw.githubuser
                             
     addscriptuniversal("Universal GUI: Very smooth animations", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal GUI: Very smooth animations.lua")
         
+
+                        
+    addscriptexist(10895555747, "Walmart Land: Infinite Tokens Methond 1", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Walmart Land: Infinite Tokens Methond 1.lua")
