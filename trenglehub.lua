1bff75d540ebc12398d399b2065caab1fd181a2c
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-10-01 13:53:47 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -12031,3 +12031,6 @@ addscriptuniversal("UniversalFEChatSlower", "astrofile", "https://raw.githubuser
 
                         
     addscriptexist(7952502098, "Impossible Glass Bridge Obby: Reveal Glass", "Terni", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Impossible Glass Bridge Obby: Reveal Glass.lua")
+                            
+    addscriptuniversal("Universal Spawn Soldier", "astrofile", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal Spawn Soldier.lua")
+        
