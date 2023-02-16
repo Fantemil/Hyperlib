6689acaffd2e817f84bc1688ca2794d7fabc3383
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 09:24:56 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -585,3 +585,4 @@ addscript(4759640416,"The-Underground-War-2", "The Underground War 2 Flag TP", "
 addscript(5902977746,"", "Ultimate Tower Defense: AutoFish, Teleports & More", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultimate%20Tower%20Defense%20AutoFish%20Teleports%20%20More.lua")
 addscriptexist(2788229376, "(DH) Zinc Hub V2.9.5 [OP]", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DH%20Zinc%20Hub%20V%20OP.lua")
 addscript(1962086868,"", "Tower Of Hell: AutoWin, Get All Items, GodMode", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20Of%20Hell%20AutoWin%20Get%20All%20Items%20GodMode.lua")
+addscript(5293755937,"Speed-Run-Simulator", "Speed Run Simulator GUI", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Speed%20Run%20Simulator%20GUI.lua")
