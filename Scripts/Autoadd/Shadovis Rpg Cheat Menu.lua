1f6cdafd1428015c50677ec7607209c288b219b6
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 12:26:45 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Shadovis Rpg Cheat Menu.lua
@@ -0,0 +1,4 @@
+--BROUGHT TO YOU BY RSCRIPTS.NET--
+repeat task.wait() until game:IsLoaded()
+getgenv().autoExecuteFolder = truedw
+loadstring(game:HttpGet('https://www.6footscripts.com/Scripts/6FootHub/Hub.lua'))();
