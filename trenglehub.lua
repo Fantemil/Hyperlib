0ef7c5d3582996ae7f274da4f7c8ec3077afaabb
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 11:50:12 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -639,3 +639,4 @@ addscript(7167649916,"Scripts for Current Game", "Trophy Script Hub", "pulled fr
 addscript(335760407,"Scripts for Current Game", "best free gui for the game lol", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/best%20free%20gui%20for%20the%20game%20lol.lua")
 addscript(6777872443,"Scripts for Current Game", "Synergy Hub Re-Written", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Synergy%20Hub%20ReWritten.lua")
 addscriptexist(2753915549, "Blox Fruits Void Hub Transform Race V4 (UPDATED)", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Void%20Hub%20Transform%20Race%20V%20UPDATED.lua")
+addscriptexist(6284583030, "ExtremeHUB - 25 Scripts", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ExtremeHUB%20%20%20Scripts.lua")
