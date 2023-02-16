3c7ec87ec3a8c4496e95e9afddf804f7e4ab1c1b
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 12:18:23 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -819,3 +819,4 @@ addscriptexist(7056922815, "Reaper 2 - AutoFarm, AutoQuest, Teleports", "pulled 
 addscriptexist(8054462345, "Goodwill - Michael's Zombies", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Goodwill%20%20Michaels%20Zombies.lua")
 addscript(2772610559,"Scripts for Current Game", "Farming and Friends Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Farming%20and%20Friends%20Script.lua")
 addscriptexist(11063612131, "Every Second You Get +1 Jump Power | nihub", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Every%20Second%20You%20Get%20%20Jump%20Power%20%20nihub.lua")
+addscriptexist(10598587051, "Anime Clicker Fight ", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Clicker%20Fight%20.lua")
