58cfdb5abbc2dcef830cba83ec2c75fc4100c4da
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 12:08:56 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -758,3 +758,4 @@ addscript(2210085102,"Scripts for Current Game", "Naval Warfare Hub", "pulled fr
 addscriptexist(335760407, "Street Soccer Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Street%20Soccer%20Script.lua")
 addscript(5972698540,"Scripts for Current Game", "Easy Obby TP Menu", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Easy%20Obby%20TP%20Menu.lua")
 addscriptexist(3956818381, "Obscure Hub (45+ GAMES)", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Obscure%20Hub%20%20GAMES.lua")
+addscriptexist(537413528, "Build A Boat For Treasure Gui", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Gui.lua")
