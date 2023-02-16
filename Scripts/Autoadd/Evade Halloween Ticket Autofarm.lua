32c006c02f89cc717b41beb16316e68244dbc25a
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 12:21:07 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Evade Halloween Ticket Autofarm.lua
@@ -0,0 +1,6 @@
+--BROUGHT TO YOU BY RSCRIPTS.NET--
+getgenv().settings = {
+    ['ticket_farm'] = true;
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/muzscripter/projects/main/rbxl/scripts/evade%20ticket%20farm'))();
