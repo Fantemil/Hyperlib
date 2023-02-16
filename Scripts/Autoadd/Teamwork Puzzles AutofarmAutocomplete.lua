19572d9dcd0a0fb39b9a90cc1ffdb3465ffe5691
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 11:20:00 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Teamwork Puzzles AutofarmAutocomplete.lua
@@ -0,0 +1,4 @@
+--[[Make sure to join the Discord btw: 
+https://discord.gg/K6JDqbwgc8
+]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WinterDinder/Teamwork-Puzzles/main/Main", true))()
