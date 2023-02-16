652a5f3a2bfb8ae961db311999205d0271999164
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 11:23:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -613,3 +613,4 @@ addscript(3351674303,"Driving-Empire", "Pearl・Driving Empire", "pulled from rs
 addscriptexist(6284583030, "Pet Simulator X Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Script.lua")
 addscript(7991339063,"Rainbow-Friends?AssetId=#ropro-quick-search", "Rainbow Friends Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rainbow%20Friends%20Script.lua")
 addscript(11542692507,"UPD-3-Anime-Souls-Simulator", "Mendopt Me Auto Quest", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mendopt%20Me%20Auto%20Quest.lua")
+addscript(920587237,"Adopt-Me", "Get all NFR and MFR pets | Adopt Me", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Get%20all%20NFR%20and%20MFR%20pets%20%20Adopt%20Me.lua")
