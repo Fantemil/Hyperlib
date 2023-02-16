91f88e085ed8093fc775c488b71872e3500a7424
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 11:27:14 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/NEW Shindo Life Teleport Locations Script  Just For Ember Village  .lua
@@ -0,0 +1,5 @@
+function MakeScriptHub()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KrakenScripting/All-Shindo-Life/main/Teleports",true))(); --SHINDO LIFE TELEPORTS PASTEBIN ! (IF YOU WANT TO CHANGE SOMETHING)
+end
+
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/KrakenScripting/KeySystem/main/IDK",true))();
