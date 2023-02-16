dc370011830c6e6371fea07e19fd19b57500d919
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 12:37:07 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Shadovis RPG Kill Aura Script.lua
@@ -0,0 +1,6 @@
+--BROUGHT TO YOU BY RSCRIPTS.NET--
+
+getgenv().Enabled = true
+getgenv().MaxRange = 25 --In studs
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dooory/OC/main/Working/shadovis_killaura.lua"))()
