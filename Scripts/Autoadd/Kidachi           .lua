b3d9aec9e30977ebb30e100565bc92c665016f03
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 12:04:15 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Kidachi           .lua
@@ -0,0 +1,6 @@
+PC Script:
+loadstring(game:HttpGet('https://kidachi.xyz/Script', true))()
+Mobile Script:
+loadstring(game:HttpGet('https://kidachi.xyz/MobileScript', true))()
+
+Hydrogen only for Mobile
