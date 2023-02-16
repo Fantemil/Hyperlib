d3a6db2bddbb6c73b3aedc0fe09194a507f6aaff
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 11:54:25 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -668,3 +668,4 @@ addscript(11929558254,"Scripts for Current Game", "Every Second You Get +1... Au
 addscriptexist(4483381587, "Rizzler (Ultimate Rizz)", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rizzler%20Ultimate%20Rizz.lua")
 addscriptexist(9984770240, "Obby but you're a ball skip level(not gui)", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Obby%20but%20youre%20a%20ball%20skip%20levelnot%20gui.lua")
 addscript(11547613362,"Scripts for Current Game", "King Hub | Anime Fly Race Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/King%20Hub%20%20Anime%20Fly%20Race%20Script.lua")
+addscriptexist(3351674303, "FREE Driving Empire Autofarm ", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FREE%20Driving%20Empire%20Autofarm%20.lua")
