3d5c4b4f62969b0f4552d5ad420b73936e0464a6
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 11:31:50 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/obby but ur a ball script hub stuff.lua
@@ -0,0 +1,7 @@
+--[[
+BROUGHT TO YOU BY RSCRIPTS.NET
+SCRIPT BY 56 CLUBS
+JOIN THE DISCORD: discord.gg/K6JDqbwgc8
+]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WinterDinder/56-Clubs-Obby-but-you-re-a-ball/main/Obby%20but%20you're%20a%20ball.lua", true))()
