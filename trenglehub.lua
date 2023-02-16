5f511c9188d6dc2105e789f42f09bef274df9a96
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 12:28:05 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -856,3 +856,4 @@ addscript(10836305188,"Scripts for Current Game", "FALL [Impossible] Script v1",
 addscriptexist(2753915549, "Blox Fruit HoHo Hub", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruit%20HoHo%20Hub.lua")
 addscript(9585537847,"Scripts for Current Game", "Shadovis Rpg Cheat Menu", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shadovis%20Rpg%20Cheat%20Menu.lua")
 addscriptexist(4483381587, "GalaxyHub - Free Hub 14+ Games", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/GalaxyHub%20%20Free%20Hub%20%20Games.lua")
+addscript(9431770682,"Scripts for Current Game", "Lost Rooms Script (ESP, Fly, Farm and More)", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Lost%20Rooms%20Script%20ESP%20Fly%20Farm%20and%20More.lua")
