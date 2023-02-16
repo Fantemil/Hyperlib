cb7ea77aefebc817ad07185404c28f51fe67ac13
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 11:29:11 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -647,3 +647,4 @@ addscript(286090429,"Arsenal", "⭐ Scourge Hub - Keyless - 20 Games - New", "pu
 addscript(71315343,"x2-UPD-Dragon-Ball-Rage", "Dragon Ball Rage Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dragon%20Ball%20Rage%20Script.lua")
 addscript(8567596064,"BETA-Anime-Showdown", " Anime Showdown Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20Anime%20Showdown%20Script.lua")
 addscriptexist(10704789056, "drive world op autofarm script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/drive%20world%20op%20autofarm%20script.lua")
+addscriptexist(6777872443, "Pixel Piece (FULL AUTOFARM)", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pixel%20Piece%20FULL%20AUTOFARM.lua")
