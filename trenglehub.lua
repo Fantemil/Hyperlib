415901cfbadfe3246a5b9c9f8d1ecfb2f2178220
Emil Toth <emiltoth1870@gmail.com>
2023-02-16 12:05:10 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -734,3 +734,4 @@ addscriptexist(3101667897, "Legends Of Speed ⚡ | Autofarm (Open Source)", "pul
 addscriptexist(1962086868, "Tower Of Hell GUI", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20Of%20Hell%20GUI.lua")
 addscriptexist(4483381587, "BetterBypasser (Chat Bypass)", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BetterBypasser%20Chat%20Bypass.lua")
 addscriptexist(4483381587, "[Universal] Common Utilities V1.0.4", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Common%20Utilities%20V.lua")
+addscriptexist(2753915549, "Space Hub (OVERPOWERED)", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Space%20Hub%20OVERPOWERED.lua")
