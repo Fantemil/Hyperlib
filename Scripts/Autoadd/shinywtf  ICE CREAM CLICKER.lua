5c6aec57c16770918442a5f8634f965a063cabb5
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 00:30:29 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/shinywtf  ICE CREAM CLICKER.lua
@@ -0,0 +1,10 @@
+--[[
+       .__    .__                           __    _____ 
+  _____|  |__ |__| ____ ___.__.  __  _  ___/  |__/ ____\
+ /  ___/  |  \|  |/    <   |  |  \ \/ \/ /\   __\   __\ 
+ \___ \|   Y  \  |   |  \___  |   \     /  |  |  |  |   
+/____  >___|  /__|___|  / ____| /\ \/\_/   |__|  |__|   
+     \/     \/        \/\/      \/  
+ Discord: https://discord.gg/fhwwpMMZw5
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/marjoriex/shiny.wtf/main/main.lua", true))()
