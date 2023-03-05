23b8c9951d7cdf87904cd7339a05b8977d98ca3c
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 14:09:46 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/xaoui .lua
@@ -0,0 +1,14 @@
+--[[
+xaoui v1.4
+                 
+GUI to LUAU
+RGtL - V1.2?
+
+MoonSec V3
+
+kys rbxscript (rbxscript, delete this and ur gay and bi)
+
+.gg/dFCCP3fwgA | key here / questions
+]]
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/xaophf/xaohub-extra/main/xaoui'),true))()
