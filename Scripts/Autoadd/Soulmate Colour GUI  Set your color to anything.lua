48229cb062c849c310978f0d202ce23d4998f2f3
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 13:59:54 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Soulmate Colour GUI  Set your color to anything.lua
@@ -0,0 +1,5 @@
+-- Brought to you by TimeFlaire
+-- For the best experience, Put this script in autoexec
+-- The script checks if the game you are playing matches the game ID, so it will only execute when the game you are playing is SC
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TimeFlaire/SoulmateColorGUI/get-out/spam",true))()
