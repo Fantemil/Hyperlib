0a6a76a238239c5bfcac19cfdd6bff26bfdd42ed
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 14:12:12 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Auto ServerHop Devil Heart Farm.lua
@@ -0,0 +1,3 @@
+getgenv().webh = 'webhookhere' -- insert a webhook here then put this into auto execute join the game and it will serverhop till it finds a server with a heart once you click play in that server it will tween you to the heart!
+--(if u already have in auto execute make sure u replace it with the new loadstring)
+l loadstring(game:HttpGet("http://sikuscripts.lol/autodevilheart.lua"))()
