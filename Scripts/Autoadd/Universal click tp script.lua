b1a070688c44415ba39264e91ef23152f10cd449
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 14:02:13 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal click tp script.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/HZA5Yg8w"))()
+If this dues not work then use:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ssmmc3/ClickTP/main/ClickTP"))()
