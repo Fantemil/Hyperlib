39c5c57d1aa3bef34ac2e2a4e3d0c9667749f042
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 14:07:44 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Holos Alts Controls.lua
@@ -0,0 +1,14 @@
+-- discord server for updates | https://discord.gg/wZrvdGZDCd
+-- script:
+
+getgenv().controller = "your main account username"
+
+alts = {
+    alt1 = "your alt", -- put none if you don't want more alts
+    alt2 = "your alt",
+    alt3 = "your alt",
+    alt4 = "your alt",
+    alt5 = "your alt"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hologrammw/holo-alt-control/main/altcontrol.lua", true))()
