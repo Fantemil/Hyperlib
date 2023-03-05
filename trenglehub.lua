3247e66e12aebe327c9818501958ac585843df44
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 14:47:48 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -664,3 +664,4 @@ end
 
 
 addscriptuniversal("17F7O's Anti Http Spy", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FOs%20Anti%20Http%20Spy.lua")
+addscriptuniversal("Team Skeet UI Library - for script devs", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Team%20Skeet%20UI%20Library%20%20for%20script%20devs.lua")
