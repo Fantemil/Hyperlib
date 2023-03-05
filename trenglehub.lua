21d204f2ae20cf4aec94ad1bf8ed5a2ed875a61c
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 14:20:00 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -784,3 +784,4 @@ addscript(71315343,"Scripts for Current Game", "Dragon Ball Rage Script", "pulle
 addscript(8567596064,"Scripts for Current Game", " Anime Showdown Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20Anime%20Showdown%20Script.lua")
 addscriptexist(10704789056, "drive world op autofarm script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/drive%20world%20op%20autofarm%20script.lua")
 addscriptexist(6777872443, "Pixel Piece (FULL AUTOFARM)", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pixel%20Piece%20FULL%20AUTOFARM.lua")
+addscriptexist(286090429, "DevHub NEW LOADSTRING", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DevHub%20NEW%20LOADSTRING.lua")
