eb7d8bd50877d5862c0f818d2eb213512af1a43f
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 13:58:17 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Aftons Family Diner.lua
@@ -0,0 +1,7 @@
+--Main Script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WinterDinder/Aftons-Family-Diner/main/Main", true))()
+
+--[[
+(Extra) All characters:
+game:GetService("Players").LocalPlayer.userId = "23467579"
+]]--
