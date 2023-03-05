1f8c8cb80554465ee9ec2e89998082e2546b9411
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 14:32:28 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -946,3 +946,4 @@ addscriptexist(286090429, "DevHub 5+ Games", "pulled from rscripts", "https://ra
 addscript(3956818381,"Scripts for Current Game", "Ninja Legends [NEW]", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ninja%20Legends%20NEW.lua")
 addscriptexist(2866967438, "Fishing Simulator Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fishing%20Simulator%20Script.lua")
 addscriptexist(142823291, "Murder Mystery 2 GUI", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Murder%20Mystery%20%20GUI.lua")
+addscriptexist(286090429, "Arsenal DevHub [OP GUIs]", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20DevHub%20OP%20GUIs.lua")
