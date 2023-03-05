4cadd768b1fa0604ca12e2112cfd0f7d89b98c9b
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 13:52:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Selenium Pilgrammed.lua
@@ -0,0 +1,8 @@
+--[[
+                                                                                                      
+Hell                                                                                                  
+> Maintained by Synergy Networks. discord.gg/evon
+
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/synnyyy/synergy/main/selenium.lua",true))()
