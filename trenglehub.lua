de53fb7db81971edab7aec030e0dc00cd702f24b
Emil Toth <emiltoth1870@gmail.com>
2023-03-05 16:14:12 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -984,3 +984,4 @@ addscriptexist(537413528, "Build A Boat For Treasure [GUI - Auto Quest, Auto Far
 addscript(11227601015,"Scripts for Current Game", "Iicrrbii Universal [Work] ", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Iicrrbii%20Universal%20Work%20.lua")
 addscriptexist(8908228901, "Pearl・Sharkbite 2", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PearlSharkbite%20.lua")
 addscript(11866253403,"Scripts for Current Game", "Ragdoll Jump Race", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ragdoll%20Jump%20Race.lua")
+addscriptexist(2753915549, "Blox Fruit Gui Hub", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruit%20Gui%20Hub.lua")
