0332b4b50ba0ec6964ea722bd3eb643bdbfe204c
Emil Toth <emiltoth1870@gmail.com>
2023-03-06 14:48:30 +0100

    Updated automatically!

--- Scripts/Autoadd/Toddys hub  games supported.lua
+++ Scripts/Autoadd/Toddys hub  games supported.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ToddyTheNoobDud/ToddyGui/main/Loader.lua'))()
 
---Current updates: Improved loader, Fixed insta hatch from rebirth champions X.
+--Current updates: big updates on troopy army simulator
