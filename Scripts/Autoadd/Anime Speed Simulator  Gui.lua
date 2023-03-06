5a390f3f6302c38e0419f3163584d2672944543a
Emil Toth <emiltoth1870@gmail.com>
2023-03-06 16:14:40 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Anime Speed Simulator  Gui.lua
@@ -0,0 +1,3 @@
+--BROUGHT TO YOU BY RSCRIPTS.NET--
+_G.Theme = "LightTheme" --"LightTheme","DarkTheme","GrapeTheme","BloodTheme","Ocean","Midnight","Sentinel","Synapse"
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/oceanhong970201/robloxscript/main/Loader.txt')))()
