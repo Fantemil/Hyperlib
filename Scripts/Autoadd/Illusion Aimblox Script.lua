b1ff7d19da7d9e2a1199a34eab047780ec29c50f
Emil Toth <emiltoth1870@gmail.com>
2023-03-06 15:45:39 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Illusion Aimblox Script.lua
@@ -0,0 +1,1 @@
+local http_request = http_request or request or (syn and syn.request) or (http and http.request) loadstring(http_request({Url="https://projectillusion.dev/beta/illusion/illusion_aimblox.lua",Method="GET"}).Body)()
