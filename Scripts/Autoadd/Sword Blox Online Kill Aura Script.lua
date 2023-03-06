8741de6d8f1edb19b278087b0b4ae1b17f934d9f
Emil Toth <emiltoth1870@gmail.com>
2023-03-06 16:11:54 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Sword Blox Online Kill Aura Script.lua
@@ -0,0 +1,3 @@
+--BROUGHT TO YOU BY RSCRIPTS.NET--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Shariiii/Sword-Blox-Online-Rebirth/main/OP%20One-Handed%20Sword%20AOE%20v1", true))()
