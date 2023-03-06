1d2c9bea09b18f24b5a7aebab75c829f1bd3f6ad
Emil Toth <emiltoth1870@gmail.com>
2023-03-06 14:51:02 +0100

    Updated automatically!

--- Scripts/Autoadd/Troopy army simulator Script.lua
+++ Scripts/Autoadd/Troopy army simulator Script.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ToddyTheNoobDud/ToddyGui/main/Loader.lua'))()
 
---script updated on 03/01/23, changelog in script
+--Updated on 03/05/23, changelog in script
