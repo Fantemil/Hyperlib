275aef83137c51085b0d8f11c26a2fb7259ca189
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:42:17 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Blox Fruits Mirage Finder Full Moon Finder Server Hop.lua
@@ -0,0 +1,4 @@
+getgenv().WebHookNotify = "here webhook"
+getgenv().OnlyMirage = false --this will only find mirage
+getgenv().FindBoth = true --finds both moon and mirage, false to only find moon 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NukeVsCity/Scripts2022/main/miragemoonfinder"))()
