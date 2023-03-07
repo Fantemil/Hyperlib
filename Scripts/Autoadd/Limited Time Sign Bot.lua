082358b17b637d1b20ec50e10d7442ed55d7cb27
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:03:17 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Limited Time Sign Bot.lua
@@ -0,0 +1,4 @@
+-- sign atm bot
+-- rj to stop, must hold normal sign before executing, dont unequip
+-- does not dupe, will take time from player
+loadstring(game:HttpGet("https://raw.githubusercontent.com/echofluff/limited-time/main/limited%20time%20bots%20owo"))()
