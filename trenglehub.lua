90e9420bdd7c5065b52cebd6da7ff73ee83776bd
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:37:12 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 12:37:04"
+lastupdate = "GMT +1: 07.03.2023 12:37:12"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -1153,3 +1153,5 @@ addscriptexist(2788229376, "Da Hood: Aimlock", "pulled from rbxscript.com", "htt
 addscriptexist(9825515356, "Hood Customs: Headless, Aim Lock & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hood%20Customs%20Headless%20Aim%20Lock%20%20More.lua")
 
 addscript(185655149, "Current Game","Welcome to Bloxburg: Pizza Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Welcome%20to%20Bloxburg%20Pizza%20Auto%20Farm.lua")
+
+addscriptexist(9825515356, "Hood Customs: AntiLock", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hood%20Customs%20AntiLock.lua")
