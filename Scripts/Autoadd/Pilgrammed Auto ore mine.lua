20dd2e29f959ee785d0432f7c2b1ce2d47c1eb5a
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:09:04 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Pilgrammed Auto ore mine.lua
@@ -0,0 +1,22 @@
+_G.Minetime=0.3--changing this may put you at a higher risk at death or being kicked.
+_G.slot=1
+_G.toggle=true
+_G.ore="Copper"--[[
+Iron
+Zinc
+Copper
+Tin
+Sapphire
+Mithril
+Emerald
+Sulfur
+Demetal
+Ruby
+Darksteel]]
+_G.pick="Old Pickaxe"--[[
+Iron Pickaxe
+Old Pickaxe
+Bronze Pickaxe
+Gemstone Pickaxe
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Disttt/Pilgrammed/main/77-Automine.lua"))()
