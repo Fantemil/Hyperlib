d0cb718279352bb8a48810900451d86a94f2fb84
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:31:05 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Zombie Merge Tycoon Kill Player.lua
@@ -0,0 +1,9 @@
+game:GetService("Players").LocalPlayer.Character.WEAPONNAMEHERE.Remotes.TakeDamage:FireServer(game:GetService("Players").PLAYERNAMEHERE.Character.Humanoid, 10)
+
+--> Names are case sensitive 
+
+--> Must be holding the weapon 
+
+-->  Weapon names are the following: Pistol, SMG, AR, Sniper, MachineGun, PulseSMG 
+
+--> Change 10 to whatever amount of damage you want to do to a player
