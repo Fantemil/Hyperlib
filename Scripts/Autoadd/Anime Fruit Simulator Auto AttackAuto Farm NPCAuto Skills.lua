aee1237e88e582379f1a34ac4a186065fff7760c
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:29:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Anime Fruit Simulator Auto AttackAuto Farm NPCAuto Skills.lua
@@ -0,0 +1,10 @@
+--[[ 
+            Key System Protected
+            https://discord.gg/9G46pAJFfR By MaGiXx#6964
+                        Version: v0.1
+
+Get Key Here: https://1.kelprepl.repl.co/getkey/WheatHub
+Discord Server: https://discord.gg/wsbyxxCQuW
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/wheathub/main/main.lua"))()
