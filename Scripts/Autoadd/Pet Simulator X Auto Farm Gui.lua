3ad29ea5a1f0c044bcb8f46abd2e80be4f260fc8
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:20:34 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Pet Simulator X Auto Farm Gui.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cipex-rbx/cipex/main/loader.lua"))([==[
+Toggle Keybind: Right Shift
+Created by: neex#6456, Pawel12d#0272, daruno#6587
+]==])
