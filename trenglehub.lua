c58fcf2fbbeb9ffeab2d68e92dbd7c04b10b43e8
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:39:22 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:39:18"
+lastupdate = "GMT +1: 07.03.2023 21:39:22"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3030,3 +3030,5 @@ addscript(2569453732, "Current Game","Rise Of Nations: Sell Electronics, Loop Pu
 addscript(11760692772, "Current Game","Sling Race: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sling%20Race%20Auto%20Farm.lua")
 
 addscriptexist(9285238704, "Race Clicker: Auto Equip Best Pets, Auto Click, Auto Craft All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Race%20Clicker%20Auto%20Equip%20Best%20Pets%20Auto%20Click%20Auto%20Craft%20All.lua")
+
+addscriptuniversal("Universal ESP, Hitbox Extender", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20ESP%20Hitbox%20Extender.lua")
