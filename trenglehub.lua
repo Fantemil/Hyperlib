27c37553b6d97059c932acc339d869f11c66ae2a
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:20:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:20:48"
+lastupdate = "GMT +1: 07.03.2023 22:20:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4198,3 +4198,5 @@ addscriptexist(10836055001, "Monster Hunt Simulator: Op Auto Farm & More!", "pul
 addscriptexist(9224601490, "Fruit Battlegrounds: Auto Spin", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fruit%20Battlegrounds%20Auto%20Spin.lua")
 
 addscript(8351248417, "Current Game","The Booth Plaza 2: Simple Tomato Aimbot", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Booth%20Plaza%20%20Simple%20Tomato%20Aimbot.lua")
+
+addscriptexist(11874473440, "Car Factory Tycoon: Auto Farm, Suto Update Cars, Auto AirDrop", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Car%20Factory%20Tycoon%20Auto%20Farm%20Suto%20Update%20Cars%20Auto%20AirDrop.lua")
