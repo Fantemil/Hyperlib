7036ab0aed3248dea8f9d30629d637ef5f0beab7
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:19:45 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:19:42"
+lastupdate = "GMT +1: 07.03.2023 21:19:45"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2502,3 +2502,5 @@ addscriptexist(11040063484, "Sword Fighters Simulator: Auto Farm, Auto Quest, Au
 addscript(5709572883, "Current Game","Lift Legends Simulator: Auto Sell, Auto Tap, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Lift%20Legends%20Simulator%20Auto%20Sell%20Auto%20Tap%20Auto%20Rebirth.lua")
 
 addscript(11846163207, "Current Game","Mage Tycoon: Spam Dropper", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mage%20Tycoon%20Spam%20Dropper.lua")
+
+addscriptexist(6735572261, "Pilgrammed: Insta Kill Mobs", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pilgrammed%20Insta%20Kill%20Mobs.lua")
