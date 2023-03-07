8647fadbaba5c31d81c02896fd975683ab01fbb9
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:32:34 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:32:30"
+lastupdate = "GMT +1: 07.03.2023 21:32:34"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2834,3 +2834,5 @@ addscriptexist(4924922222, "Brookhaven RP: Infinitely Big And Small", "pulled fr
 addscriptexist(11696117193, "High Rollers Casino & Resort Map: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/High%20Rollers%20Casino%20%20Resort%20Map%20Auto%20Farm.lua")
 
 addscript(2512643572, "Current Game","Bubble Gun Simulator: Auto Blow, Auto Sell, Auto Upgrade All & More!", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bubble%20Gun%20Simulator%20Auto%20Blow%20Auto%20Sell%20Auto%20Upgrade%20All%20%20More.lua")
+
+addscriptexist(2727067538, "World // Zero: Kill Aura, Auto Farm, Auto Skill & More!", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/World%20%20Zero%20Kill%20Aura%20Auto%20Farm%20Auto%20Skill%20%20More.lua")
