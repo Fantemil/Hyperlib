f0318c593a3c56c99a493944e547b3d1321fcbb4
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:12:03 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:11:59"
+lastupdate = "GMT +1: 07.03.2023 22:12:03"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3950,3 +3950,5 @@ addscriptexist(579955134, "Auto Rap Battles: Face Rap, Spam Hands, Vote GUI", "p
 addscriptexist(6516141723, "DOORS: Key Aura, Book Aura, Notifier", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Key%20Aura%20Book%20Aura%20Notifier.lua")
 
 addscript(8657766101, "Current Game","Marvellous Playground: Unlock All Characters", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Marvellous%20Playground%20Unlock%20All%20Characters.lua")
+
+addscriptexist(8737602449, "PLS DONATE: Walk On Walls, Anit Afk, Invisible", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PLS%20DONATE%20Walk%20On%20Walls%20Anit%20Afk%20Invisible.lua")
