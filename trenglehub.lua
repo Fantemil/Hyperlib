432c6421d4772d38497227ddcb1eac96638975e9
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:17:36 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 12:17:32"
+lastupdate = "GMT +1: 07.03.2023 12:17:36"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -816,3 +816,5 @@ addscript(6002091275, "Current Game","Stone-Haven County Asylum: Kill Aura, Auto
 addscript(10982284336, "Current Game","Anime Idle Simulator: Inf DPS, Auto Upgrade, Auto Farm Level", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Idle%20Simulator%20Inf%20DPS%20Auto%20Upgrade%20Auto%20Farm%20Level.lua")
 
 addscript(7253149844, "Current Game","SCP Games and SCP Monsters: Morph", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SCP%20Games%20and%20SCP%20Monsters%20Morph.lua")
+
+addscript(error, "Current Game","Universa Working Free limiteds", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universa%20Working%20Free%20limiteds.lua")
