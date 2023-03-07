84c7733dc10a5b2a296f1bf6ebf9c079d4fbe697
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:33:18 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Pet Simulator X Auto Farm Auto Egg Server Hops.lua
@@ -0,0 +1,5 @@
+getgenv().UseSeperateSaves=false
+getgenv().DisableStartupPrompt=false
+getgenv().DisableGamepassCheck=false
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KISOOUF/HugeGames/main/script"))()
