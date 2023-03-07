d40d91c1e550a1abe63b0ade47185831f37d555f
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:11:10 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:11:05"
+lastupdate = "GMT +1: 07.03.2023 22:11:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3924,3 +3924,5 @@ addscriptuniversal("Universal Other FPS Booster", "pulled from rbxscript.com", "
 addscriptexist(11800876530, "+One Blocks Every Second: Auto Farm, Auto Rebirth, Auto Eggs", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/One%20Blocks%20Every%20Second%20Auto%20Farm%20Auto%20Rebirth%20Auto%20Eggs.lua")
 
 addscriptexist(4913581664, "Cart Ride Into Rtide: Press All Buttons, Press All Carts Stop Button, Press All Carts Up Button", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cart%20Ride%20Into%20Rtide%20Press%20All%20Buttons%20Press%20All%20Carts%20Stop%20Button%20Press%20All%20Carts%20Up%20Button.lua")
+
+addscript(6172932937, "Current Game","Energy Assault: Silent Aim, Kill All, Fly", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Energy%20Assault%20Silent%20Aim%20Kill%20All%20Fly.lua")
