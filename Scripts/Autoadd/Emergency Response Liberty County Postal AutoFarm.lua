8486acb4d637a3fe0bde2db4e6dfadbacff357a1
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:28:37 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Emergency Response Liberty County Postal AutoFarm.lua
@@ -0,0 +1,4 @@
+getgenv().PostalFarm = true --toggle autofarm, there are always checkpoints like when you pick up the mail, deliver it or when the car farm starts again
+getgenv().TeamCheck = false --that checks if you are in the civilian team if not it will join the civilian team, is needed if you are in another team!!!!
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/Free%20scripts%20for%20users/PostalAutoFarm.lua", true))()
