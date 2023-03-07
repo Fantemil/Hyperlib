2ee3096ec6c9b211832dcab2ca87419122a1ee77
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:07:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal RConsole Admin.lua
@@ -0,0 +1,5 @@
+--[[
+Script & Plugins Documentaions: https://github.com/GamingResources/RConsole-Documentaion/blob/main/readme.md
+]]--
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/fgasfga/misc-releases/main/RConsole%20Admin.lua'))()
