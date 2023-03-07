3869a151f1c208b526d7161a689ec93386be4dc9
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:08:03 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Murder Mystery  OP Inventory.lua
@@ -0,0 +1,20 @@
+local WeaponOwnRange = {
+ min=1,
+ max=5
+}
+
+local DataBase, PlayerData = getrenv()._G.Database, getrenv()._G.PlayerData
+
+local newOwned = {}
+
+for i,v in next, DataBase.Item do
+ newOwned[i] = math.random(WeaponOwnRange.min, WeaponOwnRange.max) -- newOwned[Weapon]: ItemCount
+end
+
+local PlayerWeapons = PlayerData.Weapons
+
+game:GetService("RunService"):BindToRenderStep("InventoryUpdate", 0, function()
+ PlayerWeapons.Owned = newOwned
+end)
+
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
