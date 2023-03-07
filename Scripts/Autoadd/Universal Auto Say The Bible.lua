51cde375bd6cd80a87d80444a5facf1a226038ab
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:18:20 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Auto Say The Bible.lua
@@ -0,0 +1,25 @@
+--[[
+Available Bibles:
+- Arabic,
+- Chinese1,
+- Chinese2,
+- German,
+- Greek,
+- English1,
+- English2,
+- Esperanto,
+- Spanish,
+- Finnish1,
+- Finnish2,
+- French,
+- Korean,
+- Portuguese1,
+- Portuguese2,
+- Portuguese3,
+- Romanian,
+- Russian,
+- Vietnamese
+]]
+SelectedBible = 'English1'
+SayDelay = 5
+loadstring(game:HttpGet('https://raw.githubusercontent.com/14ms-alt/Roblox-Scripts/main/chat_bible.lua'))()
