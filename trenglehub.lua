dfb3ce20e54faa1487560103aff4b2c2e2be5bd9
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:14:26 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:14:22"
+lastupdate = "GMT +1: 07.03.2023 21:14:26"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2358,3 +2358,5 @@ addscript(8589310348, "Current Game","Glory Kill Testing: Kill All, Auto Reload,
 addscriptexist(11929558254, "+1 Per Second: Worlds Auto Farm, TP To Rebirth Place, Walkspeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20Per%20Second%20Worlds%20Auto%20Farm%20TP%20To%20Rebirth%20Place%20Walkspeed.lua")
 
 addscriptexist(8568266872, "Kill Monsters to Save Princess: Auto Farm, Auto Egg, Auto Skill", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Kill%20Monsters%20to%20Save%20Princess%20Auto%20Farm%20Auto%20Egg%20Auto%20Skill.lua")
+
+addscriptexist(7560156054, "Clicker Simulator: Collect Hearts", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Clicker%20Simulator%20Collect%20Hearts.lua")
