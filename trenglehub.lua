65086e7c4fe127d69f4cf277549e90ce82d99319
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:29:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:29:33"
+lastupdate = "GMT +1: 07.03.2023 21:29:38"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2754,3 +2754,5 @@ addscript(5144193105, "Current Game","Star Wars: Galaxy Roleplay: Free Gamepasse
 addscript(738339342, "Current Game","FE2: Annoying Snapshot", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FE%20Annoying%20Snapshot.lua")
 
 addscriptexist(10446125875, "Anime Fruit Simulator: Auto Attack,Auto Farm NPC,Auto Skills", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Fruit%20Simulator%20Auto%20AttackAuto%20Farm%20NPCAuto%20Skills.lua")
+
+addscriptexist(205224386, "Hide and Seek Extreme: Auto Collect,Auto Win,ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hide%20and%20Seek%20Extreme%20Auto%20CollectAuto%20WinESP.lua")
