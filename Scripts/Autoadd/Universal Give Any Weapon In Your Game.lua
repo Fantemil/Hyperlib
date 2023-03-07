3b54fc88389a6381617cda8f2435821fda1ee6f3
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:23:06 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Give Any Weapon In Your Game.lua
@@ -0,0 +1,6 @@
+local args = {
+    [1] = game:GetService("Lighting").Weapons:FindFirstChild("Weapon name")
+}
+ 
+game:GetService("Lighting").EquipSkin:FireServer(unpack(args))
+ loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Infinite%20Yield.txt"))()
