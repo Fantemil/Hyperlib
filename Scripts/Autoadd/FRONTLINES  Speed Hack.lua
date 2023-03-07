32b7cdbc2b5154760096c9739a24938b402b8182
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 15:41:12 +0100

    Updated automatically!

--- Scripts/Autoadd/FRONTLINES  Speed Hack.lua
+++ Scripts/Autoadd/FRONTLINES  Speed Hack.lua
@@ -1,2 +1,11 @@
-getgenv().speedMultiplier = 3
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mopsfl/roblox-scripts/main/.frontlines-speedhack.lua"))()
+syn.run_on_actor(getactors()[1], [[
+    _G.speedMultiplier = 2 --Speed Multiplier
+
+    local globals = getrenv()._G.globals
+    assert(globals, "Unable to get globals")
+    game:GetService("RunService").RenderStepped:Connect(function()
+        globals.fpv_sol_multipliers.movement = _G.speedMultiplier
+    end)
+]])
+
+--mopsfl#4864
