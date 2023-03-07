042cbe4cf1d95de11290132b10799f7a34c2740b
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 15:48:48 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 15:48:46"
+lastupdate = "GMT +1: 07.03.2023 15:48:48"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -1343,3 +1343,4 @@ addscriptexist(662417684, "LUCKY BLOCKS Battlegrounds: Fly, Spawn Blocks, Jumppo
 addhub("Gaming's Script Hub: 50+ Games", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Gamings%20Script%20Hub%20%20Games.lua")
 
 addscript(5902977746,"Current Game", "Ultimate Tower Defense: AutoFish, Teleports & More", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultimate%20Tower%20Defense%20AutoFish%20Teleports%20%20More.lua")
+addscriptexist(11298754833, "MinerShaft: Diomond Ruby Steel Iron ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MinerShaft%20Diomond%20Ruby%20Steel%20Iron%20ESP.lua")
