68e525d095070f68ba1504c64c8bdda88dff4d97
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 15:44:14 +0100

    Updated automatically!

--- Scripts/Autoadd/Rlyeh The Sunken City Unlock Everything.lua
+++ Scripts/Autoadd/Rlyeh The Sunken City Unlock Everything.lua
@@ -1,23 +1,55 @@
---M OPEN MENU
+local library = loadstring(game:HttpGet("https://github.com/GoHamza/AppleLibrary/blob/main/main.lua?raw=true"))()
 
-local Players = game:GetService("Players")
-local Player = Players.LocalPlayer
-local Namecall
-if Player == nil then
-    Players:GetPropertyChangedSignal("LocalPlayer"):Wait()
-    Player = Players.LocalPlayer
-end
-local Inventory = Player:WaitForChild("PlayerGui", 100):WaitForChild("MainMenu", 100):WaitForChild("eventsAndFunctions", 100):WaitForChild("AllowedWeapons", 100):InvokeServer(Player) -- get out of my head get out of my head get out of my head get out of my head 
-for i,v in pairs(Inventory) do
-    if typeof(v) == "table" then
-        for x,y in pairs(v) do
-            y.allowed = true
-        end
-    end
-end
-Namecall = hookmetamethod(game, "__namecall", function(Self, ...)
-    if tostring(Self) == "AllowedWeapons" and not checkcaller() then
-        return Inventory
-    end
-    return Namecall(Self, ...)
+
+local window = library:init("Titlebar", true, Enum.KeyCode.RightShift, true)
+
+window:Divider("I'm a divider!")
+
+local sectionA = window:Section("Test Elements")
+
+sectionA:Divider("I'm another divider!")
+
+sectionA:Button("Click me!", function()
+   print("Button clicked.")
 end)
+
+sectionA:Label("Lorem ipsum dolor sit amet.")
+
+sectionA:Switch("Switch me!", false, function(a)
+   print(a)
+end)
+
+sectionA:TextField("Enter text here!", "Enter text here...", function(a)
+   print(a)
+end)
+
+window:Divider("Just dividin'")
+
+local sectionB = window:Section("Test Notifications")
+
+sectionB:Divider("Dividers are cool!")
+
+sectionB:Button("Temporary Notification", function()
+   window:TempNotify("Be careful!", "We are going to beat you up.", "rbxassetid://12608259004")
+end)
+
+sectionB:Button("Notification 1", function() window:Notify("Hello!", "I am notification", "Button1", "rbxassetid://12608259004",
+   function()
+       print(1)
+   end)
+end)
+
+sectionB:Button("Notification 2", function() window:Notify2("Hello!", "I am notification", "Button 1", "Button 2", "rbxassetid://12608259004",
+   function()
+       print(1)
+   end,
+   function()
+       print(2)
+   end)
+end)
+
+window:GreenButton(function()
+   print("You clicked the green button!")
+end)
+
+sectionA:Select()
