c944daa5b307cec694fadba19218e53d4c279f2e
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:10:31 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arcane Odyssey Chest Farm.lua
@@ -0,0 +1,4 @@
+shared.chestFarm = true  --// to turn on and off *caveman*
+shared.priority = {"Golden Chest", "Armor Chest","Weapon Chest"};  --//chest u want to grab first
+shared.speed = 100  --// how zoomie u go faster = faster
+shared.discord = "https://discord.gg/FZdxeYc8WC" --// super useful yes
