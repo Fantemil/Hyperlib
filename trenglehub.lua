810930fde9dbedcd6b9e7ca703c71e2f5990731b
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 20:58:21 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 20:58:17"
+lastupdate = "GMT +1: 07.03.2023 20:58:21"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -1888,3 +1888,5 @@ addscriptexist(920587237, "Adopt Me: AutoFarm, Auto Baby", "pulled from rbxscrip
 addscriptexist(920587237, "Adopt Me: Visual Pets", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Adopt%20Me%20Visual%20Pets.lua")
 
 addscriptexist(537413528, "Build A Boat For Treasure: Server Crasher, Auto Farm, Esp", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Server%20Crasher%20Auto%20Farm%20Esp.lua")
+
+addscriptuniversal("Universal ChatGPT In Roblox", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20ChatGPT%20In%20Roblox.lua")
