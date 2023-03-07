b0fc6c231ce5e51a32185124717eb0c900eba7bf
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:06:54 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ohio Punch Aura.lua
@@ -0,0 +1,6 @@
+shared.punchAura = {
+    Key = Enum.KeyCode.V, -- the key that toggles it on and off
+    Distance = 90 -- the distance the punch aura reaches to, the max is 90, the game doesn't let you go any higher
+};
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xaxaxaxaxaxaxaxaxa/RobloxScripts/main/Ohio./punch-aura.lua"))();
