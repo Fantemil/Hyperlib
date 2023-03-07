22b189738eede0ae5d0f79088229f37ae2cd21f1
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:11:05 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Rainbow Friends Sprint Fullbright Anti Detect.lua
@@ -0,0 +1,3 @@
+_G.Key = "3Sa8tLfP0Ysr4e3JMzdYh7Svh1qazHJtkoMsIISSUzZoJKMhE1"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Launcher-Exe/Launcher-Exe/main/mainloader"))();  
+--  js posting it because it looks good showing support 
