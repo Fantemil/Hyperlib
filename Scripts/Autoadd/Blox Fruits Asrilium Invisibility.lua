356f93f5edbe2d95469a82af1c588ccf57d49dd6
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:32:42 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Blox Fruits Asrilium Invisibility.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cjdjmj/asriliuim/main/README.md", true))()
+
+-- join group to access script https://www.roblox.com/groups/16595647/CUSA-Cursed-Ugly-Sand-Ache#!/about
