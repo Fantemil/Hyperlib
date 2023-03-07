0e3fa29ee3ec62fcffbc6c4a4763e41c2733812b
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:12:43 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Prison Life Spawn Gun Remington .lua
@@ -0,0 +1,9 @@
+for i,v in pairs (game.Workspace.Prison_ITEMS.giver:GetChildren()) do 
+        if v.Name == "Remington 870" then
+for i,v in pairs(v:GetChildren()) do
+
+v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    
+end
+end
+end
