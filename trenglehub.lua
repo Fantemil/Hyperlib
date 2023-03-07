7cebe21b2af7188d784950428f4505107aec31fb
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 20:57:06 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 20:57:02"
+lastupdate = "GMT +1: 07.03.2023 20:57:06"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -1854,3 +1854,5 @@ addscript(9862832367, "Current Game","Never-Ending Cart Ride: Inf Studs", "pulle
 addscriptexist(6728870912, "World of Stands: Auto Skills, Auto Farm Player, Chest ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/World%20of%20Stands%20Auto%20Skills%20Auto%20Farm%20Player%20Chest%20ESP.lua")
 
 addscriptexist(2753915549, "Blox Fruits: Fruit Mastery, Auto Farm & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Fruit%20Mastery%20Auto%20Farm%20%20More.lua")
+
+addscript(4954096313, "Current Game","Field Trip Z: Auto Heal, Esp, God Mode & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Field%20Trip%20Z%20Auto%20Heal%20Esp%20God%20Mode%20%20More.lua")
