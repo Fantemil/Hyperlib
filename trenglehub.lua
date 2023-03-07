ac6c16cc4a0a1f4c9c44ccd0e28bd962e6154030
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:06:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:06:50"
+lastupdate = "GMT +1: 07.03.2023 21:06:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2136,3 +2136,5 @@ addscriptexist(11196588443, "Zombie Merge Tycoon: Auto Farm", "pulled from rbxsc
 addscriptexist(2768379856, "3008: Spam Something", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20Spam%20Something.lua")
 
 addscriptexist(9872472334, "Evade: Click TP, God Mode, Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Evade%20Click%20TP%20God%20Mode%20Teleports.lua")
+
+addscriptexist(4924922222, "Brookhaven RP: Teleporter, Instant Kill All & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Brookhaven%20RP%20Teleporter%20Instant%20Kill%20All%20%20More.lua")
