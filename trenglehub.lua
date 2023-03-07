159b9b55d0f964db223eac9c555bd74d30f1f1b7
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:04:09 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:04:05"
+lastupdate = "GMT +1: 07.03.2023 22:04:09"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3718,3 +3718,5 @@ addscriptexist(10895555747, "Walmart Land: Token Auto Farm, Fling Players, Rejoi
 addscriptexist(11400511154, "Monkey Tycoon: Auto Collect Bananas, Auto Deposit Bananas, Auto Buy Monkeys", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Monkey%20Tycoon%20Auto%20Collect%20Bananas%20Auto%20Deposit%20Bananas%20Auto%20Buy%20Monkeys.lua")
 
 addscriptexist(11874473440, "Car Factory Tycoon: Auto Farm, Auto Fill, Auto Sell", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Car%20Factory%20Tycoon%20Auto%20Farm%20Auto%20Fill%20Auto%20Sell.lua")
+
+addscriptexist(6516141723, "DOORS: Players Esp, Client Glow, Doors Esp,", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Players%20Esp%20Client%20Glow%20Doors%20Esp.lua")
