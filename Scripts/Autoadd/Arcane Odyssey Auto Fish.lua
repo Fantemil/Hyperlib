ce15df34e119f14a3b5e7e19cdcbb61639ae0b0f
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:30:00 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arcane Odyssey Auto Fish.lua
@@ -0,0 +1,8 @@
+getgenv().toggle=true
+getgenv().fixedloc=true-- you can toggle this, turning this off will allow you to move to your own fishing spot.
+getgenv().rod="Wooden Rod"--[[
+Wooden Rod
+Collector's Rod
+Bronze Rod
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Disttt/-Arcane-Odyssey/main/AutoFish.lua"))()
