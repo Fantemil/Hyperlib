a00b4a74bafe42774c0704d8ebd29f2365880136
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:00:24 +0100

    Updated automatically!

--- Scripts/Autoadd/STEEP STEPS Anti Cheat Bypass.lua
+++ Scripts/Autoadd/STEEP STEPS Anti Cheat Bypass.lua
@@ -1,8 +1,32 @@
---[[
-    // Name: Steep Steps AC Bypass
-    // Author: Incognito#0666
-    // Desc: "Fuck you jandel"
-    // Date: 2023/02/18 [YYYY/MM/DD]
-]]
+--[[ 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lncoognito/ROBLOX/main/SteepStepsACBypass.lua", true))()
+AC Bypass for Steep Steps
+
+Put this script in your autoexe/auto execution folder
+
+might break some stuff related to chat, but it should work normally if you just click the chatbox and write...?
+
+works for most detected things in the game, easy...
+
+]]--
+
+
+repeat wait() until game:IsLoaded()
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local PlayerScripts = LocalPlayer:WaitForChild("PlayerScripts")
+
+local AntiCheatScript_1 = PlayerScripts:WaitForChild("ChatScript")
+local AntiCheatScript_2 = PlayerScripts:WaitForChild("BubbleChat")
+
+getsenv(AntiCheatScript_1).bd = "bypassed ez"
+getsenv(AntiCheatScript_2).bd = "bypassed ez"
+
+task.wait()
+
+AntiCheatScript_1:Destroy()
+AntiCheatScript_2:Destroy()
+
+warn("anti cheat successfully bypassed ez")
