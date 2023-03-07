f6a70d3345756f15555bdb4918405ea9e60a789e
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:00:10 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Animations Hats Fling.lua
@@ -0,0 +1,23 @@
+-- V3 Key is SyntaxV3Free --
+shared.colors = {
+    Icons = Color3.fromRGB(0,255,149),
+    Version = Color3.fromRGB(0,255,149),
+    Text = Color3.fromRGB(255,255,255),
+    Description = Color3.fromRGB(125,125,125),
+    TabList = Color3.fromRGB(30,30,30),
+    Scripts = Color3.fromRGB(30,30,30),
+    Back = Color3.fromRGB(25,25,25),
+    Glow = Color3.fromRGB(0,0,0),
+}
+shared.transparency = {
+    Version = 0,
+    Text = 0,
+    Description = 0,
+    Icons = 0,
+    Back = 0,
+    Glow = 0.5,
+    TabList = 0,
+    Scripts = 1,
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Memeboiyot/Syntax-V3-Free/main/Syntax%20v3", true))()
