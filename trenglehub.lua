5a19bbd6a7da1b56521b30f78b0eb79c52e4b939
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:56:49 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:56:46"
+lastupdate = "GMT +1: 07.03.2023 21:56:49"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3506,3 +3506,5 @@ addscriptexist(11430505281, "Anime Weapon Simulator: Auto Runk Up, Auto Click, A
 addscriptexist(11040063484, "Sword Fighters Simulator: Auto Click, Auto Ascend, Auto Upgrade", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20Fighters%20Simulator%20Auto%20Click%20Auto%20Ascend%20Auto%20Upgrade.lua")
 
 addscriptexist(23578803, "Hotel Elephant: Game breaking", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hotel%20Elephant%20Game%20breaking.lua")
+
+addscriptexist(185655149, "Welcome to Bloxburg: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Welcome%20to%20Bloxburg%20Auto%20Farm.lua")
