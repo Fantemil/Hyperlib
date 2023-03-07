d781b5f8098515d6457781ab05972053b348496e
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:10:46 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Encounters Esp Legit Rage.lua
@@ -0,0 +1,9 @@
+--[[ 
+            Key System Protected
+            https://discord.gg/9G46pAJFfR By MaGiXx#6964
+                        Version: v0.1
+
+Get Key Here: https://1.kelprepl.repl.co/getkey/WheatHub
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/wheathub/main/main.lua"))()
