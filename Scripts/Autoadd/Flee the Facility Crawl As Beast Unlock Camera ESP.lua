aad3c2f3e9f7d6e80b20036dd9ca0e9738211373
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:22:04 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Flee the Facility Crawl As Beast Unlock Camera ESP.lua
@@ -0,0 +1,4 @@
+--Discord : 
+--https://discord.gg/56fGHt8vES
+--Script (!!REPORT BUGS IN THE DISCORD!!) :
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NexIsHot/Lunar/main/FTF.lua",true))()
