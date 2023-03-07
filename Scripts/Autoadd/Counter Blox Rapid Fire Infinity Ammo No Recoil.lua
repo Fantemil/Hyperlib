bc5c43b6abdb7cf712b09c78474e7fbc12db5ae5
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:41:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Counter Blox Rapid Fire Infinity Ammo No Recoil.lua
@@ -0,0 +1,15 @@
+-- discord.gg/g4EGAwjUAK
+
+_G.Settings = {
+    InfiniteAmmo = true,
+    NoRecoil = true,
+    NoSpread = true,
+    RapidFire = true,
+    FullAuto = true,
+    InstantEquip = true,
+    InstantReload = true,
+    InfinitePenetration = true,
+    InfiniteRange = true,
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mopsfl/roblox-scripts/main/cbro_gunmods.lua"))()
