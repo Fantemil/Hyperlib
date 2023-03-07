9343050d46b4d2e133ceb4255338c6faf83a92ec
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:10:11 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:10:07"
+lastupdate = "GMT +1: 07.03.2023 22:10:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3896,3 +3896,5 @@ addscriptuniversal("Universal FOV, Silent Aim", "pulled from rbxscript.com", "ht
 addscriptexist(3177438863, "Dragon Blox: Auto Strength Farm, Auto Rebirth, Get Drops", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dragon%20Blox%20Auto%20Strength%20Farm%20Auto%20Rebirth%20Get%20Drops.lua")
 
 addscriptexist(654732683, "Car Crushers 2: Mods Your Current Car", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Car%20Crushers%20%20Mods%20Your%20Current%20Car.lua")
+
+addscriptexist(286090429, "Arsenal: Walkspeed, Jump Power, Super Human", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20Walkspeed%20Jump%20Power%20Super%20Human.lua")
