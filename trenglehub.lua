e4168f7f0c8d85da0091cac2edab2b8c2f5d142c
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:36:03 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:35:59"
+lastupdate = "GMT +1: 07.03.2023 21:36:03"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2936,3 +2936,5 @@ addscriptexist(2788229376, "Da Hood: Key Bypassed, Fly, Noclip", "pulled from rb
 addscriptexist(6516141723, "DOORS: Sprint", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Sprint.lua")
 
 addscriptexist(2788229376, "Da Hood: Fake Crouch, Anti Aim", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Fake%20Crouch%20Anti%20Aim.lua")
+
+addscriptuniversal("Universal FE Smoke, FE Vape, Troll", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20FE%20Smoke%20FE%20Vape%20Troll.lua")
