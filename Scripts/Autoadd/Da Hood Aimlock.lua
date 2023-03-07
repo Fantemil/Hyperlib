ad04ce2053befb03815bcc5456644b8a30572373
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:36:54 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Aimlock.lua
@@ -0,0 +1,21 @@
+--< DM fear#0004 for help >--
+
+--< Aimviewer Settings >--
+AimviewerToggleKeybind = "z" -- Aimviewer toggle keybind
+AimviewerSwitchKeybind = "x" -- Switch player keybind
+AimviewerColor = Color3.fromRGB(255, 0, 0) -- Color of Aimviewer beam
+AimviewerBeamWidth = 0.3 -- Width of Aimviewer beam
+
+--< Desync Settings >--
+DesyncKeybind = (Enum.KeyCode.V) -- Desync toggle keybind
+DesyncOffset = 3 -- Desync Offset
+
+--< Antilock >--
+AntiKeybind = "c" -- Antilock toggle keybind
+AntiVelocity = 1450,1450,1450 -- Only change this if yk what youre doing
+
+--< Miscallaneous >--
+icon = "rbxassetid://12319510751" -- Notification Image
+
+--------------------------------------------------------------------------------
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fear0004/script/main/endlockers"))()
