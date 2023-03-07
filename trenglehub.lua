ef9c8e77e270174004249905f9a4422db8fa43fd
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:29:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:29:11"
+lastupdate = "GMT +1: 07.03.2023 21:29:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2744,3 +2744,5 @@ addscript(6229116934, "Current Game","Hoopz: Auto Click, Reach, Auto Move", "pul
 addscript(8204899140, "Current Game","Football Fusion 2: Silent Aim,Auto Jump,Auto TP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Football%20Fusion%20%20Silent%20AimAuto%20JumpAuto%20TP.lua")
 
 addscriptexist(2788229376, "Da Hood: Auto Buy,God Mode,Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Auto%20BuyGod%20ModeAuto%20Farm.lua")
+
+addscriptexist(9872472334, "Evade: AutoFarm,Inf Jump & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Evade%20AutoFarmInf%20Jump%20%20More.lua")
