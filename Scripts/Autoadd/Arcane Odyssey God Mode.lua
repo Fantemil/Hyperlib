ffb6d4771f4323de4ddd53f11fbda2856d476592
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 15:40:32 +0100

    Updated automatically!

--- Scripts/Autoadd/Arcane Odyssey God Mode.lua
+++ Scripts/Autoadd/Arcane Odyssey God Mode.lua
@@ -1,18 +1,1 @@
-shared.godMode = true
-
-local player = game.Players.LocalPlayer
-
-local namecall; namecall = hookmetamethod(game, '__namecall', function(object, ...)
-local args = {...}
-
-if getnamecallmethod() == "FireServer" then
-  if shared.godMode then
-      if object.Name == 'DealWeaponDamage' or object.Name == "DealStrengthDamage" and args[3] == player.Character then
-          args[3] = math.random(1,200);
-      elseif object.Name == 'DealBossDamage' or object.Name == "DealAttackDamage" and args[2] == player.Character then
-              args[2] = math.random(1,200);
-          end;
-      end;
-  end;
-  return namecall(object, unpack(args));
-end);
+loadstring(game:HttpGet('https://raw.githubusercontent.com/0ho1/scripts/main/Arcane%20Oddysey%20God%20Mode'))()
