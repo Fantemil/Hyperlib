341c2f56e2456333d8eeaa994ff763538fd33808
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:37:54 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 12:37:49"
+lastupdate = "GMT +1: 07.03.2023 12:37:54"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -1175,3 +1175,5 @@ addscriptexist(8568266872, "Kill Monsters To Save Princess: Auto Waves, Fast Hit
 addscriptexist(3398014311, "Restaurant Tycoon 2: Insta Finish For Npcs, Instant Cook", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Restaurant%20Tycoon%20%20Insta%20Finish%20For%20Npcs%20Instant%20Cook.lua")
 
 addscriptexist(6516141723, "DOORS: ROOMS Auto Complete", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20ROOMS%20Auto%20Complete.lua")
+
+addscriptuniversal("Universal Block Builder", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Block%20Builder.lua")
