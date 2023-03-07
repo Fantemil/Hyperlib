8f01f37d4a8d37abd53cf586ded112589c5421c8
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:09:36 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Pilgrammed Mob Auto Farm ServerHop.lua
@@ -0,0 +1,69 @@
+--strongly suggested to put inside your autoexecute for afk farming/bossfarming!
+getgenv().slot=1--put your desired slot
+getgenv().serverhop=false--joins a new server when you die or get kicked instead of a different one!
+getgenv().abovemob=12--suggested 11
+getgenv().toggle=true--will turn off when the
+getgenv().weapon="Mithril Sword"--[[-- swords recommended
+Buster Sword
+Bronze Sword
+Bronze Greatsword
+Demetal Sword
+Demetal Sword
+Scrap Longsword
+Iceberg
+Mithril Sword
+Gilded Greatsword
+Chromatic Greatsword
+Old Sword
+Executioner's Greatsword
+Iron Sword
+Owl's Straight Sword
+A Very Old Sword
+]]
+getgenv().Area="Prairie"--[[Autofarm will only work for these area's if there's already existing enemies!, leave blank to autofarm nearby enemies.
+Scarecrow
+Iceberg
+ArchMage
+CloudWilds
+Cannon
+Cliff
+Sea
+Forest
+ThePatris
+Labyrinth
+Prairie
+PrismTroll
+OrangeTemple
+BigFish
+CloudWilds
+PiratePopper
+DesertRanger
+KingSandpod
+PoopIsland
+MondoSquid
+HighwaySon
+MagmaTurtle
+Urchin
+Mountain
+Snowman
+HighlandWarden
+Hunter
+Yalgaroth
+Iceking
+Ant
+OldFart
+ColdCliffBoulder
+Spectre
+Viper
+Suslands
+EggIsland
+Granny
+DeepDesert
+Chalkman
+BearIsland
+Treelad
+DesertTemple
+Swamp
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Disttt/Pilgrammed/main/77-Mob%20autofarm.lua"))()
