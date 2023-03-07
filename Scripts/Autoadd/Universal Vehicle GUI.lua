6428cadff68b0e240bcfbe90bd5b8f24ef6a6258
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:02:17 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Vehicle GUI.lua
@@ -0,0 +1,5 @@
+_G.RedGUI = false
+_G.Theme = "Aqua" -- Must disable or remove _G.RedGUI to use
+--Themes: Light, Dark, Mocha, Aqua and Jester
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/UniversalVehicleGUI.lua"))()
