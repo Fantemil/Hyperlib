f1e3228a5b9238407025718b46adaf31139593a4
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:33:39 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:33:35"
+lastupdate = "GMT +1: 07.03.2023 21:33:39"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2866,3 +2866,5 @@ addscriptexist(2474168535, "Westbound: ESP, Tracers & More", "pulled from rbxscr
 addscriptexist(155615604, "Prison Life: Teleports, Auto Get All & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prison%20Life%20Teleports%20Auto%20Get%20All%20%20More.lua")
 
 addscriptexist(2788229376, "Da Hood: Noclip, FOV, Walkspeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Noclip%20FOV%20Walkspeed.lua")
+
+addscriptexist(3956818381, "Ninja Legends: AutoFarm, Auto Sell & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ninja%20Legends%20AutoFarm%20Auto%20Sell%20%20More.lua")
