e7b9b8e244c8fda69a77b2e8108ff85ed62f52e9
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:59:52 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Zombie Merge Tycoon Kill Boss.lua
@@ -0,0 +1,8 @@
+game:GetService("Players").LocalPlayer.Character.WEAPONNAMEHERE.Remotes.TakeDamage:FireServer(workspace.Boss.Boss.Humanoid, 10)
+
+--> Change 10 to whatever amount you want to do to a boss
+
+
+--> Must be holding the weapon 
+
+-->  Weapon names are the following: Pistol, SMG, AR, Sniper, MachineGun, PulseSMG 
