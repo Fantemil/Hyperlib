70ef3582a4c45ae0120d2e9dd01da2a47bd2e1ef
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:04:44 +0100

    Updated automatically!

--- Scripts/Autoadd/Universal Anti Kick.lua
+++ Scripts/Autoadd/Universal Anti Kick.lua
@@ -1,1 +1,31 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Anti-Kick/main/Anti%20Kick.lua"))()
+                local hook = game:GetService("Players").LocalPlayer.Kick;
+                local mt = getrawmetatable(game:GetService("Players").LocalPlayer)
+                local psuedoEnv = {
+                    ["__index"] = mt.__index,
+                    ["__namecall"] = mt.__namecall
+                }
+                local ReturnFunc = function()end
+                        
+                warn("Made By MHD444#0001")
+                setreadonly(mt, false)
+                mt.__namecall = newcclosure(function(self, Index, ...)
+                    local NameCallMethod = getnamecallmethod()
+                    if type(NameCallMethod) == "string" then
+                        if NameCallMethod == "Kick" or NameCallMethod == "kick" then
+                            print("Prevented Kick At " .. tostring(os.time()))
+                            return ReturnFunc
+                        end
+                    end
+                    return psuedoEnv.__namecall(self, Index, ...)
+                end)
+                        
+                mt.__index = newcclosure(function(self, Index, ...)
+                    if type(Index) == "string" then
+                        if Index == "Kick" or Index == "kick" then
+                            print("Prevented Kick At " .. tostring(os.time()))
+                            return ReturnFunc
+                        end
+                    end
+                    return psuedoEnv.__index(self, Index, ...)
+                end)
+                setreadonly(mt, true)
