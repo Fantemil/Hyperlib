c8f1e7d51ba0d54cc47d30dd5abb9c3ad26053db
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:18:06 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal MacNotify.lua
@@ -0,0 +1,8 @@
+local MacNotify = loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/UI-s/main/Notifications/MacNotify.folder/MacNotifyMain.lua'))()
+
+notify({
+ title = "TITLE",
+ description = "DESCRIPTION",
+ icon = "rbxassetid://",
+ time = 1 -- Only Whole Numbers
+})
