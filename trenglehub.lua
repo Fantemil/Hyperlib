13aec616ee04f62bba176cfcdefd13cec0c3380b
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:50:39 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:50:35"
+lastupdate = "GMT +1: 07.03.2023 21:50:39"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3358,3 +3358,5 @@ addscriptexist(6516141723, "DOORS: No E Wait, Spawn Seek, Fullbright", "pulled f
 addscriptexist(11542692507, "Anime Souls Simulator: Auto Attack, Auto Mob", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Souls%20Simulator%20Auto%20Attack%20Auto%20Mob.lua")
 
 addscriptexist(6284583030, "Pet Simulator X: Auto Fuse, Auto Convert & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Auto%20Fuse%20Auto%20Convert%20%20More.lua")
+
+addhub("Lunar Hub: 1 Games", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Lunar%20Hub%20%20Games.lua")
