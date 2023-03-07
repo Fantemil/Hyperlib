3c3647ad10dbae06c820851912d1795a723eed2a
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:05:40 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/PLS DONATE Сlub Teleport Bypass.lua
@@ -0,0 +1,4 @@
+game:GetService("Players").LocalPlayer.PlayerScripts.DonatorsArea.Disabled = true
+
+wait(0.2)
+game.Players.LocalPlayer.Character.PrimaryPart.CFrame  = game:GetService("Workspace").TeleportB.CFram
