f5e9e78ebe2355b1ba8015d42fd199f14baf9c41
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:09:37 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 12:09:33"
+lastupdate = "GMT +1: 07.03.2023 12:09:37"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -1305,3 +1305,5 @@ addscriptexist(10982284336, "Anime Idle Simulator: Inf DPS, Auto Upgrade, Auto F
 addscript(7253149844, "Scripts for current game","SCP Games and SCP Monsters: Morph", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SCP%20Games%20and%20SCP%20Monsters%20Morph.lua")
 
 addscript(error, "Scripts for current game","Universa Working Free limiteds", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universa%20Working%20Free%20limiteds.lua")
+
+addscriptexist(6735572261, "Pilgrammed: Mob Auto Farm, ServerHop", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pilgrammed%20Mob%20Auto%20Farm%20ServerHop.lua")
