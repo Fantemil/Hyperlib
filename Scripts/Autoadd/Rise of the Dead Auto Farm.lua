7a58a5154a0499e92d7633625ec8994dcb5b3c2d
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 20:54:50 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Rise of the Dead Auto Farm.lua
@@ -0,0 +1,3 @@
+getgenv().meleeKnife = "survivalknife" -- Go through the game and get the first knife! after that enjoy
+-- btw you can change it to your melee if needed just put its name there in lowercase
+loadstring(game:HttpGet("http://foogyscripts.medianewsonline.com/rotd.lua"))()
