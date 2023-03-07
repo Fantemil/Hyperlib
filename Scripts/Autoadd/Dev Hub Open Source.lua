3ee2e5d7c85159dd2024dc8f6027d6b5d039e81f
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:49:52 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Dev Hub Open Source.lua
@@ -0,0 +1,3 @@
+-- [[ CHECK THE DESCRIPTION FOR OTHER SOURCES ]]--
+--[[ Ill just put the arsenal script keyless and source in here ]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RiseValco/keybypasses/main/devhub/Arsenal.lua"))()
