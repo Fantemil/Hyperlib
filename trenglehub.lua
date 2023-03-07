9e9f2931b839102c2f7d8b6992d290aa2d593e48
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:10:22 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:10:18"
+lastupdate = "GMT +1: 07.03.2023 22:10:22"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3902,3 +3902,5 @@ addscriptexist(286090429, "Arsenal: Walkspeed, Jump Power, Super Human", "pulled
 addscriptexist(11542692507, "Anime Souls Simulator: Auto Attack, Auto Mobs & More!", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Souls%20Simulator%20Auto%20Attack%20Auto%20Mobs%20%20More.lua")
 
 addscriptexist(2788229376, "Da Hood: Tracers, Silent Aim", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Tracers%20Silent%20Aim.lua")
+
+addscriptexist(3101667897, "Legends Of Speed: Auto Farm Steps, Auto Farm Hoops, Auto Farm Gems", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legends%20Of%20Speed%20Auto%20Farm%20Steps%20Auto%20Farm%20Hoops%20Auto%20Farm%20Gems.lua")
