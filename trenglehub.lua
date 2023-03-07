5c5fc1c7a103012d09822f23dbd8cb7a155082b2
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:16:56 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:16:52"
+lastupdate = "GMT +1: 07.03.2023 22:16:56"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4080,3 +4080,5 @@ addscriptexist(6777872443, "Pixel Piece: Infinity Stats", "pulled from rbxscript
 addscriptexist(11542692507, "Anime Souls Simulator: Auto-Farm, Auto-Quest, Auto-Punch", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Souls%20Simulator%20AutoFarm%20AutoQuest%20AutoPunch.lua")
 
 addscript(3432786397, "Current Game","Pet Mining Simulator: BTools, Nuke, Kick", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Mining%20Simulator%20BTools%20Nuke%20Kick.lua")
+
+addscriptexist(10171072079, "Operations Siege: Hvh & Legit Features!", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Operations%20Siege%20Hvh%20%20Legit%20Features.lua")
