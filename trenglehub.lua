c5ef7e0ee79618e425ce6d920d98d84083aa41c0
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:15:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:15:10"
+lastupdate = "GMT +1: 07.03.2023 21:15:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2382,3 +2382,5 @@ addscriptexist(2788229376, "Da Hood: God mode, Aimlocks, Resolvers", "pulled fro
 addscriptexist(891852901, "Greenville: Auto Farm, Walkspeed, Infinite Jump & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Greenville%20Auto%20Farm%20Walkspeed%20Infinite%20Jump%20%20More.lua")
 
 addscript(4011063766, "Current Game","Tank Warfare: Change firerate, magazine, Reserve", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tank%20Warfare%20Change%20firerate%20magazine%20Reserve.lua")
+
+addscriptexist(4738545896, "SHOOT OUT!: Silent Aim, Gun Mods, Auto Kill All & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SHOOT%20OUT%20Silent%20Aim%20Gun%20Mods%20Auto%20Kill%20All%20%20More.lua")
