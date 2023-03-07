5561de6a6504deab429eb0cdfc8b9dce389c182a
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:10:01 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:09:57"
+lastupdate = "GMT +1: 07.03.2023 21:10:01"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2230,3 +2230,5 @@ addscriptexist(2474168535, "Westbound: No Recoil, No Spread, Silent Aim", "pulle
 addscript(6708206173, "Current Game","Rate My Avatar: Trivia Booth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rate%20My%20Avatar%20Trivia%20Booth.lua")
 
 addscriptexist(11636413564, "Meme Mergers: Auto Merge, Auto Click, Auto Obby", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Meme%20Mergers%20Auto%20Merge%20Auto%20Click%20Auto%20Obby.lua")
+
+addscriptexist(6284583030, "Pet Simulator X: Auto-Farm, Keybinds & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20AutoFarm%20Keybinds%20%20More.lua")
