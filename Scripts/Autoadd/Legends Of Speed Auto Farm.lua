cbcb74bb7a95b46d21fb4f823afe647470d80f1f
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:37:05 +0100

    Updated automatically!

--- Scripts/Autoadd/Legends Of Speed Auto Farm.lua
+++ Scripts/Autoadd/Legends Of Speed Auto Farm.lua
@@ -1,1 +1,25 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/iPigTW/Silver-HUB/main/SilverHub.lua",true))()
+-- By DylanIsAKing#2402 :)
+
+if getgenv().autoCollect then getgenv().autoCollect = false
+else getgenv().autoCollect = true
+end
+
+local function touch(object)
+    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, object, 0)
+    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, object, 1)
+end
+
+while getgenv().autoCollect do
+    for i, v in pairs(game:GetService("Workspace").orbFolder.City:GetChildren()) do
+        if v:FindFirstChild("outerOrb") then
+            touch(v.outerOrb)
+        elseif v:FindFirstChild("outerGem") then
+            touch(v.outerGem)
+        end
+        task.wait()
+    end
+    for i, v in pairs(game:GetService("Workspace").Hoops:GetChildren()) do
+        touch(v)
+        task.wait()
+    end
+end
