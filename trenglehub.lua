315e18d4f4a3735b9d3e1cace8c9065ceb3e53dc
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:22:05 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:22:01"
+lastupdate = "GMT +1: 07.03.2023 22:22:05"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4236,3 +4236,5 @@ addscriptuniversal("L Hub: 5+ Games", "pulled from rbxscript.com", "https://raw.
 addscript(7734941266, "Current Game","UTMM: Calamity: Get Everything", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/UTMM%20Calamity%20Get%20Everything.lua")
 
 addscriptexist(6512923934, "Ultra Clickers Simulator: Auto Farm, Noclip, Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultra%20Clickers%20Simulator%20Auto%20Farm%20Noclip%20Teleports.lua")
+
+addscriptexist(893973440, "Flee the Facility: Crawl As Beast, Unlock Camera, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Flee%20the%20Facility%20Crawl%20As%20Beast%20Unlock%20Camera%20ESP.lua")
