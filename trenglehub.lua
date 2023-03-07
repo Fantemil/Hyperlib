110ab0cb87520b7c16d4fc587ed77d1dbfa8b926
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:09:42 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:09:38"
+lastupdate = "GMT +1: 07.03.2023 22:09:42"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3882,3 +3882,5 @@ addscriptuniversal("Universal Login GUI", "pulled from rbxscript.com", "https://
 addscriptexist(4282985734, "Combat Warriors: Kill Aura, Inf Jump, Anti Kick", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Combat%20Warriors%20Kill%20Aura%20Inf%20Jump%20Anti%20Kick.lua")
 
 addscriptexist(6284583030, "Pet Simulator X: A set of GUI's", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20A%20set%20of%20GUIs.lua")
+
+addscriptexist(10700669209, "Control Army: Auto Attack, Inf Jump, WalkSpeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Control%20Army%20Auto%20Attack%20Inf%20Jump%20WalkSpeed.lua")
