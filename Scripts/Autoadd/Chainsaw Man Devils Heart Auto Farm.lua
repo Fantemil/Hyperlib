30cd17456a27112bb5fe5c42728c6e8a3decea08
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:13:53 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Chainsaw Man Devils Heart Auto Farm.lua
@@ -0,0 +1,6 @@
+_G.Webhook = true
+_G.toggle = true
+local Webhook = ' '
+pcall(function()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Jax-star/Chainsaw-Man-Auto-Devil-Heart/main/Auto'))()
+end)
