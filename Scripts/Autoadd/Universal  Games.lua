3f33b8f97d8370d4a4efe427cb56b0ce26475639
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:09:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal  Games.lua
@@ -0,0 +1,9 @@
+-- GUI to LUAU
+-- GtL - V3.2
+
+-- MoonSec V3
+
+-- xaoui - V1.3 release
+-- .gg/dFCCP3fwgA | key here / questions
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/xaophf/xaohub-extra/main/xaoui'),true))()
