c52346bb092b000633e3e58fc776c685f7ae4ed6
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:22:28 +0100

    Updated automatically!

--- Scripts/Autoadd/Legends Of Speed Auto Farm.lua
+++ Scripts/Autoadd/Legends Of Speed Auto Farm.lua
@@ -1,99 +1,57 @@
-local uwu = Instance.new("ScreenGui")
-local Main = Instance.new("Frame")
-local cre = Instance.new("TextLabel")
-local step = Instance.new("TextButton")
-local gems = Instance.new("TextButton")
-local out = Instance.new("TextButton")
+if getgenv().Stepped ~= nil and getgenv().WhileLoop ~= nil then
+    getgenv().Stepped:Disconnect()
+    getgenv().Stepped = nil
+    task.cancel(getgenv().WhileLoop)
+    getgenv().WhileLoop = nil
+end
 
-uwu.Name = "uwu"
-uwu.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-uwu.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-uwu.ResetOnSpawn = false
+getgenv().Enabled = false -- // Change to "false" to disable
 
-Main.Name = "Main"
-Main.Parent = uwu
-Main.BackgroundColor3 = Color3.new(0.313726, 0.313726, 0.313726)
-Main.Position = UDim2.new(0.604411781, 0, 0.214574918, 0)
-Main.Size = UDim2.new(0, 193, 0, 148)
-Main.Selectable = true
-Main.Active = true
-Main.Draggable = true
+if getgenv().Enabled == false then
+    return
+end
 
-cre.Name = "cre"
-cre.Parent = Main
-cre.BackgroundColor3 = Color3.new(0, 0, 0)
-cre.BackgroundTransparency = 1
-cre.Position = UDim2.new(-0.00310030137, 0, 0.870257616, 0)
-cre.Size = UDim2.new(0, 193, 0, 19)
-cre.Font = Enum.Font.SourceSans
-cre.Text = "by Memedog#8352"
-cre.TextColor3 = Color3.new(1, 1, 1)
-cre.TextSize = 18
+local Player = game:GetService("Players").LocalPlayer
+local RunService = game:GetService("RunService")
+local RepStorage = game:GetService("ReplicatedStorage")
 
+local Level = Player.level
+local Multiplier = Player.rebirthMultiplier
 
-step.Name = "step"
-step.Parent = Main
-step.BackgroundColor3 = Color3.new(1, 1, 1)
-step.Position = UDim2.new(0.1938546, 0, 0.19810304, 0)
-step.Size = UDim2.new(0, 117, 0, 25)
-step.Font = Enum.Font.SourceSans
-step.Text = "Steps Farm"
-step.TextColor3 = Color3.new(0, 0, 0)
-step.TextSize = 18
-step.MouseButton1Click:connect(function()
- for i=1, 7500 do
-     local A_1 = "collectOrb"
-local A_2 = "Red Orb"
-local A_3 = "City"
-local Event = game:GetService("ReplicatedStorage").rEvents.orbEvent
-Event:FireServer(A_1, A_2, A_3)
+local RemoteEvents = RepStorage.rEvents
+local RebirthEvent = RemoteEvents.rebirthEvent
+local OrbEvent = RemoteEvents.orbEvent
+
+local Functions = require(RepStorage.globalFunctions)
+
+local OrbArguments = {
+    {[1] = "collectOrb", [2] = "Red Orb", [3] = "City"},
+    {[1] = "collectOrb", [2] = "Gem", [3] = "City"},
+    {[1] = "collectOrb", [2] = "Blue Orb", [3] = "City"},
+    {[1] = "collectOrb", [2] = "Yellow Orb", [3] = "City"},
+    {[1] = "collectOrb", [2] = "Orange Orb", [3] = "City"}
+}
+
+getgenv().Stepped = RunService.Stepped:Connect(function()
+for _, Obj in next, Workspace.Hoops:GetChildren() do
+if Player.Character ~= nil then
+    firetouchinterest(Player.Character.HumanoidRootPart, Obj, 0)
+    task.wait()
+    firetouchinterest(Player.Character.HumanoidRootPart, Obj, 1)
+end
 end
 end)
 
-gems.Name = "gems"
-gems.Parent = Main
-gems.BackgroundColor3 = Color3.new(1, 1, 1)
-gems.Position = UDim2.new(0.188673258, 0, 0.515670598, 0)
-gems.Size = UDim2.new(0, 117, 0, 25)
-gems.Font = Enum.Font.SourceSans
-gems.Text = "Gems Farm"
-gems.TextColor3 = Color3.new(0, 0, 0)
-gems.TextSize = 18
-gems.MouseButton1Click:connect(function()
- for i=1, 1000 do
-     local A_1 = "collectOrb"
-local A_2 = "Gem"
-local A_3 = "City"
-local Event = game:GetService("ReplicatedStorage").rEvents.orbEvent
-Event:FireServer(A_1, A_2, A_3)
-end
-end)
-
-out.Name = "out"
-out.Parent = Main
-out.BackgroundColor3 = Color3.new(1, 1, 1)
-out.BackgroundTransparency = 1
-out.Position = UDim2.new(0.917098463, 0, 0, 0)
-out.Size = UDim2.new(0, 16, 0, 18)
-out.Font = Enum.Font.SourceSans
-out.Text = "X"
-out.TextColor3 = Color3.new(1, 0, 0)
-out.TextSize = 20
-out.MouseButton1Click:connect(function()
- Main.Visible = false
-end)
-
-_G.bruh = true
-while _G.bruh do 
-    wait()
-
-for _,v in next, game.Workspace:GetDescendants() do
-if v.Name == "outerOrb" then
-    v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-    elseif v.Name == "outerGem" then
-        v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-        elseif v.Name == "Hoop" then 
-            v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+getgenv().WhileLoop = task.spawn(function()
+    while task.wait() and getgenv().Enabled == true do
+        local NeededRebirths = Functions.calculateRequiredRebirthLevel(Multiplier.Value - 1)
+        
+        for _, Arguments in next, OrbArguments do
+        OrbEvent:FireServer(table.unpack(Arguments))
+        end
+        
+        if tonumber(NeededRebirths) == tonumber(Level.Value) then
+            RebirthEvent:FireServer("rebirthRequest")
+        end
     end
-end
-end
+end)
