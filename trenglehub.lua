d6e571ca6c529260d281728523d60bbcc381bd8c
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:13:03 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:12:59"
+lastupdate = "GMT +1: 07.03.2023 22:13:03"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3978,3 +3978,5 @@ addscriptexist(9938675423, "Oaklands: Tree Aura", "pulled from rbxscript.com", "
 addscriptexist(2788229376, "Da Hood: Fly, Noclip, Fly Speed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Fly%20Noclip%20Fly%20Speed.lua")
 
 addscriptexist(3527629287, "Big Paintball: Trigger Bot", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Big%20Paintball%20Trigger%20Bot.lua")
+
+addscriptexist(3101667897, "Legends Of Speed: Auto Collect Orbs", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legends%20Of%20Speed%20Auto%20Collect%20Orbs.lua")
