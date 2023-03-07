d32cac9f4a4ba291e79a80f89d0982420f88c724
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:16:25 +0100

    Updated automatically!

--- Scripts/Autoadd/Haram Hub  Games.lua
+++ Scripts/Autoadd/Haram Hub  Games.lua
@@ -1,1 +1,9 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AliCode14/scripts/main/ScriptHub.lua"))()
+
+--discord.gg/4VfbRmQhp6
+-- supported games 
+-- Anime Fruit Simulator
+-- Bubble Gum Clicker
+-- +1 Blocks Every Second
+-- logo made by SolomkaDev#9299
+-- scripted by me 
