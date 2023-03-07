bb319488605e7a8749b1d515be8ca803c921a8ee
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 20:58:20 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal ChatGPT In Roblox.lua
@@ -0,0 +1,2 @@
+getgenv().SECRETKEY = "Enter Secret Key Here" --https://beta.openai.com/account/api-keys
+loadstring(game:HttpGet('https://raw.githubusercontent.com/fgasfga/ChatGPT-Roblox/main/Main.lua'))()
