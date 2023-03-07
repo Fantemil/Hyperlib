6ab04bbd406841806fe8058b6ebb7c7310499e8f
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:10:25 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/DOORS Spawn Tablet.lua
@@ -0,0 +1,6 @@
+_G.UpdateStars = false -- stars disappear after picking up a book/breaker pole | false: a little lag
+_G.OnShop = true -- can buy on pre run shop
+_G.Price = 10 -- tablet price on shop
+_G.Description = "" -- tablet description on shop
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DeividComSono/Scripts/main/Scanner.lua'))()
