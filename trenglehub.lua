fa262f7bbe671c8b90035bf300259a6a2f249a2d
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:08:45 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:08:41"
+lastupdate = "GMT +1: 07.03.2023 22:08:45"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3852,3 +3852,5 @@ addscript(632574862, "Current Game","Eclipsis: Aimbot, Trigger Bot, Hitboxes", "
 addscriptexist(142823291, "Murder Mystery 2: Kill All, Unlock Emotes, Esp", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Murder%20Mystery%20%20Kill%20All%20Unlock%20Emotes%20Esp.lua")
 
 addscriptexist(11400511154, "Monkey Tycoon: Auto Buy Monkey, Auto Sell, Auto Upgrade", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Monkey%20Tycoon%20Auto%20Buy%20Monkey%20Auto%20Sell%20Auto%20Upgrade.lua")
+
+addscriptexist(2753915549, "Blox Fruits: ADMINUS Devil Fruit Finder", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20ADMINUS%20Devil%20Fruit%20Finder.lua")
