873aa26e6c2b638eaad60e8cfb8ce9ec06734efe
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:16:21 +0100

    Updated automatically!

--- Scripts/Autoadd/Universal Anti Http Spy.lua
+++ Scripts/Autoadd/Universal Anti Http Spy.lua
@@ -1,3 +1,45 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fgasfga/misc-releases/main/Anti%20Http%20Spy.lua", true))()
+local functions = {
+    rconsoleprint,
+    print,
+    setclipboard,
+    rconsoleerr,
+    rconsolewarn,
+    warn,
+    error
+}
 
---Put You'r Script Under LoadString
+for i, v in next, functions do
+    local old
+    old =
+        hookfunction(
+        v,
+        newcclosure(
+            function(...)
+                local args = {...}
+                for i, v in next, args do
+                    if tostring(i):find("https") or tostring(v):find("https") then
+                        while true do
+                        end
+                    end
+                end
+                return old(...)
+            end
+        )
+    )
+end
+
+if _G.ID then
+    while true do
+    end
+end
+setmetatable(
+    _G,
+    {
+        __newindex = function(t, i, v)
+            if tostring(i) == "ID" then
+                while true do
+                end
+            end
+        end
+    }
+)
