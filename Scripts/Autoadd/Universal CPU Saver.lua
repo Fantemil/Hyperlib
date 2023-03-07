46def543002ee1c89f4f27c5caa47acb5515907a
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:31:09 +0100

    Updated automatically!

--- Scripts/Autoadd/Universal CPU Saver.lua
+++ Scripts/Autoadd/Universal CPU Saver.lua
@@ -1,15 +1,1 @@
---[[+
-
-Roblox-GPU-SaverLITE;
-_VERSION 1.0;
-dsc.gg/empifyutility;
-Licensed under GNU General Public License v3.0;
-
-LITE VERSION
-
-+]]
-
-_G.GPUSave = false --// Toggles the GPU/CPU Saver
-_G.LowFPS = false --// Makes the FPS low (Recommened ON)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Empire4946/roblox-gpu-saverLITE/main/handler/main.lua",true))()
+while task.wait() do if iswindowactive() then setfpscap(1000) else setfpscap(5) end end 
