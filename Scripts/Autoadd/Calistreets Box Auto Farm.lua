8b1d6b42ccbbcf6761f2c5b79a80f7cc36a4a594
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:19:39 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Calistreets Box Auto Farm.lua
@@ -0,0 +1,8 @@
+-- // AUTOFARM CONFIGURATION \\ --
+
+_G.box_AF = true --> true: AF Enabled | false: AF Disabled (Use the switch key)
+_G.switchKey = Enum.KeyCode.RightShift --> List: https://create.roblox.com/docs/reference/engine/enums/KeyCode
+
+-- // DONT TOUCH \\ --
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/imvipp/roblox/main/shitware/calistreets/box_autofarm.lua'))()
