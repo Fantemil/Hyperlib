ec9721def86b5edace23ef87d638bfe800e1f4aa
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:43:17 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Car Crushers  Serverhop Atomic Server  Gamepass Only.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 654732683 then
+repeat wait() until game:IsLoaded()
+repeat wait() local a, b = pcall(function()loadstring(game:HttpGet('https://pastebin.com/raw/hJ7d8CzD'))() end) if not a then wait(30) end until a
+endjust put it in ur auto exec. also for sum reason it wont re-execute so tats why its auto excec anyways lmk if u know why it wont re exec
