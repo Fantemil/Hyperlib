12522657c82625c4bcb40c6e30e8884fd9834ad0
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:08:33 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:08:30"
+lastupdate = "GMT +1: 07.03.2023 22:08:33"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3846,3 +3846,5 @@ addscriptexist(6777872443, "Pixel Piece: Tp Bypass", "pulled from rbxscript.com"
 addhub("Yum Hub: 3 Games", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Yum%20Hub%20%20Games.lua")
 
 addscriptexist(6777872443, "Pixel Piece: Teleport to the Test Place", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pixel%20Piece%20Teleport%20to%20the%20Test%20Place.lua")
+
+addscript(632574862, "Current Game","Eclipsis: Aimbot, Trigger Bot, Hitboxes", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Eclipsis%20Aimbot%20Trigger%20Bot%20Hitboxes.lua")
