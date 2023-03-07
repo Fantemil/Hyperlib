6a43867105cd920895775cc97bfa7a794825ede9
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:07:50 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Military Tycoon Auto Raid Server Hop.lua
@@ -0,0 +1,14 @@
+--[[
+! NEED TO PUT IN AUTO EXEC FOLDER !
+
+_G.AutoRebirth = true/false -- Does rebirth if can
+_G.SafeTeleport = true/false -- Just teleport to safe (just to visualize the raid, does not affect progress)
+_G.AutoBuyButtons = true/false -- Buy Buttons
+_G.AutoBuyCard = true/false -- Buy Ñards if can
+_G.AntiAfkKick = true/false -- Makes it so that the game doesn't kick for afk
+_G.GpuSave = true/false -- Does fps to 20 for reduce the computer load (and coolers are less noisy)
+_G.BadInternet = true/false -- If you have a bad internet, turn it on to avoid errors (slows down the script)
+
+! NEED TO PUT IN AUTO EXEC FOLDER !
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/Vip-Scripts-OopssHub-/main/MilitaryTycoonFarm.lua"))()
