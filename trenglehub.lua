5314e9d03f644a692a760f0901439292049c8108
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:15:42 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:15:38"
+lastupdate = "GMT +1: 07.03.2023 21:15:42"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2396,3 +2396,5 @@ addscriptexist(286090429, "Arsenal: Kill All, Aimbot, Inf Ammo", "pulled from rb
 addscriptexist(9848789324, "Ragdoll Engine: Server Crasher, Teleports, Animations", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ragdoll%20Engine%20Server%20Crasher%20Teleports%20Animations.lua")
 
 addscriptexist(8260276694, "Ability Wars: AutoFarm, Godmode, Kill all", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ability%20Wars%20AutoFarm%20Godmode%20Kill%20all.lua")
+
+addscriptexist(298400657, "Dragon Ball Final Remastered: Auto Meditate", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dragon%20Ball%20Final%20Remastered%20Auto%20Meditate.lua")
