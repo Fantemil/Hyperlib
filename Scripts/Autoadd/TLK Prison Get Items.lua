a5c5e3caf125da7ecb3bbba51db93090ea3e83af
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:02:00 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/TLK Prison Get Items.lua
@@ -0,0 +1,5 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("TLK PRISON GUI", "RJTheme3")
+
+local Tab = Window:NewTab("Get Items")
