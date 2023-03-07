0ffea1d2d197043bfca86497bbbed77dbf505b08
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:21:44 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Auto Insulter.lua
@@ -0,0 +1,5 @@
+-- // Press Z to insult whoever's been highlighted
+-- // Press Left Alt to toggle the script
+-- // Automatically takes the game's name, description, and player name to generate unique insults through AI.
+
+loadstring(game:HttpGet('https://scripts.luawl.com/15372/AutoInsulter.lua'))()
