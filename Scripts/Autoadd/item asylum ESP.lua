5878f366f3d9a56627fff5c67d637d9d865f2e0a
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:30:12 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/item asylum ESP.lua
@@ -0,0 +1,12 @@
+getgenv().BullshitItems = {
+   "death note",
+   "bobm",
+   "gender reveal",
+   "borrow",
+   "pact",
+   "america"
+}
+
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/zntly/asylum/main/oftheitem"))()
+
+ESP:Toggle(true)
