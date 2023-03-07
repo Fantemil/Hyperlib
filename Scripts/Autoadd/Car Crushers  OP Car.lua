5dde47ae802fefbd2260441b280b2e01fbe1d9ed
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:22:21 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Car Crushers  OP Car.lua
@@ -0,0 +1,6 @@
+local config = require(game:GetService("Workspace").CarCollection[game:GetService("Players").LocalPlayer.Name]:FindFirstChild("Car").Body.Configuration)
+config.BrakeForce = 9999999999999
+config.Horsepower = 9999999999999
+config.Acceleration = 9999999999999
+config.TopSpeed = 9999999999999
+config.Handling = 10
