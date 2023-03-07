38d9f3a80ec9b0ab77fbf7ffa72675f51b05f5bc
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:02:44 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:02:40"
+lastupdate = "GMT +1: 07.03.2023 22:02:44"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3676,3 +3676,5 @@ addscriptexist(920587237, "Adopt Me: Autofarm, Autobaby, Teleports", "pulled fro
 addscriptexist(4566572536, "Vehicle Legends: Drag Race Farm, Highway Race Farm, Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Vehicle%20Legends%20Drag%20Race%20Farm%20Highway%20Race%20Farm%20Auto%20Farm.lua")
 
 addscriptuniversal("Universal Spin and Play Phonky Town", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Spin%20and%20Play%20Phonky%20Town.lua")
+
+addscriptexist(891852901, "Greenville: Auto Farm, Always Car Clean, Infinity Fuel", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Greenville%20Auto%20Farm%20Always%20Car%20Clean%20Infinity%20Fuel.lua")
