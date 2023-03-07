9a4fdf25565374fcf697af14626ffc2f3b967bd2
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:56:28 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Anime Defense Simulator Auto Farm Auto Noclip Eggs.lua
@@ -0,0 +1,3 @@
+getgenv().MyWebhook = "XXX"
+-- If you want to use Webhook features, put ur URL WEBHOOK there. if not, just ignore it.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SomeDudeMadeYouHappy/AnimeDefenseSimulator/main/IchiGoatKurosaki.lua"))()
