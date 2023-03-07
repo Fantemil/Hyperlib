e414d5fbdb8b8c608762133b6bc4d89c84dad8c9
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:21:40 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Rob A Jewellery Store Speed Coil Gravity Coil.lua
@@ -0,0 +1,4 @@
+firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").SpeedGiver.Part, 0)
+firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").SpeedGiver.Part, 1)
+firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").GravGiver.Part, 0)
+firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").GravGiver.Part, 1)
