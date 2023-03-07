2c960e6fc69d57bcd18de3ea4267a0fb01fd910b
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:10:21 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:10:18"
+lastupdate = "GMT +1: 07.03.2023 21:10:21"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -2240,3 +2240,5 @@ addscript(5740246170, "Current Game","Tower Simulator: Auto Build, Auto Upgrade"
 addscriptexist(8328351891, "Mega Mansion Tycoon: Auto collect, Auto Build", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mega%20Mansion%20Tycoon%20Auto%20collect%20Auto%20Build.lua")
 
 addscriptexist(2413927524, "The Rake REMASTERED: Esp Rake", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Rake%20REMASTERED%20Esp%20Rake.lua")
+
+addscriptexist(6735572261, "Pilgrammed: Rainbow Hair, Blank avatar, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pilgrammed%20Rainbow%20Hair%20Blank%20avatar%20ESP.lua")
