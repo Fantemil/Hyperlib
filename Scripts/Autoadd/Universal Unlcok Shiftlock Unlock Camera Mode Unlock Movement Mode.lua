30f940b972703279d65efb203d19588b43b419af
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:56:24 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Unlcok Shiftlock Unlock Camera Mode Unlock Movement Mode.lua
@@ -0,0 +1,8 @@
+local p = game:GetService('Players').LocalPlayer
+p.DevEnableMouseLock = true -- unlock shiftlock
+p.DevComputerCameraMode = 'UserChoice' -- unlock camera mode (pc)
+p.DevComputerMovementMode = 'UserChoice' -- unlock movement mode (pc)
+p.CameraMode = 'Classic' -- unlock third person
+p.DevCameraOcclusionMode = 'Zoom' -- enable camera zooming
+-- p.DevTouchMovementMode = 'UserChoice' unlock movement mode (mobile)
+-- p.DevTouchCameraMode = 'UserChoice' unlock camera mode (mobile)
