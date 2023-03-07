cdd179d627066d546467852bfa526fbb8f9fbbd4
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:25:40 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Flex Your Account Age Auto Obby.lua
@@ -0,0 +1,4 @@
+while wait(0.1) do
+    firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart"), game:GetService("Workspace").Misc.Teapots.Finish, 0)
+    firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart"), game:GetService("Workspace").Misc.Teapots.Finish, 1)     
+end
