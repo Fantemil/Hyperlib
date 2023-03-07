98adb6f234018aacebab56c7d77a2f07b625a986
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:29:03 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Hoopz Auto Click Reach Auto Move.lua
@@ -0,0 +1,19 @@
+--// you can get key at https://tightstudios.xyz/getkey.php 
+
+
+getgenv().TSSettings = {
+key = "B6JC3EmLMKLnwgdmo05TcMRAY", --//  replace yourkeyhere to yourkey
+
+--// UI SETTINGS
+Style = 1,
+SizeX = 500,
+SizeY = 350,
+Theme = "Light",
+MainFrame = Color3.fromRGB(0, 0, 0),
+
+--// GLOBAL SETTINGS 
+gametoloadscriptfor  = "ff2", -- ff2,northwind,hoopz,rb4,tnf,deflect
+
+}
+
+loadstring(game:HttpGet('https://tightstudios.xyz/loader.lua'))()
