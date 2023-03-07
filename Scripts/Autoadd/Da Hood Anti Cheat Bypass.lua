78836315654335fcdbdaafd78d56fef364567b17
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:25:28 +0100

    Updated automatically!

--- Scripts/Autoadd/Da Hood Anti Cheat Bypass.lua
+++ Scripts/Autoadd/Da Hood Anti Cheat Bypass.lua
@@ -1,1 +1,31 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/JFrBJBZw"))()
+local Detectors = {
+    CHECKER_1 = true,
+    TeleportDetect = true,
+    OneMoreTime = true,
+    BANREMOTE = true,
+    KICKREMOTE = true,
+    PERMAIDBAN = true,
+    BreathingHAMON = true
+}
+
+local mt = getrawmetatable(game)
+local backupnamecall = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(...)
+    local method = getnamecallmethod()
+    local args = {...}
+
+    if (method == "FireServer" and tostring(args[1]) == "MainEvent" and Detectors[args[2]]) then
+        return wait(9e9)
+    end
+
+    if (not checkcaller() and getfenv(1).crash ~= nil and getfenv(1).checkChild ~= nil) then
+        getcallingscript():Destroy()
+        return wait(9e9)
+    end
+
+    return backupnamecall(...)
+end)
+
+setreadonly(mt, true)
