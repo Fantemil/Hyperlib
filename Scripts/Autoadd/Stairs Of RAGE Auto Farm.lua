e1bc78376aa57bc2eac2fbe6465ef12f45e2ac03
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:46:15 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Stairs Of RAGE Auto Farm.lua
@@ -0,0 +1,12 @@
+wait(4) -- this is for low pc users remove it if you have a good pc
+
+if game.PlaceId == 11756661207 then
+
+while true do
+wait(2)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-34.2347603, 641, -5.54047918, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+wait(3) 
+game.TeleportService:Teleport(11756661207)
+
+end
+end
