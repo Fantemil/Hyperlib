0746f898d689703dc30577f834d366dcbf434355
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:37:33 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arcane Odyssey Infinite Statpoints.lua
@@ -0,0 +1,3 @@
+local Stat = "Strength" -- Any statpoint u want to go into negative (I'll try to find a better vers of this)
+
+game:GetService("ReplicatedStorage").RS.Remotes.UI.SpendSP:InvokeServer(Stat, -10000000)
