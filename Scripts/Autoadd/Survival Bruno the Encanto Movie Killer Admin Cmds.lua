636857cea2df0fda6e10a31d313132eccdb4a136
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 20:53:02 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Survival Bruno the Encanto Movie Killer Admin Cmds.lua
@@ -0,0 +1,86 @@
+--Made by Kosovo#3784
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/forumsLib/main/source.lua"))()
+local Forums = Library.new("Survival Bruno the Encanto Movie Killer! *Morphs Kick & jail all gui")
+
+local preview = Forums:NewSection("Jail all")
+
+preview:NewButton("Get tools press me a bunch idc", function()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(54.2136536, 440.710815, -23.077179, 0.932789981, 1.40455722e-06, 0.360420316, -1.3075919e-07, 1, -3.55858492e-06, -0.360420316, 3.27228418e-06, 0.932789981)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(62.2181244, 440.710815, -22.1105633, 0.546011627, -1.56272073e-09, -0.837777615, -4.76013824e-08, 1, -3.2888952e-08, 0.837777615, 5.78371235e-08, 0.546011627)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(69.5152283, 440.710815, -21.7035904, 0.366295695, 9.42433331e-08, -0.930498481, -7.57087122e-08, 1, 7.14794837e-08, 0.930498481, 4.42642119e-08, 0.366295695)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(79.2841034, 440.710815, -22.7795734, 0.110251069, 5.07696662e-10, -0.993903756, -1.91859167e-10, 1, 4.89528251e-10, 0.993903756, 1.36718539e-10, 0.110251069)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(84.6560287, 440.710815, -23.2765408, 0.092981793, -9.81377966e-08, -0.995667815, 3.40356898e-08, 1, -9.53863264e-08, 0.995667815, -2.50190482e-08, 0.092981793)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(92.1674652, 440.710815, -21.3521633, -0.914335132, 7.56010365e-08, 0.404958397, 8.03530966e-08, 1, -5.26320676e-09, -0.404958397, 2.77273244e-08, -0.914335132)
+end
+
+end)
+preview:NewButton("Bring all to cage", function()
+    local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(-2.38832974, 377.544373, -130.060608, 0.853740871, 9.61208571e-08, 0.520698071, -9.99424756e-08, 1, -2.07334718e-08, -0.520698071, -3.43388429e-08, 0.853740871)
+end
+wait(2)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DigitalityScripts/roblox-scripts/main/Bring%20All'))()
+end)
+local preview = Forums:NewSection("Kick all")
+preview:NewButton("Get tools press me a bunch idc", function()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(54.2136536, 440.710815, -23.077179, 0.932789981, 1.40455722e-06, 0.360420316, -1.3075919e-07, 1, -3.55858492e-06, -0.360420316, 3.27228418e-06, 0.932789981)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(62.2181244, 440.710815, -22.1105633, 0.546011627, -1.56272073e-09, -0.837777615, -4.76013824e-08, 1, -3.2888952e-08, 0.837777615, 5.78371235e-08, 0.546011627)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(69.5152283, 440.710815, -21.7035904, 0.366295695, 9.42433331e-08, -0.930498481, -7.57087122e-08, 1, 7.14794837e-08, 0.930498481, 4.42642119e-08, 0.366295695)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(79.2841034, 440.710815, -22.7795734, 0.110251069, 5.07696662e-10, -0.993903756, -1.91859167e-10, 1, 4.89528251e-10, 0.993903756, 1.36718539e-10, 0.110251069)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(84.6560287, 440.710815, -23.2765408, 0.092981793, -9.81377966e-08, -0.995667815, 3.40356898e-08, 1, -9.53863264e-08, 0.995667815, -2.50190482e-08, 0.092981793)
+end
+wait()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(92.1674652, 440.710815, -21.3521633, -0.914335132, 7.56010365e-08, 0.404958397, 8.03530966e-08, 1, -5.26320676e-09, -0.404958397, 2.77273244e-08, -0.914335132)
+end
+end)
+preview:NewButton("Kick all", function()
+local HRP = game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+if HRP then
+    HRP.CFrame = CFrame.new(-51.4233742, 438.802856, -4.61383104, -0.372454792, 4.61034979e-08, 0.928050339, -7.55198659e-09, 1, -5.27086392e-08, -0.928050339, -2.66402083e-08, -0.372454792)
+end
+wait(2)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DigitalityScripts/roblox-scripts/main/Bring%20All'))()
+end)
