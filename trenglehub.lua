625ce6aee6364f4da78b6f4cfb31a6f3b2ee9044
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:14:05 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 12:14:01"
+lastupdate = "GMT +1: 07.03.2023 12:14:05"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -1198,3 +1198,5 @@ addscriptexist(6872265039, "BEDWARS SERVER CRASHER!", "pulled from rscripts", "h
 
 
 addscript(8260276694, "Current Game","Ability Wars: Hitbox", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ability%20Wars%20Hitbox.lua")
+
+addscriptexist(3956818381, "Ninja Legends: Auto Swing, Auto Sell & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ninja%20Legends%20Auto%20Swing%20Auto%20Sell%20%20More.lua")
