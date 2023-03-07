87ca3c8fcbcd5cc57eb7062c7acf8c742e3def17
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:22:14 +0100

    Updated automatically!

--- Scripts/Autoadd/STEEP STEPS Anti Cheat Bypass.lua
+++ Scripts/Autoadd/STEEP STEPS Anti Cheat Bypass.lua
@@ -1,32 +1,10 @@
---[[ 
+local remote = game:GetService("ReplicatedStorage").Remotes.AC
+local old
 
-AC Bypass for Steep Steps
-
-Put this script in your autoexe/auto execution folder
-
-might break some stuff related to chat, but it should work normally if you just click the chatbox and write...?
-
-works for most detected things in the game, easy...
-
-]]--
-
-
-repeat wait() until game:IsLoaded()
-
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-
-local PlayerScripts = LocalPlayer:WaitForChild("PlayerScripts")
-
-local AntiCheatScript_1 = PlayerScripts:WaitForChild("ChatScript")
-local AntiCheatScript_2 = PlayerScripts:WaitForChild("BubbleChat")
-
-getsenv(AntiCheatScript_1).bd = "bypassed ez"
-getsenv(AntiCheatScript_2).bd = "bypassed ez"
-
-task.wait()
-
-AntiCheatScript_1:Destroy()
-AntiCheatScript_2:Destroy()
-
-warn("anti cheat successfully bypassed ez")
+old = hookmetamethod(game,"__namecall",function(self,...)
+local method = getnamecallmethod()
+    if self == remote and method == "FireServer" then
+        return nil
+    end
+    return old(self,...)
+end)
