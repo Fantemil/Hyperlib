d17366496eb0ee56d11abdbd7f2eed0feb56d7d1
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:18:48 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Icy Notifications for Developers.lua
@@ -0,0 +1,3 @@
+local Icy = loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/UI-s/main/Notifications/Icy.folder/Icy.lua'))()
+
+notify("Title", "Description", 5) -- Title, Description, Time
