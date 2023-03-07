828334b7cfeb000516a6a5389f38b212b44a0077
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:21:25 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Trident Survival YURI X V.lua
@@ -0,0 +1,4 @@
+--[[ Credits to AbstractPoo for making the fine notification module!
+@ AbstractPoo - https://v3rmillion.net/member.php?action=profile&uid=1055020 ]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bank6/yuri-X/main/123", true))()
