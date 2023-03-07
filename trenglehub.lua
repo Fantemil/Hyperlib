e16c3ec0d6a5ba9aebe4ec5ef04ae3c622c99065
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:13:52 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:13:47"
+lastupdate = "GMT +1: 07.03.2023 22:13:52"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4000,3 +4000,5 @@ addhub("Tuber's Hubb: 5 Games", "pulled from rbxscript.com", "https://raw.github
 addscriptexist(990364410, "Survive the Night: Fly, Noclip, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Survive%20the%20Night%20Fly%20Noclip%20ESP.lua")
 
 addscriptexist(11156779721, "The Survival Game [BETA]: Infinite Stamina & Free Customization Gmps", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Survival%20Game%20BETA%20Infinite%20Stamina%20%20Free%20Customization%20Gmps.lua")
+
+addscriptexist(2619187362, "Super Power Fighting Simulator: Auto Buy Chest, Auto Quest, Auto Train", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Super%20Power%20Fighting%20Simulator%20Auto%20Buy%20Chest%20Auto%20Quest%20Auto%20Train.lua")
