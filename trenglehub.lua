35809e7bbcfdbb236178abf16f35a996ddc76d2c
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:59:40 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:59:36"
+lastupdate = "GMT +1: 07.03.2023 21:59:40"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3592,3 +3592,5 @@ addscriptexist(11162791099, "Free Hatchers: Auto Buy Eggs, Teleports, Inf Money"
 addscriptexist(4508456371, "Shortest Answer Wins: Auto Type, Infinity Jumps, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shortest%20Answer%20Wins%20Auto%20Type%20Infinity%20Jumps%20ESP.lua")
 
 addscript(10347946161, "Current Game","Rat Washing Tycoon: Auto Sell, Auto Collect & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rat%20Washing%20Tycoon%20Auto%20Sell%20Auto%20Collect%20%20More.lua")
+
+addscriptexist(9872472334, "Evade: Fullbright, ESP, Godmode", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Evade%20Fullbright%20ESP%20Godmode.lua")
