fece15b3c6190dada765b82d4752a9bd18ca3a23
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:35:02 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arcane Odyssey Auto Farm Chests.lua
@@ -0,0 +1,6 @@
+--this game logs how long in between you open chest, it isn't hard to get around...
+getgenv().toggle=true
+getgenv().waitbetween=0.2-- the larger of a wait the safer this is
+getgenv().speed=100-- recommended around 100 any faster may pit you at risk of a ban
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Disttt/-Arcane-Odyssey/main/Autochest.lua"))()
