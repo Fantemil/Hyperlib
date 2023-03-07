bc451015defeff735b50eab98da8ff6ef2e2cd97
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:12:45 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Anime Idle Simulator Unlimited Dps.lua
@@ -0,0 +1,5 @@
+local ohString1 = "Upgrade"
+local ohNumber2 = 1
+local ohNumber3 = -10000000000
+
+game:GetService("ReplicatedStorage")._GAME._MODULES.Utilities.NetworkUtility.Events.Allies:FireServer(ohString1, ohNumber2, ohNumber3)
