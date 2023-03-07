b442047e621503d371178bd00b1be07b11da54e6
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 22:16:05 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 22:16:00"
+lastupdate = "GMT +1: 07.03.2023 22:16:05"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4056,3 +4056,5 @@ addscriptexist(155615604, "Prison Life: Admin Commands", "pulled from rbxscript.
 addscriptexist(9498006165, "Tapping Simulator: Auto Click, Auto Rebirth, Auto Hatch", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tapping%20Simulator%20Auto%20Click%20Auto%20Rebirth%20Auto%20Hatch.lua")
 
 addscript(70005410, "Current Game","Blox Hunt: AutoFarm Tokens", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Hunt%20AutoFarm%20Tokens.lua")
+
+addscriptexist(286090429, "Arsenal: Aimbot", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20Aimbot.lua")
