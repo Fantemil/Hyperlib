e4307334b99f50f5898c6c11610f9bbe1264934b
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:53:25 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 07.03.2023 21:53:21"
+lastupdate = "GMT +1: 07.03.2023 21:53:25"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -3440,3 +3440,5 @@ addscriptexist(2753915549, "Blox Fruits: Auto Farm Bone, Auto Farm Level, Auto S
 addscriptexist(6229116934, "Hoopz: Auto Shoot, Auto Jump, Silent Aim", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hoopz%20Auto%20Shoot%20Auto%20Jump%20Silent%20Aim.lua")
 
 addscript(4572253581, "Current Game","MURDER: ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MURDER%20ESP.lua")
+
+addscript(5872075530, "Current Game","Anarchy: Silent Aim", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anarchy%20Silent%20Aim.lua")
