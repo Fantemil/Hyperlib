60b985e3ce47bb74e5c69b8650de1b0e88084db5
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 12:08:12 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/DragonBall Ultimate Clash  Inf Money Admin Char.lua
@@ -0,0 +1,4 @@
+game:GetService("ReplicatedStorage").PropEvent:FireServer(game:GetService("Players").LocalPlayer.StatValues.Zeni,"Value",9e9)-- inf zeni
+
+game:GetService("ReplicatedStorage").CharacterChangerEvent:FireServer(game:GetService("StarterPlayer").Characters.DLC.JJ.JJBeast) -- Admin char
+game:GetService("ReplicatedStorage").CharacterChangerEvent:FireServer(game:GetService("StarterPlayer").Characters.DLC.JJ.JJBeast2) -- Admin char
