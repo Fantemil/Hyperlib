2e541b59ab5799bcdaac851add3884ee116a4181
Emil Toth <emiltoth1870@gmail.com>
2023-03-07 21:26:16 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Notifications.lua
@@ -0,0 +1,3 @@
+local Gui = loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/UI-s/main/Notifications/Unknown/first.lua'))()
+
+notify("title", "description", 1, 1) -- 1, 1 = time, time_before_destroy
