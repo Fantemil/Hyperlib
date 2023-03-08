0d80f7122af031a68f11934da964e09c0d2f460b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:33:56 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Anti Kick not server sided.lua
@@ -0,0 +1,2 @@
+-- dont skid my script pls
+xpcall(function() OldNameCall = hookmetamethod(game.Players.LocalPlayer, "__namecall", function(Self, ...) local Args = {...} local NamecallMethod = getnamecallmethod() if Self == game.Players.LocalPlayer and NamecallMethod == "Kick" then return nil end return OldNameCall(Self, ...) end) print("Anti Kick Enabled.") end, function(e) print(e) end)
