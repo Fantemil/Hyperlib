a13c74649f8b12a22478a2ffe7c4c2e798d8b0da
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:51:52 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 16:51:44"
+lastupdate = "GMT +1: 08.03.2023 16:51:52"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9264,3 +9264,5 @@ addscript(328028363, "Current Game","Typical Colors 2: Godmode, Auto Farm, Silen
 addscriptexist(10861112990, "Anime Artifacts Simulator: Auto Farm, Auto Kill Boss, Auto Upgrade", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Artifacts%20Simulator%20Auto%20Farm%20Auto%20Kill%20Boss%20Auto%20Upgrade.lua")
 
 addscriptuniversal("Universal Forcefield", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Forcefield.lua")
+
+addscriptuniversal("Statue Extras: Anime Games", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Statue%20Extras%20Anime%20Games.lua")
