f012f63b99e8b740d256f44c15836d6184e7ff3c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:04:27 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/DOORS Screech Spammer.lua
@@ -0,0 +1,4 @@
+while true do wait(0.1)
+    require(game.StarterGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.Screech)(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
+workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
+end
