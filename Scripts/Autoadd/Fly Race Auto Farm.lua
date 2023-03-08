bc2d54fd7e401dd58aa7071c7b4f1f83a9c39394
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:52:05 +0100

    Updated automatically!

--- Scripts/Autoadd/Fly Race Auto Farm.lua
+++ Scripts/Autoadd/Fly Race Auto Farm.lua
@@ -1,9 +1,15 @@
+-- made by Lucifer
+
 local plr = game.Players.LocalPlayer
 local char = plr.Character
+
 local hrp = char:WaitForChild("HumanoidRootPart")
+
 local landing = CFrame.new(150232, -103.86, 104719)
+
 local isflying = plr:WaitForChild("IsFlying")
 local autoRun = plr.PlayerGui.Main.SideTabs.AutoFly.Main.Button.Main.Switch
+
 isflying:GetPropertyChangedSignal("Value"):Connect(function()
    if isflying and autoRun.BackgroundColor3 == Color3.fromRGB(127, 255, 189) then
        task.wait(.1)
