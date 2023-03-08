28c10adc8c539bbc6d98eeb582f4d9b65eb87992
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:06:24 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Anti Ban.lua
@@ -0,0 +1,8 @@
+getgenv().credit = "HERE_CREDIT_NAME"
+
+
+if getgenv().credit == "HERE_CREDIT_NAME" then
+ print"script loaded"
+else
+ game.Players.LocalPlayer:Kick("Credit Is LOI!, Dont Touch IT")
+end
