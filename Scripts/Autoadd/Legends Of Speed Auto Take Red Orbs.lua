ad6333091d0851db67d1972b496f8e71686a7c17
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:34:13 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Legends Of Speed Auto Take Red Orbs.lua
@@ -0,0 +1,8 @@
+while true do
+ wait (0.1)
+local A_1 = "collectOrb"
+local A_2 = "Red Orb"
+local A_3 = "City"
+local Event = game:GetService("ReplicatedStorage").rEvents.orbEvent
+Event:FireServer(A_1, A_2, A_3)
+end
