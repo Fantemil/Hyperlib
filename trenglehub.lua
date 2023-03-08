3388a9380fe09084a3cb7163a42c67c97d021d74
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:48:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:48:41"
+lastupdate = "GMT +1: 08.03.2023 18:48:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12088,3 +12088,5 @@ addscriptexist(9912491609, "How Far Can You Fly: Infinite Money", "pulled from r
 addscriptexist(9912491609, "How Far Can You Fly: Auto Buy Walking Speed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/How%20Far%20Can%20You%20Fly%20Auto%20Buy%20Walking%20Speed.lua")
 
 addscriptexist(142823291, "Murder Mystery 2: Highlight Esp", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Murder%20Mystery%20%20Highlight%20Esp.lua")
+
+addscriptexist(6284583030, "Pet Simulator X: Auto Farm, Auto Eggs, Fps Booster", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Auto%20Farm%20Auto%20Eggs%20Fps%20Booster.lua")
