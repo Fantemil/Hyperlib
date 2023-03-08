9cb7bdc58194c259084ec22ddbfa7d3114b40613
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:09:24 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Refinery Caves ClientSide Item Spawner.lua
@@ -0,0 +1,2 @@
+local Item = "Forbidden"
+require(game:GetService("ReplicatedStorage").Modules.Objs).SpawnItem(Item,game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame,v,game.Players.LocalPlayer)
