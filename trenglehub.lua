f651e859aed03b39ce757b8fdf1172634e467d09
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:10:25 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:10:19"
+lastupdate = "GMT +1: 08.03.2023 17:10:25"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9736,3 +9736,5 @@ addscriptexist(11187148979, "Ultra unFair: Auto Bossing, Auto Farm, Kill Aura", 
 addscriptexist(2653064683, "Word Bomb: Aimbot, ESP, Keybinds", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Word%20Bomb%20Aimbot%20ESP%20Keybinds.lua")
 
 addscriptexist(6516141723, "DOORS: No Screech, Walkspeed & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20No%20Screech%20Walkspeed%20%20More.lua")
+
+addscriptexist(8750997647, "Tapping Legends X: Auto Equip Best Pets, Auto Farm & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tapping%20Legends%20X%20Auto%20Equip%20Best%20Pets%20Auto%20Farm%20%20More.lua")
