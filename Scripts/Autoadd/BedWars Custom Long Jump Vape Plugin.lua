14bf407f29356a8d7cc606992456632f0694dca5
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:02:56 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/BedWars Custom Long Jump Vape Plugin.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))() loadstring(game:HttpGet("https://raw.githubusercontent.com/ItsNotABedwarsHacker/vape_plugin/main/main", true))()
