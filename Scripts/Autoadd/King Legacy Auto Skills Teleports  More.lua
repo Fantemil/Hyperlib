ace07445f4ce26f6b78e6f4b2bbd059f7ebb5a41
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:47:24 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/King Legacy Auto Skills Teleports  More.lua
@@ -0,0 +1,6 @@
+-- Made by sHi.xyz#4171
+-- Join his discord server: https://discord.gg/bZPzPpN9
+pcall(function()
+    repeat wait() until game:IsLoaded()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/Hyper/main/script.lua"))()
+end)
