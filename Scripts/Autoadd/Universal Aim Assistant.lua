e6334ae229eb1b174a30b3c7cccc7e9df0e69105
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:54:20 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Aim Assistant.lua
@@ -0,0 +1,6 @@
+--[=[
+   Script created by Paygammy
+   Liscensed under GNU General Public License 3.0
+]=]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/paygammy/aim-assistant/release/scripts/main.lua"))()
