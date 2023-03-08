d0c60a4b1b1f59d4e3ccd2d910db0892220352c9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:59:24 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Tower of Hell God Mode Walkspeed Go To End.lua
@@ -0,0 +1,4 @@
+-- GET KEY HERE : http://key.jjt-x-hub.rf.gd/
+-- key is free RightNOW "FREE_HALOWEEN"
+_G.key = "FREE_HALOWEEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JJTChannel/JJTxHUB/main/main.lua", true))()
