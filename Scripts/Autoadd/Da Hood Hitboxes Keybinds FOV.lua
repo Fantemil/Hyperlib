a72fbd1b6451e67fff6b5c786d6c916682be3013
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:09:02 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Hitboxes Keybinds FOV.lua
@@ -0,0 +1,9 @@
+getgenv().Prediction = 0.12167
+getgenv().Hitbox = "HumanoidRootPart" 
+getgenv().Key = "Q" 
+getgenv().DisableKey = "P" 
+getgenv().FOV = true 
+getgenv().ShowFOV = true 
+getgenv().FOVSize = 55 
+
+loadstring(game:HttpGet'https://yift.gq/r/icy')()
