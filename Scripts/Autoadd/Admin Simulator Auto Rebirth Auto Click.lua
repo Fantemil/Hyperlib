cb566b824cd5a5f8bf8285f82b141269220cc4c9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:56:48 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Admin Simulator Auto Rebirth Auto Click.lua
@@ -0,0 +1,9 @@
+while true do
+    game:GetService("ReplicatedStorage").Remotes.Lift:FireServer()
+    wait()
+end
+
+while true do
+    game:GetService("ReplicatedStorage").Remotes.UltraLift:FireServer()
+    wait()
+end
