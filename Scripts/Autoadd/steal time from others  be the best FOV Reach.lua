4faf2ed511580888b889fb3821168de1abcc9824
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:25:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/steal time from others  be the best FOV Reach.lua
@@ -0,0 +1,4 @@
+shared.fov = 175 -- mouse fov
+shared.distance = 20 -- max distance for targerplr
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jLn0n/scripts/main/game-scripts/stfoabtb-semi-reach.lua"))()
