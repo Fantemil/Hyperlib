63c1d3e2583e94374f78d81658c3c2cd587f0989
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:28:59 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:28:55"
+lastupdate = "GMT +1: 08.03.2023 12:28:59"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6098,3 +6098,5 @@ addscriptuniversal("Universal Kill-aura, Godmode, Kill all", "pulled from rbxscr
 addscriptexist(1962086868, "Tower of Hell: Infinity Jumps, Finish, Get All Items", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20of%20Hell%20Infinity%20Jumps%20Finish%20Get%20All%20Items.lua")
 
 addscriptexist(9824221333, "Da Hood Aim Trainer: Aim Lock", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Aim%20Trainer%20Aim%20Lock.lua")
+
+addscriptexist(1537690962, "Bee Swarm Simulator: Smart Autofarm, Farm in boosted fields/Pushrooms, Autokill Stickbug", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bee%20Swarm%20Simulator%20Smart%20Autofarm%20Farm%20in%20boosted%20fieldsPushrooms%20Autokill%20Stickbug.lua")
