2ddeb51da2b4a1de8e4bd60c8447f0f3ca3c3c5c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:05:17 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 13:05:12"
+lastupdate = "GMT +1: 08.03.2023 13:05:17"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7058,3 +7058,5 @@ addscript(9911362945, "Current Game","Project: Bronze Forever: Aim Bot, Chect te
 addscriptexist(7056922815, "Reaper 2: AutoFarm, AutoQuest, Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Reaper%20%20AutoFarm%20AutoQuest%20Teleports.lua")
 
 addscriptexist(6808416928, "Aimblox BETA: Illusion Hub Free Gui", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Aimblox%20BETA%20Illusion%20Hub%20Free%20Gui.lua")
+
+addscriptexist(10404327868, "Timber Champions: Auto Farm, Craft All Pets, Collect Orbs & More!", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Timber%20Champions%20Auto%20Farm%20Craft%20All%20Pets%20Collect%20Orbs%20%20More.lua")
