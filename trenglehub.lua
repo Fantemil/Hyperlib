c3da1acbe38b33a5c0c53fc43d1c3ee35743a9c4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:04:13 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:04:08"
+lastupdate = "GMT +1: 08.03.2023 07:04:13"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5430,3 +5430,5 @@ addscriptexist(621129760, "Knife Ability Test: Auto Farm, Silent Aim, Player ESP
 addscriptexist(8328351891, "Mega Mansion Tycoon: Auto Get Presents", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mega%20Mansion%20Tycoon%20Auto%20Get%20Presents.lua")
 
 addscriptexist(3101667897, "Legends Of Speed: Auto Steps, Auto Win Race, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legends%20Of%20Speed%20Auto%20Steps%20Auto%20Win%20Race%20Auto%20Rebirth.lua")
+
+addscriptexist(10898965735, "Mine Racer: Auto Join, Anti Afk, Auto Open Eggs", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mine%20Racer%20Auto%20Join%20Anti%20Afk%20Auto%20Open%20Eggs.lua")
