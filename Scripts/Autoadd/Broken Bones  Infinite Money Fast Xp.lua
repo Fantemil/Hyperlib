ca0973fd036b3e02b5ad07b748b11c5e5b279065
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:58:55 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Broken Bones  Infinite Money Fast Xp.lua
@@ -0,0 +1,15 @@
+local FinishRun = game:GetService('ReplicatedStorage').Functions.FinishRun
+
+local namecall; namecall = hookmetamethod(game, '__namecall', function(caller, ...)
+   local args = {...}
+   local method = getnamecallmethod()
+
+   if not checkcaller() and caller == FinishRun and method == 'InvokeServer' then
+args[2][1] = 1000 -- breaks
+args[2][2] = 1000 -- sprains
+args[2][3] = 100 -- dislocations
+args[2][8]['Breaks'] = 0xFFFFFFFFFFFFFFFF -- math.huge breaks this script
+   end
+
+   return namecall(caller, unpack(args))
+end)
