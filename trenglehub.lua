b66c1bbffbf34e3e2756c843099cdacacd7a22c4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:06:52 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 13:06:44"
+lastupdate = "GMT +1: 08.03.2023 13:06:52"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7100,3 +7100,5 @@ addscriptexist(6447798030, "Funky Friday: Auto Collect Rings", "pulled from rbxs
 addscriptexist(5712833750, "Animal Simulator: Auto Collect Coins", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Animal%20Simulator%20Auto%20Collect%20Coins.lua")
 
 addscriptexist(3177438863, "Dragon Blox: Auto Punch, Auto Kills Mobs, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dragon%20Blox%20Auto%20Punch%20Auto%20Kills%20Mobs%20Auto%20Rebirth.lua")
+
+addscriptexist(920587237, "Adopt Me: Pet Manager, Cash Manager, Event Manager", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Adopt%20Me%20Pet%20Manager%20Cash%20Manager%20Event%20Manager.lua")
