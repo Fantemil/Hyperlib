271ac68801f8543bf598862e5c98ad4dff8af754
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:41:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Roblox Talent Show Rep Farm.lua
@@ -0,0 +1,11 @@
+while task.wait(120) do
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(373, 152, -725)
+task.wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(461, 162, -851)
+task.wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(79, 136, -553)
+task.wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-24, 138, -564)
+task.wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(199, 118, -201)
+end
