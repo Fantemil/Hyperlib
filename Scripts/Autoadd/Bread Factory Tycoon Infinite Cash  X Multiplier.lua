84e928327c4f0d39e6f5e3a438e7c8f8570cb2d0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:41:23 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Bread Factory Tycoon Infinite Cash  X Multiplier.lua
@@ -0,0 +1,3 @@
+local x2multiplier = 9e9 -- amount of x2multiplier time, change this if u want more or less, dont change it into math.huge
+local money = 9e9 -- amount of cash u get, change this if u want more or less
+game:GetService("ReplicatedStorage").RemoteEvents.ClaimReward:FireServer(x2multiplier,money,"fbjXcLN8NQk9ExFJH?x&&TkE3!xjxM")
