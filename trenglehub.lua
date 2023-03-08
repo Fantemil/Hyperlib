d42c92c669255a2db008ceb06c986fedc70253b9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:56:20 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:56:16"
+lastupdate = "GMT +1: 08.03.2023 14:56:20"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8286,3 +8286,5 @@ addscriptexist(9744954014, "Tha Hood: Auto Farm ATM, Buy Armor, Buy Silencer", "
 addscriptexist(2753915549, "Blox Fruits: Auto Farm Confetti", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Auto%20Farm%20Confetti.lua")
 
 addscriptuniversal("Universal RTX version of Roblox", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20RTX%20version%20of%20Roblox.lua")
+
+addscriptexist(2753915549, "Blox Fruits: Auto Farm Bosses, Auto Stats, Auto Farm Bosses", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Auto%20Farm%20Bosses%20Auto%20Stats%20Auto%20Farm%20Bosses.lua")
