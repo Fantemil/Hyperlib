1b37c70f06aee953bda65a985a5e76e37b8ee4e3
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:36:06 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Rate My Avatar Self Bot.lua
@@ -0,0 +1,9 @@
+local AfkMode = "Accurate"
+--[[
+ Options:
+   Accurate | A more accurate version of the game's current AFK detection.
+   Spoof true | Will always show you as AFK, even when you're not.
+   Spoof false | Will always show you as not AFK, even when you are.
+]]
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Extorius/Extorius-Self-Bot/resources/script.lua'))()
