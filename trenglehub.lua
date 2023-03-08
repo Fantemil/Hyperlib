a47e9fcb0421d5d854aba56ef888a55d8a05899d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:10:40 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:10:37"
+lastupdate = "GMT +1: 08.03.2023 07:10:40"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5630,3 +5630,5 @@ addscriptexist(5956785391, "Project Slayers: Infinity Breathing, Auto Farm Bosse
 addscript(9196514723, "Current Game","ChicBlocko: Teleports, Auto Farm, Shop GUI Activators", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ChicBlocko%20Teleports%20Auto%20Farm%20Shop%20GUI%20Activators.lua")
 
 addscriptexist(7952502098, "Impossible Glass Bridge Obby: Teleport to the End", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Impossible%20Glass%20Bridge%20Obby%20Teleport%20to%20the%20End.lua")
+
+addscriptexist(2788229376, "Da Hood: Press X to Anti Lock", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Press%20X%20to%20Anti%20Lock.lua")
