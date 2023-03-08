808079dfe9dfe470354a050a7dc8b1461286439f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:06:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:06:04"
+lastupdate = "GMT +1: 08.03.2023 18:06:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11152,3 +11152,5 @@ addscriptexist(8482713490, "Forgotten Memories: Marionette Block", "pulled from 
 addscriptexist(292439477, "Phantom Forces: Unlock All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Unlock%20All.lua")
 
 addscript(11312500614, "Current Game","SPTS:Custom II: ESP, Walkspeed, Auto Farm All Stats", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SPTSCustom%20II%20ESP%20Walkspeed%20Auto%20Farm%20All%20Stats.lua")
+
+addscriptexist(11187148979, "Ultra unFair: Kill Aura, Relic Spin, No Cooldown", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultra%20unFair%20Kill%20Aura%20Relic%20Spin%20No%20Cooldown.lua")
