78fbcd718364274c4e30e0d327581065dcd90b72
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:10:25 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 15:10:21"
+lastupdate = "GMT +1: 08.03.2023 15:10:25"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8662,3 +8662,5 @@ addscriptuniversal("Universal ESP, Fullbright, Aimlock", "pulled from rbxscript.
 addscriptexist(7714627632, "Winds of Fortune: Teleport Menu, Sell", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Winds%20of%20Fortune%20Teleport%20Menu%20Sell.lua")
 
 addscriptexist(11346342371, "making memes in your basement at 3 AM tycoon: Instantly Upload Memes, Auto Upload, Auto Pickup", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/making%20memes%20in%20your%20basement%20at%20%20AM%20tycoon%20Instantly%20Upload%20Memes%20Auto%20Upload%20Auto%20Pickup.lua")
+
+addscriptexist(292439477, "Phantom Forces: Hitbox Extender, Silent Aim, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Hitbox%20Extender%20Silent%20Aim%20ESP.lua")
