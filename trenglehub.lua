089719ae2dc76b4d457c223e08ef5d98964bf218
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:50:46 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:50:42"
+lastupdate = "GMT +1: 08.03.2023 14:50:46"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8140,3 +8140,5 @@ addscriptexist(292439477, "Phantom Forces: ESP Boxes Players", "pulled from rbxs
 addscriptexist(292439477, "Phantom Forces: Silent Aim and ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Silent%20Aim%20and%20ESP.lua")
 
 addscript(11298615665, "Current Game","+1 Speed Every Second: Auto Farm, Anti Afk", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20Speed%20Every%20Second%20Auto%20Farm%20Anti%20Afk.lua")
+
+addscriptexist(5036207802, "RB Battles! Unlimited Coins", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RB%20Battles%20Unlimited%20Coins.lua")
