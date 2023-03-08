7d155126bc09051bf95ba5c8e0593ce2682ab127
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:03:56 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Rebirth Champions X Auto Tap Auto Rebirth Auto Buy.lua
@@ -0,0 +1,5 @@
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/Sync1350/ViperHub/main/ViperLoader.lua",true))()
+
+-- Key is on https://discord.gg/VpjWQED9Gu
+-- Updates Soon
+-- https://discord.gg/VpjWQED9Gu
