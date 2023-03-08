307ede3e03e4d56b0860ccebbb3ce2118cb03e0a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:38:04 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/ExoHub  Games.lua
@@ -0,0 +1,6 @@
+--Impossible obby
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SchoolTrash/ExoHub/main/Impossible%20Glass%20Bridge%20Obby"))()
+--Prison Life
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SchoolTrash/ExoHub/main/Prison%20Life"))()
+--home run sim
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SchoolTrash/ExoHub/main/Home%20Run%20Simulator"))()
