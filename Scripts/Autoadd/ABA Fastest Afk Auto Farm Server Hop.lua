4f35498ab03adb8cb4f72bcfa9586ed05f928722
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:28:28 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/ABA Fastest Afk Auto Farm Server Hop.lua
@@ -0,0 +1,4 @@
+getgenv().entertimer = 60
+getgenv().rendering = true -- make it true for more fps
+_G.delay = 0
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Skrapisismyscript/Solo_Hub/main/justaba"))()
