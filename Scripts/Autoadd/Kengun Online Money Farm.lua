07d18407a197ed695a71c73b3e11900cc4fb0f80
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:48:19 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Kengun Online Money Farm.lua
@@ -0,0 +1,4 @@
+-- Key link: https://kylndantas-key-system.kylndantas.repl.co/key
+getgenv().key = 'YOUR KEY HERE'
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KylnDantas/kengun-online-gui/main/mainFile.lua", true))()
