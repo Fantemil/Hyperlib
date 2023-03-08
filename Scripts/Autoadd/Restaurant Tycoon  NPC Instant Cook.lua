c36e491403485af710ba6c3040d86ca5c482fa32
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:01:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Restaurant Tycoon  NPC Instant Cook.lua
@@ -0,0 +1,8 @@
+local chiefInstantCook = true
+local npcReach = true
+
+local Client = game.Players.LocalPlayer
+local PS = Client.PlayerScripts
+local Module = require(PS.ClientMain.Replications.Workers.WalkDummy)
+local Worker = require(PS.CookingNew.WorkerComponents.WorkerDefault)
+local M1 = require(PS.ClientMain.Replications.Customers.GetNPCFolder)
