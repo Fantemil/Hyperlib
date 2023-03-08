e1f6745358ed357c7814bfff7ac4dbbc3a48eba7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:26:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:26:09"
+lastupdate = "GMT +1: 08.03.2023 14:26:13"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7488,3 +7488,5 @@ addscriptexist(1730877806, "Grand Piece Online: Teleport to Fishman", "pulled fr
 addscriptexist(5712833750, "Animal Simulator: Auto Farm Coins", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Animal%20Simulator%20Auto%20Farm%20Coins.lua")
 
 addscriptexist(6407649031, "NO-SCOPE ARCADE: No Fire Rate, FOV & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NOSCOPE%20ARCADE%20No%20Fire%20Rate%20FOV%20%20More.lua")
+
+addscriptexist(6447798030, "Funky Friday: Auto Player, Auto Collect Rings, Options", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Funky%20Friday%20Auto%20Player%20Auto%20Collect%20Rings%20Options.lua")
