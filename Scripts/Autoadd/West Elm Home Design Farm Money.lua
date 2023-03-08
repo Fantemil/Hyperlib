aec095545b5eb432073dbda05aafcf19de92ba3c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:36:57 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/West Elm Home Design Farm Money.lua
@@ -0,0 +1,5 @@
+-- // FeIix was here <3
+while task.wait() do
+Game:GetService("ReplicatedStorage").minigameComm["__comm__"].RE['end']:FireServer()
+Game:GetService("ReplicatedStorage").Packages["_Index"]["sleitnick_knit@1.4.7"].knit.Services.TutorialServer.RF.ObbyDone:InvokeServer()
+end
