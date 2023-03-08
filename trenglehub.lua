b0e4e0fb5b873a4035746e0b66ddfadf69f4454f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:02:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:02:28"
+lastupdate = "GMT +1: 08.03.2023 07:02:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5380,3 +5380,5 @@ addscriptuniversal("Universal Fly Gui", "pulled from rbxscript.com", "https://ra
 addscriptexist(2753915549, "Blox Fruits: Mob Aura, No stun, Auto buso", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Mob%20Aura%20No%20stun%20Auto%20buso.lua")
 
 addscriptexist(662417684, "LUCKY BLOCKS Battlegrounds: Fly, TP Tool, Lucky Blocks Giver", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LUCKY%20BLOCKS%20Battlegrounds%20Fly%20TP%20Tool%20Lucky%20Blocks%20Giver.lua")
+
+addscriptexist(7655745946, "Dunking Simulator: Infinite money, Auto Rebirth, Auto Buy Ball & Jersey", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dunking%20Simulator%20Infinite%20money%20Auto%20Rebirth%20Auto%20Buy%20Ball%20%20Jersey.lua")
