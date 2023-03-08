6d83ca624ec154d86903628914575fc57e60327a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:53:24 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Update Checker.lua
@@ -0,0 +1,8 @@
+local script = -- put whatever version here, use setclipboard(tostring(game.PlaceVersion))
+local gamee = game.PlaceVersion
+
+if script == gamee then
+     print("up to date, loading")
+else
+     print("Game has updated")
+end
