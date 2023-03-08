a272d2d214ad47d1c9ce730b5c3ca5daa07b5c5a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:50:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:50:10"
+lastupdate = "GMT +1: 08.03.2023 14:50:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8128,3 +8128,5 @@ addscriptexist(2753915549, "Blox Fruits: Auto Farm, Fast Attack, Auto Select Wea
 addscriptexist(7525610732, "Kaizen: Auto Farm, Stats, Options", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Kaizen%20Auto%20Farm%20Stats%20Options.lua")
 
 addscriptexist(11445923563, "One Fruit Simulator: Kill Aura, Auto Quest, Auto Collect Fruits", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/One%20Fruit%20Simulator%20Kill%20Aura%20Auto%20Quest%20Auto%20Collect%20Fruits.lua")
+
+addscriptexist(286090429, "Arsenal: Smooth Lock, Silent Aim, Hitboxes", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20Smooth%20Lock%20Silent%20Aim%20Hitboxes.lua")
