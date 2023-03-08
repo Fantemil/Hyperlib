d333e323ebe574a46cd8e0b8c86747352f16d0b3
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:06:20 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 13:06:16"
+lastupdate = "GMT +1: 08.03.2023 13:06:20"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7086,3 +7086,5 @@ addscriptexist(10404327868, "Timber Champions: Auto Collect Orbs, Auto Attack Bo
 addscriptexist(11200197064, "Rich Clicker Simulator: Auto Click, Anti Afk, FPS Boost", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rich%20Clicker%20Simulator%20Auto%20Click%20Anti%20Afk%20FPS%20Boost.lua")
 
 addscriptexist(9984770240, "Obby but you're a ball: Auto Complete Obby Instantly", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Obby%20but%20youre%20a%20ball%20Auto%20Complete%20Obby%20Instantly.lua")
+
+addscript(5026999321, "Current Game","A Dumb Day: Auto Use Banknotes, Kill Aura, Grab Items", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/A%20Dumb%20Day%20Auto%20Use%20Banknotes%20Kill%20Aura%20Grab%20Items.lua")
