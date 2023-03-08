cd223308728225b0a6af6c523519352195182c5a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:03:48 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Your Bizarre Adventure Event Present Autofarm ServerHop.lua
@@ -0,0 +1,16 @@
+--put in autoexec before complaining
+
+getgenv().ItemToFarm = "Christmas Present"
+
+
+getgenv().NormalTeleportTime = 100
+
+
+getgenv().TimeForSpawningAllItems = 7 --Time until script actually starts ( THIS USED TO WAIT FOR ALL ITEMS TO APPEAR IN WORKSPACE )
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NukeVsCity/hackbloxfruits/main/presenthop"))()
+
+--SCRIPT ONLY SEARCHES FOR A SINGLE ITEM
+--put in autoexec folder https://streamable.com/yx474s
