717d088392bc5e509d415f58366e9805a9e76c05
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:02:46 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:02:41"
+lastupdate = "GMT +1: 08.03.2023 19:02:46"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12390,3 +12390,5 @@ addscriptexist(2653064683, "Word Bomb: Auto Join, Auto Type, Anti Kick", "pulled
 addscriptexist(4282985734, "Combat Warriors: Walkspeed, KillAura, AutoParry", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Combat%20Warriors%20Walkspeed%20KillAura%20AutoParry.lua")
 
 addscriptexist(10903978962, "Sheep Tycoon: Infinity Wool", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sheep%20Tycoon%20Infinity%20Wool.lua")
+
+addscriptexist(10903978962, "Sheep Tycoon: Auto Sell", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sheep%20Tycoon%20Auto%20Sell.lua")
