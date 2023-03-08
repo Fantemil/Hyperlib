6af0292cc392a29d3d927f3cc48d09c82c9c19c1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:25:16 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:25:13"
+lastupdate = "GMT +1: 08.03.2023 06:25:16"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4572,3 +4572,5 @@ addscriptexist(189707, "Natural Disaster Survival: Headless, Auto Jump & More", 
 addscriptexist(9498006165, "Tapping Simulator: Auto Click, Auto Rebirth & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tapping%20Simulator%20Auto%20Click%20Auto%20Rebirth%20%20More.lua")
 
 addscript(10760024537, "Current Game","Ultimate Easy Obby: Auto Skip Levels", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultimate%20Easy%20Obby%20Auto%20Skip%20Levels.lua")
+
+addscriptexist(11746859781, "Bubble Gum Clicker: Auto-Farm, Sell Bubble, Auto Collect", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bubble%20Gum%20Clicker%20AutoFarm%20Sell%20Bubble%20Auto%20Collect.lua")
