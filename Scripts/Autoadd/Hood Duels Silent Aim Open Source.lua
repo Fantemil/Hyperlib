f6d9c1d065f08b649f6ba5d9f5361a997fce56cf
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:45:39 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Hood Duels Silent Aim Open Source.lua
@@ -0,0 +1,8 @@
+getgenv().settings = {
+   prediction = .13,
+   fov_circle = true,
+   fov_radius = 120,
+   color = Color3.fromRGB(3, 252, 78)
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/LeadMarker/opensrc/main/hood_duels.lua'))()
