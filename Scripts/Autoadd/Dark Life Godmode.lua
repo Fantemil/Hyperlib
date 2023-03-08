bc6aa396170198ecae3196154875a4a48fc052be
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:03:21 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Dark Life Godmode.lua
@@ -0,0 +1,31 @@
+rconsoleclear()
+rconsoleprint("@@GREEN@@")
+rconsolename("Dark Life - God Mode Log - Made By Exxen#0001")
+
+local function char()
+    return game.Players.LocalPlayer.Character
+end
+
+local function init()
+    local hrp = char().HumanoidRootPart
+    local weld = hrp:FindFirstChildOfClass("Weld")
+    local weld2 = char().Head:FindFirstChild("Weld")
+    if weld then
+        weld:Destroy()
+    end
+    if weld2 then
+        weld2:Destroy()
+    end
+    rconsoleprint("@@GREEN@@")
+    rconsoleprint("\nToggled God")
+end
+
+init()
+
+rconsoleprint("@@RED@@")
+rconsoleprint("\n")
+
+game.Players.LocalPlayer.CharacterAdded:connect(function()
+    wait(.5)
+    init()
+end)
