38ae5dd89b61eb60fdb81de12f8004f1c7a28b42
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:39:40 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:39:37"
+lastupdate = "GMT +1: 08.03.2023 06:39:40"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4680,3 +4680,5 @@ addscriptexist(286090429, "Arsenal: Aimbot, Silent Aim, Godmode", "pulled from r
 addscriptexist(4913581664, "Cart Ride Into Rtide: Fling, Invisible, Rejoin", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cart%20Ride%20Into%20Rtide%20Fling%20Invisible%20Rejoin.lua")
 
 addscriptexist(738339342, "Flood Escape 2: Server Crasher", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Flood%20Escape%20%20Server%20Crasher.lua")
+
+addscript(281489669, "Current Game","Wizard Tycoon - 2 Player: Kill All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Wizard%20Tycoon%20%20%20Player%20Kill%20All.lua")
