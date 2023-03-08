cdb263939d94232e145289acdb8cee4216a4568a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:49:10 +0100

    Updated automatically!

--- Scripts/Autoadd/Build A Boat For Treasure Auto Farm Teleports No Water Damage.lua
+++ Scripts/Autoadd/Build A Boat For Treasure Auto Farm Teleports No Water Damage.lua
@@ -1,1 +1,5 @@
+--the script is still being created and modified at all times, for new games and updates enter the discord server: https://discord.gg/uCzBFE6
+
+SCRIPT:
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/yLegendzz/Scripts/main/LoaderAll'))()
