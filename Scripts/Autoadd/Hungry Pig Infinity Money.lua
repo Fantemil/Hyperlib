b21611662a34d910138bedb7383d63701b4f6fed
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:01:17 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Hungry Pig Infinity Money.lua
@@ -0,0 +1,6 @@
+game:GetService("ReplicatedStorage").PurchasePet:FireServer{
+ ["Description"] = "This will light up a path infront of you.",
+ ["ImageID"] = "rbxassetid://11864692764",
+ ["Name"] = "FlashLight",
+ ["Cost"] = -9999999999e9999999999
+}
