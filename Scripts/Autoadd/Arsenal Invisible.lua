b576e8c893c05d1a39135c9319f88a7c0821e4c4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:26:59 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arsenal Invisible.lua
@@ -0,0 +1,6 @@
+for _,v in pairs(game.Players.LocalPlayer.Character.LowerTorso:GetChildren()) do
+ v:Remove()
+end
+game.Players.LocalPlayer.Character.HeadHB:Remove()
+game.Players.LocalPlayer.Character.Hitbox:Remove()
+-- after execution press "Backspace" on ur keyboard
