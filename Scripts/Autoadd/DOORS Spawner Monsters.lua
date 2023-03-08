c4c817338add7036b65bbb3d56c9808004515f8b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:26:56 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/DOORS Spawner Monsters.lua
@@ -0,0 +1,31 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("WOUSH HUB", "Synapse")
+
+local Tab = Window:NewTab("Spawn")
+local Section = Tab:NewSection("Entities")
+
+
+
+
+
+
+Section:NewButton("Summon Halt", "Summon the blue blanket", function()
+    require(game.ReplicatedStorage.ClientModules.EntityModules.Shade).stuff(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
+workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
+end)
+
+
+Section:NewButton("Summon Screech", "Summon the blob of slime", function()
+    require(game.StarterGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.Screech)(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
+workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
+end)
+
+
+Section:NewButton("Summon Glitch", "Summon Mr. Teleport", function()
+    require(game.ReplicatedStorage.ClientModules.EntityModules.Glitch).stuff(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
+workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
+end)
+
+local Tab = Window:NewTab("Credits")
+local Section = Tab:NewSection("CREDITS nabewoush#5512")
