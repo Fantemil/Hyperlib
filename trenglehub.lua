37a45f048e98a071787ee3f3095b7a7f266ef19d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:33:34 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:33:27"
+lastupdate = "GMT +1: 08.03.2023 17:33:34"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10330,3 +10330,5 @@ addscriptexist(142823291, "Murder Mystery 2: Jump Power, Teleports, FOV", "pulle
 addscriptexist(2788229376, "Da Hood: Give SMG, Give Popcorn & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Give%20SMG%20Give%20Popcorn%20%20More.lua")
 
 addscriptexist(10523236737, "Multiverse Battlegrounds: Infinity Cash", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Multiverse%20Battlegrounds%20Infinity%20Cash.lua")
+
+addscriptexist(2377868063, "Strucid: Silent Aim, FOV", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Strucid%20Silent%20Aim%20FOV.lua")
