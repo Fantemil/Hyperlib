b9a211e5f49c19fd1db17b749b3adfad764b1b2d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:03:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 15:03:11"
+lastupdate = "GMT +1: 08.03.2023 15:03:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8472,3 +8472,5 @@ addscriptexist(920587237, "Adopt Me: Make pet rideable", "pulled from rbxscript.
 addscriptexist(3057050847, "Prison Anomalies: Auto Escape Farm, Remove Doors, Remove Lasers", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prison%20Anomalies%20Auto%20Escape%20Farm%20Remove%20Doors%20Remove%20Lasers.lua")
 
 addscriptexist(3837841034, "Deadline: Enemy ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Deadline%20Enemy%20ESP.lua")
+
+addscriptexist(8737602449, "PLS DONATE: Test Upd, Auto thanks, Text Color", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PLS%20DONATE%20Test%20Upd%20Auto%20thanks%20Text%20Color.lua")
