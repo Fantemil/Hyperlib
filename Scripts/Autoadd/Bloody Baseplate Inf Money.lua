e9c44c6793fcdf5b5cd773ff31e4a5d1248b22d3
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:07:21 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Bloody Baseplate Inf Money.lua
@@ -0,0 +1,6 @@
+while true do
+ wait(0)
+
+game:GetService("Players").LocalPlayer.leaderstats.Kills.Value = 10000000
+game:GetService("Players").LocalPlayer.leaderstats.Snowflakes.Value = 10000000
+end
