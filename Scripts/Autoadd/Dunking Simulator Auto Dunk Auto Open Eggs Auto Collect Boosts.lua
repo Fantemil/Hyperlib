38fb6f35463f624ac3e4f31e70ed13c5a0bbd015
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:44:04 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Dunking Simulator Auto Dunk Auto Open Eggs Auto Collect Boosts.lua
@@ -0,0 +1,6 @@
+--Discord Server for support :-> https://discord.gg/4m8863faVy
+--Get Key :-> http://darkzscripts.epizy.com
+script:
+--NOTE: YOU MUST INClUDE A KEY OR IT WONT WORK 
+_G.wl_key = 'putkeyhere'
+loadstring(game:HttpGet('https://scripts.luawl.com/13719/ScriptLoader.lua'))()
