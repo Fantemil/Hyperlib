7e0889522af5e2107b4dd7a1aa3f5fd6fb66d699
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:14:39 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Randomly Generated Droids Auto Rejoin God Mode Fly.lua
@@ -0,0 +1,6 @@
+_G.key = 'BalloonIsTheBest' --Key changes every 1-2 weeks so join my discord server for updated keys
+--keep all below off if you want gui to show up(works best in autoexecute with these features)
+_G.autofarm = false --automatically turns on autofarm
+_G.autorej = false --automatically rejoins in lobby after win
+_G.droidgod = false --turns on god mode
+loadstring(game:HttpGet("https://pastebin.com/raw/ZYkbTpDu"))()
