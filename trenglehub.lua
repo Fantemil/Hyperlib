3bf1e0450c347f186ae55d35da0a612eb19700ee
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:24:30 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:24:26"
+lastupdate = "GMT +1: 08.03.2023 17:24:30"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10102,3 +10102,5 @@ addscript(8476164859, "Current Game","Survive The Slimes: Nuke Slimes", "pulled 
 addscriptexist(9880062154, "The Last Strandeds: Server Crasher", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Last%20Strandeds%20Server%20Crasher.lua")
 
 addscriptexist(648362523, "Breaking Point: Auto Farm - 200k Per Hour", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Breaking%20Point%20Auto%20Farm%20%20k%20Per%20Hour.lua")
+
+addscriptexist(914010731, "Ro-ghoul: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roghoul%20Auto%20Farm.lua")
