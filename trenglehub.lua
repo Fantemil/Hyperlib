72fdf315f3c92881a672d0f6c524115b67cdd581
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:34:00 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:33:56"
+lastupdate = "GMT +1: 08.03.2023 12:34:00"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6236,3 +6236,5 @@ addscript(10006547495, "Current Game","Haunted Town: Auto Hit, Auto Heal, Infini
 addscriptexist(9862832367, "Never-Ending Cart Ride: Rainbow Carts, Lag Carts, Player Mods", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NeverEnding%20Cart%20Ride%20Rainbow%20Carts%20Lag%20Carts%20Player%20Mods.lua")
 
 addscriptexist(2753915549, "Blox Fruits: Bring mob, Fast Attack, Auto Buso", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Bring%20mob%20Fast%20Attack%20Auto%20Buso.lua")
+
+addscriptexist(606849621, "Jailbreak: RB Battles Badge Giver", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jailbreak%20RB%20Battles%20Badge%20Giver.lua")
