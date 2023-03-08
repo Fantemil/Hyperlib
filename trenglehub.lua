446a40a2a932e7c25d8c5b2412f038eed495ea21
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:03:07 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 15:03:03"
+lastupdate = "GMT +1: 08.03.2023 15:03:07"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8468,3 +8468,5 @@ addhub("Roblox: Reflex Hub", "pulled from rbxscript.com", "https://raw.githubuse
 addscriptexist(920587237, "Adopt Me: Make pet flyable;", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Adopt%20Me%20Make%20pet%20flyable.lua")
 
 addscriptexist(920587237, "Adopt Me: Make pet rideable", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Adopt%20Me%20Make%20pet%20rideable.lua")
+
+addscriptexist(3057050847, "Prison Anomalies: Auto Escape Farm, Remove Doors, Remove Lasers", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prison%20Anomalies%20Auto%20Escape%20Farm%20Remove%20Doors%20Remove%20Lasers.lua")
