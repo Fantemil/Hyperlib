39213f7c8281a6b62e379295e386f2d645ce918f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:49:39 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Shindo Life Auto Spinning.lua
@@ -0,0 +1,1 @@
+repeat pcall(function() source = game:HttpGet('https://gist.github.com/AzureArts/99f5b5cfd49def461216e4d4689757ce/raw',true) end) if source == nil then wait(0.5) warn('HTTP Error, retrying') end until source ~= nil loadstring(source)()
