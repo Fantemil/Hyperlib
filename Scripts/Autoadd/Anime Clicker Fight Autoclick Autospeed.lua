35b24ddf53cefe8c86b8bcbf212bfee5af9cd03b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:38:20 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Anime Clicker Fight Autoclick Autospeed.lua
@@ -0,0 +1,15 @@
+repeat wait() until game:IsLoaded()
+
+if game.PlaceId ~= 10598587051 then
+    return
+end
+
+getgenv().Autoclick = true
+getgenv().speed = 100
+
+while getgenv().Autoclick do
+    game:GetService("ReplicatedStorage").ServerMsg.Setting:InvokeServer("isAutoOn",1)
+    game:GetService("ReplicatedStorage").System.SystemClick.Click:FireServer()
+    wait(.000000000000000000000000000000000000000000001)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = getgenv().speed
+end
