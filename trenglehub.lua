dab206eae64c5decf077b8f49cf07b6850589d8f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:11:00 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:03:42"
+lastupdate = "GMT +1: 08.03.2023 20:11:00"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13460,3 +13460,5 @@ addscriptexist(9498006165, "Tapping Simulator! | Remove Egg Hatch and Click Dela
 addscriptexist(2248408710, "Destruction Simulator | Add Coins, Reset, Remove Cooldown", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Destruction%20Simulator%20%20Add%20Coins%20Reset%20Remove%20Cooldown.lua")
 
 addscriptexist(2788229376, "Da Hood | Smooth Aim Lock", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20%20Smooth%20Aim%20Lock.lua")
+
+addscriptuniversal("Universal Discord Server Auto Join", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Discord%20Server%20Auto%20Join.lua")
