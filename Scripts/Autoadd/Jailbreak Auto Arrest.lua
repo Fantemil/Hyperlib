843d2503e924401dd6e45429ec289c9cd82607ae
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:38:19 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Jailbreak Auto Arrest.lua
@@ -0,0 +1,7 @@
+--[[
+Reminder : DO NOT USE ON MAIN USE ALT ACCOUNT
+
+By MinWasTaken#2873 & Brizzy#9999 <3
+]]
+
+loadstring(game:HttpGet('https://scripts.luawl.com/14387/jailbreakautoarrest.lua'))()
