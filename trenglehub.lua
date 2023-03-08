e8fa65f822aa90709278087c1b1305e0d83ec653
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:52:59 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:52:55"
+lastupdate = "GMT +1: 08.03.2023 14:52:59"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8196,3 +8196,5 @@ addscriptexist(286090429, "Arsenal: Infinity Ammo, Auto Shoot, Options", "pulled
 addscriptexist(5985232436, "Infectious Smile: Give You Red Tag", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Infectious%20Smile%20Give%20You%20Red%20Tag.lua")
 
 addscriptexist(6516141723, "DOORS: Options, Spawn Entities & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Options%20Spawn%20Entities%20%20More.lua")
+
+addscriptexist(5956785391, "Project Slayers: Auto Farm Bosses, Killaura, Inf Stamina", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Auto%20Farm%20Bosses%20Killaura%20Inf%20Stamina.lua")
