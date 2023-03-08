63da813fc13140530b24f9a967fc5d4fa07f835e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:50:14 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Money Tycoon Walkspeed Anti Afk Auto Rebirth.lua
@@ -0,0 +1,3 @@
+-- Showcase: https://youtu.be/bjEo4ShBJQk
+getgenv().EnabledInvite = false -- set true for enable discord prompt
+loadstring(game:HttpGet("https://raw.githubusercontent.com/itsnoctural/Destiny/main/loader.lua",true))()
