09e074a4eba3e96a9fec3296c134599c9459a052
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:12:19 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/TROOP LIFE Anti Ban Kill All Get Gun.lua
@@ -0,0 +1,5 @@
+-- enjoy killing everyone
+-- here if you want to join the server or blacklist someone from using the script
+-- https://discord.gg/cUjywqGpZb
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ironicw/opscripts/main/trooplife", true))()
