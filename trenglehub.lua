964755e6d052333351d34eaf792d793310a02964
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:38:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:38:45"
+lastupdate = "GMT +1: 08.03.2023 06:38:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4656,3 +4656,5 @@ addscriptexist(11874473440, "Car Factory Tycoon: Teleports, Auto Sell, Auto Buil
 addscriptexist(537413528, "Build A Boat For Treasure: Auto Farm, No Water Damage, Walkspeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Farm%20No%20Water%20Damage%20Walkspeed.lua")
 
 addscriptexist(8131063774, "Shattered Psycho Online: Chest Auto Farm With Server Hop", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shattered%20Psycho%20Online%20Chest%20Auto%20Farm%20With%20Server%20Hop.lua")
+
+addscriptexist(8908228901, "Sharkbite 2: AutoKill Shark, AutoKill Survivors, VaseFarm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sharkbite%20%20AutoKill%20Shark%20AutoKill%20Survivors%20VaseFarm.lua")
