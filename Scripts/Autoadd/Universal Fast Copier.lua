e2ff9b6b594b6a4090783583af96c0ab3148066a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:27:40 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Fast Copier.lua
@@ -0,0 +1,2 @@
+_G.Theme = "Green" --Dark, Red, Light, Mocha, Monke, Orange, Red, Aqua, Jester, Jesterv, Green
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaQLeak/Scripts/main/Copier"))()
