5c150bd8717d35f0813b299e0d011bc7f17c6d84
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:10:50 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Tapping Simulator Auto Claim Rewards Auto Upgrader  More.lua
@@ -0,0 +1,3 @@
+-- Join the Discord for the Key! https://discord.gg/KwyPcJQ9ma --
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToothlessNight/DragonHub/main/AAAA%20Universal%20Script",true))();
