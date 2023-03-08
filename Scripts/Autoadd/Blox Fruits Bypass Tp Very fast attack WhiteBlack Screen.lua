183413a9f80b1ccaa3bb47e756877d7e07604b68
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:32:48 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Blox Fruits Bypass Tp Very fast attack WhiteBlack Screen.lua
@@ -0,0 +1,65 @@
+-- Alucard Hub Script Free
+-- Bloxfruit Kaitan Setting
+-- Discord https://discord.gg/taCreHGUx9
+getgenv().Setting = {
+    ["Join Team"] = "Pirate", -- "Pirate","Marine"
+    ["Auto Farm Level"] = false,
+    
+    -- Farm Etc
+    ["Farm Bone"] = false,
+    ["Auto Haki Rainbow"] = false,
+    ["Auto Observation Haki V2"] = false,
+    
+        
+    -- Setting etc
+    ["Select Weapon"] = "",
+    ["Auto Rejoin"] = false,
+    ["White Screen"] = false,
+    ["Black Screen"] = false,
+        
+    -- Old 
+    ["Auto New World"] = false,
+        
+    -- New World
+    ["Auto Factory"] = false,
+    ["Auto third World"] = false,
+
+    -- New Fighting Styles & etc
+    ["Auto Superhuman"] = false,
+    ["Auto Superhuman [Full]"] = false,
+    ["Auto Death Step"] = false,
+    ["Auto Dragon Talon"] = false,
+    ["Auto Electric Clow"] = false,
+    ["Auto Buy Legendary Sword"] = false,
+    ["Auto Buy Legendary Sword Hop"] = false,
+    ["Auto Buy Enhancement"] = false,
+    ["Auto Farm Select Boss Hop"] = false,
+
+    -- Auto Weapons
+    ["Farm Ken"] = false,
+    ["Farm Chest"] = false,
+    ["Open Saber"] = false,
+    ["Auto Pole v1"] = false,
+    ["Auto Quest Bartilo"] = false,
+    ["Auto true triple Katana"] = false,
+    ["Auto Rengoku"] = false,
+    ["Auto Buddy Sword"] = false,
+    ["AutoYama"] = false,
+    ["Auto Tushita"] = false,
+    ["Auto Holy Torch"] = false,
+        
+    -- Auto Stats
+    ["Melee"] = false,
+    ["Defense"] = false,
+    ["Sword"] = false,
+    ["Gun"] = false,
+    ["Demon Fruit"] = false,
+
+    -- Use Candy
+    ["Auto Buy Exp x2"] = false,
+    ["Auto Buy Exp x2[ Exp Expire ]"] = false,
+
+    -- Players
+    ["Bounty Hop"] = false
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JJTChannel/Alucard_hub/main/main.lua"))()
