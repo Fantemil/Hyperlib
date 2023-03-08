619a4d294f29c9c529a9cbf539ff981be9a04e2c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:03:11 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Animal Simulator Auto Farm Name Features Firework Spam.lua
@@ -0,0 +1,2 @@
+getgenv().LocalWebhookId = 'YOUR DISCORD WEBHOOK HERE'
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Xen101/Roblox/main/Animal%20Simulator/ONI-HUB%20Open%20Source'),true))()
