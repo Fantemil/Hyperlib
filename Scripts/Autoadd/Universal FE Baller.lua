0085d844e1277f98fc011cc5bc91860b5f6e9373
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:15:12 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal FE Baller.lua
@@ -0,0 +1,3 @@
+--by Scriptifer#9511
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Lowerrated/roblox-fe-baller-script/main/main"))()
+--https://www.roblox.com/catalog/6685365462/Red-Stickman-Head
