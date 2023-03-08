7dca6df3e02907a8d1cfbdd2b68f6485c93452f9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:59:40 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ohio Bank Valt Teleporter.lua
@@ -0,0 +1,6 @@
+local oh1 = CFrame.new(1053.5323486328125, 8.568477630615234, -338.8551940917969)
+local oh2 = game:GetService("Players")
+local oh3 = oh2.LocalPlayer.Character.HumanoidRootPart
+
+
+oh3.CFrame = oh1
