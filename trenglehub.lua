15256320259a5f0aa759e158c4a1d581fe966cc5
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:06:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:06:33"
+lastupdate = "GMT +1: 08.03.2023 18:06:37"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11162,3 +11162,5 @@ addscriptexist(9285238704, "Race Clicker: Auto Tween Win, Auto Win, Teleports", 
 addscriptexist(3956818381, "Ninja Legends: Auto Farm, Auto Sell, Auto Collect Hoops", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ninja%20Legends%20Auto%20Farm%20Auto%20Sell%20Auto%20Collect%20Hoops.lua")
 
 addscript(11150416759, "Current Game","Pet Race Clicker: Auto Farm, Auto Click, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Race%20Clicker%20Auto%20Farm%20Auto%20Click%20Auto%20Rebirth.lua")
+
+addscriptexist(6336491204, "Karate: Remove Hitbox, ReAdd Hitbox", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Karate%20Remove%20Hitbox%20ReAdd%20Hitbox.lua")
