bbf78a4adf625c90dfe668ee91c36135f7743b21
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:51:20 +0100

    Updated automatically!

--- Scripts/Autoadd/Doomspire Brickbattle AUTO KILL ALL.lua
+++ Scripts/Autoadd/Doomspire Brickbattle AUTO KILL ALL.lua
@@ -1,4 +1,3 @@
 --if you want to stop it, execute this:     getgenv().BatusMadeThisShit = false
 --Doomspire Brickbattle AUTOKILL ALL, AUTOFARM KILLS.
-
 getgenv().BatusMadeThisShit=true;loadstring(game:HttpGet("https://raw.githubusercontent.com/juywvm/-Roblox-Projects-/main/Doomspire%20Brickbattle/autofarm_kills.lua"))();
