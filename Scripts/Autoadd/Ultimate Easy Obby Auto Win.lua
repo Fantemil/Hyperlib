8588a792ed9fe1dfa3cbddb34e8178d1f4dc6173
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:48:52 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ultimate Easy Obby Auto Win.lua
@@ -0,0 +1,7 @@
+-- literally made under 30 seconds with remote spy (dont think this is serious)
+
+local args = {
+    [1] = 50
+}
+
+game:GetService("ReplicatedStorage").Honeypot.Internal.RemoteStorage:FindFirstChild("LevelUp - RemoteEvent"):FireServer(unpack(args))
