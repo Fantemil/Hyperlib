1cf36028af7e769d8d6d61aee579259fa669152f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:59:36 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Creeper Chaos Afk Farm.lua
@@ -0,0 +1,6 @@
+---turn _G.autotap = false to turn it off
+---idk why i used tween :weary:
+---made by orbito#2197
+_G.autotap = true
+
+loadstring(game:HttpGet('https://pastebin.com/raw/Cqqnzzt3'))()
