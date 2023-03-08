566d5ddf834e4e0f2708aa04225e16ed30f97392
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:29:14 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Bridge Worm Admin Rank Giver.lua
@@ -0,0 +1,3 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-17014, 71, -1186)
+wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1657, 114, -1317)
