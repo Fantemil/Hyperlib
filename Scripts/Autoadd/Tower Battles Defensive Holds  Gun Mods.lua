7717c3dc2187c5fdc1128996c86e94a61fe19265
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:56:03 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Tower Battles Defensive Holds  Gun Mods.lua
@@ -0,0 +1,2 @@
+--Gun Mod (make sure to execute it every time you respawn)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/onedecidedinstealth/There-Is-No-Scripts/main/GotItWorking.lua"))()
