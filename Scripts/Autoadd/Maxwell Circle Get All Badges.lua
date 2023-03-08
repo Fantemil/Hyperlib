ffa5850a4e32bb43e7992b6bfca9ba56c896cd30
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:43:06 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Maxwell Circle Get All Badges.lua
@@ -0,0 +1,7 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-254, 4, 45)
+wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(32782, 322, 395)
+wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-10426, 11947, -73)
+wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(41, 3, -24)
