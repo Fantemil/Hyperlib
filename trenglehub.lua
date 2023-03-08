c4882e749b6702585cf26d5c9b993e15947d7e0f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:54:10 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:54:06"
+lastupdate = "GMT +1: 08.03.2023 18:54:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12198,3 +12198,5 @@ addscript(9478827096, "Current Game","Tower of Ball: Break players balls, Spawn 
 addscriptexist(10181361874, "Death Academy Online: Kill Aura, Inf Stats, Crash Cerver", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Death%20Academy%20Online%20Kill%20Aura%20Inf%20Stats%20Crash%20Cerver.lua")
 
 addscriptexist(3214114884, "Flag Wars: Aim Bot", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Flag%20Wars%20Aim%20Bot.lua")
+
+addscriptexist(10243982775, "RedBox: Anti Arrest, Auto Arrest, Bomb All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RedBox%20Anti%20Arrest%20Auto%20Arrest%20Bomb%20All.lua")
