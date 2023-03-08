137c1e0f433d74d8ed8831863e6ececf8a17501b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:50:23 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:50:16"
+lastupdate = "GMT +1: 08.03.2023 20:50:23"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14480,3 +14480,5 @@ addscriptexist(5872075530, "Anarchy [Esp, Hitboxes]", "pulled from rbxscript.com
 addhub("Wheat Hub [Games 41]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Wheat%20Hub%20Games%20.lua")
 
 addscriptexist(2788229376, "Da Hood [SilentAim]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20SilentAim.lua")
+
+addscriptexist(3351674303, "Driving Empire [AutoFarm, AutoDragRace]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Driving%20Empire%20AutoFarm%20AutoDragRace.lua")
