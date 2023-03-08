a8d87011bfaf5f9b51c7ebf93b50131a1ee88ee7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:21:35 +0100

    Updated automatically!

--- Scripts/Autoadd/Your Bizarre Adventure Pity Farm.lua
+++ Scripts/Autoadd/Your Bizarre Adventure Pity Farm.lua
@@ -1,3 +1,3 @@
-getgenv().PityWanted = "" --Example: "1.52%", "1.56%", "1.6%", "1.64%", "1.68%" because it's from 4 in 4. So if for example you want 4 pity you put 4.0% because it adds a "0" or 4.2% it's +4 so you can calculate it easily
+ getgenv().PityWanted = "4%" --Example: "1.52%", "1.56%", "1.6%", "1.64%", "1.68%" because it's from 4 in 4. So if for example you want 4 pity you put 4.0% because it adds a "0" or 4.4% or you calculate it
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/senS24t/sensnebunu/main/PityFarmBysenS"))()
