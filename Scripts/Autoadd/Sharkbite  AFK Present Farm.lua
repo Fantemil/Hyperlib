8965794eaa31b4409786e1d0cd85e22a92f683e8
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:33:08 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Sharkbite  AFK Present Farm.lua
@@ -0,0 +1,2 @@
+getgenv().StartCollectGifts = true -- Toggle for farming
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KrystekYTpv/Scripts/main/SharkBite2_PresentAutoFarm.lua", true))()
