e479cd3adab205720ac37073d8c6a6b20227dd0f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:48:24 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Every Second You Jump Higher Auto Win.lua
@@ -0,0 +1,5 @@
+while wait(6) do
+    local model = game:GetService("Workspace").MainWorldRevamp.IceIsland["Meshes/Button2"]
+    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, model, 0)
+    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, model, 1)
+end
