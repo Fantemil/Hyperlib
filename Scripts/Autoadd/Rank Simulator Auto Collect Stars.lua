eee206e37ade6e3cbdea79dafa9fa856bcaf5f69
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:38:35 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Rank Simulator Auto Collect Stars.lua
@@ -0,0 +1,105 @@
+getgenv().MainStars = true
+getgenv().BeachStars = true
+getgenv().JungleStars = true
+getgenv().HellStars = true
+getgenv().CaveStars = true
+getgenv().IceStars = true
+getgenv().CandyStars = true
+    
+    
+    spawn(function()
+     local playerHead = game.Players.LocalPlayer.Character.Head
+     while wait() do
+        if not getgenv().MainStars then break end
+        for i, v in pairs(game:GetService("Workspace").Spawners.Candy:GetDescendants()) do
+        if v.Name == "TouchInterest" and v.Parent then
+            firetouchinterest(playerHead, v.Parent, 0)
+            wait()
+            end
+        end
+     end
+end)
+
+    
+    spawn(function()
+     local playerHead = game.Players.LocalPlayer.Character.Head
+     while wait() do
+        if not getgenv().BeachStars then break end
+        for i, v in pairs(game:GetService("Workspace").Spawners.Candy:GetDescendants()) do
+        if v.Name == "TouchInterest" and v.Parent then
+            firetouchinterest(playerHead, v.Parent, 0)
+            wait()
+            end
+        end
+     end
+end)
+    
+    
+    spawn(function()
+     local playerHead = game.Players.LocalPlayer.Character.Head
+     while wait() do
+        if not getgenv().JungleStars then break end
+        for i, v in pairs(game:GetService("Workspace").Spawners.Candy:GetDescendants()) do
+        if v.Name == "TouchInterest" and v.Parent then
+            firetouchinterest(playerHead, v.Parent, 0)
+            wait()
+            end
+        end
+     end
+end)
+
+
+spawn(function()
+     local playerHead = game.Players.LocalPlayer.Character.Head
+     while wait() do
+        if not getgenv().HellStars then break end
+        for i, v in pairs(game:GetService("Workspace").Spawners.Candy:GetDescendants()) do
+        if v.Name == "TouchInterest" and v.Parent then
+            firetouchinterest(playerHead, v.Parent, 0)
+            wait()
+            end
+        end
+     end
+end)
+
+
+spawn(function()
+     local playerHead = game.Players.LocalPlayer.Character.Head
+     while wait() do
+        if not getgenv().CaveStars then break end
+        for i, v in pairs(game:GetService("Workspace").Spawners.Candy:GetDescendants()) do
+        if v.Name == "TouchInterest" and v.Parent then
+            firetouchinterest(playerHead, v.Parent, 0)
+            wait()
+            end
+        end
+     end
+end)
+
+
+spawn(function()
+     local playerHead = game.Players.LocalPlayer.Character.Head
+     while wait() do
+        if not getgenv().IceStars then break end
+        for i, v in pairs(game:GetService("Workspace").Spawners.Candy:GetDescendants()) do
+        if v.Name == "TouchInterest" and v.Parent then
+            firetouchinterest(playerHead, v.Parent, 0)
+            wait()
+            end
+        end
+     end
+end)
+
+
+spawn(function()
+     local playerHead = game.Players.LocalPlayer.Character.Head
+     while wait() do
+        if not getgenv().CandyStars then break end
+        for i, v in pairs(game:GetService("Workspace").Spawners.Candy:GetDescendants()) do
+        if v.Name == "TouchInterest" and v.Parent then
+            firetouchinterest(playerHead, v.Parent, 0)
+            wait()
+            end
+        end
+     end
+end)
