c1cd7846b749229ba8c79d0e868a3e244589e66a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:08:35 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Treasure Quest Auto Farm.lua
@@ -0,0 +1,25 @@
+-- https://www.roblox.com/games/2960777560/DUNGEON-Treasure-Quest
+getgenv().TreasureQuestAutofarm = {
+    Enabled = true;
+    Dungeon = "Ancient Jungle";
+    Difficulty = "Easy";
+    GameMode = "Classic";
+    FailSafeRejoin = false; -- Tries to go back to lobby if Character is stuck somewhere...
+    FailSafeSeconds = 200;
+}
+
+getgenv().TreasureQuestAutoSell = {
+    Enabled = false;
+    AutoUpgradeGear = false;
+    KeepAbilities = false; -- this doesn't exist yet
+    KeepTiers = {
+        "Common";
+        "Rare";
+        "Legendary";
+        "Mythical";
+        "Ultimate";
+        "Unique";
+    }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Treasure%20Quest/TreasureQuestLoadstring"))()
