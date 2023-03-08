2a14a0a09ce5a89eaf9940608f760d38a6d31ee4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:16:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:16:28"
+lastupdate = "GMT +1: 08.03.2023 20:16:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13610,3 +13610,5 @@ addscriptuniversal("Universal Stream Sniper", "pulled from rbxscript.com", "http
 addscriptuniversal("Universal Infinity Store", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Infinity%20Store.lua")
 
 addscriptuniversal("Universal Basic Dash", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Basic%20Dash.lua")
+
+addscript(10275942471, "Current Game","Color Race! | Get alot of Wins + Speed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Color%20Race%20%20Get%20alot%20of%20Wins%20%20Speed.lua")
