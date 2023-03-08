0957e34948ca52cbe1d6e5ed79f260da145c006b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:07:10 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Every Second Your Neck Grows Auto Grow.lua
@@ -0,0 +1,8 @@
+_G.spam = true
+while _G.spam == true do
+local A_1 = 999
+local A_2 = 1671950723.7336342
+local Event = game:GetService("ReplicatedStorage").Knit.Services.NeckService.RE.NeckGrowthState
+Event:FireServer(A_1, A_2)
+wait(0)
+end
