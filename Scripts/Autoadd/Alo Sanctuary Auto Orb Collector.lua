9076e5d531d00831802fc081d584ca2ebf6194cf
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:19:28 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Alo Sanctuary Auto Orb Collector.lua
@@ -0,0 +1,10 @@
+local orb = game.Workspace.Map.OrangeOrbs
+while true do
+for i,v in pairs (orb:GetChildren()) do
+  if v:IsA("Model") then
+      wait(0.1)
+      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v:GetPivot()
+      game.Players.LocalPlayer.Character.Humanoid.Jump = true
+    end
+  end
+end
