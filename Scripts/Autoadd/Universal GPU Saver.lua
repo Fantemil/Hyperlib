290b16ef2cbb4c75e0f5e19590cc5202b24ad2f1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:37:49 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal GPU Saver.lua
@@ -0,0 +1,5 @@
+--[=[
+    Script created by Shawn Bragdon
+    Liscensed under GNU General Public License 3.0
+]=]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shawnjb/roblox-gpu-saver/release/scripts/GPUSaver.lua"))()
