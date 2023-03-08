1023aa11835d747393a6836c629351014292bf95
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:33:19 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:33:14"
+lastupdate = "GMT +1: 08.03.2023 19:33:19"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13052,3 +13052,5 @@ addscriptuniversal("Universal Google Translator", "pulled from rbxscript.com", "
 addscriptexist(8737602449, "PLS DONATE: Fake Donate Nuke", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PLS%20DONATE%20Fake%20Donate%20Nuke.lua")
 
 addscript(542016179, "Current Game","Teethyz Dentist: Anti Cheat Delete", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Teethyz%20Dentist%20Anti%20Cheat%20Delete.lua")
+
+addscriptexist(4860819035, "Plants vs Zombies Garden Problem: Infinity money", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Plants%20vs%20Zombies%20Garden%20Problem%20Infinity%20money.lua")
