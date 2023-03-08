de512d33fa058134324a7a864031190cbeac8eff
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:08:29 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:08:26"
+lastupdate = "GMT +1: 08.03.2023 07:08:29"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5564,3 +5564,5 @@ addscriptexist(8908228901, "Sharkbite 2: Present Afk Auto Farm", "pulled from rb
 addscriptexist(9534337535, "Shrek In The Backrooms: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shrek%20In%20The%20Backrooms%20Auto%20Farm.lua")
 
 addscript(11187148979, "Current Game","Ultra UnFair: Kill Aura", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultra%20UnFair%20Kill%20Aura.lua")
+
+addscriptexist(6298464951, "Roblox Is Unbreakable: Get Values Of Selected Items", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Is%20Unbreakable%20Get%20Values%20Of%20Selected%20Items.lua")
