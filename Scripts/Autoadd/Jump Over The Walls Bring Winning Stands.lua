037d9de338b8ca15c86fba0d61a7a7f19bca218c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:57:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Jump Over The Walls Bring Winning Stands.lua
@@ -0,0 +1,3 @@
+game:GetService("Workspace").End.Platform.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+game:GetService("Workspace").End2.Platform.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+game:GetService("Workspace").End3.Platform.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
