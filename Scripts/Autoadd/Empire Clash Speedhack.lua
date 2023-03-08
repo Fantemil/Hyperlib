765d366cb4665a65d7ce758fd00bc878528b4820
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:10:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Empire Clash Speedhack.lua
@@ -0,0 +1,9 @@
+--READ DESCRIPTION!!
+--READ DESCRIPTION!!READ DESCRIPTION!!
+--READ DESCRIPTION!!
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+--READ DESCRIPTION!!
+--READ DESCRIPTION!!
+--READ DESCRIPTION!!
+--READ DESCRIPTION!!
+--READ DESCRIPTION!!
