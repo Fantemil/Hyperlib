e0c125df054a59ca7d54ca5e45e4a8e2319b8ec7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:10:15 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Project Slayers Fast KillAura Infinite Stamina Boss Farming.lua
@@ -0,0 +1,11 @@
+getgenv().flags = {
+    ['settings'] = {
+        ['key'] = 'Key Here';
+        ['write_file'] = false;
+    }
+}
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/muzscripter/projects/main/muz.cc/KEY_SYSTEM.lua"))();
+
+-- // join discord for key https://discord.gg/QBdvJ5QymP \\ --
