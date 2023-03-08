cd7e5bf2f7f5db5858db004103edcd665ed0c6af
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:47:50 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Press Q to target Aim.lua
@@ -0,0 +1,11 @@
+getgenv().Settings = {
+        Key = "q",
+        LOCK = true,
+        NOTIF = true,
+        Enable = true,
+        AIRSHOT = true,
+        AUTOPRED = true,
+        RESOVLER = false,
+        CHANCE = math.huge
+}
+loadstring(game:HttpGet'https://vode.gq/r/v2')()
