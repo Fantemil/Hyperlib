e57770711fa38104749a00d9d53942c9eadef64f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:58:14 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Sheep Tycoon Auto Double Money.lua
@@ -0,0 +1,5 @@
+while true do
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-934, 183, 837)
+wait(0.5)
+end
+-- if you want it slow change the time from 0.5 to something bigger
