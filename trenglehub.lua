acc307acfcdb337cbc56d7a7521d3729c45848c9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:25:21 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:25:16"
+lastupdate = "GMT +1: 08.03.2023 17:25:21"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10122,3 +10122,5 @@ addscriptexist(8540346411, "Rebirth Champions X: Auto Collect Pumpkins, Auto Cli
 addscriptexist(11448052802, "Pet Rift: Auto Farm, Auto Egg", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Rift%20Auto%20Farm%20Auto%20Egg.lua")
 
 addscript(47324, "Current Game","Sword Fights on The Heights IV: Invisible, Force Field & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20Fights%20on%20The%20Heights%20IV%20Invisible%20Force%20Field%20%20More.lua")
+
+addscriptexist(7305309231, "Taxi Boss: Infinity Candies and Cash", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Taxi%20Boss%20Infinity%20Candies%20and%20Cash.lua")
