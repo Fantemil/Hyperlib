ae4146afc0980ae8b0b1e98e796307d359c50f11
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:45:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:45:09"
+lastupdate = "GMT +1: 08.03.2023 14:45:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7990,3 +7990,5 @@ addscriptuniversal("Universal FE Lagswitch", "pulled from rbxscript.com", "https
 addscriptexist(286090429, "Arsenal: Aimbot, Hitbox, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20Aimbot%20Hitbox%20ESP.lua")
 
 addscriptexist(292439477, "Phantom Forces: Go to Ban Land, Hitboxes & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Go%20to%20Ban%20Land%20Hitboxes%20%20More.lua")
+
+addscriptexist(6516141723, "DOORS: Christmas Gun Giver", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Christmas%20Gun%20Giver.lua")
