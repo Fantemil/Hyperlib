14fb97e75246f0e97a9c939e82e78be5cdb693b4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:34:33 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:34:29"
+lastupdate = "GMT +1: 08.03.2023 20:34:33"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14084,3 +14084,5 @@ addscriptexist(9658251985, "The Rake Kill Test [Kill people]", "pulled from rbxs
 addscriptexist(8328351891, "Mega Mansion Tycoon [Auto Buy, Auto Collect, Money Make Day]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mega%20Mansion%20Tycoon%20Auto%20Buy%20Auto%20Collect%20Money%20Make%20Day.lua")
 
 addscriptexist(286090429, "Arsenal [Kill All, Silent Aim, Esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20Kill%20All%20Silent%20Aim%20Esp.lua")
+
+addscriptuniversal("Roblox Universal Script [AntiTP Bypass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Universal%20Script%20AntiTP%20Bypass.lua")
