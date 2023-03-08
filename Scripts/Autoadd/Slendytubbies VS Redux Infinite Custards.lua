9653d3016325f7dac2f2b0f3072aa840c4c4dfa3
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:13:05 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Slendytubbies VS Redux Infinite Custards.lua
@@ -0,0 +1,13 @@
+local run
+ run = game:GetService("RunService").RenderStepped:connect(function()
+    for i=1,5 do
+function getNil(name,class) for _,v in pairs(getnilinstances())do if v.ClassName==class and v.Name==name then return v;end end end
+
+local args = {
+   [1] = "collected",
+   [2] = getNil("c14", "Part")
+}
+
+game:GetService("ReplicatedStorage").collectedCustardEvent:FireServer(unpack(args))
+end
+end) 
