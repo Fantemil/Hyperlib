e06e8598dd675bd4db264697a4aa8ec99894c05c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:50:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 16:50:09"
+lastupdate = "GMT +1: 08.03.2023 16:50:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9224,3 +9224,5 @@ addscriptexist(6198039277, "Cyber City Tycoon: Infinity Money", "pulled from rbx
 addscriptexist(11196588443, "Zombie Merge Tycoon: Auto Deposit, Inf Sample, Auto Upgrade", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Zombie%20Merge%20Tycoon%20Auto%20Deposit%20Inf%20Sample%20Auto%20Upgrade.lua")
 
 addscriptexist(5780309044, "Stands Awakening: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Stands%20Awakening%20Auto%20Farm.lua")
+
+addscriptuniversal("Universal Create A Pastebin With Your AssetID", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Create%20A%20Pastebin%20With%20Your%20AssetID.lua")
