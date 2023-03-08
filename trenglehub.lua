359d89c163b1b5f84cd03beebefb100e68ef9986
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:53:59 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:53:56"
+lastupdate = "GMT +1: 08.03.2023 12:53:59"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6770,3 +6770,5 @@ addscriptexist(10291129032, "Flappy Bird Race: Auto Farm Coins, Auto Collect Bir
 addscriptexist(6679968919, "Fly Race: Studs Farm, Bring Orbs, Anti Afk.", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fly%20Race%20Studs%20Farm%20Bring%20Orbs%20Anti%20Afk.lua")
 
 addscriptexist(9285238704, "Race Clicker: Teleports, Auto Win, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Race%20Clicker%20Teleports%20Auto%20Win%20Auto%20Rebirth.lua")
+
+addscriptexist(11040063484, "Sword Fighters Simulator: Webhooks, Auto Farm Quests & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20Fighters%20Simulator%20Webhooks%20Auto%20Farm%20Quests%20%20More.lua")
