b39a4de9b42d943dfb631813fea440ab5149349b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:20:12 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Queen Elizebeth Obby All GamePasses.lua
@@ -0,0 +1,8 @@
+game:GetService("ReplicatedStorage").GC:FireServer()
+game:GetService("ReplicatedStorage").GH:FireServer()
+game:GetService("ReplicatedStorage").JP:FireServer()
+game:GetService("ReplicatedStorage").R:FireServer()
+game:GetService("ReplicatedStorage").RC:FireServer()
+game:GetService("ReplicatedStorage").SC:FireServer()
+game:GetService("ReplicatedStorage").TP:FireServer()
+game:GetService("ReplicatedStorage").XX:FireServer()
