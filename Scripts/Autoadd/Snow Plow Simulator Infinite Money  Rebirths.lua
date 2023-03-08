171f24979f39d74334221067711111160d699535
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:58:18 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Snow Plow Simulator Infinite Money  Rebirths.lua
@@ -0,0 +1,2 @@
+game:GetService("ReplicatedStorage").Shop.Events.BuyItem:FireServer(-999999999999999, "1 Stick", true) -- Money
+game:GetService("ReplicatedStorage").RebirthEvent:FireServer(100000) -- Rebirths
