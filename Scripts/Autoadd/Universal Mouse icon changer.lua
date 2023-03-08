a99d16e084c44e8fd34d56598084e5ad227eab75
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:02:08 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Mouse icon changer.lua
@@ -0,0 +1,6 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+while true do
+wait(0.1)
+local mouse = game.Players.LocalPlayer:GetMouse() mouse.Icon = "http://www.roblox.com/asset?id=7767269282" -- change the numbers to your decal id
+end
