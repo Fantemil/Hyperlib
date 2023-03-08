e0db32da03ec49675010e48bfb6af181991010bc
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:39:21 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:39:15"
+lastupdate = "GMT +1: 08.03.2023 18:39:20"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11910,3 +11910,5 @@ addscriptexist(606849621, "Jailbreak: Infinity Nitro, Open All Doors & More", "p
 addscriptexist(6403373529, "Slap Battles: Auto Farm AI", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Slap%20Battles%20Auto%20Farm%20AI.lua")
 
 addscriptexist(536102540, "DBZ Final Stand: Hides Your Level", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DBZ%20Final%20Stand%20Hides%20Your%20Level.lua")
+
+addscriptexist(10925589760, "Merge Simulator: Free Money", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Merge%20Simulator%20Free%20Money.lua")
