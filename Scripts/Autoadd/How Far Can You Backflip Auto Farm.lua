7ce032c7f55d26d01bdd3258585edc82193f5d03
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:52:00 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/How Far Can You Backflip Auto Farm.lua
@@ -0,0 +1,7 @@
+firetouchinterest(game.Players.LocalPlayer.Character.Head, game:GetService("Workspace").Ramp.Part, 0)
+task.wait(0.3)
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.2").knit.Services.SpeedMultiplierService.RE.Reset:FireServer(math.huge, CFrame.new(0, -434.147003, 10518864, 0, 0, 1, 0, 1, -0, -1, 0, 0))  
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.2").knit.Services.BuyService.RF.BuySM:InvokeServer()
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.2").knit.Services.BuyService.RF.BuyRebirth:InvokeServer()
+
+--You Need To execute it so many times.
