2c08fb6ae0b34784f20a34d94c4c23d21e7b0b7a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 21:04:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 21:04:48"
+lastupdate = "GMT +1: 08.03.2023 21:04:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14838,3 +14838,5 @@ addscriptexist(4282985734, "Combat Warriors [KillAura, No parry]", "pulled from 
 addscriptexist(8540346411, "Rebirth Champions X [Auto Buy, Auto Clicker]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rebirth%20Champions%20X%20Auto%20Buy%20Auto%20Clicker.lua")
 
 addscriptexist(2788229376, "Da Hood [Trash Talk]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Trash%20Talk.lua")
+
+addscriptuniversal("Roblox Universal Script [FE Rich Items]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Universal%20Script%20FE%20Rich%20Items.lua")
