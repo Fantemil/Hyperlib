c47008cd942244e51db6342351e83fcd2f324061
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:46:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:46:49"
+lastupdate = "GMT +1: 08.03.2023 06:46:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4904,3 +4904,5 @@ addscriptexist(3351674303, "Driving Empire: Event Auto Farm Slow, Event Auto Far
 addscriptexist(6278885452, "Strong Katana Simulator: Auto Strenght", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Strong%20Katana%20Simulator%20Auto%20Strenght.lua")
 
 addscript(3226555017, "Current Game","SCP: Site Roleplay | Open doors, Spam Radio, Invisible", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SCP%20Site%20Roleplay%20%20Open%20doors%20Spam%20Radio%20Invisible.lua")
+
+addscript(4695287616, "Current Game","F3X SandBox: Server Crasher", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FX%20SandBox%20Server%20Crasher.lua")
