bae067dd6558bc12a2af11749748fa1a0a0677f6
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:28:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:28:30"
+lastupdate = "GMT +1: 08.03.2023 17:28:38"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10208,3 +10208,5 @@ addscript(4860819035, "Current Game","Plants vs Zombies Garden Problem: Break Se
 addscriptexist(10347946161, "Rat Washing Tycoon: Auto Collect Rats", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rat%20Washing%20Tycoon%20Auto%20Collect%20Rats.lua")
 
 addscript(2041312716, "Current Game","Ragdoll Engine: Teleports, No Push Cooldown & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ragdoll%20Engine%20Teleports%20No%20Push%20Cooldown%20%20More.lua")
+
+addscriptexist(4924922222, "Brookhaven RP: Rainbow House, Smoking Gun & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Brookhaven%20RP%20Rainbow%20House%20Smoking%20Gun%20%20More.lua")
