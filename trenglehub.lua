3bf60b5f58fea274bcffe6bf86e7ea6d55580126
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:11:42 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 13:11:38"
+lastupdate = "GMT +1: 08.03.2023 13:11:42"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7226,3 +7226,5 @@ addscriptexist(2788229376, "Da Hood: Melee Reach, Knife Pickup, Fake Macro", "pu
 addscriptexist(662417684, "LUCKY BLOCKS Battlegrounds: Bring All Players, Teleporter, Get All Items", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LUCKY%20BLOCKS%20Battlegrounds%20Bring%20All%20Players%20Teleporter%20Get%20All%20Items.lua")
 
 addscriptexist(621129760, "KAT: God mode, Silent aim, Esp", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/KAT%20God%20mode%20Silent%20aim%20Esp.lua")
+
+addhub("Voxle Hub: 1 Game", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Voxle%20Hub%20%20Game.lua")
