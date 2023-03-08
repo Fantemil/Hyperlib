3af427d5eeffa7f018f871514ee674254aeac3c7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:43:07 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:43:03"
+lastupdate = "GMT +1: 08.03.2023 12:43:07"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6492,3 +6492,5 @@ addscriptexist(11572573905, "Power Wash Tycoon: Infinity Capacity", "pulled from
 addscriptexist(5895971007, "Allusions: Gui, Stat Change & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Allusions%20Gui%20Stat%20Change%20%20More.lua")
 
 addscriptexist(6737970321, "Livetopia: Collect all candy's", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Livetopia%20Collect%20all%20candys.lua")
+
+addscript(11789124018, "Current Game","Maxwell Circle: Get All Badges", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Maxwell%20Circle%20Get%20All%20Badges.lua")
