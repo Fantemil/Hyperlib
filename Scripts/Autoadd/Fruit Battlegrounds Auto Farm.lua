a75b2492cf075845298c391a32c3998a6c3406cc
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:18:20 +0100

    Updated automatically!

--- Scripts/Autoadd/Fruit Battlegrounds Auto Farm.lua
+++ Scripts/Autoadd/Fruit Battlegrounds Auto Farm.lua
@@ -1,6 +1,1 @@
--- Configurations
-getgenv().Position  = CFrame.new(67, 822, 1616) -- Position you want to farm at
-getgenv().Stamina   =  10   -- The Percentage Of Stamina You Want To Reset At ( If you dont wanna reset make it tonumber('-inf') or -math.huge )
-getgenv().Autofarm  =  true -- AutoFarm Toggle
--- Autofarm
-loadstring(game:HttpGet("https://raw.githubusercontent.com/508droid/FruitBattlegrounds/main/FruitBattlegrounds.lua",true))() -- Open Source
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/BillsTheGod/V3RMILLION_PROJECTS/main/Fruit%20Battlegrounds.lua")))()
