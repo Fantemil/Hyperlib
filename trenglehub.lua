f1bc45b0c30066f2591972436883a0a953318c74
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:06:46 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:06:42"
+lastupdate = "GMT +1: 08.03.2023 18:06:46"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11166,3 +11166,5 @@ addscript(11150416759, "Current Game","Pet Race Clicker: Auto Farm, Auto Click, 
 addscriptexist(6336491204, "Karate: Remove Hitbox, ReAdd Hitbox", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Karate%20Remove%20Hitbox%20ReAdd%20Hitbox.lua")
 
 addscriptexist(10861112990, "Anime Artifacts: Teleports, Auto Farm, Auto Hit Air", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Artifacts%20Teleports%20Auto%20Farm%20Auto%20Hit%20Air.lua")
+
+addscriptexist(1458767429, "ABA: Auto Play, Auto Block, Auto Use Skills", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ABA%20Auto%20Play%20Auto%20Block%20Auto%20Use%20Skills.lua")
