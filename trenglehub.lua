12f4bc3024ccbcbc7e20ac8851b7e0e3e33a3daa
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:33:23 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:33:18"
+lastupdate = "GMT +1: 08.03.2023 17:33:23"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10326,3 +10326,5 @@ addscriptexist(2534724415, "Emergency Response: Liberty County | Anti Arrest, An
 addscriptexist(6949542776, "Pixel Gun Tower Defense: Auto Strat", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pixel%20Gun%20Tower%20Defense%20Auto%20Strat.lua")
 
 addscriptexist(142823291, "Murder Mystery 2: Jump Power, Teleports, FOV", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Murder%20Mystery%20%20Jump%20Power%20Teleports%20FOV.lua")
+
+addscriptexist(2788229376, "Da Hood: Give SMG, Give Popcorn & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Give%20SMG%20Give%20Popcorn%20%20More.lua")
