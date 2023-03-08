da4b8305b2fbab268b90bda2ad7d8379a340ede1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:40:14 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Flee The Facility Get Hammer Without.lua
@@ -0,0 +1,5 @@
+game:GetService("Workspace").username.Hammer.Handle.SoundChaseMusic:Destroy()
+
+game:GetService("Workspace").username.Hammer.Handle.SoundHeartBeat:Destroy()
+
+game:GetService("Workspace").username.Gemstone.Handle:Destroy()
