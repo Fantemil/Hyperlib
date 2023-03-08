51e924121af50ed8e5b690c6bc89f95d1cb5e682
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:24:43 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:24:38"
+lastupdate = "GMT +1: 08.03.2023 20:24:43"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13828,3 +13828,5 @@ addscriptexist(6753004901, "Swing City | Unlock All Skills, Farm Money, Exp FAST
 addscriptexist(4639625707, "Warfare Tycoon | Kill Players, Kill item, Kill All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Warfare%20Tycoon%20%20Kill%20Players%20Kill%20item%20Kill%20All.lua")
 
 addscriptexist(2788229376, "Da Hood | NoClip, Auto Farm, Fly", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20%20NoClip%20Auto%20Farm%20Fly.lua")
+
+addscriptexist(10087074695, "Knife Strife! | Walkspeed, Fly, Inf Jump", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Knife%20Strife%20%20Walkspeed%20Fly%20Inf%20Jump.lua")
