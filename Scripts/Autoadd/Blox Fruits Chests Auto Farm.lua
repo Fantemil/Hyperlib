6044cb6de74f2d3a494e98556801fe98a15a367f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:55:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Blox Fruits Chests Auto Farm.lua
@@ -0,0 +1,4 @@
+getgenv().AutoChest = true
+getgenv().ServerHop = false
+getgenv().WaitTime = 35
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nHieu1302/luascript/main/AutoFarmChest.lua"))()
