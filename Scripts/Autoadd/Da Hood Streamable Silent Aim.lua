a68fcc996c258dc20400448ffd4a6c1a9d109bb7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:32:11 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Streamable Silent Aim.lua
@@ -0,0 +1,9 @@
+-- 0.138
+-- 0.11243
+loadstring(game:HttpGet("https://raw.githubusercontent.com/topillesrevenge/Streamable-Silent/main/Main"))()
+DaHoodSettings.Prediction = 0.121
+Aiming.TargetPart = {"Head", "LeftHand", "RightHand", "LeftLowerArm", "RightLowerArm", "LeftUpperArm", "RightUpperArm", "LeftFoot", "LeftLowerLeg", "UpperTorso", "HumanoidRootPart", "LeftUpperLeg", "RightLowerLeg", "RightFoot", "LowerTorso"}
+Aiming.FOV = 50
+Aiming.FOVSides = 25
+Aiming.HitChance = 100
+Aiming.ShowFOV = false
