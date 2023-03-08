a5df60e4fa8e508cd6af60acf9546640b31d7b00
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:38:54 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Notify Auto Pred Air Shot.lua
@@ -0,0 +1,12 @@
+getgenv().Settings = {
+        Enabled = true,
+        Key = "q",
+        Enable = true,
+        AIRSHOT = true,
+        NOTIF = true,
+        AUTOPRED = true,
+  RESOVLER = false,
+        FOV = math.huge
+}
+
+loadstring(game:HttpGet"https://ezki.gq/r/lock")()
