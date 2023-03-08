2e2c119782ad068637c7c63baa41775e39ac7ef3
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:11:57 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:11:52"
+lastupdate = "GMT +1: 08.03.2023 18:11:57"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11292,3 +11292,5 @@ addscriptexist(10758955211, "Skydive Race Clicker: Auto Farm, Auto Rebirths, Aut
 addhub("Ophelia Hub: 8 Games", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ophelia%20Hub%20%20Games.lua")
 
 addscriptexist(3840352284, "Volleyball 4.2: Infinity Stamina", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Volleyball%20%20Infinity%20Stamina.lua")
+
+addscript(8929050629, "Current Game","Mos Academy: Free Items", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mos%20Academy%20Free%20Items.lua")
