5beb47c9c4f45e88da832424961f7b3132715e30
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:59:42 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:59:34"
+lastupdate = "GMT +1: 08.03.2023 19:59:42"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13394,3 +13394,5 @@ addscriptexist(9414511685, "Drive Cars Down A Hill! | Crash Server", "pulled fro
 addscriptexist(183364845, "Speed Run 4 | Fly, AutoFarm, Infinity Jump", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Speed%20Run%20%20%20Fly%20AutoFarm%20Infinity%20Jump.lua")
 
 addscriptexist(4042427666, "Anime Fighting Simulator | Fly, AutoFarm, Auto Quests", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Fighting%20Simulator%20%20Fly%20AutoFarm%20Auto%20Quests.lua")
+
+addscriptexist(2474168535, "Westbound | Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Westbound%20%20Auto%20Farm.lua")
