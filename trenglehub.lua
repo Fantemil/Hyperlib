0877293f8ad3420724bc07b16a5ae96ac55d686a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:40:26 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:40:23"
+lastupdate = "GMT +1: 08.03.2023 06:40:26"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4704,3 +4704,5 @@ addscriptexist(137877687, "RoCitizens: Data game changer", "pulled from rbxscrip
 addscriptexist(155615604, "Prison Life: Summon M9", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prison%20Life%20Summon%20M.lua")
 
 addscriptexist(10277607801, "Apeirophobia: Fullbright, ESP & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apeirophobia%20Fullbright%20ESP%20%20More.lua")
+
+addscriptuniversal("Universal Korblox And Headless", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Korblox%20And%20Headless.lua")
