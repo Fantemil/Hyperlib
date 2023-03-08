cc1a690bed2d0bd5964b4715f53df7fe5bca438a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:34:33 +0100

    Updated automatically!

--- Scripts/Autoadd/Galaxy Hub  Games.lua
+++ Scripts/Autoadd/Galaxy Hub  Games.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/MHD-444/Galaxy-Hub/main/Galaxy-Loader'))()
- 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MHD-444/Galaxy-Hub/main/Galaxy-Loader"))() 
