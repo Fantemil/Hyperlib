298b6f7997f1500e06c67470669a4442bc5bf07f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:57:26 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Spectate Player Aim Viewer.lua
@@ -0,0 +1,5 @@
+--// This was originally made by Nosss and wrongfully stolen by Yukrix !!!
+
+--// Roblox Group ( TeamNosss! ): https://www.roblox.com/groups/16003304/TeamNosss#!/about
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nosssa/NossLock/main/VinGUI"))()
