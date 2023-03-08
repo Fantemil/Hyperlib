e781b588e2580b0cfdbc9d1d5b32276bf5b7fa7a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:00:07 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Combat Warriors Hub.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/WinterDinder/sadness/main/keysystem"), true))()
+
+local Module = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+
+Module.Prompt({ invite = "https://discord.gg/K6JDqbwgc8", name = "56 Clubs" }) -- name is optional
+
+Module.Join("https://discord.gg/K6JDqbwgc8K6JDqbwgc8")
