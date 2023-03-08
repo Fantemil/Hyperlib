c12f2eebf71d2140e5ab8fe345eea140119a2dc5
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:58:17 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:58:13"
+lastupdate = "GMT +1: 08.03.2023 12:58:17"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6880,3 +6880,5 @@ addscriptexist(3652625463, "Lifting Simulator: Auto lift, Auto Sell", "pulled fr
 addscriptexist(9942628622, "Son Goku: The Finale: Unlock SSJB, Unlock SSJG", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Son%20Goku%20The%20Finale%20Unlock%20SSJB%20Unlock%20SSJG.lua")
 
 addscript(6523078987, "Current Game","Slap Battles: Killstreak Edition: Equip tool to Kill All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Slap%20Battles%20Killstreak%20Edition%20Equip%20tool%20to%20Kill%20All.lua")
+
+addscriptexist(6447798030, "Funky Friday: Auto Player", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Funky%20Friday%20Auto%20Player.lua")
