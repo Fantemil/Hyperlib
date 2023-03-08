3f36a5a6ff1c1c8d1c841d525c5ec9db3daf3fa5
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:36:01 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Bubble Gum Simulator Auto Farm Snowflakes.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SomebodyYouDidntMeet/providehub/main/utils/scriptsforfun/bgsautosnowflake.lua"))()
+--do _G.enabled = false to disable
+--TUTORIAL https://youtu.be/fQSCGSLR4HI
