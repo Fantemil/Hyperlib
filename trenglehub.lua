06d2ebb6c9f8e19f29d550df022e9e14a176cb82
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:44:59 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:44:56"
+lastupdate = "GMT +1: 08.03.2023 06:44:59"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4844,3 +4844,5 @@ addscriptuniversal("Universal Aimbot, ESP", "pulled from rbxscript.com", "https:
 addscriptexist(9872472334, "Evade: Infinity Jumps, Auto Farm & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Evade%20Infinity%20Jumps%20Auto%20Farm%20%20More.lua")
 
 addscriptexist(621129760, "Knife Ability Test: Ctrl TP, Teleports, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Knife%20Ability%20Test%20Ctrl%20TP%20Teleports%20ESP.lua")
+
+addscript(2916899287, "Current Game","Blackhawk Rescue Mission 5: Aimbot, Silent Aim & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blackhawk%20Rescue%20Mission%20%20Aimbot%20Silent%20Aim%20%20More.lua")
