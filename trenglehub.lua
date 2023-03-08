7e911e63484c50f30e0b4276db45139ad766e86d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:11:35 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:11:30"
+lastupdate = "GMT +1: 08.03.2023 20:11:35"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13476,3 +13476,5 @@ addscriptexist(9284097280, "Apartment Tycoon | AutoBuy, AutoCollect, Fly", "pull
 addscript(10447882390, "Current Game","Zombie Research Tycoon | AutoCollect, AutoBuy, AutoMine", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Zombie%20Research%20Tycoon%20%20AutoCollect%20AutoBuy%20AutoMine.lua")
 
 addscriptexist(10631181587, "Stone Miner Simulator 2 | Claim All Codes, Claims All login Rewards", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Stone%20Miner%20Simulator%20%20%20Claim%20All%20Codes%20Claims%20All%20login%20Rewards.lua")
+
+addscriptexist(2788229376, "Da Hood | Cash Collect, Fly, Noclip", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20%20Cash%20Collect%20Fly%20Noclip.lua")
