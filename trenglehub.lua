a7305f2d60825ebb39170a33162fbc06e1d4d372
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:56:27 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:56:23"
+lastupdate = "GMT +1: 08.03.2023 14:56:27"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8290,3 +8290,5 @@ addscriptuniversal("Universal RTX version of Roblox", "pulled from rbxscript.com
 addscriptexist(2753915549, "Blox Fruits: Auto Farm Bosses, Auto Stats, Auto Farm Bosses", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Auto%20Farm%20Bosses%20Auto%20Stats%20Auto%20Farm%20Bosses.lua")
 
 addscriptexist(6284583030, "Pet Simulator X: Lucky Blocks Autofarm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Lucky%20Blocks%20Autofarm.lua")
+
+addscriptexist(11563572312, "Ultimate Magnet Simulator: Infinity Money", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultimate%20Magnet%20Simulator%20Infinity%20Money.lua")
