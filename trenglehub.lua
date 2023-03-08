66bc719f80b1f3459dd4ff2c3e409cedcb6a5b6a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:50:09 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:50:05"
+lastupdate = "GMT +1: 08.03.2023 06:50:09"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5006,3 +5006,5 @@ addhub("ReQiuYTPL Hub: 2 Games", "pulled from rbxscript.com", "https://raw.githu
 addscriptexist(286090429, "Arsenal: Silent Aim, Aimbot, Hit Part", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20Silent%20Aim%20Aimbot%20Hit%20Part.lua")
 
 addscript(10106105124, "Current Game","Wheat Farming Simulator: Auto Mega Rebirth, Redeem All Codes, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Wheat%20Farming%20Simulator%20Auto%20Mega%20Rebirth%20Redeem%20All%20Codes%20Auto%20Rebirth.lua")
+
+addscriptexist(2753915549, "Blox Fruits: AutoFarm Level, AutoFarm Selected Quest, AutoFarm Multi Quest", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20AutoFarm%20Level%20AutoFarm%20Selected%20Quest%20AutoFarm%20Multi%20Quest.lua")
