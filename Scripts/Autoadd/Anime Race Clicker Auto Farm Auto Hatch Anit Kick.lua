72d267582d55ea82c64593a5b83a439577e758cc
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:12:07 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Anime Race Clicker Auto Farm Auto Hatch Anit Kick.lua
@@ -0,0 +1,3 @@
+-- Unlock Worlds Before Auto Farming - If you don't do this it will get you kicked if they see you on the leaderboard, you will get backlisted 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LOAKS-HUB/RBLX/main/LOAKS/Loader.lua"))()
