1be92e4d82474364ce8ea52a2938235806b6be5e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 21:16:21 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/RoCitizens AutoFarm Troll.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Empire4946/rocitizens-funny-empifysouth/main/main.lua",true))()
+
+-- made by empire4946
+
+--[[
+if you are going to skid this script atleast give me credit
+]]
