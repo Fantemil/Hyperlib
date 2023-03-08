f2e3050b531484d2dea287471b122dcab40a5690
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:09:56 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:09:52"
+lastupdate = "GMT +1: 08.03.2023 07:09:56"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5608,3 +5608,5 @@ addscriptexist(1537690962, "Bee Swarm Simulator: Auto Farm, Auto Kill Crab, Auto
 addscriptexist(335760407, "TPS: Street Soccer: Auto Goal, Auto Goal Keeper, Auto Shoot", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/TPS%20Street%20Soccer%20Auto%20Goal%20Auto%20Goal%20Keeper%20Auto%20Shoot.lua")
 
 addscriptexist(2788229376, "Da Hood: Auto Farm, Auto Heal & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Auto%20Farm%20Auto%20Heal%20%20More.lua")
+
+addscript(9919640604, "Current Game","Gem Mining Incremental: Infinity Gems", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Gem%20Mining%20Incremental%20Infinity%20Gems.lua")
