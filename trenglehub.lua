2e4ffc1510e5208e15ebc1d6ff7c9fbe71fa5287
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:32:56 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:32:52"
+lastupdate = "GMT +1: 08.03.2023 19:32:56"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13042,3 +13042,5 @@ addscriptexist(855499080, "SKYWARS: Server Lagger", "pulled from rbxscript.com",
 addscriptexist(5732973455, "HOURS: Unlock All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/HOURS%20Unlock%20All.lua")
 
 addscriptexist(2753915549, "Blox Fruits: Auto Farm, AutoSuperhuman", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Auto%20Farm%20AutoSuperhuman.lua")
+
+addscriptexist(10676523834, "Racing Rocket: Auto Clicker, Auto Star & more", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Racing%20Rocket%20Auto%20Clicker%20Auto%20Star%20%20more.lua")
