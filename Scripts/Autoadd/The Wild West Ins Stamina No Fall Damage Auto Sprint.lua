7678ba18347a15d30856cecdb55ab7331a833b74
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:47:55 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/The Wild West Ins Stamina No Fall Damage Auto Sprint.lua
@@ -0,0 +1,15 @@
+local Name = "Wanwood"
+
+for _, instance in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+    if instance:IsA("LocalScript") and instance.Name ~= "ClickDetectorScript" then
+        getgenv().Code1 = getupvalue(getsenv(instance).afflictstatus, 16)
+        getgenv().Code2 = getupvalue(getsenv(instance).afflictstatus, 17)
+    end
+end
+
+for _, player in pairs(game.Players:GetPlayers()) do
+    if string.lower(player.Name) == string.lower(Name) or string.lower(player.DisplayName) == string.lower(Name) then
+        workspace.ServerStuff.dealDamage:FireServer("revolver_shot", { player.Character, math.huge - math.huge }, getgenv().Code1, getgenv().Code2)
+        break
+    end
+end
