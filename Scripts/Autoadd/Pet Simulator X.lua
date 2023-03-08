f253d942ca5525adc372d549d2bbe4637e22e546
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:52:39 +0100

    Updated automatically!

--- Scripts/Autoadd/Pet Simulator X.lua
+++ Scripts/Autoadd/Pet Simulator X.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet'https://raw.githubusercontent.com/RunDTM/ZeeroxHub/main/Loader.lua')()
+loadstring(game:HttpGet("https://gitlab.com/littlekiller2927/deltacore/-/raw/main/deltapsx.lua"))()
