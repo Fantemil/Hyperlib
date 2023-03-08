f01ade76c5ccbca1b831f638d2b6e3e4f5a8ccbc
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:02:24 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:02:21"
+lastupdate = "GMT +1: 08.03.2023 07:02:24"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5376,3 +5376,5 @@ addscriptexist(6125589657, "Touch Football: Infinity Money, Anti Afk, Walkspeed"
 addscript(6170705260, "Current Game","Blending Simulator 2: Auto Farm, Auto Blend", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blending%20Simulator%20%20Auto%20Farm%20Auto%20Blend.lua")
 
 addscriptuniversal("Universal Fly Gui", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Fly%20Gui.lua")
+
+addscriptexist(2753915549, "Blox Fruits: Mob Aura, No stun, Auto buso", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Mob%20Aura%20No%20stun%20Auto%20buso.lua")
