281e195d08262f3c71bab7378680255042ccea77
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:36:16 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Booga Booga REBORN Kill Aura Instant Heal Silent aim.lua
@@ -0,0 +1,7 @@
+--// Supported Games: https://gabescripts.com/supported.php
+--// Discord Server: https://gabescripts.com/discord.php
+--// Get Key: https://gabescripts.com/getkey.php
+--// Youtube Channel: gabescripts
+
+getgenv().Key = "putkeyhere";
+loadstring(game:HttpGet("https://gabescripts.com/access.lua"))()
