e647c809168a87426aeac159af161d65e37b1281
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:30:07 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Dragon Ball Online Generations Auto Collect Dragon Balls.lua
@@ -0,0 +1,14 @@
+Script for level 400: 
+Code:
+wait(2)
+getgenv().ServerHop = true
+getgenv().findballs = true
+loadstring(game:HttpGet('https://pastebin.com/raw/FzF5bU7i', true))()
+
+
+Script for level 100: 
+Code:
+wait(2)
+getgenv().ServerHop = true
+getgenv().findballs = true
+loadstring(game:HttpGet('https://pastebin.com/raw/tj1fa4vW', true))()
