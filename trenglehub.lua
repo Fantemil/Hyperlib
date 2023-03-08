46b0cc4660747a7c36a7b85d0f54f2faf77dd8c0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:34:44 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:34:40"
+lastupdate = "GMT +1: 08.03.2023 14:34:44"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7706,3 +7706,5 @@ addscriptexist(11596351062, "prove mom wrong by being a famous rapper tycoon: An
 addscript(8192066499, "Current Game","Pirate Piece: Break Server, Kill Players, Grab Chests", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pirate%20Piece%20Break%20Server%20Kill%20Players%20Grab%20Chests.lua")
 
 addscriptexist(2753915549, "Blox Fruits: Bring Mobs, Auto Chests, Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Bring%20Mobs%20Auto%20Chests%20Auto%20Farm.lua")
+
+addscriptexist(192800, "Work at a Pizza Place: Auto Farm Money and Cash", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Work%20at%20a%20Pizza%20Place%20Auto%20Farm%20Money%20and%20Cash.lua")
