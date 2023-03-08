a1134405ebe125dbc8622a9ea50efc318e9e33bf
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:53:57 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:53:52"
+lastupdate = "GMT +1: 08.03.2023 14:53:57"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8222,3 +8222,5 @@ addscriptuniversal("Universal Anti Fling", "pulled from rbxscript.com", "https:/
 addscriptexist(4872321990, "Islands: Mob Farming, Boss Farming, Kill aura", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Islands%20Mob%20Farming%20Boss%20Farming%20Kill%20aura.lua")
 
 addscriptexist(9872472334, "Evade: Auto Farm, Fast Revive & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Evade%20Auto%20Farm%20Fast%20Revive%20%20More.lua")
+
+addhub("NauHub Material Icons Kit", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NauHub%20Material%20Icons%20Kit.lua")
