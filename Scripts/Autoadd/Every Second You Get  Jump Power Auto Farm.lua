1690ea4dbe00df7b1d637657033efb8f39ca4ff3
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:10:32 +0100

    Updated automatically!

--- Scripts/Autoadd/Every Second You Get  Jump Power Auto Farm.lua
+++ Scripts/Autoadd/Every Second You Get  Jump Power Auto Farm.lua
@@ -1,15 +1,18 @@
-getgenv().farm = true
-task.spawn(function()
-local pp = game.Players.LocalPlayer.Character.Head
-while task.wait() do
-    if not getgenv().farm then break end
-for i, v in pairs(game:GetService("Workspace").Wins.World10:GetDescendants()) do
-    if v.Name == "TouchInterest" and v.Parent then
-        firetouchinterest(pp, v.Parent, 0)
-        wait(0.1)
-        firetouchinterest(pp, v.Parent, 1)
-        break
-    end
+while wait() do
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10266, 27323.252, 173, 0, 0, -1, -1, 0, 0, 0, 1, 0)
+        wait(12.0)
 end
-end
-end)
+-- World coord wins --
+-- World 1 : -1, 1868.94995, 130, 0, 0, -1, -1, 0, 0, 0, 1, 0
+-- World 2 : 2049, 6205.05029, 131, 0, 0, -1, -1, 0, 0, 0, 1, 0
+-- World 3 : 4049, 8734.84961, 141, 0, 0, -1, -1, 0, 0, 0, 1, 0
+-- World 4 : 6049, 13429.8496, 152, 0, 0, -1, -1, 0, 0, 0, 1, 0
+-- World 5 : 8049, 19189.8516, 161, 0, 0, -1, -1, 0, 0, 0, 1, 0
+-- World 6 : 10266, 27323.252, 173, 0, 0, -1, -1, 0, 0, 0, 1, 0
+
+-- Secret Badge --
+-- Begin : 17.0961304, -114.21283, -49.8958893, 0, 0, -1, -1, 0, 0, 0, 1, 0
+-- End : 17.0000038, -107.300003, 125.900009, -1, 0, -0, 0, 0, -1, 0, -1, -0
+
+-- Teleport --
+-- game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(coord)
