ea45b24f3f4feacaafeffffdea73332f74c0b3d6
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:52:23 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:52:19"
+lastupdate = "GMT +1: 08.03.2023 14:52:23"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8180,3 +8180,5 @@ addscriptexist(9224601490, "Fruit Battlegrounds: Auto Farm, Auto Xp, Attack Targ
 addscriptexist(9224601490, "Fruit Battlegrounds: Auto Farm, Esp, Box Esp", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fruit%20Battlegrounds%20Auto%20Farm%20Esp%20Box%20Esp.lua")
 
 addscriptexist(142823291, "Murder Mystery 2: Auto Farm, Auto Grab, Auto Kill", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Murder%20Mystery%20%20Auto%20Farm%20Auto%20Grab%20Auto%20Kill.lua")
+
+addscript(7663817366, "Current Game","Prison Roleplay: Escape Plan: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prison%20Roleplay%20Escape%20Plan%20Auto%20Farm.lua")
