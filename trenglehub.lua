b616d8c509fe68373dbba2f6054d51e09a790f20
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:43:51 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:43:45"
+lastupdate = "GMT +1: 08.03.2023 17:43:51"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10590,3 +10590,5 @@ addscript(10439295403, "Current Game","Anime Champions: Auto Stats, Teleports, F
 addscript(6076186907, "Current Game","Truck Syndicate: Kick All, Kill All & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Truck%20Syndicate%20Kick%20All%20Kill%20All%20%20More.lua")
 
 addscriptexist(10925589760, "Merge Simulator: Auto Merge Blocks, Auto Upgrade, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Merge%20Simulator%20Auto%20Merge%20Blocks%20Auto%20Upgrade%20Auto%20Rebirth.lua")
+
+addscriptexist(5865858426, "Retail Tycoon 2: Auto Collect Pumpkins", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Retail%20Tycoon%20%20Auto%20Collect%20Pumpkins.lua")
