6fff82ad9444300537e50c4030368a0c49172792
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:39:10 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:39:06"
+lastupdate = "GMT +1: 08.03.2023 20:39:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14206,3 +14206,5 @@ addscriptexist(2537430692, "Jenga [End Game, Moai]", "pulled from rbxscript.com"
 addscriptexist(8737602449, "PLS DONATE [AutoBeg, Webhook]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PLS%20DONATE%20AutoBeg%20Webhook.lua")
 
 addscriptuniversal("Roblox Universal Script [Blue Screen]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Universal%20Script%20Blue%20Screen.lua")
+
+addscriptexist(8054462345, "Michael's Zombies [KillAura]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Michaels%20Zombies%20KillAura.lua")
