489ac4c125d3fd61f4fa3b68112dd1dbde5533da
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:19:00 +0100

    Updated automatically!

--- Scripts/Autoadd/Project Lazarus Inf Ammo.lua
+++ Scripts/Autoadd/Project Lazarus Inf Ammo.lua
@@ -1,34 +1,37 @@
 local old
 old = hookmetamethod(game, "__namecall", newcclosure(function(self,...)
-local args = {...}
-if not checkcaller() and getnamecallmethod() == "FireServer" and tostring(self) == "SendData" then
-return;
-end
-return old(self,...)
+   local args = {...}
+   if not checkcaller() and getnamecallmethod() == "FireServer" and tostring(self) == "SendData" then
+       return;
+   end
+  return old(self,...)
 end))
+
 folder = game:GetService("Players").LocalPlayer.Backpack
+
 for i,v in pairs(folder:GetChildren()) do
-if v:IsA("ModuleScript") then
-local gun = require(v)
-gun.StoredAmmo = math.huge
-gun.Ammo = math.huge
-gun.Damage = {Max = 1500000, Min = 10000}
-gun.Spread = {Min = 0, Max = 0}
-gun.Semi = false
-gun.Pitch = { Min = 0, Max = 0 }
-gun.ViewKick = { Pitch = {Min = 0, Max = 0}, Yaw = {Min = 0, Max = 0} }
+   if v:IsA("ModuleScript") then
+       local gun = require(v)
+       gun.StoredAmmo = math.huge
+       gun.Ammo = math.huge
+       gun.Damage = {Max = 1500000, Min = 10000}
+       gun.Spread = {Min = 0, Max = 0}
+       gun.Semi = false
+       gun.Pitch = { Min = 0, Max = 0 }
+       gun.ViewKick = { Pitch = {Min = 0, Max = 0}, Yaw = {Min = 0, Max = 0} }
+       end
 end
-end
+
 folder.ChildAdded:connect(function(child)
-if child.ClassName == "ModuleScript" then
-task.wait(1)
-local gun = require(child)
-gun.StoredAmmo = math.huge
-gun.Ammo = math.huge
-gun.Damage = {Max = math.huge, Min = math.huge}
-gun.Spread = {Min = 0, Max = 0}
-gun.Semi = false
-gun.Pitch = { Min = 0, Max = 0 }
-gun.ViewKick = { Pitch = {Min = 0, Max = 0}, Yaw = {Min = 0, Max = 0} }
-end
+  if child.ClassName == "ModuleScript" then
+      task.wait(1)
+       local gun = require(child)
+       gun.StoredAmmo = math.huge
+       gun.Ammo = math.huge
+       gun.Damage = {Max = math.huge, Min = math.huge}
+       gun.Spread = {Min = 0, Max = 0}
+       gun.Semi = false
+       gun.Pitch = { Min = 0, Max = 0 }
+       gun.ViewKick = { Pitch = {Min = 0, Max = 0}, Yaw = {Min = 0, Max = 0} }
+      end
 end)
