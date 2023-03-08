e0b075ca3a7e4f04799a1cd2cbf499555e867a2b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:50:08 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Stands Awakening Auto Farm.lua
@@ -0,0 +1,26 @@
+getgenv().items = {
+    ["AJA Mask"] = false,
+    ["Arrow"] = false,
+    ["Banknote"] = true,
+    ["Bone"] = true,
+    ["Camera"] = true,
+    ["DIO's Diary"] = false,
+    ["DIO's Skull"] = true,
+    ["Ender Pearl"] = false,
+    ["Frog"] = false,
+    ["Hell Arrow"] = false,
+    ["Ornstein's Spear"] = true,
+    ["Pot Platinum Diary"] = true,
+    ["Red Heart"] = true,
+    ["Requiem Arrow"] = false,
+    ["Rokakaka Fruit"] = false,
+    ["Samurai Diary"] = false,
+    ["Toxic Chemicals"] = false,
+    ["True Requiem Arrow"] = false,
+    ["Uncanny Key"] = true,
+    ["Uncanny Pumpkin"] = true,
+    ["Vampire Mask"] = false,
+    ["Volcanic Rock"] = false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tiyixif29josda/StandAwakening/e0297281cf3deb4c745f26c3e41955e743d81405/Afk%20Farm"))()
