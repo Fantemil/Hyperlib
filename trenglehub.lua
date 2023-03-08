de10921742f51dd460a53cc5207b21031dfea964
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:13:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:13:43"
+lastupdate = "GMT +1: 08.03.2023 20:13:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13538,3 +13538,5 @@ addscriptexist(4924922222, "Brookhaven RP | Teleports", "pulled from rbxscript.c
 addscriptexist(3956818381, "Ninja Legends | AutoSwing, AutoSell, Unlock All Elements", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ninja%20Legends%20%20AutoSwing%20AutoSell%20Unlock%20All%20Elements.lua")
 
 addscriptexist(286090429, "Arsenal | Hitbox Expander, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20%20Hitbox%20Expander%20ESP.lua")
+
+addscriptexist(155615604, "Prison Life | Btools, Fly, Walkspeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prison%20Life%20%20Btools%20Fly%20Walkspeed.lua")
