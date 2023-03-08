b41ccfdb0ec2634d1ce2163ceb3dbd0ce3e9de10
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:27:31 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Speed Simulator Speed.lua
@@ -0,0 +1,7 @@
+x = 99999999999 -- your own number
+local ohNumber1 = x
+local ohNumber2 = x
+game:GetService("ReplicatedStorage").GlobalFunctions.AddPlayerSpeed:FireServer(ohNumber1, ohNumber2)
+if x == x then
+game:GetService("ReplicatedStorage").GlobalFunctions.RebirthPlayerCallFromClient:FireServer()
+end 
