c1f08b8719cb189e1e78b4ae28b6e332410ccce8
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:08:31 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:08:27"
+lastupdate = "GMT +1: 08.03.2023 19:08:31"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12512,3 +12512,5 @@ addscriptexist(7180042682, "Military Tycoon: Auto Complete Event", "pulled from 
 addscriptexist(1401417393, "Ragdoll System Test: Godmode, No Cooldown & more", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ragdoll%20System%20Test%20Godmode%20No%20Cooldown%20%20more.lua")
 
 addscriptexist(9848789324, "Ragdoll Engine: AntiFling, Teleports & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ragdoll%20Engine%20AntiFling%20Teleports%20%20More.lua")
+
+addscriptexist(2569453732, "Rise of Nations: Esp, Auto Determines, Clear Alerts", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rise%20of%20Nations%20Esp%20Auto%20Determines%20Clear%20Alerts.lua")
