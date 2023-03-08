6b7a56418c68167b0b79184b47f9c061af575174
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:46:51 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Roblox but every second you become stronger WalkSpeed Jump Power.lua
@@ -0,0 +1,5 @@
+local SpeedValue = 300
+local JumpValue = 300
+
+game:GetService("ReplicatedStorage").JumpChanger:FireServer(JumpValue)
+game:GetService("ReplicatedStorage").SpeedChanger:FireServer(SpeedValue)
