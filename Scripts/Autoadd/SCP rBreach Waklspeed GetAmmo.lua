cf40d317cb313e9018fc14663bb46bc1078c267c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:50:57 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/SCP rBreach Waklspeed GetAmmo.lua
@@ -0,0 +1,3 @@
+-- Read Changelogs on the GitHub post https://github.com/502Development/502Lua/blob/main/games/rBreach.lua
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/502Development/502Lua/main/games/rBreach.lua'))()
