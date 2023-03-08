776504e5c65d3a9b8c5560840c0ba93382a7bf40
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:46:09 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:46:05"
+lastupdate = "GMT +1: 08.03.2023 17:46:09"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10650,3 +10650,5 @@ addscriptexist(6913960180, "Undertale Crazy Multiverse Timeline: Custom Characte
 addscriptexist(6516141723, "DOORS: Spawn Rick Astley", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Spawn%20Rick%20Astley.lua")
 
 addscriptexist(2753915549, "Blox Fruits: Auto Farm, Teleports & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Auto%20Farm%20Teleports%20%20More.lua")
+
+addscriptexist(306964494, "Loomian Legacy: Auto Disc Drop, Auto Heal & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Loomian%20Legacy%20Auto%20Disc%20Drop%20Auto%20Heal%20%20More.lua")
