a8294dd57b3441e92f7ecf1fb89a8d4aba3b6bdd
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:42:26 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:42:22"
+lastupdate = "GMT +1: 08.03.2023 12:42:26"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6472,3 +6472,5 @@ addscriptexist(6918802270, "Project New World: Infinite Dash, Quest Farm, Infini
 addscriptexist(5865858426, "Retail Tycoon 2: Christmas Event Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Retail%20Tycoon%20%20Christmas%20Event%20Farm.lua")
 
 addscriptexist(6918802270, "Project New World: Auto Attack, Auto Quest, KillAura", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20New%20World%20Auto%20Attack%20Auto%20Quest%20KillAura.lua")
+
+addscriptexist(11197013890, "Merge Slash Tycoon: Auto Collect Money, Auto Spint, Infinity Money", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Merge%20Slash%20Tycoon%20Auto%20Collect%20Money%20Auto%20Spint%20Infinity%20Money.lua")
