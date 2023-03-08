78781f73f38974a4f20355f93f8e763f13933d52
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:35:16 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Driving Empire Presents Auto Farm.lua
@@ -0,0 +1,3 @@
+---- Use with Bike not by me noob
+---- not supported For KRNL Use Evon,Flux,Yub V Or Other Paid Executor  
+loadstring(game:HttpGetAsync'https://raw.githubusercontent.com/GXNATION/Main-script/main/de%20event')();
