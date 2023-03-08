3756ffb9eff8b22daf281ba4215f9b6a2d5e1ed8
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:12:49 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:12:44"
+lastupdate = "GMT +1: 08.03.2023 19:12:49"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12604,3 +12604,5 @@ addscriptexist(10861112990, "Anime Artifacts Simulator 2: Kill Aura For Bosses",
 addscriptexist(10861112990, "Anime Artifacts Simulator 2: Kill Aura For Normal Dungeons", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Artifacts%20Simulator%20%20Kill%20Aura%20For%20Normal%20Dungeons.lua")
 
 addscriptexist(10861112990, "Anime Artifacts Simulator 2: Kill Aura For Event Dungeons", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Artifacts%20Simulator%20%20Kill%20Aura%20For%20Event%20Dungeons.lua")
+
+addscriptexist(6284583030, "Pet Simulator X: Auto Farm, Teleports, Fun & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Auto%20Farm%20Teleports%20Fun%20%20More.lua")
