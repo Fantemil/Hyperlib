0cb1e15b1f3d87bd32d58440b4364cb4b0b84088
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 21:16:23 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 21:16:17"
+lastupdate = "GMT +1: 08.03.2023 21:16:23"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14928,3 +14928,5 @@ addscriptexist(3101667897, "Legends Of Speed⚡[Auto Farm, Auto Rebirth]", "pull
 addscript(9940051005, "Current Game","Blox Legends [AutoCollect]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Legends%20AutoCollect.lua")
 
 addscript(2817130270, "Current Game","Digging Simulator [Inf Bag, Inf Money]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Digging%20Simulator%20Inf%20Bag%20Inf%20Money.lua")
+
+addscriptexist(137877687, "RoCitizens [AutoFarm, Troll]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RoCitizens%20AutoFarm%20Troll.lua")
