863c53d401d840e72fe02a69192e2fb473dbfec9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:44:35 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:44:30"
+lastupdate = "GMT +1: 08.03.2023 20:44:35"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14338,3 +14338,5 @@ addscriptexist(286090429, "Arsenal [No Spread, Aimbot]", "pulled from rbxscript.
 addscriptexist(662417684, "LUCKY BLOCKS Battlegrounds [Open Any LuckyBlock]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LUCKY%20BLOCKS%20Battlegrounds%20Open%20Any%20LuckyBlock.lua")
 
 addscriptexist(8884334497, "Mining Clicker Simulator [AutoRebirth, AutoClick]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Clicker%20Simulator%20AutoRebirth%20AutoClick.lua")
+
+addscriptexist(2537430692, "Jenga [Stop Game]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jenga%20Stop%20Game.lua")
