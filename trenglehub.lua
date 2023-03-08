9fc59fb55c33f28ad264056a45f0c4100b4b9027
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:40:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:40:28"
+lastupdate = "GMT +1: 08.03.2023 12:40:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6420,3 +6420,5 @@ addscriptuniversal("Platinium Hub: 10+ Games Support", "pulled from rbxscript.co
 addscriptexist(443406476, "Project Lazarus: Teleports, Aimbot, Fly", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Lazarus%20Teleports%20Aimbot%20Fly.lua")
 
 addscriptexist(8750997647, "Tapping Legends X: Auto Tap, Auto Rebirth, Auto Hatch", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tapping%20Legends%20X%20Auto%20Tap%20Auto%20Rebirth%20Auto%20Hatch.lua")
+
+addscript(4787647409, "Current Game","Blacksite Zeta: Anti Cheat Bypass", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blacksite%20Zeta%20Anti%20Cheat%20Bypass.lua")
