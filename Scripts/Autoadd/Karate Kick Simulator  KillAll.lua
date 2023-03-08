ead38675c2c692255e47e3cb4cd202f5822b3066
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:11:41 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Karate Kick Simulator  KillAll.lua
@@ -0,0 +1,17 @@
+--set to false if you want to stop loop killing everyone
+
+_G.KillAll = true
+
+while _G.KillAll do task.wait(.1)
+    for i,v in pairs(game.Players:GetPlayers()) do
+        if v.Character:FindFirstChild("LeftHand") then
+            local args = {
+                [1] = v.Character,
+                [2] = v.Character.LeftHand,
+                [3] = Vector3.new(0,0,0)
+            }
+            
+            game:GetService("ReplicatedStorage").REvents.OnHit:FireServer(unpack(args))
+        end
+    end
+end
