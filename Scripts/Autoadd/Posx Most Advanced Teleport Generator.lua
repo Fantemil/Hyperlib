604b202b1006f06670e55b047e47e72a65ce5301
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:12:19 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Posx Most Advanced Teleport Generator.lua
@@ -0,0 +1,3 @@
+-- all information here: https://raw.githubusercontent.com/juywvm/-Roblox-Projects-/main/posX/intro.raw
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/juywvm/-Roblox-Projects-/main/posX/loader.lua'),true))() 
