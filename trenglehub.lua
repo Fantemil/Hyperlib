d512bbcd6cdde1012cca75ceccd52ac9204e0dc1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:51:37 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:51:34"
+lastupdate = "GMT +1: 08.03.2023 14:51:37"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8162,3 +8162,5 @@ addscriptexist(147848991, "Be A Parkour Ninja: Hitbox Expander", "pulled from rb
 addscriptexist(537413528, "Build A Boat For Treasure: Auto Farm, Auto Buy, Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Farm%20Auto%20Buy%20Teleports.lua")
 
 addhub("EmptyNess Hub: 15+ Games", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/EmptyNess%20Hub%20%20Games.lua")
+
+addscriptexist(286090429, "Arsenal: ESP, Aimbot, Mods", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20ESP%20Aimbot%20Mods.lua")
