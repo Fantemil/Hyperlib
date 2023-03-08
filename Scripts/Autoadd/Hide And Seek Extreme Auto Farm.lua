b3eb0cbf4e5c18947f76f057ba6955f1155446b3
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:12:48 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Hide And Seek Extreme Auto Farm.lua
@@ -0,0 +1,4 @@
+_G.Enabled = true --turn on/off the autofarm
+SafePlatform = true -- this will teleport you ontop of a platform to stay afk
+WAIT = 5 --the time it waits to collect coins and to teleport you to the safe place
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FOXTROXHACKS/Roblox-Scripts/main/Hide-and-Seek-AF"))()
