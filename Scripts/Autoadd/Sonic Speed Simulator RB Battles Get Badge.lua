15a5995bbb704808605df774efece3afaefd06c3
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:57:50 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Sonic Speed Simulator RB Battles Get Badge.lua
@@ -0,0 +1,2 @@
+--[[MUST BE IN THE RBB SEASON 3 MAP IT WILL NOT WORK IF YOU ARE NOT IN THE MAP]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/arjanQ/scripts/main/SSSRbbS3GetBadge",true))()
