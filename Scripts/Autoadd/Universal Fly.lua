b185fad9dacbd7a209505bfd5a605abdf83ac08d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:58:24 +0100

    Updated automatically!

--- Scripts/Autoadd/Universal Fly.lua
+++ Scripts/Autoadd/Universal Fly.lua
@@ -1,1 +1,72 @@
-loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+-- FE SELF FLING REAL
+-- used for trolling example: bang someone and when they turn around and look at you, FLING YOURSELF
+-- hotkey dont have to be capitals lol
+-- press e to fling urself and q to go back where u previously were
+
+local Config = {
+    FlingHotkey = "E",
+    TpBackHotkey = "Q",
+    Permanent = false
+}
+
+local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
+
+local Player = Players.LocalPlayer
+
+local Character = Player.Character
+local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+local RootPart = Humanoid.RootPart
+
+for _, x in next, Config do
+    if type(x) == "string" then
+        Config[_] = x:upper()
+    end
+end
+
+local CurrentPos
+local Connection
+
+Connection = UserInputService.InputBegan:Connect(function(Key, Typing)
+    if not Typing then
+        if Key.KeyCode == Enum.KeyCode[Config.FlingHotkey] then
+            if Config.Permanent then
+                if Player.Character.Humanoid.RootPart.Velocity.Magnitude <= 20 then
+                    CurrentPos = Player.Character.Humanoid.RootPart.CFrame
+                end
+                Player.Character.Humanoid.RootPart.Velocity = (Player.Character.Humanoid.RootPart.Velocity + Vector3.new(0, 50, 0)) + (Player.Character.Humanoid.RootPart.CFrame.LookVector * 100)
+                Player.Character.Humanoid.RootPart.RotVelocity = (Player.Character.Humanoid.RootPart.RotVelocity + Vector3.new(math.random(200, 300), math.random(-200, 300), math.random(200, 300))) 
+            else
+                if RootPart.Velocity.Magnitude <= 20 then
+                    CurrentPos = RootPart.CFrame
+                end
+                RootPart.Velocity = (RootPart.Velocity + Vector3.new(0, 50, 0)) + (RootPart.CFrame.LookVector * 100)
+                RootPart.RotVelocity = (RootPart.RotVelocity + Vector3.new(math.random(200, 300), math.random(-200, 300), math.random(200, 300)))
+            end
+        elseif Key.KeyCode == Enum.KeyCode[Config.TpBackHotkey] then
+            if CurrentPos then
+                if Config.Permanent then
+                    if Player.Character.Humanoid.RootPart.Velocity.Magnitude >= 20 then
+                        Player.Character.Humanoid.RootPart.Velocity = Vector3.new()
+                        Player.Character.Humanoid.RootPart.RotVelocity = Vector3.new()
+                        Player.Character.Humanoid.RootPart.CFrame = CurrentPos
+                        Player.Character.Humanoid:ChangeState(Enum.HumanoidStateType.GettingUp)
+                    end
+                else
+                    if RootPart.Velocity.Magnitude >= 20 then
+                        RootPart.Velocity = Vector3.new()
+                        RootPart.RotVelocity = Vector3.new()
+                        RootPart.CFrame = CurrentPos
+                        Humanoid:ChangeState(Enum.HumanoidStateType.GettingUp)
+                    end
+                end
+            end
+        end
+    end
+end)
+
+if not Config.Permanent then
+    Humanoid.Died:Connect(function()
+        Connection:Disconnect()
+    end)
+end
