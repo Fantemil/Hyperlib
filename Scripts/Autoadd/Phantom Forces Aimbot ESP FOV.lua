8d23230969ca140326a5b6fc58278ccc587b7183
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:44:12 +0100

    Updated automatically!

--- Scripts/Autoadd/Phantom Forces Aimbot ESP FOV.lua
+++ Scripts/Autoadd/Phantom Forces Aimbot ESP FOV.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/rblxscriptsnet/scripts/main/phantomforces1'),true))()
+_G.key = "novembr" --the key is between the two " "
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NukeVsCity/TheALLHACKLoader/main/NukeLoader"))()
+
+
+--Key for the manual loader is "      novembr      "
