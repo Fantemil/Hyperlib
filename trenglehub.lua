f1f79e84b510f69032c4e77b4509cda733c1891c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:27:01 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:26:56"
+lastupdate = "GMT +1: 08.03.2023 19:27:01"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12908,3 +12908,5 @@ addscriptexist(4282985734, "Combat Warriors: Legit Anti Cheat & Auto Parry", "pu
 addscriptexist(9285238704, "Race Clicker: Auto Win, Auto Farm, Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Race%20Clicker%20Auto%20Win%20Auto%20Farm%20Teleports.lua")
 
 addscriptexist(537413528, "Build A Boat For Treasure: Autofarm Chest, Walkspeed, Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Autofarm%20Chest%20Walkspeed%20Teleports.lua")
+
+addscript(10371228324, "Current Game","Camp Bonkers: Infinity Coins", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Camp%20Bonkers%20Infinity%20Coins.lua")
