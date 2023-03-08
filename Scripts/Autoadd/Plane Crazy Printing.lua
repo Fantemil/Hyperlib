c583dc50906b4d5acd2825ead1d74cae10358c1f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:58:19 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Plane Crazy Printing.lua
@@ -0,0 +1,12 @@
+--[[
+    ZEPHYR X
+ 
+
+    Scripting - SYL#0011
+
+    LOADER
+
+]]
+pcall(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ECLIPSEXHUB/ECLIPSE-X/main/ECLIPSE%20X.txt", true))()
+end)
