64fc9df65b0266480f9d3671d5d05abb99794199
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:31:26 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arsenal Press F to lock your Aim.lua
@@ -0,0 +1,17 @@
+getgenv().RecurringPoint = "Head"
+getgenv().Hitbox = "Head"
+getgenv().Keybind = "f"
+getgenv().AimbotStrengthAmount = 0.0289
+getgenv().PredictionAmount = 10
+getgenv().Radius = 27
+getgenv().UsePrediction = true
+getgenv().AimbotStrength = true
+getgenv().FirstPerson = true
+getgenv().ThirdPerson = true
+getgenv().TeamCheck = false
+getgenv().Enabled = true
+
+
+-- // main script use with silent aim / / -- 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tenaaki/GenericAimbot/main/v1.0.0"))() 
