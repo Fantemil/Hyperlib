7278b38faeb2e91acaf28cfa5261d81e37b19ef8
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:00:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:00:47"
+lastupdate = "GMT +1: 08.03.2023 20:00:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13420,3 +13420,5 @@ addscript(6289365295, "Current Game","Sky's Difficulty Chart Obby | AutoComplete
 addscriptexist(9993529229, "Counter Blox: Remastered | Esp, Rage Bot, Auto Wall", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Counter%20Blox%20Remastered%20%20Esp%20Rage%20Bot%20Auto%20Wall.lua")
 
 addscriptexist(155615604, "Prison Life | Gun Mode, Give Gun, Walkspeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prison%20Life%20%20Gun%20Mode%20Give%20Gun%20Walkspeed.lua")
+
+addscriptexist(8508303900, "Tower of Flood Escape 2 | Free Open Case.", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20of%20Flood%20Escape%20%20%20Free%20Open%20Case.lua")
