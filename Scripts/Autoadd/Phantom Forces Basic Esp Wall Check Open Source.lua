c9640289cff302bb40ca19155c64231862383fff
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:43:49 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Phantom Forces Basic Esp Wall Check Open Source.lua
@@ -0,0 +1,6 @@
+-- // ESP CONFIGURATION \\ --
+_G.sex = true --> true: ESP Enabled | false: ESP Disabled (Use the switch key)
+_G.switchKey = Enum.KeyCode.RightShift --> List: https://create.roblox.com/docs/reference/engine/enums/KeyCode
+
+-- // DONT TOUCH \\ --
+loadstring(game:HttpGet('https://raw.githubusercontent.com/imvipp/roblox/main/shitware/phantomforces/basic_esp.lua'))()
