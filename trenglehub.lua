13a5a601678cb557e45d0290895b4686b8d9e4e8
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:54:07 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:54:00"
+lastupdate = "GMT +1: 08.03.2023 14:54:07"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8226,3 +8226,5 @@ addscriptexist(9872472334, "Evade: Auto Farm, Fast Revive & More", "pulled from 
 addhub("NauHub Material Icons Kit", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NauHub%20Material%20Icons%20Kit.lua")
 
 addscript(11513339030, "Current Game","Jump Higher Per Second: Auto Farm, Walkspeed, BTools", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jump%20Higher%20Per%20Second%20Auto%20Farm%20Walkspeed%20BTools.lua")
+
+addscriptuniversal("Universal FE PNG Loader", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20FE%20PNG%20Loader.lua")
