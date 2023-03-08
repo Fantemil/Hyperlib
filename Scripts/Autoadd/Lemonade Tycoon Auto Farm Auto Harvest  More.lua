8a31c0ef75b85092b6765a9d6681ec7abcaad511
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:49:55 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Lemonade Tycoon Auto Farm Auto Harvest  More.lua
@@ -0,0 +1,3 @@
+-- Showcase: https://youtu.be/4ry26-mzZhw
+getgenv().EnabledInvite = false -- set true for enable discord prompt
+loadstring(game:HttpGet("https://raw.githubusercontent.com/itsnoctural/Destiny/main/loader.lua",true))()
