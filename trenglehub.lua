c80c20f4a3ca7f5d51a6f094520255efc64eeab1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:55:54 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:55:48"
+lastupdate = "GMT +1: 08.03.2023 19:55:54"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13314,3 +13314,5 @@ addhub("UnnamedHub | 3 Games", "pulled from rbxscript.com", "https://raw.githubu
 addscriptexist(3823781113, "Saber Simulator | GUI - Auto Farming, Auto Upgrade & More!", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Saber%20Simulator%20%20GUI%20%20Auto%20Farming%20Auto%20Upgrade%20%20More.lua")
 
 addscriptexist(155615604, "Prison Life | Get All Guns, Auto Get Key Card, Super Punch", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prison%20Life%20%20Get%20All%20Guns%20Auto%20Get%20Key%20Card%20Super%20Punch.lua")
+
+addscriptexist(2066381781, "Deadly Sins Retribution | Fast Auto Spin For Magic", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Deadly%20Sins%20Retribution%20%20Fast%20Auto%20Spin%20For%20Magic.lua")
