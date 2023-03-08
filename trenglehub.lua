6644e4e93840f2bf59462141368c1683b3231b8a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:30:46 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:30:43"
+lastupdate = "GMT +1: 08.03.2023 06:30:46"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4557,3 +4557,5 @@ addscriptexist(2474168535, "Westbound: Silent Aim, Gun Mod", "pulled from rbxscr
 
 
 addscriptexist(9601565001, "Car Crash System: Anti Afk, Trolling, Fly", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Car%20Crash%20System%20Anti%20Afk%20Trolling%20Fly.lua")
+
+addhub("DevHub: 6 Games", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DevHub%20%20Games.lua")
