0b922cdc2371d91d53660f030b777f2bf15900f4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:00:57 +0100

    Updated automatically!

--- Scripts/Autoadd/One Blocks Every Second Auto Build Auto Rebirth.lua
+++ Scripts/Autoadd/One Blocks Every Second Auto Build Auto Rebirth.lua
@@ -1,1 +1,35 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/rblxscriptsnet/scripts/main/%2B1%20Blocks%20Every%20Second'),true))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/0x"))()
+local window = library:Window("+1 Blocks Every Second")
+window:Toggle(
+    "Auto Build",
+    "Auto Build",
+    false,
+    function(t)
+        _G.AutoFarm = t
+        while _G.AutoFarm do
+            wait()
+            pcall(
+                function()
+                    game:GetService("ReplicatedStorage").Remotes.PlaceBlock:FireServer()
+                end
+            )
+        end
+    end
+)
+window:Toggle(
+    "Auto Rebirth",
+    "Auto Rebirth",
+    false,
+    function(t)
+        _G.Auto = t
+        while _G.Auto do
+            wait()
+            pcall(
+                function()
+                    game:GetService("ReplicatedStorage").Remotes.Rebirth:InvokeServer("RebirthShop")
+                end
+            )
+        end
+    end
+)
+window:Label("YT: Tora IsMe")
