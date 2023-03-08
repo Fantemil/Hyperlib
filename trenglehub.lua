4b69ec98966a644db2a2ac816b292f62701cd81e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:38:06 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:38:02"
+lastupdate = "GMT +1: 08.03.2023 14:38:06"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7794,3 +7794,5 @@ addscriptuniversal("Universal Spinbot, BHop, Options", "pulled from rbxscript.co
 addscriptuniversal("PistolaBollaHUB: Ice Hub, Ghost Hub, V Hub", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PistolaBollaHUB%20Ice%20Hub%20Ghost%20Hub%20V%20Hub.lua")
 
 addscriptexist(3351674303, "Driving Empire: Auto Farm, Speed Hack, Bypass", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Driving%20Empire%20Auto%20Farm%20Speed%20Hack%20Bypass.lua")
+
+addscriptexist(5796917097, "Road to Gramby's: Grab bucks, Godmode, TP to temple", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Road%20to%20Grambys%20Grab%20bucks%20Godmode%20TP%20to%20temple.lua")
