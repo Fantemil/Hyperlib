fb963e7a38b7a30ad2df7c0117a23397a70343b0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:23:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:23:10"
+lastupdate = "GMT +1: 08.03.2023 20:23:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13790,3 +13790,5 @@ addscript(4556951723, "Current Game","Ragdoll Physics | Free Badge", "pulled fro
 addscriptexist(9133513813, "Hunted Hood | AutoSteal", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hunted%20Hood%20%20AutoSteal.lua")
 
 addscriptexist(6753004901, "Swing City | Unlock All Skills, Farm Money, Instant Quests", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Swing%20City%20%20Unlock%20All%20Skills%20Farm%20Money%20Instant%20Quests.lua")
+
+addscriptuniversal("Roblox Universal Script | First Person", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Universal%20Script%20%20First%20Person.lua")
