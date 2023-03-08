e7ca8c94d62664534e460958e48b8566a8e50dbb
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:57:46 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:57:42"
+lastupdate = "GMT +1: 08.03.2023 17:57:46"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10940,3 +10940,5 @@ addscriptexist(292439477, "Phantom Forces: Enable Esp", "pulled from rbxscript.c
 addscriptexist(2788229376, "Da Hood: Fly, Anti Lock, Auto Farms", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Fly%20Anti%20Lock%20Auto%20Farms.lua")
 
 addscriptexist(4991214437, "town: ESP, Aimbot, Plot Stealer", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/town%20ESP%20Aimbot%20Plot%20Stealer.lua")
+
+addscriptuniversal("Universal AntiHook Function", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20AntiHook%20Function.lua")
