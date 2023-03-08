c54e42696323ad8194c6c5c68fe124fd02ddb780
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:09:20 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Easy Jump Clicker Universal Auto Farm For All Stats.lua
@@ -0,0 +1,53 @@
+-- Enable/Disable
+local RebirthE = true
+local JumpPower = true
+-- Services
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+-- Locals/Assets
+local LocalPlayer = Players.LocalPlayer
+local Rebirth = LocalPlayer.PlayerGui.MainGui.Frame.Rebirth.Frame.Rebirth
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+local Stages = Workspace:FindFirstChild("Stages")
+-- Functions
+local function OnCharacterAdded(NewCharacter)
+Character = NewCharacter
+HumanoidRootPart = NewCharacter:WaitForChild("HumanoidRootPart")
+end
+-- Connections
+LocalPlayer.CharacterAdded:Connect(OnCharacterAdded)
+-- Threads
+-- Farm Wins
+task.spawn(function()
+while true do
+task.wait(2)
+if HumanoidRootPart then
+for _,v in pairs(Stages:GetDescendants()) do
+if v:IsA("TouchTransmitter") then
+firetouchinterest(v.Parent, HumanoidRootPart, 0)
+firetouchinterest(v.Parent, HumanoidRootPart, 1)
+wait(.5)
+end
+end
+end
+end
+end)
+--Farm Rebirths
+task.spawn(function()
+while true do
+task.wait()
+if RebirthE then
+game:GetService("ReplicatedStorage").CommonEvents.Rebirth:FireServer()
+end
+end
+end)
+--Farm Jump Power
+task.spawn(function()
+while true do
+task.wait()
+if JumpPower then
+game:GetService("ReplicatedStorage").CommonEvents.Clicking:FireServer()
+end
+end
+end)
