8d6862dce9a27d4dce7e7f78269a2a5b171adcb4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:05:38 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/ABA Fastest Afk Gold Farm.lua
@@ -0,0 +1,3 @@
+getgenv().entertimer = 45 -- hops until the timer is under this number 45-80 recommended
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Skrapisismyscript/Solo_Hub/main/Loader"))() 
