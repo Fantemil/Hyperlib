920bfff92fd596547287e4513a62e94bc8907d18
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:43:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Mortem Metallum BETA Get every weapon.lua
@@ -0,0 +1,967 @@
+local args = {
+    [1] = 1,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 2,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 3,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 4,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 5,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 6,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 7,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 8,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 9,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 10,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 11,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 13,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 14,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 15,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 16,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 17,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 18,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 19,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 20,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 21,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 22,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 23,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 24,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 25,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 26,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 28,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 29,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 30,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 31,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 32,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 33,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 34,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 35,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 36,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 37,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 38,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 39,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 40,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 41,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 42,
+    [2] = "0:0:0:0"
+}
+
+local args = {
+    [1] = 1,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 2,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 3,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 4,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 5,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 6,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 7,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 8,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 9,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 10,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 11,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 13,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 14,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 15,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 16,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 17,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 18,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 19,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 20,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 21,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 22,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 23,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 24,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 25,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 26,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 28,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 29,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 30,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 31,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 32,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 33,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 34,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 35,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 36,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 37,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 38,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 39,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 40,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 41,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 42,
+    [2] = "0:0:0:0"
+}
+
+local args = {
+    [1] = 1,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 2,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 3,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 4,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 5,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 6,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 7,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 8,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 9,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 10,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 11,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 13,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 14,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 15,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 16,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 17,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 18,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 19,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 20,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 21,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 22,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 23,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 24,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 25,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 26,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 28,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 29,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 30,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 31,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 32,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 33,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 34,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 35,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 36,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 37,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 38,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 39,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 40,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 41,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 42,
+    [2] = "0:0:0:0"
+}
+
+local args = {
+    [1] = 1,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 2,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 3,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+
+local args = {
+    [1] = 4,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 5,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 6,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 7,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 8,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 9,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 10,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 11,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 13,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 14,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 15,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 16,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 17,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 18,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 19,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 20,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 21,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 22,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 23,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 24,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 25,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 26,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 28,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 29,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 30,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 31,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 32,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 33,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 34,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 35,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 36,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 37,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 38,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 39,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 40,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 41,
+    [2] = "0:0:0:0"
+}
+
+game:GetService("ReplicatedStorage").Item:FireServer(unpack(args))
+local args = {
+    [1] = 42,
+    [2] = "0:0:0:0"
+}
