58710535af064634926fb88873a872153eaf7725
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:48:23 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:48:11"
+lastupdate = "GMT +1: 08.03.2023 17:48:23"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10702,3 +10702,5 @@ addscriptexist(10070062081, "Minion Simulator: Auto Collect, Auto Farm, Multi Fa
 addscript(8534845015, "Current Game","Sakura Stand: Tp to boxes ban bypass", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sakura%20Stand%20Tp%20to%20boxes%20ban%20bypass.lua")
 
 addscriptexist(9880062154, "The Last Strandeds: Kill Player", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Last%20Strandeds%20Kill%20Player.lua")
+
+addscriptexist(8084034728, "Roblox but every second you become stronger: 10 Gems gift", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20but%20every%20second%20you%20become%20stronger%20%20Gems%20gift.lua")
