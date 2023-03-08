df5941cbd5449cb5eb81edf0c62fc7da2b0e0b4d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:27:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:27:28"
+lastupdate = "GMT +1: 08.03.2023 18:27:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11676,3 +11676,5 @@ addscriptexist(9941633436, "Rank Simulator: Infinty Gems, Auto Click", "pulled f
 addscriptexist(537413528, "Build A Boat For Treasure: Auto Farm, Gravity & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Farm%20Gravity%20%20More.lua")
 
 addscript(2580982329, "Current Game","Texting Simulator: Auto Text, Auto Redeem All Codes & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Texting%20Simulator%20Auto%20Text%20Auto%20Redeem%20All%20Codes%20%20More.lua")
+
+addscriptexist(7026828578, "Speed Simulator: Speed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Speed%20Simulator%20Speed.lua")
