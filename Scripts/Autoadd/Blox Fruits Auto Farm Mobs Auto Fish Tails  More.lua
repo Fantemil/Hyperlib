219cda008c4de99b675c7de3fb8f0d7214a4f723
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:29:42 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Blox Fruits Auto Farm Mobs Auto Fish Tails  More.lua
@@ -0,0 +1,3 @@
+_G.Color = Color3.fromRGB(0, 255, 255)
+_G.Toggle = Enum.KeyCode.RightControl
+loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua")()
