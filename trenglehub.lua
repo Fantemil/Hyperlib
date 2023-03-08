6586b19be0d413e264a8cdf105f324b08e8ad35a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:29:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:29:40"
+lastupdate = "GMT +1: 08.03.2023 12:29:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6120,3 +6120,5 @@ addhub("Universal VHub", "pulled from rbxscript.com", "https://raw.githubusercon
 addscriptexist(2753915549, "Blox Fruits: Mob Aura, Auto get all items, Auto Collect Items", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Mob%20Aura%20Auto%20get%20all%20items%20Auto%20Collect%20Items.lua")
 
 addscriptuniversal("Universal FE Kill Closet Player", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20FE%20Kill%20Closet%20Player.lua")
+
+addscript(4811576324, "Current Game","Totally Accurate Gun Simulator: Rapid Fire, Silent Aim", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Totally%20Accurate%20Gun%20Simulator%20Rapid%20Fire%20Silent%20Aim.lua")
