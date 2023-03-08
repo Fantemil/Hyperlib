191cbbe412e9e8be5722f5498894fff99ab084c7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:15:25 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Your Bizarre Adventure Auto Farm.lua
@@ -0,0 +1,28 @@
+getgenv().HidingSpot = 
+--^^ Number 1 or 2
+getgenv().omg = {
+--True = you want to keep; false = you don't want to keep
+    ["Star Platinum"] = false,
+    ["The World"] = false,
+    ["Crazy Diamond"] = false,
+    ["King Crimson"] = false,
+    ["Gold Experience"] = false,
+    ["Mr. President"] = false,
+    ["Sticky Fingers"] = false,
+    ["Magician's Red"] = false,
+    ["Purple Haze"] = false,
+    ["Killer Queen"] = false,
+    ["Aerosmith"] = false,
+    ["Silver Chariot"] = false,
+    ["Hierophant Green"] = false,
+    ["Hermit Purple"] = false,
+    ["Six Pistols"] = false,
+    ["Stone Free"] = false,
+    ["Cream"] = false,
+    ["Beach Boy"] = false,
+    ["Anubis"] = false,
+    ["The Hand"] = false,
+    ["White Album"] = false,
+    ["Whitesnake"] = true,
+    ["Red Hot Chili Pepper"] = false}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/senS24t/sensnebunu/main/AutoStandFarmBysenS"))()
