6fbda97349d5db84ca4d60c6506fb7247406a0c7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:45:35 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Emergency Response Liberty County Auto Get Code For Bank Robbery House ESP Jewerly ESP.lua
@@ -0,0 +1,15 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/proton123456/noarceusx-ERLC-SCRIPT/main/subscribe%20to%20n1wu'))()
+
+--[[
+v1.1
+
+updatelog 
+1 --< added bank logCode
+2 --< BankEnterCode pops up when u log the bank code
+3 --< removed walkspeed slider
+--------------------------------
+n1wu#0001
+
+use an alt or a ban may be issued by the anticheat
+**DONT USE AUTOEXEC OR ELSE YOU WILL INSTANTLY GET BANNED!**
+]]--
