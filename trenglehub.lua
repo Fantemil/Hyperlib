e5818d59bc58e8c9c24ee68e0074f9666412a443
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:18:16 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:18:12"
+lastupdate = "GMT +1: 08.03.2023 07:18:16"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5864,3 +5864,5 @@ addscriptexist(11729688377, "Booga Booga [REBORN]: Auto Heal, Kill Aura, Pickup 
 addscriptexist(2248408710, "Destruction Simulator: Max Level, Infinity Money, No Cooldown", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Destruction%20Simulator%20Max%20Level%20Infinity%20Money%20No%20Cooldown.lua")
 
 addscriptexist(3623096087, "Muscle Legends: Auto Rebirth, Auto Strength, Safe Place", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Muscle%20Legends%20Auto%20Rebirth%20Auto%20Strength%20Safe%20Place.lua")
+
+addscript(11327460595, "Current Game","Cart Ride Race: Race, Cart Max Speed, Auto Click", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cart%20Ride%20Race%20Race%20Cart%20Max%20Speed%20Auto%20Click.lua")
