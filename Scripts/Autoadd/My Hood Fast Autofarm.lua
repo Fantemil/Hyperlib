95fcba8c21198480468f2f33b98679582d29875d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:54:43 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/My Hood Fast Autofarm.lua
@@ -0,0 +1,20 @@
+autofarm = true
+
+game.ProximityPromptService.PromptButtonHoldBegan:Connect(function(a)a.HoldDuration=0 end)
+while autofarm == true do
+ task.wait()
+ for i, v in next, game:GetService("Workspace"):GetDescendants() do
+  if ((v.Name == "CA$HIER") or (v.Name == "Model")) then
+   if v:FindFirstChild("Hitbox") then
+    if (v:FindFirstChild("Opened").BrickColor ~= "Really red") then
+     task.wait(0)
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Opened.CFrame
+     task.wait(1)
+     fireproximityprompt(v.Hitbox.ProximityPrompt)
+     task.wait()
+     wait(0)
+    end
+   end
+  end
+ end
+end
