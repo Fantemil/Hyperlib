c49e7e6b0bc8e10523598beb4bd2a64ee61b096a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:54:35 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Karate Auto Block.lua
@@ -0,0 +1,7 @@
+getgenv().config = {
+   enabled = true,
+   blockHoldTime = 0.2,
+   distanceCheck = 7
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sw1ndlerScripts/RobloxScripts/main/Misc%20Scripts/Karate!%20Autoblock.lua",true))()
