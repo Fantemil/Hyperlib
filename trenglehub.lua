48ab89bbe8afb117fc26924da7ae669b4113439b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:24:46 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:24:42"
+lastupdate = "GMT +1: 08.03.2023 19:24:46"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12860,3 +12860,5 @@ addscriptexist(155615604, "Prison Life: Kill Players", "pulled from rbxscript.co
 addscriptexist(4793176968, "Mano County Police Patrol: Tase All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mano%20County%20Police%20Patrol%20Tase%20All.lua")
 
 addscriptexist(10758111998, "2019 Booga Booga [REBORN]: Auto Pickup Items", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20Booga%20Booga%20REBORN%20Auto%20Pickup%20Items.lua")
+
+addscript(1487722208, "Current Game","Barren: Inf Ammo", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Barren%20Inf%20Ammo.lua")
