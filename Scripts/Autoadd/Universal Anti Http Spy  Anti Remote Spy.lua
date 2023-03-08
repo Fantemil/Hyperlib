e70f0a70d8eea9e0b8957e8c1dd8d17810266bb8
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:25:33 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Anti Http Spy  Anti Remote Spy.lua
@@ -0,0 +1,4 @@
+_G.AntiRemoteSpy = true -- You Can Set It To False If You Need
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fgasfga/misc-releases/main/ARSandAHS.lua", true))()
+
+-- Put You'r Script Here.
