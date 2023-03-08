c4a2e2c50e737dcaae04143dfa4695629bda49e1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:19:35 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Hood Modded Press X to Fling Boombox.lua
@@ -0,0 +1,13 @@
+--// Created By Nosssa Supports mostly, all hood games!
+
+--// Roblox Group ( TeamNosss! ): https://www.roblox.com/groups/16003304/TeamNosss#!/about
+
+getgenv().Toggle_Key = "x"
+
+getgenv().Part_Follow_Mouse = true -- [ If its on true, Flinger will follow the mouse, else part will target players ]
+
+getgenv().Target_Display_Name = "" -- [ Leave this blank if you want fling everyone, else type specific DISPLAY NAME within quotation marks ]
+
+getgenv().RageBot_Prediction = 5  -- [ How far the fidget spinner will predict infront of people in rage mode ]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nosssa/NossLock/main/MouseToolFling"))();
