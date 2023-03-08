cbcd47d6a3d3afb13ae38393130d504ba90a8a03
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:37:08 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 16:37:04"
+lastupdate = "GMT +1: 08.03.2023 16:37:08"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8904,3 +8904,5 @@ addscriptexist(662417684, "LUCKY BLOCKS Battlegrounds: Fly, Spawn Blocks, Hitbox
 addscriptexist(9680886326, "West Elm Home Design: Farm Money", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/West%20Elm%20Home%20Design%20Farm%20Money.lua")
 
 addscriptexist(1962086868, "Tower Of Hell: Auto Farm Wins, Instant Win, Godmode", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20Of%20Hell%20Auto%20Farm%20Wins%20Instant%20Win%20Godmode.lua")
+
+addscriptexist(1537690962, "Bee Swarm Simulator: New Overpowered Farming Gui", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bee%20Swarm%20Simulator%20New%20Overpowered%20Farming%20Gui.lua")
