d2cf5946204ce67b99ac70182e4a9617d68e69be
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:53:35 +0100

    Updated automatically!

--- Scripts/Autoadd/roblox but you get smarter every second Auto Collect Gems.lua
+++ Scripts/Autoadd/roblox but you get smarter every second Auto Collect Gems.lua
@@ -1,4 +1,5 @@
 getgenv().toggle = true
+
 local table = {
    game:GetService("Workspace").Z2Gems,
    game:GetService("Workspace").Z1Gems,
@@ -8,12 +9,13 @@ local table = {
    game:GetService("Workspace").Z7Gems,
    game:GetService("Workspace").Z5Gems,
 }
+
 while task.wait(3) and toggle do
    for a,b in pairs(table) do
        for i,v in pairs(b:GetDescendants()) do
            if v.Name == "TouchInterest" then
                firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0)
-               firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1)
+               firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1)    
            end
        end
    end
