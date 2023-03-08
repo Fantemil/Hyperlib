7863db03891f99b859b4df489b92bb1e1dc2f4fa
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:40:35 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Underground resolver.lua
@@ -0,0 +1,13 @@
+local RunService = game:GetService("RunService")
+
+RunService.Heartbeat:Connect(function()
+    pcall(function()
+        for i,v in pairs(game.Players:GetChildren()) do
+            if v.Name ~= game.Players.LocalPlayer.Name then
+                local hrp = v.Character.HumanoidRootPart
+                hrp.Velocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)    
+                hrp.AssemblyLinearVelocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)   
+            end
+        end
+    end)
+end)
