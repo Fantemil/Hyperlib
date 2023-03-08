fdc6358468277ca5f37917a47aaeba4a27a93e7f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:02:39 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Every Second You Get  Health Auto Rebirth Auto Win.lua
@@ -0,0 +1,6 @@
+-- made by beta
+    while true do
+wait(2)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1063.60693, 349.100067, -144.750214, 0, 0, 1, -1, 0, 0, 0, -1, 0)
+game:GetService("ReplicatedStorage").RebirthEvent:FireServer()
+    end
