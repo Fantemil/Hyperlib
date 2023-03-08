ac988d400d2e2adec42af0d107e3d2f4aa60ce2e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:57:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:57:11"
+lastupdate = "GMT +1: 08.03.2023 17:57:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10926,3 +10926,5 @@ addscriptexist(7305309231, "Taxi Boss: Auto Spin Halloween Wheel", "pulled from 
 addscriptexist(6284583030, "Pet Simulator X: Auto Farm, Auto Open Egg & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Auto%20Farm%20Auto%20Open%20Egg%20%20More.lua")
 
 addscriptexist(537413528, "Build A Boat For Treasure: Auto Farm, Auto Buy Chests, Auto Buy Items", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Farm%20Auto%20Buy%20Chests%20Auto%20Buy%20Items.lua")
+
+addscriptexist(4616652839, "Shindo Life: Infinite Spins + Max Rank", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shindo%20Life%20Infinite%20Spins%20%20Max%20Rank.lua")
