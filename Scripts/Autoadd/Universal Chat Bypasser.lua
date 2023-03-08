769fabb49f4f9d30979360c9e0146f348f61a9f9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:58:58 +0100

    Updated automatically!

--- Scripts/Autoadd/Universal Chat Bypasser.lua
+++ Scripts/Autoadd/Universal Chat Bypasser.lua
@@ -1,3 +1,1 @@
--- Made by ArasOfficial#8439 and tav#8870
-
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/yenil140/ChatBypasser/main/Universal"), true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/katrina367/ScriptBlox-Projects/main/Chat%20Bypasser'))()
