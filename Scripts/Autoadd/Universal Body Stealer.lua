df8473bec7a8c6a87e7f0396491054492724f8f5
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:26:12 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Body Stealer.lua
@@ -0,0 +1,2 @@
+getgenv().URL = "https://example.com" -- This is the link to the website you want to steal the body from
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/projects/main/Roblox/Body%20Stealer.lua"))()
