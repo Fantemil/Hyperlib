89c320aeefb1122ca9732d28fa17bdb62258fb56
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:17:12 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:17:08"
+lastupdate = "GMT +1: 08.03.2023 19:17:12"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12698,3 +12698,5 @@ addscriptexist(6516141723, "DOORS: Auto Skip Level, Instant Interact, Chams", "p
 addscriptexist(7002808196, "Симулятор падения в канаву: Auto Farm, Badges, Teleport", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%D0%A1%D0%B8%D0%BC%D1%83%D0%BB%D1%8F%D1%82%D0%BE%D1%80%20%D0%BF%D0%B0%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%B2%20%D0%BA%D0%B0%D0%BD%D0%B0%D0%B2%D1%83%20Auto%20Farm%20Badges%20Teleport.lua")
 
 addscriptexist(662417684, "LUCKY BLOCKS Battlegrounds: Fly, WalkSpeed, Admin Panel and more", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LUCKY%20BLOCKS%20Battlegrounds%20Fly%20WalkSpeed%20Admin%20Panel%20and%20more.lua")
+
+addscriptexist(6284583030, "Pet Simulator X: Auto Farm Auto Collect, Auto Egg", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Auto%20Farm%20Auto%20Collect%20Auto%20Egg.lua")
