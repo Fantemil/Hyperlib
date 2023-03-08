4f25c9645888ee6aaaaf6bd0c07ad8730c201773
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:14:04 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/A Universal Time God Mode Auto Farm Walkspeed.lua
@@ -0,0 +1,5 @@
+_G.key = "imsmart" --the key is between the two " "
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NukeVsCity/TheALLHACKLoader/main/NukeLoader"))()
+
+
+--Key for the manual loader is "      imsmart      "
