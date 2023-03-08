1d9fde5587557a569c416d7654265a14c7c35c2f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:06:21 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:06:17"
+lastupdate = "GMT +1: 08.03.2023 17:06:21"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9632,3 +9632,5 @@ addscriptexist(10972284553, "Skala Simulator: Auto Clicker", "pulled from rbxscr
 addscriptexist(6679968919, "Fly Race: Auto Race, Auto Collect Orb, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fly%20Race%20Auto%20Race%20Auto%20Collect%20Orb%20Auto%20Rebirth.lua")
 
 addscriptexist(10758111998, "2019 Booga Booga [REBORN]: Hide Name, Hide Health, Auto Build", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20Booga%20Booga%20REBORN%20Hide%20Name%20Hide%20Health%20Auto%20Build.lua")
+
+addscriptexist(8726743209, "Refinery Caves: No Water Blur, Clear Weather & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Refinery%20Caves%20No%20Water%20Blur%20Clear%20Weather%20%20More.lua")
