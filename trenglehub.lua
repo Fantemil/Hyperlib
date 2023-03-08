525fc380963c3b26e2094e0acfcb6a5edaaf805e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:19:41 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 13:19:37"
+lastupdate = "GMT +1: 08.03.2023 13:19:41"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7434,3 +7434,5 @@ addscript(8523408215, "Current Game","Alo Sanctuary: Auto Orb Collector", "pulle
 addscript(8531522502, "Current Game","Facility Of Redemption: Kill Aura, Grab all items, Insta Kill", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Facility%20Of%20Redemption%20Kill%20Aura%20Grab%20all%20items%20Insta%20Kill.lua")
 
 addscript(3411100258, "Current Game","prtty much evry bordr gam evr: Auto Buy Items, Make Potions, Options", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/prtty%20much%20evry%20bordr%20gam%20evr%20Auto%20Buy%20Items%20Make%20Potions%20Options.lua")
+
+addscriptexist(1537690962, "Bee Swarm Simulator: Autofarm, Auto dig & autodig mode, Auto sprinkler", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bee%20Swarm%20Simulator%20Autofarm%20Auto%20dig%20%20autodig%20mode%20Auto%20sprinkler.lua")
