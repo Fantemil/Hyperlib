10b4b36601ce1019e5926e079adfba6675c0848f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:22:20 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Crasher.lua
@@ -0,0 +1,6 @@
+--If your crash doesn't work, try increasing these values below :)
+getgenv().SprayCrashStrength = 110 -- Percentage out of 100 (increase for a stronger crash, e.g. 130 = 130% crash strength)
+getgenv().ItemCrashStrength = 110 -- Percentage out of 100
+getgenv().UseSnowballs = false -- If you want the crash to use snowballs or not
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BetterDaHood/BetterDaHoodCrasher/main/Crash'))()
