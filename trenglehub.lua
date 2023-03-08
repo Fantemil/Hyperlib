a8322c4e418284d4efc5a069b577e9ad462c41af
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:04:24 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:04:20"
+lastupdate = "GMT +1: 08.03.2023 17:04:24"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9586,3 +9586,5 @@ addscriptexist(8304191830, "Anime Adventures: Script Hub", "pulled from rbxscrip
 addscriptexist(8737602449, "PLS DONATE: Auto Beg, Walkspeed, Anti Afk", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PLS%20DONATE%20Auto%20Beg%20Walkspeed%20Anti%20Afk.lua")
 
 addscriptexist(537413528, "Build A Boat For Treasure: Auto Chests, Auto Farm, Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Chests%20Auto%20Farm%20Teleports.lua")
+
+addscriptexist(6516141723, "DOORS: Fullbright, Noclip, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Fullbright%20Noclip%20ESP.lua")
