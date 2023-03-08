c781088240bb6cbbef1024656bc779a6e3629b34
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:10:10 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:10:06"
+lastupdate = "GMT +1: 08.03.2023 18:10:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11250,3 +11250,5 @@ addscript(3198259055, "Current Game","Jailbreak: Infinity Jump, Fly & More", "pu
 addscriptexist(10984838211, "Battlefield Tycoon: Freeze all NPC", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Battlefield%20Tycoon%20Freeze%20all%20NPC.lua")
 
 addscriptexist(6764302962, "Junk Simulator: Auto Farm, Auto Eggs, Auto Sell", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Junk%20Simulator%20Auto%20Farm%20Auto%20Eggs%20Auto%20Sell.lua")
+
+addscriptexist(2788229376, "Da Hood: Server Crasher", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Server%20Crasher.lua")
