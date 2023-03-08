2c6baf329a5f4787f9977cca2f1d53b6ec219f51
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:01:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:01:07"
+lastupdate = "GMT +1: 08.03.2023 20:01:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13428,3 +13428,5 @@ addscriptexist(8750997647, "Tapping legends X | AutoClick, AutoRebirth, AutoHatc
 addscriptexist(8737602449, "Pls Donate | Auto Thank, Auto Beg, AntiAfk", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pls%20Donate%20%20Auto%20Thank%20Auto%20Beg%20AntiAfk.lua")
 
 addscriptexist(537413528, "Build a boat for treasure | Auto Farm, AutoBuy, Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20a%20boat%20for%20treasure%20%20Auto%20Farm%20AutoBuy%20Teleports.lua")
+
+addscriptexist(2551991523, "Broken Bones 4 | Auto Break Bones", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Broken%20Bones%20%20%20Auto%20Break%20Bones.lua")
