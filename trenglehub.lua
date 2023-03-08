116d8ed12c58a0e9c64004e705cba7cf8970e930
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 21:01:03 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 21:00:58"
+lastupdate = "GMT +1: 08.03.2023 21:01:03"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14748,3 +14748,5 @@ addscriptexist(920587237, "Adopt Me [Auto Farm, Buy any egg]", "pulled from rbxs
 addscriptexist(1962086868, "Tower of Hell [Godmode, Win]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20of%20Hell%20Godmode%20Win.lua")
 
 addscriptexist(537413528, "Build A Boat For Treasure [AutoFarm, Morhps]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20AutoFarm%20Morhps.lua")
+
+addscriptexist(537413528, "Build A Boat For Treasure [Walkspeed, Teleports]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Walkspeed%20Teleports.lua")
