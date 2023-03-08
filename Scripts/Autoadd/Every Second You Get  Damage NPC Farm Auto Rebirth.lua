364a5ebbe74081100846dacd3e9a892347c2592e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:49:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Every Second You Get  Damage NPC Farm Auto Rebirth.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XRoLLu/UWU/main/Every%20Second%20You%20Get%201%2B%20Damage%20GUI.lua", true))()
+
+--join my discord to see more new games gui and new update
+--https://discord.gg/dWUdqZUvNB
