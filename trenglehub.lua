1bbbddde6567fd25a54ff19faa1e93df756b8be4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:53:43 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:53:38"
+lastupdate = "GMT +1: 08.03.2023 12:53:43"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6762,3 +6762,5 @@ addscriptexist(11445923563, "One Fruit Simulator: Gorilla Farm, Auto Farm Level,
 addscriptexist(8540346411, "Rebirth Champions X: Auto Collect Presents, Auto Claim Daily Chest & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rebirth%20Champions%20X%20Auto%20Collect%20Presents%20Auto%20Claim%20Daily%20Chest%20%20More.lua")
 
 addscriptexist(6125589657, "Touch Football: Coin farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Touch%20Football%20Coin%20farm.lua")
+
+addscriptexist(11040063484, "Sword Fighters Simulator: Auto Farm, Fight Aura, Auto Collect.", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20Fighters%20Simulator%20Auto%20Farm%20Fight%20Aura%20Auto%20Collect.lua")
