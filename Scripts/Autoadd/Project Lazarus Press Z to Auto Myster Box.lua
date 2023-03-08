c95e2f18eaea5b8febf0fd8a71fce80128cbda8e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:40:17 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Project Lazarus Press Z to Auto Myster Box.lua
@@ -0,0 +1,15 @@
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+local w = library:CreateWindow("by") -- Creates the window
+local b = w:CreateFolder("OOOGA BOOGA") -- Creates the folder(U will put here your buttons,etc)
+b:Bind("On",Enum.KeyCode.Z,function() --Default bind
+getgenv().Ks = true
+while Ks == true do
+   wait()
+keypress(0x46) -- spacebar
+wait()
+keyrelease(0x46)
+end
+end)
+b:Bind("Off",Enum.KeyCode.X,function() --Default bind
+getgenv().Ks = false
+end)
