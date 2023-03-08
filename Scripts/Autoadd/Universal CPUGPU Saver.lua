97de15df041e1a5236d2921fbb25f3f720db08e7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:09:53 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal CPUGPU Saver.lua
@@ -0,0 +1,12 @@
+--[[+
+
+Roblox-GPU-SaverFIXED;
+_VERSION 1.0;
+dsc.gg/empifyutility;
+Licensed under GNU General Public License v3.0;
+
++]]
+
+_G.ShowUp = false --// Shows the gui above (Useful for games like TPT2)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Empire4946/roblox-gpu-saverBETTER/main/handler/main.txt"))()
