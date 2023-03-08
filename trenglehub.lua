d789169e319e8bf3db1425c166fea8aa259396be
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:22:12 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:22:08"
+lastupdate = "GMT +1: 08.03.2023 20:22:12"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13760,3 +13760,5 @@ addscriptexist(8482451541, "Brick Defense | Auto Farm Grind Infinite Tix", "pull
 addscript(9814637986, "Current Game","Slap Battles: Average FanMade | Kill, Give Panel, Kick, Ban", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Slap%20Battles%20Average%20FanMade%20%20Kill%20Give%20Panel%20Kick%20Ban.lua")
 
 addscriptexist(10148047155, "Car Mechanic Tycoon | Walkspeed, AntiKick, AutoFarm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Car%20Mechanic%20Tycoon%20%20Walkspeed%20AntiKick%20AutoFarm.lua")
+
+addscriptexist(6403373529, "Slap Battles | KillAura, Anti Chase, Anti Ragdoll, No Time Stop", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Slap%20Battles%20%20KillAura%20Anti%20Chase%20Anti%20Ragdoll%20No%20Time%20Stop.lua")
