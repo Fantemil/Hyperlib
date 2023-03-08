1250f076b53636aa2bf2ea2c809bcf9a016e7d01
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:44:47 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Project Slayers Remove Hit Delay Killaura Fast Pickup items.lua
@@ -0,0 +1,10 @@
+ --[[
+^~^  ,                                    
+ ('Y') )        Updated At [UPDATE 1] DEC 4th                          
+ /   \/  Â»Â»Â» SLAYER LEAN -- By ðð®ð»ð±ððð²ð®ð»#ð®ð²ð°ð±
+(\|||/)                               
+      
+                          Discord.gg/WdtXKgNPGJ                              
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KeoneGithub/KeoneGithub/main/LeanSlayer"))()
