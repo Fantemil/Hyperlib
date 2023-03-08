96d623119c8af3b0b13bac06f987bb963017a4fb
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:43:06 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:43:02"
+lastupdate = "GMT +1: 08.03.2023 17:43:06"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10572,3 +10572,5 @@ addscriptexist(6447798030, "Funky Friday: Infinity Stats", "pulled from rbxscrip
 addscriptexist(9498006165, "Tapping Simulator: Auto Tap, Auto Upgrade & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tapping%20Simulator%20Auto%20Tap%20Auto%20Upgrade%20%20More.lua")
 
 addscript(3823850583, "Current Game","Street Simulator: Auto Farm Small and Bigger", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Street%20Simulator%20Auto%20Farm%20Small%20and%20Bigger.lua")
+
+addscriptexist(6913960180, "Undertale Crazy Multiverse Timeline: Simple Auto Farm GUI", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Undertale%20Crazy%20Multiverse%20Timeline%20Simple%20Auto%20Farm%20GUI.lua")
