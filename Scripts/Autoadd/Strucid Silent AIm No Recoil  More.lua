5c78b4a2cb634739a82f265f25f98e546f8055fe
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:37:35 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Strucid Silent AIm No Recoil  More.lua
@@ -0,0 +1,5 @@
+_G.key = "novembr" --the key is between the two " "
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NukeVsCity/TheALLHACKLoader/main/NukeLoader"))()
+
+
+--Key for the manual loader is "      novembr      "
