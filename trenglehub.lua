88e4ff47fe77f2a087bfcd08de5544ecabf9dd7c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 21:02:57 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 21:02:53"
+lastupdate = "GMT +1: 08.03.2023 21:02:57"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14790,3 +14790,5 @@ addscriptexist(7728848215, "Slashing Simulator [Auto Farm, Teleport All Worlds, 
 addscriptexist(9961650006, "Shockwave Racing [Auto Win]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shockwave%20Racing%20Auto%20Win.lua")
 
 addscriptexist(8260276694, "Ability Wars [Esp, Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ability%20Wars%20Esp%20Farm.lua")
+
+addscriptexist(3527629287, "BIG Paintball [Silent AimBot, Esp. Chams]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BIG%20Paintball%20Silent%20AimBot%20Esp%20Chams.lua")
