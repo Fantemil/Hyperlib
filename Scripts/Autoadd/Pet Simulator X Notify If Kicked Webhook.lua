a9884ae30f260f864f767eb855a226efabcc3844
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:42:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Pet Simulator X Notify If Kicked Webhook.lua
@@ -0,0 +1,3 @@
+getgenv().Webhook = "" -- Discord Webhook [ Don't delete the ""!
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KISOOUF/Cat-Core/main/Pet%20Simulator%20X/Webhook-Kick"))()
