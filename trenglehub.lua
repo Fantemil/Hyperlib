3277c9b3e0d17ca7ab2ac24d81588e941fb02015
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:13:02 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:12:58"
+lastupdate = "GMT +1: 08.03.2023 17:13:02"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9804,3 +9804,5 @@ addscriptexist(537413528, "Build A Boat For Treasure: Auto Farm, Auto Chest, Ant
 addscriptexist(2788229376, "Da Hood: Auto Kill, Fake Macro & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Auto%20Kill%20Fake%20Macro%20%20More.lua")
 
 addscriptexist(606849621, "Jailbreak: Instant Teleport To Museum", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jailbreak%20Instant%20Teleport%20To%20Museum.lua")
+
+addscriptexist(606849621, "Jailbreak: Instant Teleport To Tomb", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jailbreak%20Instant%20Teleport%20To%20Tomb.lua")
