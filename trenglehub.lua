2937e667e7e12c5be2789c0133e377aa948e7405
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:37:17 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:37:13"
+lastupdate = "GMT +1: 08.03.2023 14:37:17"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7772,3 +7772,5 @@ addscriptexist(7722306047, "Pet Simulator X: Auto Farm Coins, Auto Hatch Eggs, A
 addscriptexist(8540346411, "Rebirth Champions X: Auto Rebirth, Auto Click & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rebirth%20Champions%20X%20Auto%20Rebirth%20Auto%20Click%20%20More.lua")
 
 addscriptexist(1962086868, "Tower Of Hell: Insta Win, Get All Items, Auto Buy", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20Of%20Hell%20Insta%20Win%20Get%20All%20Items%20Auto%20Buy.lua")
+
+addscriptexist(286090429, "Arsenal: ESP, Third Person, Infinity Jumps", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20ESP%20Third%20Person%20Infinity%20Jumps.lua")
