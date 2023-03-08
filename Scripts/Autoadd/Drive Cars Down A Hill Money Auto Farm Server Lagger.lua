863f71bdb9cb989a714423d252d2d33230e730b0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:12:14 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Drive Cars Down A Hill Money Auto Farm Server Lagger.lua
@@ -0,0 +1,6 @@
+getgenv().settings = {
+    MONEY_FARM = false, -- set to true to enable
+    SERVER_LAGGER = false -- set to true to enable, cant toggle off when on
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/b00Iean/roblox-scripts/main/cars'))()
