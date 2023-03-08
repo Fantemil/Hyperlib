89fb3c5df5a56b5e6001e5e8eb7b23644befbd3f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:21:39 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:21:35"
+lastupdate = "GMT +1: 08.03.2023 17:21:39"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10026,3 +10026,5 @@ addscriptexist(9848789324, "Ragdoll Engine: Fling, Kill Fling, Fe Character Size
 addscriptexist(3851622790, "Break In (Story): Item Giver, Kil All, Godmode", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Break%20In%20Story%20Item%20Giver%20Kil%20All%20Godmode.lua")
 
 addscript(7184600262, "Current Game","Gridiron Football 2.0: Auto Catch, Auto Snap Ball, Speed Hack", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Gridiron%20Football%20%20Auto%20Catch%20Auto%20Snap%20Ball%20Speed%20Hack.lua")
+
+addhub("Unfair Hub: 40+ Games Supported", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Unfair%20Hub%20%20Games%20Supported.lua")
