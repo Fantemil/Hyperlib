b30c16323fd9fd1b7cda0bdb090a9e0ae2fea12b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:02:20 +0100

    Updated automatically!

--- Scripts/Autoadd/Universal Fly GUI.lua
+++ Scripts/Autoadd/Universal Fly GUI.lua
@@ -1,63 +1,127 @@
---[[
- --MADE BY MY FRIEND
-]]
+local lp = game:FindService("Players").LocalPlayer
 
-local Fly = false
-local FlySpeed = 25
-
---[[
-    da gui
-]]
-
-local FlyGui = Instance.new('ScreenGui')
-local Frame = Instance.new('Frame')
-local TextButton = Instance.new('TextButton')
-
-FlyGui.Parent = game.Players.LocalPlayer:WaitForChild('PlayerGui')
-FlyGui.Name = 'FlyGui'
-
-Frame.Parent = FlyGui
-Frame.Active = true
-Frame.BackgroundColor3 = Color3.new(0, 0, 0)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.25, 0, 0.25, 0)
-Frame.Size = UDim2.new(0.5, 0, 0.5, 0)
-
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
-TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.25, 0, 0.25, 0)
-TextButton.Size = UDim2.new(0.5, 0, 0.5, 0)
-TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = 'Fly'
-TextButton.TextColor3 = Color3.new(1, 1, 1)
-TextButton.TextSize = 20
-
---[[
-    mostly important the "functions"
-]]
-
-function FlyToggle()
-    if Fly == false then
-        Fly = true
-        TextButton.Text = 'Stop'
-        game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-        game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character.Humanoid
-        game.Players.LocalPlayer.Character.Humanoid.PlatformStand = true
-        while Fly == true do
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, FlySpeed, 0)
-            wait()
-        end
-    else
-        Fly = false
-        TextButton.Text = 'Fly'
-        game.Players.LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-        game.Players.LocalPlayer.Character.Humanoid.PlatformStand = false
-    end
+local function gplr(String)
+ local Found = {}
+ local strl = String:lower()
+ if strl == "all" then
+  for i,v in pairs(game:FindService("Players"):GetPlayers()) do
+   table.insert(Found,v)
+  end
+ elseif strl == "others" then
+  for i,v in pairs(game:FindService("Players"):GetPlayers()) do
+   if v.Name ~= lp.Name then
+    table.insert(Found,v)
+   end
+  end 
+ elseif strl == "me" then
+  for i,v in pairs(game:FindService("Players"):GetPlayers()) do
+   if v.Name == lp.Name then
+    table.insert(Found,v)
+   end
+  end 
+ else
+  for i,v in pairs(game:FindService("Players"):GetPlayers()) do
+   if v.Name:lower():sub(1, #String) == String:lower() then
+    table.insert(Found,v)
+   end
+  end 
+ end
+ return Found 
 end
 
---[[
-    idk
-]]
+local function notif(str,dur)
+ game:FindService("StarterGui"):SetCore("SendNotification", {
+  Title = "yeet gui",
+  Text = str,
+  Icon = "rbxassetid://2005276185",
+  Duration = dur or 3
+ })
+end
 
-TextButton.MouseButton1Down:Connect(FlyToggle)
+--// sds
+
+local h = Instance.new("ScreenGui")
+local Main = Instance.new("ImageLabel")
+local Top = Instance.new("Frame")
+local Title = Instance.new("TextLabel")
+local TextBox = Instance.new("TextBox")
+local TextButton = Instance.new("TextButton")
+
+h.Name = "h"
+h.Parent = game:GetService("CoreGui")
+h.ResetOnSpawn = false
+
+Main.Name = "Main"
+Main.Parent = h
+Main.Active = true
+Main.Draggable = true
+Main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Main.BorderSizePixel = 0
+Main.Position = UDim2.new(0.174545452, 0, 0.459574461, 0)
+Main.Size = UDim2.new(0, 454, 0, 218)
+Main.Image = "rbxassetid://2005276185"
+
+Top.Name = "Top"
+Top.Parent = Main
+Top.BackgroundColor3 = Color3.fromRGB(57, 57, 57)
+Top.BorderSizePixel = 0
+Top.Size = UDim2.new(0, 454, 0, 44)
+
+Title.Name = "Title"
+Title.Parent = Top
+Title.BackgroundColor3 = Color3.fromRGB(49, 49, 49)
+Title.BorderSizePixel = 0
+Title.Position = UDim2.new(0, 0, 0.295454562, 0)
+Title.Size = UDim2.new(0, 454, 0, 30)
+Title.Font = Enum.Font.SourceSans
+Title.Text = "FE Yeet Gui (trollface edition)"
+Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+Title.TextScaled = true
+Title.TextSize = 14.000
+Title.TextWrapped = true
+
+TextBox.Parent = Main
+TextBox.BackgroundColor3 = Color3.fromRGB(49, 49, 49)
+TextBox.BorderSizePixel = 0
+TextBox.Position = UDim2.new(0.0704845786, 0, 0.270642221, 0)
+TextBox.Size = UDim2.new(0, 388, 0, 62)
+TextBox.Font = Enum.Font.SourceSans
+TextBox.PlaceholderText = "Who do i destroy(can be shortened)"
+TextBox.Text = ""
+TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.TextScaled = true
+TextBox.TextSize = 14.000
+TextBox.TextWrapped = true
+
+TextButton.Parent = Main
+TextButton.BackgroundColor3 = Color3.fromRGB(49, 49, 49)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.10352423, 0, 0.596330225, 0)
+TextButton.Size = UDim2.new(0, 359, 0, 50)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Cheese em'"
+TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+
+TextButton.MouseButton1Click:Connect(function()
+ local Target = gplr(TextBox.Text)
+ if Target[1] then
+  Target = Target[1]
+  
+  local Thrust = Instance.new('BodyThrust', lp.Character.HumanoidRootPart)
+  Thrust.Force = Vector3.new(9999,9999,9999)
+  Thrust.Name = "YeetForce"
+  repeat
+   lp.Character.HumanoidRootPart.CFrame = Target.Character.HumanoidRootPart.CFrame
+   Thrust.Location = Target.Character.HumanoidRootPart.Position
+   game:FindService("RunService").Heartbeat:wait()
+  until not Target.Character:FindFirstChild("Head")
+ else
+  notif("Invalid player")
+ end
+end)
+
+--//fsddfsdf
+notif("Loaded successfully! Created by scuba#0001", 5)
