57bd671553b83c09e877e440c42796c3a0e5d52f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:45:18 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/ball and axe Auto Win.lua
@@ -0,0 +1,19 @@
+--by TOOM#6777
+
+
+getyourname = game.Players.LocalPlayer.Character.Name 
+
+
+Game:GetService("Workspace").playerModels[getyourname].ball.CFrame = CFrame.new(-109.874924, 513.699951, 0.574999571, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+wait(0.1)
+Game:GetService("Workspace").playerModels[getyourname].ball.CFrame = CFrame.new(-109.874924, 513.699951, 0.574999571, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+wait(0.1)
+Game:GetService("Workspace").playerModels[getyourname].ball.CFrame = CFrame.new(-109.874924, 513.699951, 0.574999571, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+wait(0.1)
+Game:GetService("Workspace").playerModels[getyourname].ball.CFrame = CFrame.new(-109.874924, 513.699951, 0.574999571, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+wait(0.1)
+Game:GetService("Workspace").playerModels[getyourname].ball.CFrame = CFrame.new(-109.874924, 513.699951, 0.574999571, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+wait(0.1)
+Game:GetService("Workspace").playerModels[getyourname].ball.CFrame = CFrame.new(-109.874924, 513.699951, 0.574999571, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+
+wait(0.1)
