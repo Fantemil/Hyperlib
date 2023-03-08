ec1c63f9e3e97d538ece86a3e41c0db86b8d33d2
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:29:51 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:29:47"
+lastupdate = "GMT +1: 08.03.2023 14:29:51"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7580,3 +7580,5 @@ addscriptexist(1224212277, "Mad City: Chapter 2: Silent Aim, Autorob, Anti-AFK",
 addscriptexist(2753915549, "Blox Fruits: Auto Farm Mobs, Auto Fish Tails & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Auto%20Farm%20Mobs%20Auto%20Fish%20Tails%20%20More.lua")
 
 addscriptexist(6191637341, "Weapon Simulator 2: Auto Farm Power", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Weapon%20Simulator%20%20Auto%20Farm%20Power.lua")
+
+addscript(338521019, "Current Game","Identity Fraud: Anticheat bypass, Remove Doors", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Identity%20Fraud%20Anticheat%20bypass%20Remove%20Doors.lua")
