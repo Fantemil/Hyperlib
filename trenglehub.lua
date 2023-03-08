6141533dd9a3af9d14336d673842df9a0b3181e6
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:37:11 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:37:06"
+lastupdate = "GMT +1: 08.03.2023 19:37:11"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13140,3 +13140,5 @@ addscript(5610197459, "Current Game","gang up on people simulator: AutoFarm, Aut
 addscript(5108997584, "Current Game","Reason 2 Die: Teleports, Buy any item, Fly", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Reason%20%20Die%20Teleports%20Buy%20any%20item%20Fly.lua")
 
 addscript(7419509075#ropro-quick-search, "Current Game","cart ride around used car batter: Teleports, Auto Coins, Get tools", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/cart%20ride%20around%20used%20car%20batter%20Teleports%20Auto%20Coins%20Get%20tools.lua")
+
+addscriptexist(3177438863, "Dragon Blox: Auto Quest, Auto Farm, AutoRebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dragon%20Blox%20Auto%20Quest%20Auto%20Farm%20AutoRebirth.lua")
