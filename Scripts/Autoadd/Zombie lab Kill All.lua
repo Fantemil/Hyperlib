22bdab3221e4e1a25b0f005ea5e279f8457d34b4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:58:22 +0100

    Updated automatically!

--- Scripts/Autoadd/Zombie lab Kill All.lua
+++ Scripts/Autoadd/Zombie lab Kill All.lua
@@ -1,15 +1,19 @@
-for i=2,#game.Players:GetPlayers() do
-v=game.Players:GetPlayers()[i]
 local args = {
-    [1] = v.Character:WaitForChild("Humanoid"),
-    [2] = v.Character:WaitForChild("HumanoidRootPart"),
-    [3] = math.huge,
+    [3] = 100,
+    [4] = Vector3.new(0.8440878391265869, -0.1331290602684021, 0.5194154977798462),
+    [5] = 2,
+    [6] = 0,
+    [7] = false
 }
-pcall(function()
-if not game.Players.LocalPlayer.Backpack:FindFirstChild("Pistol") then
-game:GetService("Players").LocalPlayer.Character.Pistol.GunScript_Server.InflictTarget:FireServer(unpack(args))
-else
-game:GetService("Players").LocalPlayer.Backpack.Pistol.GunScript_Server.InflictTarget:FireServer(unpack(args))
+local loopEnabled = true
+
+while loopEnabled do
+for i, player in pairs(game:GetService("Players"):GetPlayers()) do
+    if player.Character:FindFirstChild("HumanoidRootPart") then
+    args[1] = player.Character.Humanoid
+    args[2] = player.Character.HumanoidRootPart
+    game:GetService("Players").LocalPlayer.Character.Pistol.GunScript_Server.InflictTarget:FireServer(unpack(args))
 end
-end)
+end
+  wait(1)
 end
