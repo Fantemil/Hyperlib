83b34d2cb852d0d8533b2f33d395697baa594bce
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:08:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:08:42"
+lastupdate = "GMT +1: 08.03.2023 18:08:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11216,3 +11216,5 @@ addscriptexist(10710676163, "SPTS : Origin: Auto Farm", "pulled from rbxscript.c
 addscriptexist(8726743209, "Refinery Caves: Auto Sell Ores", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Refinery%20Caves%20Auto%20Sell%20Ores.lua")
 
 addscriptexist(8726743209, "Refinery Caves: Auto Ore Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Refinery%20Caves%20Auto%20Ore%20Farm.lua")
+
+addscriptexist(8596797381, "Northside Chicago: Infinite Stamina, Map Destruction, Audio Abuse", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Northside%20Chicago%20Infinite%20Stamina%20Map%20Destruction%20Audio%20Abuse.lua")
