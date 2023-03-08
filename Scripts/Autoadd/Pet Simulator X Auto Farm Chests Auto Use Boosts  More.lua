cbcb6b984c68185cee0b1a0be50b938c2a1873ca
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:45:55 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Pet Simulator X Auto Farm Chests Auto Use Boosts  More.lua
@@ -0,0 +1,3 @@
+getgenv().key = "projectWBIsAwesomemrcrapcrappypattt"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"))()
+---key from https://discord.gg/u7JNWQcgsU
