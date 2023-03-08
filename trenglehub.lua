4422add74e15a7c88ad243e8fd41bf9c78349ede
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:03:28 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 13:03:22"
+lastupdate = "GMT +1: 08.03.2023 13:03:28"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7012,3 +7012,5 @@ addscript(10028173768, "Current Game","Queen Elizebeth Obby: Teleport to The End
 addscriptexist(11040063484, "Sword Fighters Simulator: Auto Farm Mobs, Auto Farm Quests & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20Fighters%20Simulator%20Auto%20Farm%20Mobs%20Auto%20Farm%20Quests%20%20More.lua")
 
 addscriptexist(286090429, "Arsenal: Silent Aim, Infinity Ammo & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20Silent%20Aim%20Infinity%20Ammo%20%20More.lua")
+
+addscript(10684709969, "Current Game","ShimmerVille RP: Find Presents Go to santa, Collect Snowflakes", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ShimmerVille%20RP%20Find%20Presents%20Go%20to%20santa%20Collect%20Snowflakes.lua")
