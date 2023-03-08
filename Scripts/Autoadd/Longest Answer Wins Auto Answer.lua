68872c8724cc97109be7fe4debd2622ffb53c65c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:17:03 +0100

    Updated automatically!

--- Scripts/Autoadd/Longest Answer Wins Auto Answer.lua
+++ Scripts/Autoadd/Longest Answer Wins Auto Answer.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AdvancedWare/VScript/main/Script", true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/UnknownStraz/Longest-Answer-Wins/main/Script"), true))()
