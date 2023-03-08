77c1dd6fbb28062bdfc873520b858497d0e6674b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:38:23 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:38:19"
+lastupdate = "GMT +1: 08.03.2023 12:38:23"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6360,3 +6360,5 @@ addscript(7336477134, "Current Game","Minerscave: Dupe Sapphire", "pulled from r
 addscript(6165106805, "Current Game","get a snack at 4 am: Unlock ll endings", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/get%20a%20snack%20at%20%20am%20Unlock%20ll%20endings.lua")
 
 addscriptexist(9834528893, "+One Speed Every Second: Auto Farm Wins", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/One%20Speed%20Every%20Second%20Auto%20Farm%20Wins.lua")
+
+addscriptexist(11656036986, "make roblox games to become rich and famous: Auto Create Games, Anti Afk, Auto Build", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/make%20roblox%20games%20to%20become%20rich%20and%20famous%20Auto%20Create%20Games%20Anti%20Afk%20Auto%20Build.lua")
