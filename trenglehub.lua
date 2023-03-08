869fd70742f3f3476f0df90081ff98fe2d8b063d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 21:07:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 21:07:11"
+lastupdate = "GMT +1: 08.03.2023 21:07:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14896,3 +14896,5 @@ addscript(4003872968, "Current Game","Giant Survival! [Inf Money, Best Gun]", "p
 addscriptexist(4967465336, "Ray's Mod [Inf Money, Inf Ammo]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rays%20Mod%20Inf%20Money%20Inf%20Ammo.lua")
 
 addscriptexist(8531522502, "Facility Of Redemption [WalkSpeed]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Facility%20Of%20Redemption%20WalkSpeed.lua")
+
+addscript(391104146, "Current Game","Let's Party∞ +Gear Testing Edition [Ban All, Fly]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Lets%20Party%20Gear%20Testing%20Edition%20Ban%20All%20Fly.lua")
