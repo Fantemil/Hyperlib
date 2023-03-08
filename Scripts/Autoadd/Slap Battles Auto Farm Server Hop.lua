da50d6490618e723d8a636765bafbbb8bbcdef8c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:44:02 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Slap Battles Auto Farm Server Hop.lua
@@ -0,0 +1,4 @@
+-- PLEASE MAKE SURE TO PUT THIS INTO YOUR EXPLOIT'S AUTOEXEC FOLDER
+
+getgenv().farmEnabled = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitzoon/Roblox-Scripts/main/SlapBattles_Slapples.lua",true))()
