191830d1f6c3d40e64fcca0c5eb1c529eb4d8550
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:35:54 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:35:50"
+lastupdate = "GMT +1: 08.03.2023 14:35:54"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7736,3 +7736,5 @@ addscriptexist(6552369796, "neon Knights: Quick Speed", "pulled from rbxscript.c
 addscriptexist(8737602449, "PLS DONATE: Fortune Cookie", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PLS%20DONATE%20Fortune%20Cookie.lua")
 
 addscript(2988554876, "Current Game","Military Simulator: Full Auto, Firerate, No Recoil", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Military%20Simulator%20Full%20Auto%20Firerate%20No%20Recoil.lua")
+
+addscriptexist(10723695195, "Anime Evolution Simulator: Auto Farm, Auto Collect Coins, Power Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Evolution%20Simulator%20Auto%20Farm%20Auto%20Collect%20Coins%20Power%20Teleports.lua")
