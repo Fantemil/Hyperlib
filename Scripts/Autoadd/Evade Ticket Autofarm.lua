646df0cc7c31f38885ee227ba1c2f2e53956f71f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:04:59 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Evade Ticket Autofarm.lua
@@ -0,0 +1,3 @@
+getgenv().LoopAutofarm = true -- if you want to stop it change it to false and execute it again 
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/juywvm/-Roblox-Projects-/main/evadeticketfarm.lua'),true))()
