c226f4046d4c2257ad6fc7fb01412fbb81d09fc4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:44:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 16:44:33"
+lastupdate = "GMT +1: 08.03.2023 16:44:38"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9086,3 +9086,5 @@ addscriptexist(2788229376, "Da Hood: God mode, Fly, Tp", "pulled from rbxscript.
 addscript(9297410766, "Current Game","Crossbar Challenge Simulator: Get all hats", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Crossbar%20Challenge%20Simulator%20Get%20all%20hats.lua")
 
 addscriptexist(10723695195, "Anime Evolution Simulator: Auto Kill Mob, Auto Click, Magnet", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Evolution%20Simulator%20Auto%20Kill%20Mob%20Auto%20Click%20Magnet.lua")
+
+addscriptexist(4872321990, "Islands: God mode", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Islands%20God%20mode.lua")
