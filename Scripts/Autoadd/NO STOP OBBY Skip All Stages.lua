3dacc577208d7072cf59d07154df9e5953b3b366
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:17:30 +0100

    Updated automatically!

--- Scripts/Autoadd/NO STOP OBBY Skip All Stages.lua
+++ Scripts/Autoadd/NO STOP OBBY Skip All Stages.lua
@@ -1,7 +1,10 @@
 for i=1,600 do
-local v1 = 1
-local v2 = true
-local event = game:GetService("ReplicatedStorage")["Remote Events"]["Checkpoint Touched"]
-event:FireServer(v1, v2)
+
+   local v1 = 1
+   local v2 = true
+   local event = game:GetService("ReplicatedStorage")["Remote Events"]["Checkpoint Touched"]
+   
+   event:FireServer(v1, v2)    
 end
+
 game.Players.LocalPlayer.Character.Humanoid.Health = 0
