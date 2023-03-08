4180c4f2c8604d4c9c7eeb848f76806233d6594a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:49:20 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:49:16"
+lastupdate = "GMT +1: 08.03.2023 20:49:20"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14454,3 +14454,5 @@ addscriptexist(286090429, "Arsenal [God mode]", "pulled from rbxscript.com", "ht
 addscriptexist(6284583030, "Pet Simulator X [AutoFarm, AutoCollect]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20AutoFarm%20AutoCollect.lua")
 
 addscript(9757510382, "Current Game","Trade Simulator [AutoFarm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Trade%20Simulator%20AutoFarm.lua")
+
+addscriptexist(7952502098, "Impossible Glass Bridge Obby [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Impossible%20Glass%20Bridge%20Obby%20Auto%20Farm.lua")
