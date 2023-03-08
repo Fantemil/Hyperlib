38318b25c8fbbf9e0742919fd39c317d366daa76
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:45:01 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Fencing Dupe Spray.lua
@@ -0,0 +1,12 @@
+local sex = true
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local virtualUser = game:GetService('VirtualUser')
+virtualUser:CaptureController()
+
+while sex == true do wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(188.08993530273438, 3.747999668121338, 106.30188751220703)
+wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-132.34100341796875, 3.3999993801116943, 65.11122131347656)
+wait(0.5)
+ game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").Parent = game.Workspace
+end
