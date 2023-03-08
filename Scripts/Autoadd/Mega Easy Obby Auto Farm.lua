b4fc7213e4f17a9b1ceb014e047fcb9b64b78796
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:55:09 +0100

    Updated automatically!

--- Scripts/Autoadd/Mega Easy Obby Auto Farm.lua
+++ Scripts/Autoadd/Mega Easy Obby Auto Farm.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+for i = 2, #workspace.Checkpoints:GetChildren() do
+   task.wait(1)
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Checkpoints:FindFirstChild(tostring(i)).CFrame + Vector3.new(0,10,0)
+end
