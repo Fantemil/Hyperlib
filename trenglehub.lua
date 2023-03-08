c39a7adc53517fe23e29fc19667353819a54c7b0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:47:41 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:47:37"
+lastupdate = "GMT +1: 08.03.2023 06:47:41"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4928,3 +4928,5 @@ addscriptuniversal("Universal Detect People", "pulled from rbxscript.com", "http
 addscriptexist(6872265039, "BedWars: Kil Aura", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BedWars%20Kil%20Aura.lua")
 
 addscriptexist(11103424163, "Heist Tycoon: Infinity Money", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Heist%20Tycoon%20Infinity%20Money.lua")
+
+addscriptexist(2788229376, "Da Hood: Options, ESP, Aimlock", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Options%20ESP%20Aimlock.lua")
