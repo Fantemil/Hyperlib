d48c3c262e2bd218d0712d512c647ac2a2f86567
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:15:57 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Taxi Boss Auto Farm Server Hop.lua
@@ -0,0 +1,3 @@
+ _G.rating = 1 -- Minimum rating
+_G.customers = 5 -- How many customers until server hop recommend 5 and below
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Marco8642/science/main/taxi%20boss%20new%20autofarm", true))()
