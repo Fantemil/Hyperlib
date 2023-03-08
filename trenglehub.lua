8226d7ea2f418f403aa17c07e7e0d38290a87693
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:59:33 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:59:29"
+lastupdate = "GMT +1: 08.03.2023 14:59:33"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8374,3 +8374,5 @@ addscriptuniversal("Universal Realistic Day and Night", "pulled from rbxscript.c
 addscriptexist(3101667897, "Legends Of Speed: Auto Collect Orbs, Hoops, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legends%20Of%20Speed%20Auto%20Collect%20Orbs%20Hoops%20Auto%20Rebirth.lua")
 
 addscript(4292776423, "Current Game","Unit: Classified: Kill All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Unit%20Classified%20Kill%20All.lua")
+
+addscriptexist(1962086868, "Tower Of Hell: Auto Buy, Auto WIn, Remove Anti Cheat", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20Of%20Hell%20Auto%20Buy%20Auto%20WIn%20Remove%20Anti%20Cheat.lua")
