0dbb390f67190cb566c795ad2806a0a5caff4a7b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:13:29 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Warship Roleplay Radio Chat Spammer.lua
@@ -0,0 +1,60 @@
+game:GetService'RunService'.Heartbeat:Connect(function()
+    
+
+local crashcode = "L warship (ððð)" -- what you wanna say 
+
+local args = {
+    [1] = crashcode,
+    [2] = "Global"
+}
+
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RadioService.ChattedEvent:FireServer(unpack(args))
+
+
+local args = {
+    [1] = crashcode,
+    [2] = "Special Boat Section"
+}
+
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RadioService.ChattedEvent:FireServer(unpack(args))
+
+local args = {
+    [1] = crashcode,
+    [2] = "Staff"
+}
+
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RadioService.ChattedEvent:FireServer(unpack(args))
+
+local args = {
+    [1] = crashcode,
+    [2] = "Police"
+}
+
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RadioService.ChattedEvent:FireServer(unpack(args))
+
+local args = {
+    [1] = crashcode,
+    [2] = "Incident 3"
+}
+
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RadioService.ChattedEvent:FireServer(unpack(args))
+local args = {
+    [1] = crashcode,
+    [2] = "Incident 2"
+}
+
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RadioService.ChattedEvent:FireServer(unpack(args))
+local args = {
+    [1] = crashcode,
+    [2] = "Incident 1"
+}
+
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RadioService.ChattedEvent:FireServer(unpack(args))
+
+local args = {
+    [1] = crashcode,
+    [2] = "Royal Navy Police"
+}
+
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RadioService.ChattedEvent:FireServer(unpack(args))
+end);
