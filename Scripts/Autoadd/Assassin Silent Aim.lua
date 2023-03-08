3ca0bf2d1586d823d0cd461c97e27320a32595d6
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:10:21 +0100

    Updated automatically!

--- Scripts/Autoadd/Assassin Silent Aim.lua
+++ Scripts/Autoadd/Assassin Silent Aim.lua
@@ -1,105 +1,42 @@
---[[
-   Assassin Silent Aim.lua
-   Made by Rileyy#0808
-]]
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+local Client = Players.LocalPlayer
 
---// Variables
-local Players = game:GetService('Players')
-local RunService = game:GetService('RunService')
-local UserInputService = game:GetService('UserInputService')
-
-local LocalPlayer = Players.LocalPlayer
-local Mouse = LocalPlayer:GetMouse()
-local CurrentCamera = workspace.CurrentCamera
-
---// Tables
-local Client = {}
-local Drawings = {
-   Drawing.new('Circle'),
-   Drawing.new('Circle')
-}
-
---// FOV
-Drawings[1].Color = Color3.fromRGB(248, 200, 220)
-Drawings[1].Thickness = 2
-Drawings[1].Visible = true
-Drawings[1].Radius = 150
-
-Drawings[2].Thickness = 4
-Drawings[2].Visible = true
-Drawings[2].Radius = 150
-Drawings[2].ZIndex = -1
-
-UserInputService.InputChanged:Connect(function(input)
-   if input.UserInputType == Enum.UserInputType.MouseMovement then
-       Drawings[1].Position = UserInputService:GetMouseLocation()
-       Drawings[2].Position = UserInputService:GetMouseLocation()
-   end
-end)
-
---// Functions
-function IsVisible(position, model)
-   local ray = Ray.new(CurrentCamera.CFrame.p, CFrame.new(CurrentCamera.CFrame.p, position).LookVector * 10000)
-   local hit, position, normal = workspace:FindPartOnRayWithIgnoreList(ray, {LocalPlayer.Character, CurrentCamera})
-   if not hit then
-       return false
-   end
-   return hit:IsDescendantOf(model), hit, position, normal
+local function GetPlayers()
+    local Playerss, Characters = {}, {}
+    for i,v in next, Workspace:GetChildren() do
+        if table.find(Players:GetPlayers(), Players:FindFirstChild(v.Name)) then
+            table.insert(Playerss, Players:FindFirstChild(v.Name))
+            table.insert(Characters, v)
+        end
+    end
+    return Playerss, Characters
 end
 
---// Loops
+local function GetClosestPlayer(index)
+    local plr, dist = nil, math.huge
+    local Players, Characters = GetPlayers()
+    for i,v in next, Characters do
+        if v == Client.Character then continue end
+        if not v:FindFirstChild("HumanoidRootPart") then continue end
+        
+        local mag = (Client.Character:FindFirstChild("HumanoidRootPart").Position - v:FindFirstChild("HumanoidRootPart").Position).Magnitude
+        if mag < dist then
+            dist = mag
+            plr  = index and v[index] or v
+        end
+    end
+    return plr
+end
 
-RunService.PostSimulation:Connect(function()
-   local MousePosition = UserInputService:GetMouseLocation()
-   Client.Target = nil
-   Client.TargetDistance = Drawings[1].Radius
-
-   for _, player in next, Players:GetChildren() do
-
-       local character = workspace:FindFirstChild(player.Name)
-       local Humanoid = character and character:FindFirstChildWhichIsA('Humanoid')
-       local HumanoidRootPart = character and character:FindFirstChild('HumanoidRootPart')
-
-       if not Humanoid or not HumanoidRootPart then
-           continue
-       end
-
-       if Humanoid.Health <= 0 then
-           continue
-       end
-
-       -- if not IsVisible(HumanoidRootPart.Position, player.Character) then
-       --     continue
-       -- end
-
-       local ScreenPosition, ScreenVisible = CurrentCamera:WorldToViewportPoint(HumanoidRootPart.Position)
-       local MouseDistance = (MousePosition - Vector2.new(ScreenPosition.X, ScreenPosition.Y)).magnitude
-
-       if MouseDistance < Client.TargetDistance then
-           Client.Target = character
-           Client.TargetDistance = MouseDistance
-       end
-   end
-end)
-
---// Hooks
-
-local index ; index = hookmetamethod(game, '__index', function(obj, idx)
-
-   if idx:lower() == 'unitray' and Client.Target then
-       local origin = index(obj, idx)
-       return {
-           Origin = origin.Origin,
-           Direction = CFrame.new(origin.Origin, Client.Target.HumanoidRootPart.Position).LookVector
-       }
-   end
-
-   return index(obj, idx)
-end)
-
---// Notify :3
-game.StarterGui:SetCore('SendNotification', {
-   Title = 'Loaded';
-   Text = 'Made by Rileyy#0808';
-   Duration = 3;
-})
+local old; old = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    local caller = checkcaller()
+    local args = {...}
+    if not caller and method == "FireServer" and self.Name == "ThrowKnife" then
+        if typeof(args[1]) == "Vector3" or typeof(args[1]) == "CFrame" then
+            args[1] = GetClosestPlayer("HumanoidRootPart").Position
+        end
+    end
+    return old(self, args)
+end))
