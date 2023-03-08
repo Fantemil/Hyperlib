2b8843a4bbca40e29d91bdfbd2d05619cc87e6e9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:48:00 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Phantom Forces Silent Aim V.lua
@@ -0,0 +1,4 @@
+getgenv().fov = 180 -- Any angle between 0-180 degrees
+getgenv().visibleCheck = false -- true, false
+getgenv().targetedPart = "Head" -- "Head", "Torso" or "Random"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Spoorloos/scripts/main/silent_aim_v2.lua"))()
