5a8fe07fa01f2032383cd1f79b5d410211be4538
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:41:00 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:40:57"
+lastupdate = "GMT +1: 08.03.2023 06:41:00"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4722,3 +4722,5 @@ addscriptuniversal("Universal Free future lighting", "pulled from rbxscript.com"
 addscriptuniversal("Universal Teleports, Walkspeed, Noclip", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Teleports%20Walkspeed%20Noclip.lua")
 
 addscriptuniversal("Universal Auto Chat", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Auto%20Chat.lua")
+
+addscript(11895850995, "Current Game","Viet Pirates X: Auto Drop Fruit, Auto Spawn Fruit", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Viet%20Pirates%20X%20Auto%20Drop%20Fruit%20Auto%20Spawn%20Fruit.lua")
