c6a454412617146b6a89ba652aa6ee682ffdce75
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:12:37 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 13:12:33"
+lastupdate = "GMT +1: 08.03.2023 13:12:37"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7252,3 +7252,5 @@ addscript(11177482306, "Current Game","Streetz War 2: Auto Farm", "pulled from r
 addscriptexist(6679968919, "Fly Race: Auto Laps, Auto Orbs, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fly%20Race%20Auto%20Laps%20Auto%20Orbs%20Auto%20Rebirth.lua")
 
 addscriptexist(6447798030, "Funky Friday: Auto Collect Rings, Sonic Prime Event", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Funky%20Friday%20Auto%20Collect%20Rings%20Sonic%20Prime%20Event.lua")
+
+addscriptexist(9224601490, "Fruit Battlegrounds: Auto Farm V2", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fruit%20Battlegrounds%20Auto%20Farm%20V.lua")
