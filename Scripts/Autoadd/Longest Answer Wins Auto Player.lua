4deb748f79f8ebfc0217e5bbd748340c06e20164
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:23:12 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Longest Answer Wins Auto Player.lua
@@ -0,0 +1,6 @@
+--Dev note may need to send the user a private chat to the person for the privatechatanswers feature to work
+multiplier = 1 -- a block multiplier multiplies the amount of blocks that you go up
+chatanswers = false -- chats answers to everyone
+Privatechatanswers = "" -- Chat the answers to the homies just put your friends username in the string dont wanna private chat then just type in your username
+AnswerSpeed = 14 -- 14 is fastest 1 is slowest dont do outside of these numbers or shit will break set AnswerSpeed to math.random(3,9) if you want it to look really legit
+loadstring(game:HttpGet("https://pastebin.com/raw/y8x6Z9Z9", true))()
