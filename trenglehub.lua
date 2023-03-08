a9094eb5c34aabbf45d93bcf6d58fa96ee6c37d1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:11:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:11:43"
+lastupdate = "GMT +1: 08.03.2023 07:11:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5664,3 +5664,5 @@ addscriptexist(9872472334, "Evade: Fast Revive, Respawn, Fullbright", "pulled fr
 addscript(8283207656, "Current Game","Easy Glass Bridge: Glass Esp", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Easy%20Glass%20Bridge%20Glass%20Esp.lua")
 
 addscript(988099446, "Current Game","MMC Zombies Project: Knife InstaKill", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MMC%20Zombies%20Project%20Knife%20InstaKill.lua")
+
+addscriptexist(2788229376, "Da Hood: Mango Hub", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Mango%20Hub.lua")
