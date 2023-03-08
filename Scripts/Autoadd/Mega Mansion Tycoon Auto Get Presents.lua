69cca325dc50f39c41ab5bf691d91e01bb4f71d6
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:04:03 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Mega Mansion Tycoon Auto Get Presents.lua
@@ -0,0 +1,2 @@
+ game:GetService("ReplicatedStorage").RemoteEvents.RequestGift:FireServer()
+inifinate yield loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
