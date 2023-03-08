1b4782bce2259ec350aa8de2874c5576ba1d58d4
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:42:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:42:34"
+lastupdate = "GMT +1: 08.03.2023 06:42:38"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4774,3 +4774,5 @@ addscriptexist(11701792069, "Snow Plow Simulator: Auto Buy Eggs, Infinity Rebirt
 addscriptexist(9281034297, "Goal Kick Simulator: Anti Afk, Infinity Jumps, Walkspeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Goal%20Kick%20Simulator%20Anti%20Afk%20Infinity%20Jumps%20Walkspeed.lua")
 
 addscriptexist(1962086868, "Tower of Hell: Anti Cheat, Instant Win", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20of%20Hell%20Anti%20Cheat%20Instant%20Win.lua")
+
+addscriptexist(292439477, "Phantom Forces: Hitboxes Expand", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Hitboxes%20Expand.lua")
