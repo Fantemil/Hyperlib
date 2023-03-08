55d26bd7a73f58d43f161dadcb394a0c95907069
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:03:10 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/How Far Can You Fly Inf Coins.lua
@@ -0,0 +1,3 @@
+firetouchinterest(game.Players.LocalPlayer.Character.Head, game:GetService("Workspace").Ramp.Part, 0)
+task.wait(0.3)
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.2").knit.Services.SpeedMultiplierService.RE.Reset:FireServer(math.huge, CFrame.new(0, -434.147003, 10518864, 0, 0, 1, 0, 1, -0, -1, 0, 0))
