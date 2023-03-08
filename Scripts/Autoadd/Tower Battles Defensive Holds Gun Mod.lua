ab3f305a3212aa2440acf5b75f433135f0373b02
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:53:11 +0100

    Updated automatically!

--- Scripts/Autoadd/Tower Battles Defensive Holds Gun Mod.lua
+++ Scripts/Autoadd/Tower Battles Defensive Holds Gun Mod.lua
@@ -1,1 +1,20 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/onedecidedinstealth/There-Is-No-Scripts/main/GotItWorking.lua"))()
+while true do
+   for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+      if v:IsA("Tool") then
+         local deez = require(v.Setting)
+         deez.FireRate = 0
+         deez.Auto = true
+         deez.ReloadTime = 0
+         deez.AmmoPerMag = 999
+         deez.Spread = 0
+         deez.Recoil = 0
+         deez.ShotgunReload = false
+         deez.BulletPerShot = 20
+         deez.BulletPerBurst = 20
+         deez.BulletTracerEnabled = false
+         deez.BulletShellEnabled = false
+         deez.BulletLightEnabled = false
+      end
+   end
+task.wait()
+end
