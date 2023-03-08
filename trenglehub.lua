1a1db6fbf98dc2c402b987ebe12545d3b6401e0d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:56:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:56:42"
+lastupdate = "GMT +1: 08.03.2023 18:56:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12254,3 +12254,5 @@ addscriptexist(9961650006, "Shockwave Racing: Instant Win", "pulled from rbxscri
 addscript(8659293202, "Current Game","The Rake Blood Hour Test: Developer Gui", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Rake%20Blood%20Hour%20Test%20Developer%20Gui.lua")
 
 addscriptexist(3233893879, "Bad Business: Silent Aim, FOV & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bad%20Business%20Silent%20Aim%20FOV%20%20More.lua")
+
+addscriptexist(383793228, "Adopt and Raise a Baby: Infinity Money", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Adopt%20and%20Raise%20a%20Baby%20Infinity%20Money.lua")
