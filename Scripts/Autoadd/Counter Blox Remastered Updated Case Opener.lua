db0792cf4b1b61db295429b72a0e6f6684a92602
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:47:41 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Counter Blox Remastered Updated Case Opener.lua
@@ -0,0 +1,15 @@
+--DADDYWARE FREE CASE OPENER <3
+--discord.gg/7ujuBbCnNJ
+
+local cases = {
+    "Weeb",
+    "Twitch",
+    "Tournament",
+    "Glove",
+    "Halloween 2022",
+    "Case Hardened",
+    "Banana"
+    
+}
+
+local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit;local v4=v3.bxor or v3.bxor;local v5=table.concat;local v6=table.insert;local function v7(v8,v9)local v10={};for v13=1 + (0 -0), #v8 do v6(v10,v0(v4(v1(v2(v8,v13,v13 + 1)),v1(v2(v9,(4 -(765 -(600 + 162))) + ((v13-1)% #v9),1 + 0 + ((v13-1)% #v9) + 1 + 0)))%(349 -93)));end return v5(v10);end while task.wait(0.5) do local chat = cases[math.random(1,table.getn(cases))] getgenv().CaseToOpen = chat for v11,v12 in pairs(game:GetService(v7("\252\187\106\234\38\218\5\218\187\126\213\59\214\22\207\185\127","\174\222\26\134\79\185\100")).COMMUNICATION:GetChildren()) do if (v12:IsA(v7("\5\123\254\31\210\219\226\193\50\112\231","\87\30\147\112\166\190\167\183")) and (v12.Name==v7("\7\63\57\115\135\49\3","\73\122\109\36\200\99\72\155"))) then v12:FireServer(v7("\82\48\31\221\255\80\214\120","\29\64\122\179\188\49\165"),CaseToOpen);end end end
