f016e7b9cdcb8285d9f8780c986409d4d250272b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:24:58 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:24:53"
+lastupdate = "GMT +1: 08.03.2023 17:24:58"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10112,3 +10112,5 @@ addscriptexist(3527629287, "BIG Paintball: Start Auto Farm, Stop Auto Farm, Play
 addscriptexist(8726743209, "Refinery Caves: Woood Auto Farm, Ore Teleports, Keybinds", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Refinery%20Caves%20Woood%20Auto%20Farm%20Ore%20Teleports%20Keybinds.lua")
 
 addscriptexist(5171347293, "Cabin Crew Simulator: Get Gamepasses, Fix All NPC'S problem, Serve all NPC'S Meal", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cabin%20Crew%20Simulator%20Get%20Gamepasses%20Fix%20All%20NPCS%20problem%20Serve%20all%20NPCS%20Meal.lua")
+
+addscriptexist(1537690962, "Bee Swarm Simulator: Auto Farm Field, Auto Dig, Auto Collect", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bee%20Swarm%20Simulator%20Auto%20Farm%20Field%20Auto%20Dig%20Auto%20Collect.lua")
