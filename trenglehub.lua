bce2cc010e932b7a85621c2de5ce2a082376d277
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:18:21 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:18:16"
+lastupdate = "GMT +1: 08.03.2023 18:18:21"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11446,3 +11446,5 @@ addscript(8679682580, "Current Game","Burn The Minions: TPs", "pulled from rbxsc
 addscriptexist(164051105, "Super Bomb Survival: Auto farm, Anti Ragdoll, God mode", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Super%20Bomb%20Survival%20Auto%20farm%20Anti%20Ragdoll%20God%20mode.lua")
 
 addscriptexist(2788229376, "Da Hood: Silent Aim, Teleports, Walkspeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Silent%20Aim%20Teleports%20Walkspeed.lua")
+
+addscript(6159892946, "Current Game","Aim Trainer: Auto Play", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Aim%20Trainer%20Auto%20Play.lua")
