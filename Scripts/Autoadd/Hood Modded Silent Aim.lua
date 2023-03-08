fe40441d57c3b0d314496389107466aba3458891
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:46:12 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Hood Modded Silent Aim.lua
@@ -0,0 +1,7 @@
+_G.FOV =  (  20  )
+
+_G.Prediction =  (  .18  )
+
+_G.AimKey =  (  "c"  )
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Nessa-Rurina/Nosslock/main/SilentAim.lua"), true))()
