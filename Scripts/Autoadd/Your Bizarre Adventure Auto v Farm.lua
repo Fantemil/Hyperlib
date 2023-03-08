cc1bad37d081d33b195f531a2e0c7bcea78366a0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:13:44 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Your Bizarre Adventure Auto v Farm.lua
@@ -0,0 +1,3 @@
+getgenv().MainAcc = "NAME HERE"--Put the name of the account you have TWOH quest or the one u want wins
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/senS24t/sensnebunu/main/OPAuto1v1Rework"))()
