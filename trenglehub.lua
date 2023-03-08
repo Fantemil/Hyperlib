c39928fe85c893df856db99691f8aca60ec77397
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:54:24 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:54:16"
+lastupdate = "GMT +1: 08.03.2023 18:54:24"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12202,3 +12202,5 @@ addscriptexist(3214114884, "Flag Wars: Aim Bot", "pulled from rbxscript.com", "h
 addscriptexist(10243982775, "RedBox: Anti Arrest, Auto Arrest, Bomb All", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RedBox%20Anti%20Arrest%20Auto%20Arrest%20Bomb%20All.lua")
 
 addscript(6939849554, "Current Game","Cart Ride Tycoon thru a Cave 2: Coin Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cart%20Ride%20Tycoon%20thru%20a%20Cave%20%20Coin%20Farm.lua")
+
+addscriptexist(10180880196, "Fling Champions: Auto Complete Launch Area", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fling%20Champions%20Auto%20Complete%20Launch%20Area.lua")
