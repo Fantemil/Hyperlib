6a92e536d9e9c7b1290da471cdbbbdb59f172b1a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:21:28 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Every Second You Get  Speed Auto Farm Wins.lua
@@ -0,0 +1,16 @@
+--NOT FWDS SCRIPT CREDIT TO OWNER
+--Variables to make it easier for ppl to understand
+Wins = game.Workspace.Wins.World7
+Chara = Game.Players.LocalPlayer.Character
+Plr = game.Players.LocalPlayer
+basepart = Chara:FindFirstChildWhichIsA("BasePart")
+--Script
+getgenv().Farm = true
+while getgenv().Farm == true do task.wait()
+   pcall(function() --Because I am lazy and don't wanna spend time optimizing
+       firetouchinterest(Wins,basepart,0) --This gets you the ez dubz
+       task.wait(.2)
+       firetouchinterest(Wins,basepart,1)
+       game.ReplicatedStorage.RebirthEvent:FireServer() --Rebirth remote
+   end)
+end
