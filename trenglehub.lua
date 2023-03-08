dd9945ffedbc8e851a71422598331b42c0f02272
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:35:50 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -4546,8 +4546,6 @@ addscriptexist(5670218884, "Item Asylum: Item Esp", "pulled from rbxscript.com",
 
 addscript(4829985161, "Current Game","Lookout Battle: Auto Collect Bones, Server Crasher & More!", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Lookout%20Battle%20Auto%20Collect%20Bones%20Server%20Crasher%20%20More.lua")
 
-addscriptexist(6284583030, "Pet Simulator X!: Auto Farm, Auto Collect, Auto Hatch", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Auto%20Farm%20Auto%20Collect%20Auto%20Hatch.lua")
-
 addscriptexist(2768379856, "SCP - 3008: God Mode", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SCP%20%20%20God%20Mode.lua")
 
 addscriptexist(10462101644, "Tall Man Run: Kick All, Destroy Everything, Destroy map", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tall%20Man%20Run%20Kick%20All%20Destroy%20Everything%20Destroy%20map.lua")
