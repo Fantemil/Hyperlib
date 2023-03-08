726481f3d55a1e2d0b4de7c0c85b8e951e1f9bda
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:10:55 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:10:50"
+lastupdate = "GMT +1: 08.03.2023 19:10:55"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12560,3 +12560,5 @@ addscriptexist(292439477, "Phantom Forces: Unlock All Guns", "pulled from rbxscr
 addscriptexist(10861112990, "Anime Artifacts Simulator 2: Inf Money", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Artifacts%20Simulator%20%20Inf%20Money.lua")
 
 addscriptexist(9167501050, "Lightsaber Arena: Kill Aura, Ear Rape, Auto Swap Forms", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Lightsaber%20Arena%20Kill%20Aura%20Ear%20Rape%20Auto%20Swap%20Forms.lua")
+
+addscriptexist(2753915549, "Blox Fruits: Auto Farm, Fruit Farm & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Auto%20Farm%20Fruit%20Farm%20%20More.lua")
