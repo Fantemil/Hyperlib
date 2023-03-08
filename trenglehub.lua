a482724263ef7703e9ab7fc080a09537e8a65241
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:43:23 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:43:19"
+lastupdate = "GMT +1: 08.03.2023 17:43:23"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10580,3 +10580,5 @@ addscriptexist(286090429, "Arsenal: Aimbot, ESP, Gun Mods", "pulled from rbxscri
 addscript(10758955211, "Current Game","Skydive Race Clicker: Auto Farm Speeds, Auto Farm Wins", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Skydive%20Race%20Clicker%20Auto%20Farm%20Speeds%20Auto%20Farm%20Wins.lua")
 
 addscriptexist(11187148979, "Ultra Unfair: Auto Farm, Kill Aura, Auto Quest & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultra%20Unfair%20Auto%20Farm%20Kill%20Aura%20Auto%20Quest%20%20More.lua")
+
+addscriptexist(2809202155, "Your Bizarre Adventure: Auto Sbr", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Your%20Bizarre%20Adventure%20Auto%20Sbr.lua")
