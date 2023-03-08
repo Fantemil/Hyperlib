fbb91c89769b00111a7f538b2bd07b1f0ffe5d81
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:33:25 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:33:18"
+lastupdate = "GMT +1: 08.03.2023 12:33:25"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6220,3 +6220,5 @@ addscriptexist(8908228901, "Sharkbite 2: AFK Present Farm", "pulled from rbxscri
 addscriptexist(6361937392, "steal time from others & be the best: Blinker", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/steal%20time%20from%20others%20%20be%20the%20best%20Blinker.lua")
 
 addscriptexist(6361937392, "steal time from others & be the best: Auto Leave", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/steal%20time%20from%20others%20%20be%20the%20best%20Auto%20Leave.lua")
+
+addscriptexist(189707, "Natural Disaster Survival: Walkspeed Slider, Teleports, Vote for Map", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Natural%20Disaster%20Survival%20Walkspeed%20Slider%20Teleports%20Vote%20for%20Map.lua")
