6dee74e835e4c76063d8e685dcfe33168c8bf017
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:24:02 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Sharkbite  Modify Turrets.lua
@@ -0,0 +1,12 @@
+for _,v in pairs(getgc(true)) do
+   if type(v) == "table" and rawget(v,"Mode") then
+       if v.Name == "Big Cannon" then
+               v.FireRate = 10
+               v.Mode = 2
+               v.Range = 5000
+           else
+               v.FireRate = 8
+               v.Range = 5000
+       end
+   end
+end
