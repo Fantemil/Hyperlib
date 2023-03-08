beaccc932cf13159598032f95aef21fab3225e92
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:27:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:27:28"
+lastupdate = "GMT +1: 08.03.2023 20:27:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13906,3 +13906,5 @@ addscriptexist(5076734934, "Rooms [Hunting Status Check GUI]", "pulled from rbxs
 addscriptexist(2753915549, "Blox Fruits [GUI - Auto Farm Level, Auto Farm Mastery & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20GUI%20%20Auto%20Farm%20Level%20Auto%20Farm%20Mastery%20%20More.lua")
 
 addscript(6884476776, "Current Game","Friend Hangout [Crash - Get All Tools & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Friend%20Hangout%20Crash%20%20Get%20All%20Tools%20%20More.lua")
+
+addscriptexist(10676523834, "Racing Rocket [Infinite Speed]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Racing%20Rocket%20Infinite%20Speed.lua")
