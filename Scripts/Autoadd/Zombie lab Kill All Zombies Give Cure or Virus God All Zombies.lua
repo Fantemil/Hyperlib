d7e89dfe55d8270c29173ec3aaf4dfe69d51ad97
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:46:18 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Zombie lab Kill All Zombies Give Cure or Virus God All Zombies.lua
@@ -0,0 +1,7 @@
+-- discord.gg/E3CJyA7vFH if you want to know when its patched or not.
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ironicw/opscripts/main/Skilledware/Loader", true))()
+
+while wait() do
+    game.Players.LocalPlayer.Character["Anti-Exploits"].Disabled = true
+end
