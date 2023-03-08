d80fc339b1cffa2ac646d579be0b47c3705b895f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:46:30 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:46:26"
+lastupdate = "GMT +1: 08.03.2023 12:46:30"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6588,3 +6588,5 @@ addscriptuniversal("Universal Voice Chat Bypass", "pulled from rbxscript.com", "
 addscript(6607361476, "Current Game","Shopping Wars: RB Battles Event", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shopping%20Wars%20RB%20Battles%20Event.lua")
 
 addscriptexist(6422372837, "Snipers [BETA]: Silent Aim, No Shot Cooldown & More!", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Snipers%20BETA%20Silent%20Aim%20No%20Shot%20Cooldown%20%20More.lua")
+
+addscriptexist(11547613362, "Anime Fly Race: Auto Chi Getter", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Fly%20Race%20Auto%20Chi%20Getter.lua")
