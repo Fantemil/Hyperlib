52c1e9ab28a9303427994aca8e2967f1854fbd4c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:41:20 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:41:14"
+lastupdate = "GMT +1: 08.03.2023 17:41:20"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10526,3 +10526,5 @@ addscriptexist(10714365287, "Anime Race Clicker: Auto Hatch Eggs, Auto Equip Bes
 addscriptexist(2377868063, "Strucid: Makes skiders more powerfull", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Strucid%20Makes%20skiders%20more%20powerfull.lua")
 
 addscriptexist(2248408710, "Destruction Simulator: Auto Sell, Instant FIre, Fast Rocket", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Destruction%20Simulator%20Auto%20Sell%20Instant%20FIre%20Fast%20Rocket.lua")
+
+addscript(9528144165, "Current Game","0ne Piece: Auto Farm B", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ne%20Piece%20Auto%20Farm%20B.lua")
