d48afb94080d469bd6eecb8e4839f5729f9da801
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:40:54 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:40:47"
+lastupdate = "GMT +1: 08.03.2023 18:40:54"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11938,3 +11938,5 @@ addscript(4499822233, "Current Game","Star Wars Bounty Hunters: Outer Rim: Infin
 addscript(9415736218, "Current Game","Sushi Restaurant Tycoon: Infinity Golden Eggs", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sushi%20Restaurant%20Tycoon%20Infinity%20Golden%20Eggs.lua")
 
 addscript(4540286603, "Current Game","NORTHWIND: Infinity Stamina, No gun spread, ESP For chests", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NORTHWIND%20Infinity%20Stamina%20No%20gun%20spread%20ESP%20For%20chests.lua")
+
+addscriptuniversal("Universal Sword Auto Killer", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Sword%20Auto%20Killer.lua")
