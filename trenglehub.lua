36117da7ca9175a8ee5d50629bc3ec55d46b50a0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:17:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:17:27"
+lastupdate = "GMT +1: 08.03.2023 17:17:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9924,3 +9924,5 @@ addscriptexist(2866967438, "Fishing Simulator: Auto Farm, Auto Kill Sharks & Mor
 addscriptexist(9285238704, "Race Clicker: Tp To Haloween, Auto Farm, Server Hop", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Race%20Clicker%20Tp%20To%20Haloween%20Auto%20Farm%20Server%20Hop.lua")
 
 addscript(10628787331, "Current Game","Hill Disasters: Inf Coin", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hill%20Disasters%20Inf%20Coin.lua")
+
+addscriptexist(445664957, "Parkour: Sr Flow, Set Combo, Infinite Magrail", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Parkour%20Sr%20Flow%20Set%20Combo%20Infinite%20Magrail.lua")
