8ff8da8ad45cf040ad290b09072e0f28f1618fa0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:54:06 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal FE PNG Loader.lua
@@ -0,0 +1,6 @@
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Lowerrated/rbx-f3x-png-loader/main/script.lua"))()
+--https://www.roblox.com/games/7797017666/F3X
+--https://www.roblox.com/games/4641455041
+--https://www.roblox.com/games/1071384685
+--https://www.roblox.com/games/6156255015
+--https://www.roblox.com/games/4695287616
