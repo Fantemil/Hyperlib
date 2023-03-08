c8c5fc3fd60c0f5f9c3e8c1b4c9a38bae807994c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:51:21 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:51:17"
+lastupdate = "GMT +1: 08.03.2023 17:51:21"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10778,3 +10778,5 @@ addscriptexist(5244411056, "Bloxy Bingo: Auto Bingo, Auto Mark, Dark Theme", "pu
 addscript(7050008107, "Current Game","Hospital Tycoon: Auto Buy Buttons, Auto Collect Cash, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hospital%20Tycoon%20Auto%20Buy%20Buttons%20Auto%20Collect%20Cash%20Auto%20Rebirth.lua")
 
 addscriptexist(142823291, "Murder Mystery 2: Gun Grabber, Noclip & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Murder%20Mystery%20%20Gun%20Grabber%20Noclip%20%20More.lua")
+
+addscript(4109933355, "Current Game","House Tycoon: Auto Collect Cash, Anti Afk, Auto Build", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/House%20Tycoon%20Auto%20Collect%20Cash%20Anti%20Afk%20Auto%20Build.lua")
