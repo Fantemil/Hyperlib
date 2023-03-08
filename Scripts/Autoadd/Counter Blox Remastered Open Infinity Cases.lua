f372c459e7145a5acd0d73cc5b3d40dad039a116
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:00:19 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Counter Blox Remastered Open Infinity Cases.lua
@@ -0,0 +1,189 @@
+local VLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vep1032/VepStuff/main/VL"))()
+
+
+
+local s = VLib:Window("Edit of Kodescripts  script ", "Welcome!", "Pro")
+
+
+
+local Main = s:Tab("Main")
+
+
+
+
+
+Main:Button('Inf weeb case',function()
+
+getgenv().CaseToOpen = "Weeb"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf Twitch case',function()
+
+getgenv().CaseToOpen = "Twitch"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf tournamement case',function()
+
+getgenv().CaseToOpen = "Tournament"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf Kitter case',function()
+
+getgenv().CaseToOpen = "Kitter"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf SCR case',function()
+
+getgenv().CaseToOpen = "SCR"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf Vortax case',function()
+
+getgenv().CaseToOpen = "Vortax"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf mordern case',function()
+
+getgenv().CaseToOpen = "Modern"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf Imaginem case',function()
+
+getgenv().CaseToOpen = "Imaginem"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf Hiato case',function()
+
+getgenv().CaseToOpen = "Hiato"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf Glove case',function()
+
+getgenv().CaseToOpen = "Glove"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
+
+Main:Button('Inf Karambit case',function()
+
+getgenv().CaseToOpen = "Karambit"
+
+getgenv().caseopen = true
+
+getgenv().cooldown = 0.1
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kodescripts/scripts/main/CaseOpener.lua"))()
+
+
+
+end)
