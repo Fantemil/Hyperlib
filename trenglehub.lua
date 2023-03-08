1514e9838d5fcd5767e3c022af4cb5a162ff7022
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:19:43 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:19:39"
+lastupdate = "GMT +1: 08.03.2023 18:19:43"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11480,3 +11480,5 @@ addscriptuniversal("Universal Silent Aim Gui", "pulled from rbxscript.com", "htt
 addscript(10232731562, "Current Game","Pro Piece PRO MAX: God MOde, Insta Kill, Auto Fruits", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pro%20Piece%20PRO%20MAX%20God%20MOde%20Insta%20Kill%20Auto%20Fruits.lua")
 
 addscriptexist(6516141723, "DOORS: Summon Entities", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Summon%20Entities.lua")
+
+addhub("Space Hub UI Icons", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Space%20Hub%20UI%20Icons.lua")
