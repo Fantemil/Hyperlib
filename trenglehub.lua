c509ff118f56343f0b281af8a88bb1e1111c663b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:27:56 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:27:51"
+lastupdate = "GMT +1: 08.03.2023 18:27:56"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11686,3 +11686,5 @@ addscriptexist(8726743209, "Refinery Caves: Tp Waypoint, WalkSpeed,WalkJump", "p
 addscriptexist(3260590327, "Tower Defense Simulator: Auto Collect Razors", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20Defense%20Simulator%20Auto%20Collect%20Razors.lua")
 
 addscriptuniversal("Universal Synapse V3", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Synapse%20V.lua")
+
+addscriptexist(189707, "Natural Disaster Survival: NO Fall Damage, Auto Farm, Remove Snow", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Natural%20Disaster%20Survival%20NO%20Fall%20Damage%20Auto%20Farm%20Remove%20Snow.lua")
