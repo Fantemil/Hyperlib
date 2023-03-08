bd5d53ba3609ea5b66cc31a12ce523c370225f1e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:53:40 +0100

    Updated automatically!

--- Scripts/Autoadd/Lumber Tycoon  Dupe Axe.lua
+++ Scripts/Autoadd/Lumber Tycoon  Dupe Axe.lua
@@ -3,10 +3,13 @@ local client = game:GetService('Players').LocalPlayer;
 local clientInteracted = game:GetService('ReplicatedStorage').Interaction.ClientInteracted;
 local function dupeAxe()
    if (not client.Character or not client.Character:FindFirstChild('Humanoid') or not client.Character:FindFirstChild('Head')) then return end;
+
    client.Character.Humanoid:EquipTool(client.Backpack[axeName]);
    clientInteracted:FireServer(client.Character.Tool, 'Drop Tool', client.Character.Head.CFrame);
    client.Character.Head:Destroy();
 end;
+
+
 while true do
    dupeAxe();
    task.wait(2);
