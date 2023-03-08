c5a6f798173919779b18cec589b13c5f127e98f1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:11:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 15:11:28"
+lastupdate = "GMT +1: 08.03.2023 15:11:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -8692,3 +8692,5 @@ addscriptuniversal("Universal Anti Cheat", "pulled from rbxscript.com", "https:/
 addscriptexist(6407649031, "NO-SCOPE ARCADE: Silent Aim, No Recoil, No Spread", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NOSCOPE%20ARCADE%20Silent%20Aim%20No%20Recoil%20No%20Spread.lua")
 
 addscriptexist(9825515356, "Hood Customs: Press E to Silent Aim", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hood%20Customs%20Press%20E%20to%20Silent%20Aim.lua")
+
+addscriptexist(292439477, "Phantom Forces: FOV, Silent Aim & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20FOV%20Silent%20Aim%20%20More.lua")
