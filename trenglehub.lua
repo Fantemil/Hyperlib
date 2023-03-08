d9e2820f04e08572e1ee372851c079c753b18304
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:11:19 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:11:15"
+lastupdate = "GMT +1: 08.03.2023 18:11:19"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11276,3 +11276,5 @@ addscriptexist(137877687, "RoCitizens: Account Generator", "pulled from rbxscrip
 addscriptexist(920587237, "Adopt Me: Auto Win", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Adopt%20Me%20Auto%20Win.lua")
 
 addscriptexist(8369888266, "Redcliff City RP: GAmepass, RainBow car, RainBow Avatar", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Redcliff%20City%20RP%20GAmepass%20RainBow%20car%20RainBow%20Avatar.lua")
+
+addscriptexist(286090429, "Arsenal: Silent Aim, Aimbot, ESP", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20Silent%20Aim%20Aimbot%20ESP.lua")
