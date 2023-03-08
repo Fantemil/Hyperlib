8287f997a708861b208babf6276eefe7ae97490f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:39:04 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Blox Fruits  GUI  Auto Saber Auto Farm  More.lua
@@ -0,0 +1,4 @@
+-- Key: 8f93bbb7b45b07ba45468b796b81182992e7feefd22d0a36214dbade76e361b8
+-- key changes often so if it dont work say it in comments and ill update it
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Corrupt2625/pjv/main/Bloxfruit.lua", true))()
