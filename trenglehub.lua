d67be9fd95e938bdb4b9b4a9e1b2f1643ecf1406
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:55:18 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:55:14"
+lastupdate = "GMT +1: 08.03.2023 06:55:18"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5162,3 +5162,5 @@ addscript(8508161757, "Current Game","Raise An Albert: Auto Collect Money", "pul
 addscriptexist(7560156054, "Clicker Simulator: Auto Click, Auto Claim Gifts, Auto Collect Chests", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Clicker%20Simulator%20Auto%20Click%20Auto%20Claim%20Gifts%20Auto%20Collect%20Chests.lua")
 
 addscriptexist(8131063774, "Shattered Psycho Online: Data Rollback", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shattered%20Psycho%20Online%20Data%20Rollback.lua")
+
+addscriptexist(292439477, "Phantom Forces: Knife Aura", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Knife%20Aura.lua")
