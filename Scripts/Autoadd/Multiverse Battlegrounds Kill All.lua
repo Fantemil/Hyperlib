e8b83b265fa3792147f4d18de048d2ec550a6299
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:23:56 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Multiverse Battlegrounds Kill All.lua
@@ -0,0 +1,38 @@
+local Players = game:GetService("Players")
+local Client = game:GetService("Players").LocalPlayer
+
+local Stepped = game:GetService("RunService").Stepped
+
+firetouchinterest(Client.Character.HumanoidRootPart, Workspace.Morphs["Darth Vader"].Pad, 0)
+
+task.wait(1)
+
+Client.Backpack["DarthVader-3"].Parent = Client.Character
+
+task.wait(1)
+
+local Target
+
+Stepped:Connect(function ()
+    for i,v in next, Players:GetPlayers() do
+        if v ~= Client and v.Character ~= nil and v.Character:FindFirstChild("HumanoidRootPart") and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health > 0 and v.Backpack:FindFirstChildOfClass("Tool") then
+            Target = v.Character
+        end
+    end
+end)
+
+task.spawn(function ()
+    while[color=#c3ccdb][font="JetBrains Mono", Consolas, "Courier New", monospace, Consolas, "Courier New", monospace] [/font][/color]task.wait(.1) do
+        if Target ~= nil then
+            local Tool = Client.Character:FindFirstChild("DarthVader-3")
+            local Root = Target.HumanoidRootPart
+
+            if Tool then
+                Tool.GetEvent:FireServer(Root.Position + Root.Velocity / 5)
+                Tool.Event:FireServer(0)
+            elseif not Tool then
+                Client.Backpack["DarthVader-3"].Parent = Client.Character
+            end
+        end
+    end
+end)
