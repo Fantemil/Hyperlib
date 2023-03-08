4c1d3a13db4305a9e09e7ecc247e0f3c7cb07389
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:20:53 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Sharkbite  Modify Speed.lua
@@ -0,0 +1,7 @@
+local Speed = 350
+local hook; hook = hookmetamethod(game, "__index", newcclosure(function(int,property)
+   if tostring(int) == "MaxSpeed" and property == "Value" then
+       return Speed
+   end
+   return hook(int,property)
+end))
