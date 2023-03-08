c155ea48be2e7495940a71a02b3e6e0141b6ec25
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:37:40 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:37:36"
+lastupdate = "GMT +1: 08.03.2023 20:37:40"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14166,3 +14166,5 @@ addscriptexist(5602055394, "Hood Modded [Silent Aim, Esp]", "pulled from rbxscri
 addscriptuniversal("Roblox Universal Script [Off Chat]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Universal%20Script%20Off%20Chat.lua")
 
 addscriptexist(10347946161, "Rat Washing Tycoon [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rat%20Washing%20Tycoon%20Auto%20Farm.lua")
+
+addscript(10705886260, "Current Game","Strucid [Anti-Cheat Bypass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Strucid%20AntiCheat%20Bypass.lua")
