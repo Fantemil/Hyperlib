03d22f476a20688bcebf4ae53d8ab18d29951490
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:26:55 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Killstreak Sword Fighting BETA Auto Farm Glacier Location.lua
@@ -0,0 +1,6 @@
+while true do
+    wait(1)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1181, 5025, -51)
+
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1132, 5025, -51)
+end 
