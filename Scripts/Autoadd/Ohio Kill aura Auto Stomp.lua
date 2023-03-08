195a8f7070961b624ca68eeaf8147d40ae048215
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:54:39 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ohio Kill aura Auto Stomp.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/BeboScripts/main/Ohio.lua", true))()
+
+            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").BankRobbery.BankCash.Main.CFrame
+-- Bank Tp 
