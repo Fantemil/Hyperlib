6b8205f6de08a1ccd22506730f2d5041aba3644a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:12:54 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:12:49"
+lastupdate = "GMT +1: 08.03.2023 17:12:54"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9800,3 +9800,5 @@ addscriptexist(10919241870, "Rarity Factory Tycoon: Auto Collect, Auto Deposit, 
 addscriptexist(5956785391, "Project Slayers: Auto Breath", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Auto%20Breath.lua")
 
 addscriptexist(537413528, "Build A Boat For Treasure: Auto Farm, Auto Chest, Anti AFK", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Farm%20Auto%20Chest%20Anti%20AFK.lua")
+
+addscriptexist(2788229376, "Da Hood: Auto Kill, Fake Macro & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Auto%20Kill%20Fake%20Macro%20%20More.lua")
