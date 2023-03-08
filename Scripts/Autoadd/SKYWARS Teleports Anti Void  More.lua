c1f4bcdd900ced64ce13c20c8268a8455d529bb8
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:13:13 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/SKYWARS Teleports Anti Void  More.lua
@@ -0,0 +1,5 @@
+_G.Key = "ROFLUX-e0bPt-toD-rzCTXd"
+--- https://discord.gg/8gNrnWmgcb
+pcall(function()
+  loadstring(game:HttpGet("https://roflux.net/scripts/games/" .. tostring(game.PlaceId) .. ".lua"))()
+end)
