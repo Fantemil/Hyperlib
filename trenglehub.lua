4c165faec0aea6fe79a538e8d900f4c88d0b706b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:16:27 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:16:23"
+lastupdate = "GMT +1: 08.03.2023 18:16:27"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11402,3 +11402,5 @@ addscriptexist(9941633436, "Rank Simulator: Auto Collect Candy Corn", "pulled fr
 addscriptexist(606849621, "Jailbreak: KillAura, Open Source", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jailbreak%20KillAura%20Open%20Source.lua")
 
 addscriptexist(2753915549, "Blox Fruits: Auto Farm, Buy Items & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Auto%20Farm%20Buy%20Items%20%20More.lua")
+
+addscript(7002808196, "Current Game","Симулятор падения в Канаву: AutoFarm, Remove Animations, Anti Afk", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%D0%A1%D0%B8%D0%BC%D1%83%D0%BB%D1%8F%D1%82%D0%BE%D1%80%20%D0%BF%D0%B0%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%B2%20%D0%9A%D0%B0%D0%BD%D0%B0%D0%B2%D1%83%20AutoFarm%20Remove%20Animations%20Anti%20Afk.lua")
