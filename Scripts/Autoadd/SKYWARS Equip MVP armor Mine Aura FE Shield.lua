bfc7bec5f5e3bcdc50df166cb0a16f68ddfbb281
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:56:03 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/SKYWARS Equip MVP armor Mine Aura FE Shield.lua
@@ -0,0 +1,5 @@
+-- Select Your Version
+-- Versions: 1.3 and 1.4 (Loads Latest)
+_Version = "1.3"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHanki/Shitux/main/Versions/".. "v" .. _Version .. ".lua", true))()
