7fa6862ec43b54c036205db44da2425499b8afea
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:22:09 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Vliccs Difficulty Chart Obby  Get All Nametags.lua
@@ -0,0 +1,7 @@
+ 
+game:GetService("ReplicatedStorage").BuyEvent:FireServer("Bought1")
+game:GetService("ReplicatedStorage").BuyEvent:FireServer("Bought2")
+game:GetService("ReplicatedStorage").BuyEvent:FireServer("Bought3")
+game:GetService("ReplicatedStorage").BuyEvent:FireServer("Bought4")
+game:GetService("ReplicatedStorage").BuyEvent:FireServer("Bought5")
+game:GetService("ReplicatedStorage").BuyEvent:FireServer("Bought6")
