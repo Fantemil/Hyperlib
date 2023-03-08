9a516da48a26b58930c00fe258ec8e3c36e1e111
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:37:15 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Kaizen Auto Log Quest.lua
@@ -0,0 +1,33 @@
+--my discord server: https://discord.gg/3xHHGB3pRG
+--my discord tag: Redninjaawesome#9097
+--my discord id: 840685139393511425
+--this script is made by me
+if game.PlaceId == 7525610732 then
+    if not game:IsLoaded() then game.Loaded:Wait() end
+    local behindamountmain = -15
+    local mainnpc = game.Workspace.Interactions.Sidequests.Midlands.TreeLogSideQuest
+    local behindamountsecond = -20
+    local secondnpc = game.Workspace.Interactions.Sidequests.Midlands.TreeLogOtherNPC
+    pcall(function() print("") wait() 
+    wait(4.5)
+    game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = mainnpc.CFrame + mainnpc.CFrame.UpVector * behindamountmain
+    wait(.35)
+    game:GetService("ReplicatedStorage").Knit.Services.interactService.RF.GetOptionData:InvokeServer("TreeLogSideQuest", "Yes")
+    wait(.35)
+    game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = secondnpc.CFrame + secondnpc.CFrame.UpVector * behindamountsecond
+    wait(.35)
+    game:GetService("ReplicatedStorage").Knit.Services.interactService.RF.GetInitFunction:InvokeServer("TreeLogOtherNPC")
+    wait(.35)
+    game:GetService("ReplicatedStorage").Knit.Services.interactService.RF.GetOptionData:InvokeServer("TreeLogOtherNPC", "Okay")
+    wait(.35)
+    game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = mainnpc.CFrame + mainnpc.CFrame.UpVector * behindamountmain
+    wait(.35)
+    for i = 1, 5 do
+        wait(.10)
+    game:GetService("ReplicatedStorage").Knit.Services.interactService.RF.GetInitFunction:InvokeServer("TreeLogSideQuest")
+    end
+    wait(.25)
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+    end)
+    
+    else return end
