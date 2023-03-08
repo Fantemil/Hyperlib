461357ed3853893a2ad6f86c1ea71dad772de5cc
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:20:50 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:20:46"
+lastupdate = "GMT +1: 08.03.2023 17:20:50"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10006,3 +10006,5 @@ addscriptexist(10723695195, "Anime Evolution Simulator: Auto Farm & More", "pull
 addscriptexist(7817016260, "Bloxston Mystery: Expose Roles In Chat", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bloxston%20Mystery%20Expose%20Roles%20In%20Chat.lua")
 
 addscript(4188447592, "Current Game","Cat Simulator: Auto Farm Claw & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cat%20Simulator%20Auto%20Farm%20Claw%20%20More.lua")
+
+addscriptexist(10628787331, "Hill Disasters: Free Coins", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hill%20Disasters%20Free%20Coins.lua")
