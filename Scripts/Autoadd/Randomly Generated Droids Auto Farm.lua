6f756b45146984fea8db6ce328b3940c6e3a0598
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:54:14 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Randomly Generated Droids Auto Farm.lua
@@ -0,0 +1,7 @@
+getgenv().Setting = {
+ AutoFarm = true,
+ AutoRestart = true,
+ DebugConsole = false,
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/GhostDuckyy/main/Source/Randomly%20Generated%20Droids.lua"))()
