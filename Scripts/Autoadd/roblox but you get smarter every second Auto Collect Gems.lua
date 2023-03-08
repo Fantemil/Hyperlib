f9eb0346ae566a4442db464d583b79074db3eed2
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:35:23 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/roblox but you get smarter every second Auto Collect Gems.lua
@@ -0,0 +1,20 @@
+getgenv().toggle = true
+local table = {
+   game:GetService("Workspace").Z2Gems,
+   game:GetService("Workspace").Z1Gems,
+   game:GetService("Workspace").Z3Gems,
+   game:GetService("Workspace").Z6Gems,
+   game:GetService("Workspace").Z4Gems,
+   game:GetService("Workspace").Z7Gems,
+   game:GetService("Workspace").Z5Gems,
+}
+while task.wait(3) and toggle do
+   for a,b in pairs(table) do
+       for i,v in pairs(b:GetDescendants()) do
+           if v.Name == "TouchInterest" then
+               firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0)
+               firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1)
+           end
+       end
+   end
+end
