317d0ceb625ae58ada542c977e80dab3e8b77a8d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:03:02 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Adopt Me Make pet rideable.lua
@@ -0,0 +1,8 @@
+while true do
+   wait()
+local clientData = require(game.ReplicatedStorage.ClientModules.Core.ClientData)
+local playerData = clientData.get_data()[tostring(game.Players.LocalPlayer)]
+for i,v in pairs(playerData.inventory.pets) do
+v.properties.rideable = true
+end
+end
