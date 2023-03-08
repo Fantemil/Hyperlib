0357dd1fb75edf30579dd64da84b477a7cb0dc91
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:09:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:09:09"
+lastupdate = "GMT +1: 08.03.2023 19:09:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12528,3 +12528,5 @@ addscript(10118504428, "Current Game","My Prison: Arrest All, Auto Clean, Auto F
 addscriptexist(6875469709, "Strongest Punch Simulator: Teleport Speed, Auto Farm, Auto Punch", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Strongest%20Punch%20Simulator%20Teleport%20Speed%20Auto%20Farm%20Auto%20Punch.lua")
 
 addscriptexist(9284097280, "Apartment Tycoon: Auto Collect Cash, Auto Buy Buttons, Anti Afk", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apartment%20Tycoon%20Auto%20Collect%20Cash%20Auto%20Buy%20Buttons%20Anti%20Afk.lua")
+
+addscriptexist(10675066724, "Slime Tower Tycoon: Auto Merge, Auto Collect, Auto Deposit", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Slime%20Tower%20Tycoon%20Auto%20Merge%20Auto%20Collect%20Auto%20Deposit.lua")
