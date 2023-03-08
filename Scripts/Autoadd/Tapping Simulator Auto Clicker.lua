77394e7eee379a185adecff95da7bef9abb5c9da
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 21:16:13 +0100

    Updated automatically!

--- Scripts/Autoadd/Tapping Simulator Auto Clicker.lua
+++ Scripts/Autoadd/Tapping Simulator Auto Clicker.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DylanIsAKingL/Lua-Scripts/main/autoClicker.lua"))()
+while true do 
+    game:GetService("ReplicatedStorage").Events.Tap:FireServer()
+    wait()
+    game:GetService("ReplicatedStorage").Events.Rebirth:FireServer((game.Players.LocalPlayer.PlayerGui.UI.QuickRebirth.CurrentRebirth.Amount.LocalizedText))
+    wait()
+end
