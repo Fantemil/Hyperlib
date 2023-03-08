54958a6c0cf6e0a2a043ee2c57eee744706e640d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:58:49 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 16:58:44"
+lastupdate = "GMT +1: 08.03.2023 16:58:48"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9444,3 +9444,5 @@ addscript(4870984160, "Current Game","Cornerstone Arcade: Auto Farm Work", "pull
 addscriptexist(5956785391, "Project Slayers: Kill Aura, Noclip, Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Kill%20Aura%20Noclip%20Auto%20Farm.lua")
 
 addscriptexist(5865858426, "Retail Tycoon 2: Philips Auto Farm, Get Ugc Item", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Retail%20Tycoon%20%20Philips%20Auto%20Farm%20Get%20Ugc%20Item.lua")
+
+addscriptexist(3260590327, "Tower Defense Simulator: Auto Chain", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20Defense%20Simulator%20Auto%20Chain.lua")
