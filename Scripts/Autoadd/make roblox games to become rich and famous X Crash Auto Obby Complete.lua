9dd223fed7b263cb3a749becb22eadede3521fc0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:59:02 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/make roblox games to become rich and famous X Crash Auto Obby Complete.lua
@@ -0,0 +1,15 @@
+local SavePos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(67.47209167480469, 16.500003814697266, -315.5617980957031)
+task.wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(67.47196960449219, 0.7999792098999023, -443.66168212890625)
+task.wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(SavePos)
+task.wait()
+while wait(302) do
+    local SavePos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(67.47209167480469, 16.500003814697266, -315.5617980957031)
+    task.wait()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(67.47196960449219, 0.7999792098999023, -443.66168212890625)
+    task.wait(0.5)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(SavePos)
+    end 
