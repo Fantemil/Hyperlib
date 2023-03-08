3921646f5602bdc7fd2222ab194dd8184df9fa67
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:21:29 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Emergency Response Liberty County Anti Arrest  Anti Taze  More.lua
@@ -0,0 +1,20 @@
+if syn then
+    rconsoleprint('ouughhh my booty itchin\' so bad\n')
+end
+task.spawn(function()
+   while wait() do
+       local hum = game.Players.LocalPlayer.Character:WaitForChild('Humanoid')
+       if hum:GetState(Enum.HumanoidStateType.PlatformStanding) then
+           if game.StarterGui:GetCoreGuiEnabled(
+               Enum.CoreGuiType.EmotesMenu
+           ) == false then
+               game.Players.LocalPlayer.Character.UpperTorso:Destroy()
+               --fart = game.Players.LocalPlayer.Character['HumanoidRootPart'].CFrame
+               --repeat task.wait() until game.Players.LocalPlayer.Character:FindFirstChild('UpperTorso')
+                --wait(1)
+                --_G.TP_Exception()
+                --game.Players.LocalPlayer.Character['HumanoidRootPart'].CFrame = fart
+           end
+       end
+   end
+end)
