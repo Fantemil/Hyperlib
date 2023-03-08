fc356677ad512e7cbdeb264e0f4cd62bc0384e7c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:49:02 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ant Life Beta Testing Kill Aura ESP.lua
@@ -0,0 +1,38 @@
+_G.killaura = true --Set to false if u want stop kill aura.
+
+--// CUSTOMIZABLE VARS
+
+--// ANT OPTIONS
+_G.antchamColors = {
+   ["Fire Ant"] = Color3.fromRGB(255,60,0),
+   ["Yellow Crazy Ant"] = Color3.fromRGB(0,255,0),
+   ["Carpenter Ant"] = Color3.fromRGB(0,0,255)
+}
+
+_G.antdefaultColors = {
+   ["Larvae"] = Color3.fromRGB(255,255,255),
+   ["Eggs"] = Color3.fromRGB(255,255,255)
+}
+
+_G.antdefaultSize = { --Increases to make smaller
+   ["Queen"] = 1
+}
+
+--//SPIDER OPTIONS
+
+_G.spiderchamColors = {
+   ["Spider"] = Color3.fromRGB(130,130,130),
+   ["Spiderling"] = Color3.fromRGB(160,160,160)
+}
+
+_G.spiderdefaultSize = { --increase to make smaller
+   ["Eggs"] = 1
+}
+
+loadstring(game:HttpGet("https://pastebin.com/raw/ncCVArfN"))() --chams
+
+_G.maxdistance = 500
+loadstring(game:HttpGet("https://pastebin.com/raw/JCBj4xL5"))() --ESP
+
+--_G.killaura = true
+loadstring(game:HttpGet("https://pastebin.com/raw/FhLDKube"))() --Killaura
