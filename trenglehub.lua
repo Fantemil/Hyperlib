cd4fe946437202c7345e69a4669dd334bf94ac19
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:48:36 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:48:31"
+lastupdate = "GMT +1: 08.03.2023 18:48:36"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12084,3 +12084,5 @@ addscript(9649198724, "Current Game","My Kennel: Infinite Coins", "pulled from r
 addscriptexist(8540346411, "Rebirth Champions X: Get All Crystals", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rebirth%20Champions%20X%20Get%20All%20Crystals.lua")
 
 addscriptexist(9912491609, "How Far Can You Fly: Infinite Money", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/How%20Far%20Can%20You%20Fly%20Infinite%20Money.lua")
+
+addscriptexist(9912491609, "How Far Can You Fly: Auto Buy Walking Speed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/How%20Far%20Can%20You%20Fly%20Auto%20Buy%20Walking%20Speed.lua")
