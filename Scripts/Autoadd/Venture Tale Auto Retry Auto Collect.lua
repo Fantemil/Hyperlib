f36c4ef0b3a63f9b424f6de689cabb4891378a56
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 21:17:42 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Venture Tale Auto Retry Auto Collect.lua
@@ -0,0 +1,12 @@
+--[[
+   ventureAI
+   
+   by saucekid (https://discord.gg/eX5k7TKN4F)
+   
+   CREDITS:
+   - sukaretto#8874 (https://discord.gg/PFuQMWMhQQ)
+   - ObscureScapter (https://v3rmillion.net/member.php?action=profile&uid=387561)
+--]]
+
+_G.safemode = true -- Kicks you if game is updated
+loadstring(game:HttpGet("https://raw.githubusercontent.com/saucekid/scripts/main/Venture%20Tale/ventureAI.lua"))()
