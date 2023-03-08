11a2466747b0a8e356d211b5123cb2194047c170
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:52:50 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Phantom Forces Speed.lua
@@ -0,0 +1,3 @@
+game.Players.LocalPlayer.Character.Humanoid:Destroy()
+Instance.new('Humanoid').Parent = game.Players.LocalPlayer.Character
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
