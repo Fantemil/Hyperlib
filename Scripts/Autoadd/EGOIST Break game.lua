ed165f659d12f92ce13ce638494e5a19936e56ab
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:37:16 +0100

    Updated automatically!

--- Scripts/Autoadd/EGOIST Break game.lua
+++ Scripts/Autoadd/EGOIST Break game.lua
@@ -1,9 +1,8 @@
 while wait() do
 local args = {
-   [1] = (0/0),
-   [2] = CFrame.new(Vector3.new(-39.07132339477539, 11.168498039245605, 65.45663452148438), Vector3.new(-0, -0.25881797075271606, -0.9659261703491211)),
-   [3] = "Front"
+[1] = (0/0),
+[2] = CFrame.new(Vector3.new(-39.07132339477539, 11.168498039245605, 65.45663452148438), Vector3.new(-0, -0.25881797075271606, -0.9659261703491211)),
+[3] = "Front"
 }
-
 game:GetService("ReplicatedStorage").Kick:FireServer(unpack(args))
 end
