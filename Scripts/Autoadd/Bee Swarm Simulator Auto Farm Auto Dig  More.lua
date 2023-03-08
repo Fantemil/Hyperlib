58ce6b72c49cfe16f8bce41caafed9c29e9aa25a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:00:23 +0100

    Updated automatically!

--- Scripts/Autoadd/Bee Swarm Simulator Auto Farm Auto Dig  More.lua
+++ Scripts/Autoadd/Bee Swarm Simulator Auto Farm Auto Dig  More.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BadScripters/CloudHub/main/hub"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Main.lua"))()
