e9e4c65c7b7d25cff9c4689bbb67c73807e47b0c
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:11:11 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 19:11:06"
+lastupdate = "GMT +1: 08.03.2023 19:11:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -12566,3 +12566,5 @@ addscriptexist(2753915549, "Blox Fruits: Auto Farm, Fruit Farm & More", "pulled 
 addscriptexist(537413528, "Build A Boat For Treasure: Auto Farm, Anti Afk", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Farm%20Anti%20Afk.lua")
 
 addscriptexist(292439477, "Phantom Forces: Aimbot, ESP, Wallcheck", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Aimbot%20ESP%20Wallcheck.lua")
+
+addscriptexist(8360240897, "World of Trollge: Auto Farm, Anti Afk, Walkspeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/World%20of%20Trollge%20Auto%20Farm%20Anti%20Afk%20Walkspeed.lua")
