69693abc6cc4f62b5b7f0e5bba0ba0b1fcf6de9b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:31:42 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 14:31:36"
+lastupdate = "GMT +1: 08.03.2023 14:31:42"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7626,3 +7626,5 @@ addscriptexist(8657766101, "Marvellous Playground: Get all characters", "pulled 
 addscriptexist(3233893879, "Bad Business: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bad%20Business%20Auto%20Farm.lua")
 
 addscriptexist(6872265039, "Bedwars: Vape V4", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bedwars%20Vape%20V.lua")
+
+addscriptexist(6872265039, "Bedwars: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bedwars%20Auto%20Farm.lua")
