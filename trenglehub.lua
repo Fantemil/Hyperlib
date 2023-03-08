91e84164358813c547bdd48e50ce57e10dd4ce47
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:55:27 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 12:55:21"
+lastupdate = "GMT +1: 08.03.2023 12:55:27"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -6808,3 +6808,5 @@ addscriptuniversal("Universal back door scanner", "pulled from rbxscript.com", "
 addscriptexist(10742364019, "[BETA] EGOIST: Max Shoot", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BETA%20EGOIST%20Max%20Shoot.lua")
 
 addscriptexist(2788229376, "Da Hood: Skin Changer", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Skin%20Changer.lua")
+
+addscriptexist(9224601490, "Fruit Battlegrounds: Infinite Spin", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fruit%20Battlegrounds%20Infinite%20Spin.lua")
