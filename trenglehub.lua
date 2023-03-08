6fad39540b1c8858087eab98c917da0638a21962
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:21:22 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:21:17"
+lastupdate = "GMT +1: 08.03.2023 18:21:22"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11524,3 +11524,5 @@ addscriptexist(9993529229, "Counter Blox Remastered: Aimbot + More", "pulled fro
 addscriptexist(11189979930, "Pet Crafting Simulator: Auto Farm, Insta KIll, Auto Hit", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Crafting%20Simulator%20Auto%20Farm%20Insta%20KIll%20Auto%20Hit.lua")
 
 addscript(8980446061, "Current Game","Ultaverse: Collect All Bugs", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultaverse%20Collect%20All%20Bugs.lua")
+
+addscriptexist(10925589760, "Merge Simulator: Auto Marge, Auto buy, Auto rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Merge%20Simulator%20Auto%20Marge%20Auto%20buy%20Auto%20rebirth.lua")
