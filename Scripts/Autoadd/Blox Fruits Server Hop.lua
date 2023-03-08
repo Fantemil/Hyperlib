971d069c97bddf2f5a49beeb0c8059c9eac19a33
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:48:18 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Blox Fruits Server Hop.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/a/main/autoBuyHakiColorWithHopServer-obfuscated.lua'))()
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/a/main/autoBuyHakiColorWithHopServer-obfuscated.lua'))()")
