c4f5de5f35ae8d38982822e04ff7adf952ee30cc
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:02:14 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Project Slayers Infinity Hp.lua
@@ -0,0 +1,15 @@
+local args = {
+    [1] = true
+}
+
+game:GetService("ReplicatedStorage").Remotes.heal_tang123asd:FireServer(unpack(args))
+local args = {
+    [1] = true
+}
+
+game:GetService("ReplicatedStorage").Remotes.heal_tang123asd:FireServer(unpack(args))
+local args = {
+    [1] = true
+}
+
+game:GetService("ReplicatedStorage").Remotes.heal_tang123asd:FireServer(unpack(args))
