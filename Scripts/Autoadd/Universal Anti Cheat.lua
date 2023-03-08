02640e52313a7f959113e0238ad2e1a342cda81f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 15:11:19 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Anti Cheat.lua
@@ -0,0 +1,9 @@
+loadstring(game:HttpGet'https://raw.githubusercontent.com/Extorius/Anti-Cheat/main/script.lua')()
+
+--[[
+Copy the source:
+setclipboard(game:HttpGet'https://raw.githubusercontent.com/Extorius/Anti-Cheat/main/script.lua')
+
+GitHub Page:
+https://github.com/Extorius/Anti-Cheat/blob/main/README.md
+]]
