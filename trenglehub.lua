41e559a102fd749b6d05b484071f51c025f616c3
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:35:18 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 18:35:14"
+lastupdate = "GMT +1: 08.03.2023 18:35:18"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -11822,3 +11822,5 @@ addscriptexist(7180042682, "Military Tycoon: Auto Farm GUI", "pulled from rbxscr
 addhub("Galaxy Hub: Many Games Support", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Galaxy%20Hub%20Many%20Games%20Support.lua")
 
 addscriptexist(9486506804, "FIFA World: Auto Grab Words", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FIFA%20World%20Auto%20Grab%20Words.lua")
+
+addscriptexist(7002808196, "Simulator of falling into a Ditch: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Simulator%20of%20falling%20into%20a%20Ditch%20Auto%20Farm.lua")
