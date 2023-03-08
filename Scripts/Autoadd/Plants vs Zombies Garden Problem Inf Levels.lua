e94d1210481c4ec32cd18510ea39df6849e69afc
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 19:34:33 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Plants vs Zombies Garden Problem Inf Levels.lua
@@ -0,0 +1,152 @@
+-- https://www.roblox.com/games/4860819035/UPD-Plants-vs-Zombies-Garden-Problem
+-- levels up every single character, you can keep spamming this script to level them up faster
+
+while true do
+
+local args = {
+    [1] = "pea", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "chomper", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "cactus", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "rose", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "rose", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "acorn", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "sun", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "snap", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "kernel", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "electric", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "captain", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "action", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "imp", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "super", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "engineer", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "allstar", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "scientist", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+local args = {
+    [1] = "soldier", 
+    [2] = 9999999999,
+    [3] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("ReplicatedStorage").Remote.LVLUPP:FireServer(unpack(args))
+
+
+wait()
+end
