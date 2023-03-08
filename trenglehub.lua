a557c7217ab32f15b76b210575ff56bf1060d843
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:15:34 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:15:30"
+lastupdate = "GMT +1: 08.03.2023 07:15:34"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5778,3 +5778,5 @@ addscriptexist(7863262550, "FOBLOX: Equip Any Gun, Godmode, Hitboxes", "pulled f
 addscript(5985232436, "Current Game","Infectious Smile: Hit Aura, No Slowdown, Give Items", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Infectious%20Smile%20Hit%20Aura%20No%20Slowdown%20Give%20Items.lua")
 
 addscript(6490016198, "Current Game","Slayer Tycoon: Hitbox Changer, Teleports, Walkspeed Slider", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Slayer%20Tycoon%20Hitbox%20Changer%20Teleports%20Walkspeed%20Slider.lua")
+
+addscript(6407649031, "Current Game","NO-SCOPE ARCADE: Aimbot, Instatnt Equip & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NOSCOPE%20ARCADE%20Aimbot%20Instatnt%20Equip%20%20More.lua")
