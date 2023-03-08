60473181f4877fedd6a42e276a26f2b841b096b2
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 14:45:42 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Prison Life Inf Stamina Silent CMD Anti Arrest.lua
@@ -0,0 +1,2 @@
+loadstring('HASHPX,LOADPX = "0092foz62c3unz2exvc32e3ur75ez9x6",true')()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DaRuthlessPWR/prevail-remake/main/prevv2.lua", true))()
