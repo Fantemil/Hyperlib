361a9279e0eaca577073bcd586ffb84959478ce9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:52:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Anime Evolution Simulator Auto Farm Kill Aura Power Farm.lua
@@ -0,0 +1,12 @@
+-- GUI 1
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLking123456/Anime44/main/evolutionSim"))()
+-- key : ANIME_Legend^1238__Sim76
+
+
+
+
+-- GUI 2
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLking123456/anime5512/main/Anime%20Evolution12"))()
+-- key : ANIME_EVOLUTION^^^778_SIMULATOr543!
