66eb62f09d0cfcecab317c122efd9151cfab25a7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:39:10 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Gui Detection Bypass.lua
@@ -0,0 +1,9 @@
+--// UPDATED: November,  2022.
+ 
+--// Created By Nosssa supports, mostly all games!
+ 
+--// Roblox Group ( TeamNosss! ): https://www.roblox.com/groups/16003304/TeamNosss#!/about
+ 
+--// Execute first, and parent your ScreenGui into "CoreGui" for the bypass to function as intended
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nosssa/NossLock/main/UniversalGuiDetectionBypass"))()
