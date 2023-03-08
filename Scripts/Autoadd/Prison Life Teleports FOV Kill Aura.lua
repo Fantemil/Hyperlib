b194599d914abb7ffa9f8e52dd5b40a7fbb715a1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:29:41 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Prison Life Teleports FOV Kill Aura.lua
@@ -0,0 +1,7 @@
+--[[in my opinion this script is in the middle of good and bad
+so i dont really expect the users making anything positive 
+its just in the middle]]
+
+-- the fast submit thing wont work so im just not doing it for now lol
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Foaamy/Foamys-Prison-Life-GUI-Script/main/Script.lua"))()
