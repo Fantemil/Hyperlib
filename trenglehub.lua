d846c2105be378501edb30d9f55ab11d85f02fbf
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:45:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:45:33"
+lastupdate = "GMT +1: 08.03.2023 20:45:37"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14364,3 +14364,5 @@ addscriptexist(9993529229, "Counter Blox: Remastered [Headshots only]", "pulled 
 addscriptexist(340227283, "RoBowling [Hitbox, Aimbot]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RoBowling%20Hitbox%20Aimbot.lua")
 
 addscriptexist(7664413141, "Creeper Chaos [Beta] [Auto Farm, Esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Creeper%20Chaos%20Beta%20Auto%20Farm%20Esp.lua")
+
+addscript(10201298018, "Current Game","Vampire Hunters 2 Remake [Gun grab, Outfit saver, Role esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Vampire%20Hunters%20%20Remake%20Gun%20grab%20Outfit%20saver%20Role%20esp.lua")
