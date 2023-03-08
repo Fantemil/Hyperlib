8eba78a6c768565bd8087cbcfa89d966657dcf3d
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:37:04 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Drivers Paradise Unlimited Cash  Free Cars.lua
@@ -0,0 +1,50 @@
+ local args = {
+    [1] = {
+        [1] = {
+            [1] = "\5",
+            [2] = "Mcleaner F1"
+        }
+    }
+}
+
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer(unpack(args))
+local args = {
+    [1] = {
+        [1] = {
+            [1] = "\5",
+            [2] = "Mcleaner F1"
+        }
+    }
+}
+
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer(unpack(args))
+local args = {
+    [1] = {
+        [1] = {
+            [1] = "\5",
+            [2] = "Mcleaner F1"
+        }
+    }
+}
+
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer(unpack(args))
+local args = {
+    [1] = {
+        [1] = {
+            [1] = "\5",
+            [2] = "Mcleaner F1"
+        }
+    }
+}
+
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer(unpack(args))
+local args = {
+    [1] = {
+        [1] = {
+            [1] = "\5",
+            [2] = "Mcleaner F1"
+        }
+    }
+}
+
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer(unpack(args))
