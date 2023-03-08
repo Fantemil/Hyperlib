1530181a528c870b8cd3e0e032232914e193cfc9
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:09:48 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Blox Fruits Level Up Detector.lua
@@ -0,0 +1,20 @@
+pcall(function()
+
+_G.Webhook = "WebHookHere";
+
+
+
+--[[
+  Want to learn how to make a webhook?
+  1. Go to the Integrations page on your Discord server as described above and click Create Webhook.
+  2. Give your webhook a descriptive name and select the channel you want your messages to be sent to.
+  3. Remember to click Save Changes when you make any change to your webhook configuration.
+ 
+  Paste the webhook into _G.Webhook box and than you are good to go!
+--]]
+
+
+
+loadstring(game:HttpGet('https://crystalhub.ml/crystalhub/main/BloxFruitsLevelDetector.lua', true;))();
+
+end)
