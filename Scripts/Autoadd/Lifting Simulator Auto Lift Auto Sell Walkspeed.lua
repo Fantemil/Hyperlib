6c23d0f8e570d714c4b90ab9bc775d61024672c2
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:51:25 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Lifting Simulator Auto Lift Auto Sell Walkspeed.lua
@@ -0,0 +1,4 @@
+-- Get the key FREE by joining the server and becoming an OG member! --
+-- https://discord.gg/KwyPcJQ9ma --
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToothlessNight/DragonHub/main/Lifting%20Simulator",true))();
