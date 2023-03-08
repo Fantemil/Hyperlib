2435f47b95648c915938edaa8cea1e9d62238a87
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 07:14:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 07:14:28"
+lastupdate = "GMT +1: 08.03.2023 07:14:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -5748,3 +5748,5 @@ addscriptexist(286090429, "Arsenal: Fly, Speed, Aimbot", "pulled from rbxscript.
 addscriptexist(662417684, "LUCKY BLOCKS Battlegrounds: Get any Block, Hitbox Expander, Local Player", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LUCKY%20BLOCKS%20Battlegrounds%20Get%20any%20Block%20Hitbox%20Expander%20Local%20Player.lua")
 
 addscriptexist(3214114884, "Flag Wars: Hitbox Expander, ESP, Chams", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Flag%20Wars%20Hitbox%20Expander%20ESP%20Chams.lua")
+
+addscriptexist(662417684, "LUCKY BLOCKS Battlegrounds: Kill Aura, Kill All, Get any Block", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LUCKY%20BLOCKS%20Battlegrounds%20Kill%20Aura%20Kill%20All%20Get%20any%20Block.lua")
