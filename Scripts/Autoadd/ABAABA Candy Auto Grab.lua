b1e7399dbaa6de2ad03b576f014217e70e42f198
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:25:41 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/ABAABA Candy Auto Grab.lua
@@ -0,0 +1,9 @@
+local player = game.Players.LocalPlayer
+
+while task.wait() do
+   for i,v in pairs(game.Workspace.Thrown:GetChildren()) do
+       if v.Name == "Candy" and v:IsA("BasePart") then
+           player.Character.HumanoidRootPart.CFrame = v.CFrame
+       end
+   end
+end
