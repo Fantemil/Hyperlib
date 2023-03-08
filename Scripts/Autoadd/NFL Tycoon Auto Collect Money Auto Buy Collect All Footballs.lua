ea3abad9713d62ca43dd35bbc08c628d7d1f89eb
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 18:53:33 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/NFL Tycoon Auto Collect Money Auto Buy Collect All Footballs.lua
@@ -0,0 +1,5 @@
+_G.Credits = "discord.gg/rips"
+_G.RedGUI = false -- disabled it so your eyes wont bleed
+_G.Theme = "Dark" -- Must disable or remove _G.RedGUI to use
+--Themes: Light, Dark, Mocha, Aqua and Jester
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/NFLTycoon.lua"))()
