a368799249f36b6e1fa3804d53ce991bd940124b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:31:55 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:31:51"
+lastupdate = "GMT +1: 08.03.2023 20:31:55"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -14018,3 +14018,5 @@ addscriptexist(5130598377, "A Universal Time [Auto Farms, Thug Farm, Godmode]", 
 addscriptexist(9648883891, "Festival Tycoon [Auto Buy All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Festival%20Tycoon%20Auto%20Buy%20All.lua")
 
 addscriptexist(8130299583, "Trident Survial [Aimbot, Silent Aim, ESP]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Trident%20Survial%20Aimbot%20Silent%20Aim%20ESP.lua")
+
+addscript(8293257677, "Current Game","Surreal RPG [100 damage]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Surreal%20RPG%20%20damage.lua")
