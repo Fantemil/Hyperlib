eea576d2ca8d52a025899bdb32a8f7d95f2a9a4e
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:37:20 +0100

    Updated automatically!

--- Scripts/Autoadd/Blox Fruits Auto Team Auto Eat Fruit Auto Store Fruit.lua
+++ Scripts/Autoadd/Blox Fruits Auto Team Auto Eat Fruit Auto Store Fruit.lua
@@ -1,1 +1,47 @@
-loadstring(game:HttpGet('https://visionhub.dev/loader.lua'))()
+Settings = {
+   AutoTeam = "Marines", -- Pirates or Marines
+   AutoEat = false, -- true or false
+   AutoStore = true, -- true or false
+   ContinueHopAfterStoredFruit = true, -- true or false
+   WebhookURL = "", --Put your webhook links in here
+   WebhookPingEveryone = true, -- true or false
+   WantedFruits = {
+       "Kilo Fruit",
+       "Spin Fruit",
+       "Chop Fruit",
+       "Spring Fruit",
+       "Bomb Fruit",
+       "Smoke Fruit",
+       "Spike Fruit",
+       "Flame Fruit",
+       "Falcon Fruit",
+       "Ice Fruit",
+       "Sand Fruit",
+       "Dark Fruit",
+       "Revive Fruit",
+       "Diamond Fruit",
+       "Light Fruit",
+       "Love Fruit",
+       "Rubber Fruit",
+       "Barrier Fruit",
+       "Magma Fruit",
+       "Door Fruit",
+       "Quake Fruit",
+       "Buddha Fruit",
+       "String Fruit",
+       "Phoenix Fruit",
+       "Rumble Fruit",
+       "Paw Fruit",
+       "Gravity Fruit",
+       "Dough Fruit",
+       "Shadow Fruit",
+       "Venom Fruit",
+       "Control Fruit",
+       "Soul Fruit",
+       "Dragon Fruit",
+       "Leopard Fruit"
+       --List of fruits, delete fruits that you don't want
+   }
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GodClass/EpicScripts/main/Blox%20Fruit%20DF%20Script.txt'))()
