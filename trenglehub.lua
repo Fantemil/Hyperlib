155b9ffee3b099b3bf1c4ab3fd2557f2f539944a
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 16:56:22 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 16:56:17"
+lastupdate = "GMT +1: 08.03.2023 16:56:21"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -9380,3 +9380,5 @@ addscript(3376584594, "Current Game","Flight Simulator: Infinite Money", "pulled
 addscript(10476933477, "Current Game","Strong Ninja Simulator: Auto Strength, Auto Rebirth, Trade Spam", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Strong%20Ninja%20Simulator%20Auto%20Strength%20Auto%20Rebirth%20Trade%20Spam.lua")
 
 addscriptexist(155615604, "Prison Life: Gun Mods, Get All Guns, Get Key", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prison%20Life%20Gun%20Mods%20Get%20All%20Guns%20Get%20Key.lua")
+
+addhub("Viper Hub: 3 Games", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Viper%20Hub%20%20Games.lua")
