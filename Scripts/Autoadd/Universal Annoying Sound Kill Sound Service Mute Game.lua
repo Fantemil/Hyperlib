837b74a98597ef28923f9ba1b0ae6c498d82852b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:27:04 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Annoying Sound Kill Sound Service Mute Game.lua
@@ -0,0 +1,327 @@
+--sound thingy
+
+--[[
+Made By Scripty#2063
+If You Gonna showcase this , make sure to Credit me , do not take that you are owner of the script
+This Gui is Undetectable
+RespectFilteringEnabled must be false to use it
+--]]
+
+local ScreenGui = Instance.new("ScreenGui")
+local Draggable = Instance.new("Frame")
+local Frame = Instance.new("Frame")
+local Frame_2 = Instance.new("Frame")
+local Time = Instance.new("TextLabel")
+local _1E = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local _3E = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local SE = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local Path = Instance.new("TextLabel")
+local Top = Instance.new("Frame")
+local Top_2 = Instance.new("Frame")
+local ImageLabel = Instance.new("ImageLabel")
+local TextLabel = Instance.new("TextLabel")
+local Exit = Instance.new("TextButton")
+local Minizum = Instance.new("TextButton")
+local Stop = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local IY = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local TextLabel_2 = Instance.new("TextLabel")
+local Wait = Instance.new("TextBox")
+
+--Properties:
+
+ScreenGui.Name = ". Ç¤ÒÌ·ÒÌµÒÌ¸ÒÌ·ÒÌµÒÌ¸ÒÌ¡ÒÌ¡ÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÍ¤ÒÍ¬ÒÌÒÍ¤ÒÍ¬"
+ScreenGui.Parent = game:GetService("CoreGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ScreenGui.ResetOnSpawn = false
+
+Draggable.Name = "Ç¤ÒÌ·ÒÌµÒÌ¸ÒÌ·ÒÌµÒÌ¸ÒÌ¡ÒÌ¡ÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÍ¤ÒÍ¬ÒÌÒÍ¤ÒÍ¬."
+Draggable.Parent = ScreenGui
+Draggable.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Draggable.BackgroundTransparency = 1.000
+Draggable.BorderSizePixel = 0
+Draggable.Position = UDim2.new(0.35026139, 0, 0.296158612, 0)
+Draggable.Size = UDim2.new(0, 438, 0, 277)
+
+Frame.Name = ". . Ç¤ÒÌ·ÒÌµÒÌ¸ÒÌ·ÒÌµÒÌ¸ÒÌ¡ÒÌ¡ÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÍ¤ÒÍ¬ÒÌÒÍ¤ÒÍ¬. "
+Frame.Parent = Draggable
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(-0.00133678317, 0, -0.00348037481, 0)
+Frame.Size = UDim2.new(0, 438, 0, 277)
+
+Frame_2.Name = " . Ç¤ÒÌ·ÒÌµÒÌ¸ÒÌ·ÒÌµÒÌ¸ÒÌ¡ÒÌ¡ÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÍ¤ÒÍ¬ÒÌÒÍ¤ÒÍ¬. "
+Frame_2.Parent = Frame
+Frame_2.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Frame_2.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Frame_2.BorderSizePixel = 0
+Frame_2.Position = UDim2.new(-0.00133678142, 0, -0.0179207586, 0)
+Frame_2.Size = UDim2.new(0, 438, 0, 238)
+Frame_2.Active = true
+Frame_2.Draggable = true
+
+Time.Name = "Time"
+Time.Parent = Frame_2
+Time.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Time.BackgroundTransparency = 1.000
+Time.Position = UDim2.new(0, 0, 0.0126050422, 0)
+Time.Size = UDim2.new(0, 437, 0, 31)
+Time.Font = Enum.Font.GothamSemibold
+Time.Text = "RespectFilteringEnabled(RFE) : nil"
+Time.TextColor3 = Color3.fromRGB(255, 255, 255)
+Time.TextScaled = true
+Time.TextSize = 14.000
+Time.TextWrapped = true
+
+_1E.Name = "1E"
+_1E.Parent = Frame_2
+_1E.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+_1E.BorderSizePixel = 0
+_1E.Position = UDim2.new(0.0182648394, 0, 0.676470578, 0)
+_1E.Size = UDim2.new(0, 208, 0, 33)
+_1E.Font = Enum.Font.SourceSans
+_1E.Text = "Mute Game"
+_1E.TextColor3 = Color3.fromRGB(255, 255, 255)
+_1E.TextScaled = true
+_1E.TextSize = 30.000
+_1E.TextWrapped = true
+
+UICorner.Parent = _1E
+
+_3E.Name = "3E"
+_3E.Parent = Frame_2
+_3E.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+_3E.BorderSizePixel = 0
+_3E.Position = UDim2.new(0.0159817357, 0, 0.142857149, 0)
+_3E.Size = UDim2.new(0, 209, 0, 33)
+_3E.Font = Enum.Font.SourceSans
+_3E.Text = "Annoying  Sound"
+_3E.TextColor3 = Color3.fromRGB(255, 255, 255)
+_3E.TextSize = 30.000
+_3E.TextWrapped = true
+
+UICorner_2.Parent = _3E
+
+SE.Name = "SE"
+SE.Parent = Frame_2
+SE.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+SE.BorderSizePixel = 0
+SE.Position = UDim2.new(0.509132445, 0, 0.142857149, 0)
+SE.Size = UDim2.new(0, 209, 0, 33)
+SE.Font = Enum.Font.SourceSans
+SE.Text = "Kill Sound Service"
+SE.TextColor3 = Color3.fromRGB(255, 255, 255)
+SE.TextSize = 30.000
+SE.TextWrapped = true
+
+UICorner_3.Parent = SE
+
+Path.Name = "Path"
+Path.Parent = Frame_2
+Path.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Path.BackgroundTransparency = 1.000
+Path.Position = UDim2.new(0.00684931502, 0, 0.815126061, 0)
+Path.Size = UDim2.new(0, 435, 0, 44)
+Path.Font = Enum.Font.GothamSemibold
+Path.Text = "Dev Note : This Script is FE and it only FE when RespectFilteringEnabled(RFE) is disabled , elseif RespectFilteringEnabled(RFE) is true then it only be client , mostly RespectFilteringEnabled(RFE) disabled game are classic game"
+Path.TextColor3 = Color3.fromRGB(255, 0, 0)
+Path.TextScaled = true
+Path.TextSize = 14.000
+Path.TextWrapped = true
+
+Top.Name = "Top"
+Top.Parent = Frame_2
+Top.BackgroundColor3 = Color3.fromRGB(41, 60, 157)
+Top.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Top.BorderSizePixel = 0
+Top.Position = UDim2.new(-0.00133678142, 0, -0.128059402, 0)
+Top.Size = UDim2.new(0, 438, 0, 30)
+Top_2.Name = "Top"
+Top_2.Parent = Top
+Top_2.BackgroundColor3 = Color3.fromRGB(30, 45, 118)
+Top_2.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Top_2.BorderSizePixel = 0
+Top_2.Position = UDim2.new(0, 0, 0.967638671, 0)
+Top_2.Size = UDim2.new(0, 438, 0, 5)
+
+ImageLabel.Parent = Top
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.Position = UDim2.new(0, 0, 0.0666666701, 0)
+ImageLabel.Size = UDim2.new(0, 29, 0, 27)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=8798286232"
+
+TextLabel.Parent = ImageLabel
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.997245014, 0, 0, 0)
+TextLabel.Size = UDim2.new(0, 397, 0, 30)
+TextLabel.Font = Enum.Font.GothamSemibold
+TextLabel.Text = "FEAG"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextWrapped = true
+
+Exit.Name = "Exit"
+Exit.Parent = Top
+Exit.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Exit.BackgroundTransparency = 1.000
+Exit.Position = UDim2.new(0.924657524, 0, 0, 0)
+Exit.Size = UDim2.new(0, 32, 0, 25)
+Exit.Font = Enum.Font.GothamSemibold
+Exit.Text = "x"
+Exit.TextColor3 = Color3.fromRGB(255, 255, 255)
+Exit.TextScaled = true
+Exit.TextSize = 14.000
+Exit.TextWrapped = true
+
+Minizum.Name = "Minizum"
+Minizum.Parent = Top
+Minizum.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Minizum.BackgroundTransparency = 1.000
+Minizum.Position = UDim2.new(0.851598203, 0, 0, 0)
+Minizum.Size = UDim2.new(0, 32, 0, 23)
+Minizum.Font = Enum.Font.GothamSemibold
+Minizum.Text = "_"
+Minizum.TextColor3 = Color3.fromRGB(255, 255, 255)
+Minizum.TextScaled = true
+Minizum.TextSize = 14.000
+Minizum.TextWrapped = true
+
+Stop.Name = "Stop"
+Stop.Parent = Frame_2
+Stop.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+Stop.BorderSizePixel = 0
+Stop.Position = UDim2.new(0.0182648394, 0, 0.310924381, 0)
+Stop.Size = UDim2.new(0, 424, 0, 33)
+Stop.Font = Enum.Font.SourceSans
+Stop.Text = "Stop"
+Stop.TextColor3 = Color3.fromRGB(255, 255, 255)
+Stop.TextSize = 30.000
+Stop.TextWrapped = true
+
+UICorner_4.Parent = Stop
+
+IY.Name = "IY"
+IY.Parent = Frame_2
+IY.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+IY.BorderSizePixel = 0
+IY.Position = UDim2.new(0.509132445, 0, 0.676470578, 0)
+IY.Size = UDim2.new(0, 209, 0, 33)
+IY.Font = Enum.Font.SourceSans
+IY.Text = "Unmute Game"
+IY.TextColor3 = Color3.fromRGB(255, 255, 255)
+IY.TextScaled = true
+IY.TextSize = 30.000
+IY.TextWrapped = true
+
+UICorner_5.Parent = IY
+
+TextLabel_2.Parent = Frame_2
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(0.0182648394, 0, 0.466386557, 0)
+TextLabel_2.Size = UDim2.new(0, 426, 0, 50)
+TextLabel_2.Font = Enum.Font.GothamSemibold
+TextLabel_2.Text = "Wait Speed       :"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextSize = 30.000
+TextLabel_2.TextWrapped = true
+TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
+
+Wait.Name = "Wait()"
+Wait.Parent = Frame_2
+Wait.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Wait.BackgroundTransparency = 1.000
+Wait.Position = UDim2.new(0.531963468, 0, 0.466386557, 0)
+Wait.Size = UDim2.new(0, 199, 0, 50)
+Wait.ZIndex = 99999
+Wait.ClearTextOnFocus = false
+Wait.Font = Enum.Font.GothamSemibold
+Wait.Text = "0.5"
+Wait.TextColor3 = Color3.fromRGB(255, 255, 255)
+Wait.TextSize = 30.000
+Wait.TextWrapped = true
+
+--Sound Service:
+local notification = Instance.new("Sound")
+notification.Parent = game:GetService("SoundService")
+notification.SoundId = "rbxassetid://9086208751"
+notification.Volume = 5
+notification.Name = ". Ç¤ÒÌ·ÒÌµÒÌ¸ÒÌ·ÒÌµÒÌ¸ÒÌ¡ÒÌ¡ÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÌ¼ÒÌ±ÒÍÒÍÒÌÒÍ¤ÒÍ¬ÒÌÒÍ¤ÒÍ¬"
+
+--funuction:
+Exit.MouseButton1Click:Connect(function()
+ ScreenGui:Destroy()
+end)
+local Mute = false
+IY.MouseButton1Click:Connect(function()
+ Mute = false
+end)
+
+_1E.MouseButton1Click:Connect(function()
+ Mute = true
+ while Mute == true do 
+  wait()
+  for _, sound in next, workspace:GetDescendants() do
+   if sound:IsA("Sound") then
+    sound:Stop()
+   end
+  end
+ end
+end)
+
+_3E.MouseButton1Click:Connect(function()
+ for _, sound in next, workspace:GetDescendants() do
+  if sound:IsA("Sound") then
+   sound:Play()
+  end
+ end
+end)
+
+local Active = true
+SE.MouseButton1Click:Connect(function()
+ Active = true
+ while Active == true do
+  local StringValue = Wait.Text
+  wait(StringValue)
+  for _, sound in next, workspace:GetDescendants() do
+   if sound:IsA("Sound") then
+    sound:Play()
+   end
+  end
+ end
+end)
+
+Stop.MouseButton1Click:Connect(function()
+ Active = false
+end)
+--Credit:
+notification:Play()
+game:GetService("StarterGui"):SetCore("SendNotification", {
+ Title = "FEAG";
+ Text = "FEAG Has Been Loaded , Made By Scripty#2063 (gamer14_123)";
+ Icon = "";
+ Duration = 10; 
+ Button1 = "Yes Sir";
+})
+--Check:
+while true do
+ wait(0.5)
+ local setting = game:GetService("SoundService").RespectFilteringEnabled
+ if setting == true then
+  Time.TextColor = BrickColor.new(255,0,0)
+  Time.Text ="RespectFilteringEnabled(RFE) : true"
+ elseif setting == false then
+  Time.Text ="RespectFilteringEnabled(RFE) : false"
+  Time.TextColor = BrickColor.new(0,255,0)
+ end
+end
