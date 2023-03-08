e9caf7fd404076fde40132a1f7784cefcb893ae1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:16:16 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 13:16:11"
+lastupdate = "GMT +1: 08.03.2023 13:16:16"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -7346,3 +7346,5 @@ addhub("Eternity Hub: 5 Games", "pulled from rbxscript.com", "https://raw.github
 addscriptexist(9055015233, "My Hood: Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/My%20Hood%20Auto%20Farm.lua")
 
 addscriptexist(11040063484, "Sword Fighters Simulator: Auto Farm Quest, Auto Farm Nearest, Auto Click", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20Fighters%20Simulator%20Auto%20Farm%20Quest%20Auto%20Farm%20Nearest%20Auto%20Click.lua")
+
+addscript(7722306047, "Current Game","Pet Simulator X: Auto Egg Open, Open Gui's, Unlock Gamepass", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Auto%20Egg%20Open%20Open%20Guis%20Unlock%20Gamepass.lua")
