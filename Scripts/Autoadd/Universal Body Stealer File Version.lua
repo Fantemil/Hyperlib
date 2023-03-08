841cdca846f1a7d9adf260ec1c74f233494788b0
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:23:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Universal Body Stealer File Version.lua
@@ -0,0 +1,5 @@
+getgenv().FILE = "" -- This is the name of the file. Make sure to have .txt or .lua at the end or it may not work ONLY USE LETTERS NO SYMBOLS OR THE SCRIPT WILL BREAK, periods are an end type statement
+
+getgenv().URL = "https://example.com" -- The Website Which you want to steal the body from
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/projects/main/Roblox/Body%20Stealer%20File%20Version.lua"))()
