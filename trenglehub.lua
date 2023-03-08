6a2477cc0759e88dbe740900f5abcf8bd62c97b1
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:50:19 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 17:50:15"
+lastupdate = "GMT +1: 08.03.2023 17:50:19"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -10752,3 +10752,5 @@ addscriptexist(1224212277, "Mad City Chapter 2: Auto Rob", "pulled from rbxscrip
 addscript(6442957604, "Current Game","Custom PC Tycoon: Auto Buy, Auto Sell, Auto Build Parts", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Custom%20PC%20Tycoon%20Auto%20Buy%20Auto%20Sell%20Auto%20Build%20Parts.lua")
 
 addscript(9898710720, "Current Game","Money Tycoon: Walkspeed, Anti Afk, Auto Rebirth", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Money%20Tycoon%20Walkspeed%20Anti%20Afk%20Auto%20Rebirth.lua")
+
+addscriptexist(6875469709, "STRONGEST PUNCH SIMULATOR: Auto Farm, Auto Punch, Settings", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/STRONGEST%20PUNCH%20SIMULATOR%20Auto%20Farm%20Auto%20Punch%20Settings.lua")
