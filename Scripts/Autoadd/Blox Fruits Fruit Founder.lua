7942e4d74bb4f039e5b2655673b4b92ea6be8235
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:39:53 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Blox Fruits Fruit Founder.lua
@@ -0,0 +1,24 @@
+repeat wait() until game:IsLoaded()
+Fruits = {
+"Human: Buddha Fruit",
+"Bird: Phoenix Fruit",
+"Rumble Fruit",
+"Paw Fruit",
+"Gravity Fruit",
+"Dough Fruit",
+"Control Fruit",
+"Venom Fruit",
+"Shadow Fruit",
+"Dragon Fruit" ,
+"Soul Fruit",
+"Leopard Fruit"
+}
+
+Valuable = {"Dough Fruit","Shadow Fruit","Venom Fruit","Control Fruit","Dragon Fruit","Soul Fruit","Leopard Fruit"} ------ it will stop the script if you got this fruit
+Webhook = "" --------------------------------Optional
+Store = true  --------------------------------Auto Store after it got the fruit
+Safeplace = true ----------------------------- Safeplace 
+Repeat = true --------------------------------- Repeat continues hopping
+WaitTime = 10 ---------------------------------------------------- Set time how much time does the script execute higher number means slow but will not bug out For Synapse only 
+-----------------------------------------------------------------------
+loadstring(game:HttpGet"https://gist.githubusercontent.com/NotHubris/4e6fdc88d84c30afa9b28c590f273bbf/raw")()
