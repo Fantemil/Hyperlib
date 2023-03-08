720209aca2cce85c6570a1666d968d9651a725ee
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:42:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:42:50"
+lastupdate = "GMT +1: 08.03.2023 06:42:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4782,3 +4782,5 @@ addscriptexist(11701792069, "Snow Plow Simulator: Auto Farm", "pulled from rbxsc
 addscriptexist(11701792069, "Snow Plow Simulator: Infinity Snow, Infinity Rebirths", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Snow%20Plow%20Simulator%20Infinity%20Snow%20Infinity%20Rebirths.lua")
 
 addscriptexist(11606818992, "STEEP STEPS: Click TP, Teleports, AC Bypass", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/STEEP%20STEPS%20Click%20TP%20Teleports%20AC%20Bypass.lua")
+
+addscriptuniversal("Universal depth of field", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20depth%20of%20field.lua")
