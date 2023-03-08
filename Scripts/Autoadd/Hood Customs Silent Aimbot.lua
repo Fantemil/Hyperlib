aac0136d3aeaa3b33a5366fa474ec1152b7d2311
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 12:46:00 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Hood Customs Silent Aimbot.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/topillesrevenge/Streamable-Silent/main/Main"))()
+DaHoodSettings.Prediction = 0.123
+Aiming.TargetPart = {"Head", "LeftHand", "RightHand", "LeftLowerArm", "RightLowerArm", "LeftUpperArm", "RightUpperArm", "LeftFoot", "LeftLowerLeg", "UpperTorso", "HumanoidRootPart", "LeftUpperLeg", "RightLowerLeg", "RightFoot", "LowerTorso"}
+Aiming.FOV = 25
+Aiming.FOVSides = 25
+Aiming.HitChance = 100
+Aiming.ShowFOV = true 
