dc10779c3bb40f22db522a4305be4c51c42367d7
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 06:49:00 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 06:48:56"
+lastupdate = "GMT +1: 08.03.2023 06:49:00"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -4972,3 +4972,5 @@ addscriptexist(7239319209, "Ohio: Auto Register Rob, Auto Bank Rob, Fly", "pulle
 addscriptexist(10760024537, "Ultimate Easy Obby: Auto Win", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultimate%20Easy%20Obby%20Auto%20Win.lua")
 
 addscriptexist(11729688377, "Booga Booga [REBORN]: Auto Farm, Speed Bypass & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Booga%20Booga%20REBORN%20Auto%20Farm%20Speed%20Bypass%20%20More.lua")
+
+addscriptexist(8916037983, "starving artists: Themes", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/starving%20artists%20Themes.lua")
