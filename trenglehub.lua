d73a769b8be120a2883403a4e730aa9d4cde2e4b
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 20:20:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 08.03.2023 20:20:10"
+lastupdate = "GMT +1: 08.03.2023 20:20:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -13708,3 +13708,5 @@ addscriptexist(9742783208, "Raise a Yippee | AutoClick, AutoFood", "pulled from 
 addscript(8891045283, "Current Game","Find The Bacons | GetAllBacons", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Find%20The%20Bacons%20%20GetAllBacons.lua")
 
 addscriptexist(298400657, "Dragon Ball Final Remastered | GUI - Infinite Ki, Infinite Stamina", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dragon%20Ball%20Final%20Remastered%20%20GUI%20%20Infinite%20Ki%20Infinite%20Stamina.lua")
+
+addscriptexist(7560156054, "Clicker Simulator | AutoRebirth, AutoClick", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Clicker%20Simulator%20%20AutoRebirth%20AutoClick.lua")
