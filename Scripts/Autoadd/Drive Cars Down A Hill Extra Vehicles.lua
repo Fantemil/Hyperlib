977fdfc004899fb75403fe3792f5a733180a541f
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 17:25:50 +0100

    Updated automatically!

--- Scripts/Autoadd/Drive Cars Down A Hill Extra Vehicles.lua
+++ Scripts/Autoadd/Drive Cars Down A Hill Extra Vehicles.lua
@@ -1,29 +1,24 @@
 --Jitter1337
---Glitch
+--DrWaffles
 
 local plr = game.Players.LocalPlayer;
-local RockCrawlerClone = plr.PlayerGui.ShopGui.Frame.ScrollingFrame["Jalopy"]:Clone();
-local V2Clone = game:GetService("Players").LocalPlayer.PlayerGui.ShopGui.Frame.ScrollingFrame.Roadster.V2:Clone();
-
+local TruckFullClone = plr.PlayerGui.ShopGui.Frame.ScrollingFrame["Jalopy"]:Clone();
 function ExtraVehicles()
-    RockCrawlerClone.Parent = plr.PlayerGui.ShopGui.Frame.ScrollingFrame;
-    RockCrawlerClone.Name = "RockCrawler";
-    RockCrawlerClone.Text = "Rock Crawler";
-    V2Clone.Parent = RockCrawlerClone;
+    plr.PlayerGui.ShopGui.Frame.ScrollingFrame['Scout_Car'].Visible = true;
+    TruckFullClone.Parent = plr.PlayerGui.ShopGui.Frame.ScrollingFrame;
+    TruckFullClone.Name = "Truck Full";
+    TruckFullClone.Text = "Truck Full";
 end
-
 ExtraVehicles();
-
 task.wait();
-
-plr.PlayerGui.ShopGui.Frame.ScrollingFrame["RockCrawler"].MouseEnter:Connect(function()
-    game:GetService("ReplicatedStorage").CarViewmodels["RockCrawler"].Parent = plr.PlayerGui.ShopGui.Frame.ViewportFrame;
+plr.PlayerGui.ShopGui.Frame.ScrollingFrame["Truck Full"].MouseEnter:Connect(function()
+    game:GetService("ReplicatedStorage").CarViewmodels["Truck Full"].Parent = plr.PlayerGui.ShopGui.Frame.ViewportFrame;
 end);
-plr.PlayerGui.ShopGui.Frame.ScrollingFrame["RockCrawler"].MouseLeave:Connect(function()
-    plr.PlayerGui.ShopGui.Frame.ViewportFrame["RockCrawler"].Parent = game:GetService("ReplicatedStorage").CarViewmodels;
+plr.PlayerGui.ShopGui.Frame.ScrollingFrame["Truck Full"].MouseLeave:Connect(function()
+    plr.PlayerGui.ShopGui.Frame.ViewportFrame["Truck Full"].Parent = game:GetService("ReplicatedStorage").CarViewmodels;
 end);
-plr.PlayerGui.ShopGui.Frame.ScrollingFrame["RockCrawler"].MouseButton1Click:Connect(function()
-    local args = {[1]=0,[2]="RockCrawler"};
+plr.PlayerGui.ShopGui.Frame.ScrollingFrame["Truck Full"].MouseButton1Click:Connect(function()
+    local args = {[1]=0,[2]="Truck Full"};
     game:GetService("ReplicatedStorage").SpawnCar:FireServer(unpack(args));
     plr.PlayerGui.ShopGui.Frame.Visible = false;
 end);
