d14d4cfe0305ea3f3a91144a666e515d3bb8f980
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:57:50 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:57:45"
+lastupdate = "GMT +1: 09.03.2023 14:57:49"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17866,3 +17866,5 @@ addscriptexist(9488143445, "Raise A Bloppa [Auto Collect Money, Add Money, Infin
 addscriptexist(9508087919, "Apeirophobia [ESP Exit]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apeirophobia%20ESP%20Exit.lua")
 
 addscriptexist(6610021055, "Noob Army Tycoon [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Noob%20Army%20Tycoon%20Auto%20Farm.lua")
+
+addscriptexist(9030362964, "Field Goal Simulator [Aimbot]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Field%20Goal%20Simulator%20Aimbot.lua")
