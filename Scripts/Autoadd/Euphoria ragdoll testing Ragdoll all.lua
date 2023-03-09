9027114bbe5d1c7f0f2ec60718d99eeaef570aad
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:43:50 +0100

    Updated automatically!

--- Scripts/Autoadd/Euphoria ragdoll testing Ragdoll all.lua
+++ Scripts/Autoadd/Euphoria ragdoll testing Ragdoll all.lua
@@ -1,12 +1,3 @@
-for i,v in pairs(game.Players:GetChildren()) do
-    if v ~= game.Players.LocalPlayer then
-        task.spawn(function()
-            while v do
-                task.wait(0.2)
-                v.Character:WaitForChild("activeragdoll")
-                v.Character.activeragdoll:WaitForChild("rd")
-                v.Character.activeragdoll.rd:FireServer(v.Character.activeragdoll.V)
-            end
-        end)
-    end
+for _,human in pairs(game.Players:GetPlayers()) do
+game:GetService("Players").LocalPlayer.Character.activeragdoll.rd:FireServer(human.Character.activeragdoll.V)
 end
