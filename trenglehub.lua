23f7838238c6577fb72f3f4874cb72ba5f76a37a
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:38:55 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:38:51"
+lastupdate = "GMT +1: 09.03.2023 15:38:55"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18890,3 +18890,5 @@ addscriptexist(632642533, "DeadMist 2 Beta [Grab Dead Bodies]", "pulled from rbx
 addscript(7871169780, "Current Game","bLockerman666`s Minesweeper [Find Safe Tiles And Bombs]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/bLockermans%20Minesweeper%20Find%20Safe%20Tiles%20And%20Bombs.lua")
 
 addscript(6843988672, "Current Game","County Jail Roleplay [AC Bypass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/County%20Jail%20Roleplay%20AC%20Bypass.lua")
+
+addscript(3829055572, "Current Game","The Underground War [Unique HUB V2]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Underground%20War%20Unique%20HUB%20V.lua")
