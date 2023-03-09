8945bd28305561ae3ab9cd5ee140e624fc1923df
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:56:11 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:56:07"
+lastupdate = "GMT +1: 09.03.2023 14:56:11"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17824,3 +17824,5 @@ addscript(9488143445, "Current Game","Raise A Bloppa [Auto Farm GUI]", "pulled f
 addscriptexist(189707, "Natural Disaster Survival [Part Flinger]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Natural%20Disaster%20Survival%20Part%20Flinger.lua")
 
 addscriptexist(7167319176, "Chicago Remastered [GUI - ESP, No Recoil & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Chicago%20Remastered%20GUI%20%20ESP%20No%20Recoil%20%20More.lua")
+
+addscript(9107946567, "Current Game","Warriors Army Simulator [GUI - Auto Farm, Auto Revice & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Warriors%20Army%20Simulator%20GUI%20%20Auto%20Farm%20Auto%20Revice%20%20More.lua")
