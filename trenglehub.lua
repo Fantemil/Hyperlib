f8393b239608ce89d91ec5d8b1d896df21d2d4a5
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:41:35 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:41:30"
+lastupdate = "GMT +1: 09.03.2023 13:41:34"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15896,3 +15896,5 @@ addscriptexist(5571328985, "Bloodlines [Infinite Cash & More!]", "pulled from rb
 addscriptexist(9702206679, "Heroes Daydream [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Heroes%20Daydream%20Auto%20Farm.lua")
 
 addscriptexist(10204556059, "NARS Color Quest [Auto Collect All Shades - Complete All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NARS%20Color%20Quest%20Auto%20Collect%20All%20Shades%20%20Complete%20All.lua")
+
+addscriptexist(10118559731, "nico`s nextbots [BHOP]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/nicos%20nextbots%20BHOP.lua")
