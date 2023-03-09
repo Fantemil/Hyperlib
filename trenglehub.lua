de59aec7ddf2fc3b1210d54b0d2d2d9937290820
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:36:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:36:32"
+lastupdate = "GMT +1: 09.03.2023 15:36:38"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18832,3 +18832,5 @@ addscriptexist(4753520418, "The Hood [Egg ESP]", "pulled from rbxscript.com", "h
 addscriptexist(8982709021, "YouTube Life [Collect All Eggs]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/YouTube%20Life%20Collect%20All%20Eggs.lua")
 
 addscriptexist(6788787407, "Warrior Champions [Auto Farm GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Warrior%20Champions%20Auto%20Farm%20GUI.lua")
+
+addscriptexist(6125809531, "Untitled AoT [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Untitled%20AoT%20Auto%20Farm.lua")
