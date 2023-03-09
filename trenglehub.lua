ff84d8f3d994d25a309cbb7e8c3fd75022ac68f2
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:35:10 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:35:06"
+lastupdate = "GMT +1: 09.03.2023 15:35:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18796,3 +18796,5 @@ addscriptexist(6677985923, "Millionaire Empire Tycoon [Auto Collect Money - Auto
 addscript(2249346496, "Current Game","Blood Moon The Tycoon [Melee Instant Kill]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blood%20Moon%20The%20Tycoon%20Melee%20Instant%20Kill.lua")
 
 addscriptexist(6284583030, "Pet Simulator X [Anti Steal]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Anti%20Steal.lua")
+
+addscriptexist(5610197459, "gang up on people simulator [Money]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/gang%20up%20on%20people%20simulator%20Money.lua")
