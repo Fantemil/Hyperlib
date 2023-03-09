0db49c0a11567be11e26d73ef282484e893a309d
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:51:56 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Murderers vs Sheriffs Insta Kill.lua
@@ -0,0 +1,21 @@
+while true do wait(2)
+local A_1 = game:GetService("Workspace").player.Head --replace player with target
+local A_2 = "Throw"
+local Event = game:GetService("Workspace").player.Knife.Damage --Murder Team Only replace player with your user
+Event:FireServer(A_1, A_2)
+wait(2)
+local A_1 = game:GetService("Workspace").player.Head --replace player with target
+local A_2 = "Throw"
+local Event = game:GetService("Workspace").player.Knife.Damage --Murder Team Only replace player with your user
+Event:FireServer(A_1, A_2)
+wait(2)
+local A_1 = game:GetService("Workspace").player.Head --replace player with target
+local A_2 = "Throw"
+local Event = game:GetService("Workspace").player.Knife.Damage --Murder Team Only replace player with your user
+Event:FireServer(A_1, A_2)
+wait(2)
+local A_1 = game:GetService("Workspace").player.Head --replace player with target
+local A_2 = "Throw"
+local Event = game:GetService("Workspace").player.Knife.Damage --Murder Team Only replace player with your user
+Event:FireServer(A_1, A_2)
+end
