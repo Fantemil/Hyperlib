d8fa59e7838d0425ba716deea953d7fa7d2acf98
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:04:52 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:04:48"
+lastupdate = "GMT +1: 09.03.2023 14:04:52"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16510,3 +16510,5 @@ addscriptexist(1224212277, "Mad City [Get Weekly Rewards]", "pulled from rbxscri
 addscript(10105990977, "Current Game","Samsung Space Tycoon [Destroy Everyones FPS]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Samsung%20Space%20Tycoon%20Destroy%20Everyones%20FPS.lua")
 
 addscriptexist(8304191830, "Anime Adventures {Full Auto Farm, Auto Upgrade, Select World]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Adventures%20Full%20Auto%20Farm%20Auto%20Upgrade%20Select%20World.lua")
+
+addscriptexist(10105990977, "Samsung Space Tycoon [Get All Items]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Samsung%20Space%20Tycoon%20Get%20All%20Items.lua")
