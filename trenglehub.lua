72dd12f9e68d7d6683331a3f41f181f9d82421e8
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:09:12 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:09:07"
+lastupdate = "GMT +1: 09.03.2023 14:09:12"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16620,3 +16620,5 @@ addscriptexist(6872265039, "BedWars [Insta Kill]", "pulled from rbxscript.com", 
 addscript(4237861040, "Current Game","Treacherous Tower [GUI - Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Treacherous%20Tower%20GUI%20%20Auto%20Farm.lua")
 
 addscriptexist(9551640993, "Mining Simulator 2 [Remove Egg Animations]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simulator%20%20Remove%20Egg%20Animations.lua")
+
+addscriptexist(7540727946, "Hoops Life [Auto Time - Semi Aimbot - Open Source]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hoops%20Life%20Auto%20Time%20%20Semi%20Aimbot%20%20Open%20Source.lua")
