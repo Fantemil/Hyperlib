f07cb1038b4369bca806466cfdbb73c8e295ef3b
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:43:36 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:43:32"
+lastupdate = "GMT +1: 09.03.2023 15:43:36"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19010,3 +19010,5 @@ addscriptexist(5116869569, "Doodle World [Attack Wild Doodles Anywhere]", "pulle
 addscriptexist(8492553855, "Project Upright [New Auto Farm GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Upright%20New%20Auto%20Farm%20GUI.lua")
 
 addscriptexist(8781849572, "Star Simulator Beta [Complete Auto Farm GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Star%20Simulator%20Beta%20Complete%20Auto%20Farm%20GUI.lua")
+
+addscriptexist(8781849572, "Star Simulator Beta [Infinite Resources]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Star%20Simulator%20Beta%20Infinite%20Resources.lua")
