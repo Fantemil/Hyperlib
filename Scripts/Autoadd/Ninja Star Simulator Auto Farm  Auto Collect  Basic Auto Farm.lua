14a8e1d395e834e4497dbbc84b557a51cd9bc29f
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 16:05:15 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ninja Star Simulator Auto Farm  Auto Collect  Basic Auto Farm.lua
@@ -0,0 +1,6 @@
+_G.Toggle = true
+_G.AutoCollect = true
+_G.Map = "Map1" -- "Map1", "Map2", "Map3", Map4", "Map5" -- All of these work for main parts of japan
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptRUs/Ryans/main/NinjaStarSimulator.lua"))()
