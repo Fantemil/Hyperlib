6e6e9c5789420e6f07922012de8a36ea9b963aaf
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:38:16 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:38:12"
+lastupdate = "GMT +1: 09.03.2023 15:38:16"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18872,3 +18872,5 @@ addscript(6960816467, "Current Game","Untitled Door Game 2 [Get All Badges]", "p
 addscriptexist(606849621, "Jailbreak [Instant Teleport To Criminal Base]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jailbreak%20Instant%20Teleport%20To%20Criminal%20Base.lua")
 
 addscriptexist(4588604953, "Criminality [Free ESP GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Criminality%20Free%20ESP%20GUI.lua")
+
+addscriptexist(292439477, "Phantom Forces [Rage - Legit - Invisible - Gun Mods]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Rage%20%20Legit%20%20Invisible%20%20Gun%20Mods.lua")
