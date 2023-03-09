19cd947358f8acba6030a2de5f26869324fdb9fa
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:58:44 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:58:39"
+lastupdate = "GMT +1: 09.03.2023 14:58:43"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17890,3 +17890,5 @@ addscriptexist(9508087919, "Apeirophobia [Simulation Cores ESP]", "pulled from r
 addscript(9065768804, "Current Game","War Simulator [Kill All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/War%20Simulator%20Kill%20All.lua")
 
 addscript(8438695556, "Current Game","Luck Simulator [upgrade Luck, Auto Magnet, Upgrade luck again ]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Luck%20Simulator%20upgrade%20Luck%20Auto%20Magnet%20Upgrade%20luck%20again%20.lua")
+
+addscriptexist(9438506890, "raise a sogga [Money Giver]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/raise%20a%20sogga%20Money%20Giver.lua")
