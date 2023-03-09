de26f3e111b537d9505a4fd1db86ba3414fbe1ee
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:23:37 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Base Raiders Auto Farm  Auto Crate  Anti AFK.lua
@@ -0,0 +1,5 @@
+getgenv().autoFarm = true
+getgenv().autoCrate = true
+getgenv().antiAFK = true
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Arth795-scripts/Luashit/main/base%20raiders%20new'))()
