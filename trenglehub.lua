32c1f4d14bec0ea8f71a669883ff15e2bf9d046b
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:34:52 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:34:47"
+lastupdate = "GMT +1: 09.03.2023 15:34:52"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18788,3 +18788,5 @@ addscriptexist(1224212277, "Mad City [Diamond HUB Auto Rob]", "pulled from rbxsc
 addscriptexist(9123917180, "Teleporter Simulator [GUI - Auto Orbs - Auto Buy & Kill All + More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Teleporter%20Simulator%20GUI%20%20Auto%20Orbs%20%20Auto%20Buy%20%20Kill%20All%20%20More.lua")
 
 addscriptexist(4042427666, "Anime Fighting Simulator [Chikara,Yen Generator]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Fighting%20Simulator%20ChikaraYen%20Generator.lua")
+
+addscriptexist(5712833750, "Animal Simulator [Collect Treasuers]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Animal%20Simulator%20Collect%20Treasuers.lua")
