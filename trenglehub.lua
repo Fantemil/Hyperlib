cbac5d76a1be3e9699404c99fff1d8dc5ccc463b
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:25:40 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:25:33"
+lastupdate = "GMT +1: 09.03.2023 13:25:40"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15484,3 +15484,5 @@ addscriptexist(2534724415, "Emergency Response Liberty County [No Recoil]", "pul
 addscriptexist(6902912928, "GET TO THE TOP! [Unlimited Points]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/GET%20TO%20THE%20TOP%20Unlimited%20Points.lua")
 
 addscriptexist(2506738224, "Fallen [Esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fallen%20Esp.lua")
+
+addscriptexist(7378050129, "Kill R63 with DOOM music [Changes the Numbers at All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Kill%20R%20with%20DOOM%20music%20Changes%20the%20Numbers%20at%20All.lua")
