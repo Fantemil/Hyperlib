321358a4562d82f74784ea952c22461f5bc2084e
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:41:33 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Home Run Simulator Auto Home Run.lua
@@ -0,0 +1,3 @@
+getgenv().AutoHomeRun = true --false to disable might take a bit to actually disable thou
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dooory/OC/main/Working/home_run_sim.lua"))()
