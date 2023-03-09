f62bc721014faed9b78d23a6ad1fb3ab11834f70
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:05:09 +0100

    Updated automatically!

--- Scripts/Autoadd/Flag Wars Kill All.lua
+++ Scripts/Autoadd/Flag Wars Kill All.lua
@@ -1,26 +1,19 @@
-_G.a = true
-while _G.a == true do
-   for i,v in pairs(game:GetService('Players'):GetPlayers()) do
-if v.Character and v.Character:FindFirstChildOfClass("Humanoid") and v ~= game.Players.LocalPlayer then
-         humanoid = v.Character.Humanoid
-local args = {
-   [1] = game:GetService("Players").LocalPlayer.Character.Pistol,  -- your gun here
-   [2] = {
-       ["p"] = Vector3.new(127.40184783935547, 15.57960033416748, -51.664894104003906),
-       ["pid"] = 1,
-       ["part"] = humanoid,
-       ["d"] = 252.61036682128906,
-       ["maxDist"] = 252.60980224609375,
-       ["h"] = humanoid,
-       ["m"] = Enum.Material.SmoothPlastic,
-       ["sid"] = 1,
-       ["t"] = 0.8584426607754916,
-       ["n"] = Vector3.new(-0.11215610802173615, 0.37135982513427734, 0.9216901063919067)
-   }
-}
-
-game:GetService("ReplicatedStorage").WeaponsSystem.Network.WeaponHit:FireServer(unpack(args))
-task.wait()
-end
-end
+for i, v in pairs(game.Players:GetPlayers()) do
+    if v ~= game.Players.LocalPlayer then
+        local args = {
+        [1] = game:GetService("Players").LocalPlayer.Character.Sniper,
+        [2] = {
+            ["p"] = Vector3.new(716.1422729492188, 4.201920032501221, -52.15347671508789),
+            ["pid"] = 1,
+            ["part"] = v.Character.Head,
+            ["d"] = 197.02999877929688,
+            ["maxDist"] = 2000,
+            ["h"] = v.Character.Humanoid,
+            ["m"] = Enum.Material.SmoothPlastic,
+            ["sid"] = 2,
+            ["t"] = 0.468864430009719,
+            ["n"] = Vector3.new(0.8855897784233093, 0.29755425453186035, 0.3566398322582245)
+        }}
+        game:GetService("ReplicatedStorage").WeaponsSystem.Network.WeaponHit:FireServer(unpack(args))
+    end
 end
