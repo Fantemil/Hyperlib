794529535d0e14df054dfa61845a323f9d1df7ba
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:40:16 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/xSPETER HUB  Games.lua
@@ -0,0 +1,9 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x2SPETER/x2SPETER/main/"..game.PlaceId..""))()
+
+--[[
+BLOX FRUITS
+NINJA LEGENDS
+SMASH LEGENDS
+BUILD A BOAT FOR TREASURE
+ANIME CLICKER SIMULATOR
+--]]
