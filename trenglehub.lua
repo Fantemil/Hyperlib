084a0ceb7d90c93ac487c5f7e556bf51a7818634
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:21:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:21:33"
+lastupdate = "GMT +1: 09.03.2023 14:21:38"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16940,3 +16940,5 @@ addscript(8656044807, "Current Game","Swinging Simulator [Unlock Every Badge]", 
 addscriptexist(8123652942, "Legend Piece [Kill Aura]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legend%20Piece%20Kill%20Aura.lua")
 
 addscriptexist(9534337535, "Shrek in the Backrooms [GUI - ESP, Loot Stuff]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shrek%20in%20the%20Backrooms%20GUI%20%20ESP%20Loot%20Stuff.lua")
+
+addscriptexist(301549746, "Counter Blox [Best Custom Skins Changer & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Counter%20Blox%20Best%20Custom%20Skins%20Changer%20%20More.lua")
