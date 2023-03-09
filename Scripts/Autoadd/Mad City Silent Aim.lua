b52d3b50a847ccd9cdde94343e557b2da0afa693
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:46:51 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Mad City Silent Aim.lua
@@ -0,0 +1,3 @@
+getgenv() = 260 -- Field of View: The silent aim is only targetted at the target inside the fov's radius.
+getgenv = "Head" -- Targetting: "Head", "Torso". For example: Using "Head" will only deal headshots.
+loadstring(game:HttpGet('https://raw.githubusercontent.com/my-scripts/main/SAMCH2', true))()
