6667c1f5fd8c560b936612f8229b5548f4569f19
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:54:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:54:41"
+lastupdate = "GMT +1: 09.03.2023 15:54:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19286,3 +19286,5 @@ addscriptexist(2745661925, "Jump Over The Walls [Auto Farm & More!]", "pulled fr
 addscriptexist(6783611814, "Build a Boat to Survive [24KGOLDN Event]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20a%20Boat%20to%20Survive%20KGOLDN%20Event.lua")
 
 addscriptexist(292439477, "Phantom Forces [Skin editing]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Skin%20editing.lua")
+
+addscriptexist(221718525, "Ninja Tycoon [Auto Click]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ninja%20Tycoon%20Auto%20Click.lua")
