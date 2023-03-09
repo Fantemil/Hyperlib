eadbe7bb78dac95c63f3d1ce4ca9159d3631be71
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:43:44 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:43:38"
+lastupdate = "GMT +1: 09.03.2023 14:43:44"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17504,3 +17504,5 @@ addscriptexist(9534337535, "Shrek in the Backrooms [Inf Item Giver, Entity Kille
 addscriptexist(4566572536, "Universal Vehicle [SpeedHack]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Vehicle%20SpeedHack.lua")
 
 addscriptexist(292439477, "Phantom Forces [Unlock All Guns, Admin Guns, Attachments]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Unlock%20All%20Guns%20Admin%20Guns%20Attachments.lua")
+
+addscriptexist(9551640993, "Mining Simulator 2 [Auto Mine - Auto Rebirth & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simulator%20%20Auto%20Mine%20%20Auto%20Rebirth%20%20More.lua")
