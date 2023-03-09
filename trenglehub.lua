1e222df343b77b6f9e970359284eb38241d33281
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:55:30 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:55:25"
+lastupdate = "GMT +1: 09.03.2023 13:55:30"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16264,3 +16264,5 @@ addscriptexist(9170761016, "Magic Clicker [Auto Farm - GUI]", "pulled from rbxsc
 addscriptexist(9551640993, "Mining Simulator 2 [Pet Guesser]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simulator%20%20Pet%20Guesser.lua")
 
 addscript(2043040358, "Current Game","Vyyrahk City [Auto Farm GUI - Inf Stamina & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Vyyrahk%20City%20Auto%20Farm%20GUI%20%20Inf%20Stamina%20%20More.lua")
+
+addscriptexist(8962384769, "Fireworks Simulator [Auto Fireworks & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fireworks%20Simulator%20Auto%20Fireworks%20%20More.lua")
