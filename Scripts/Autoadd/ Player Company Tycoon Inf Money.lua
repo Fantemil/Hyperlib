242684b51e2d5d675c844cd8b08a7c5d71f40688
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:03:48 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/ Player Company Tycoon Inf Money.lua
@@ -0,0 +1,7 @@
+local hum = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+local place = game:GetService("Workspace").vip["More money!"].Head
+
+while task.wait() do
+firetouchinterest(place, hum, 0)
+firetouchinterest(place, hum, 1)
+end
