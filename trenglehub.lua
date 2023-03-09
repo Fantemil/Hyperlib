b053269ec0ea943b467c8f3627dab785fad25e14
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:16:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:16:28"
+lastupdate = "GMT +1: 09.03.2023 14:16:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16812,3 +16812,5 @@ addscriptexist(4598019433, "generic roleplay gaem. [Auto Farm]", "pulled from rb
 addscript(7905476552, "Current Game","Beekeepers [Auto Tokens - Auto Dig]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Beekeepers%20Auto%20Tokens%20%20Auto%20Dig.lua")
 
 addscriptexist(6677985923, "Millionaire Empire Tycoon [Infinite Money Remote]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Millionaire%20Empire%20Tycoon%20Infinite%20Money%20Remote.lua")
+
+addscriptexist(6206893534, "Cobra Kai Karate [GUI - Auto Farming]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cobra%20Kai%20Karate%20GUI%20%20Auto%20Farming.lua")
