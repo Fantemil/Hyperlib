8bceb5434668865c2fe7f0e5d3cb9ab224cc36fe
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:57:29 +0100

    Updated automatically!

--- Scripts/Autoadd/Race Clicker Auto Win.lua
+++ Scripts/Autoadd/Race Clicker Auto Win.lua
@@ -1,21 +1,8 @@
-while true do
-   wait(0.5)
-spawn(function()
-local args = {
-   [1] = "/w MostGoatedGamer booga",
-   [2] = "All"
-}
-
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-
-for i,v in pairs(game:GetService("Workspace").Environment:GetDescendants()) do
-if v:IsA("TouchTransmitter") then
-
-firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0)
-wait()
-firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1)
-
-end
-end
-end)
+while task.wait() do
+   for i,v in pairs(workspace.Environment:GetDescendants()) do
+       if v:FindFirstChildWhichIsA'TouchTransmitter' then
+           firetouchinterest(v, game:service'Players'.LocalPlayer.Character.HumanoidRootPart, 0)
+           task.wait((v.Parent.Name == 'Stage1K' and 1) or .1)
+       end
+   end
 end
