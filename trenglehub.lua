761efb54114c68915058823015dd30c13940d71a
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:59:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:59:42"
+lastupdate = "GMT +1: 09.03.2023 15:59:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19412,3 +19412,5 @@ addscriptexist(6494523288, "Blade Quest [Auto Farm, Kill Aura, Auto Rewards]", "
 addscriptexist(537413528, "Build A Boat For Treasure [Auto Farm, God Mode Water, Auto Common Chests]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Farm%20God%20Mode%20Water%20Auto%20Common%20Chests.lua")
 
 addscriptexist(8897454112, "Pet Salon Tycoon [Inf Money]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Salon%20Tycoon%20Inf%20Money.lua")
+
+addscriptexist(4042427666, "Anime Fighting Simulator [Auto Farm GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Fighting%20Simulator%20Auto%20Farm%20GUI.lua")
