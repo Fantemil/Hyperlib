80e20af762d69269d3e5c7825901237a3411375a
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:02:25 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:02:21"
+lastupdate = "GMT +1: 09.03.2023 14:02:25"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16448,3 +16448,5 @@ addscriptexist(2753915549, "Blox Fruits [Unlock Abilyti]", "pulled from rbxscrip
 addscriptexist(863266079, "Apocalypse Rising 2 [Anti Zombie - Teleport]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apocalypse%20Rising%20%20Anti%20Zombie%20%20Teleport.lua")
 
 addscriptexist(9359379224, "Chaotic Bean Simulator [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Chaotic%20Bean%20Simulator%20Auto%20Farm.lua")
+
+addscriptexist(1537690962, "Bee Swarm Simulator [GUI - Auto Dig, Auto Farm & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bee%20Swarm%20Simulator%20GUI%20%20Auto%20Dig%20Auto%20Farm%20%20More.lua")
