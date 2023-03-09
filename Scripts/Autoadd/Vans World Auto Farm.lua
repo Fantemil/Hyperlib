feabf6056f8f17376d5f5577b012075f2c1bf7c0
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:37:31 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Vans World Auto Farm.lua
@@ -0,0 +1,5 @@
+getgenv().autoEXP = true
+getgenv().autoCoin = true
+getgenv().antiAFK = true
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Arth795-scripts/Luashit/main/vans%20world'))()
