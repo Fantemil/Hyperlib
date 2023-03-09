71596446e81ee0fa66c86b03ca8530f895f641be
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:41:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:41:08"
+lastupdate = "GMT +1: 09.03.2023 14:41:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17442,3 +17442,5 @@ addscriptexist(2000062521, "State of Anarchy 0.15.83.0 [Kill All]", "pulled from
 addscriptexist(70005410, "Blox Hunt [Prop Chams]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Hunt%20Prop%20Chams.lua")
 
 addscriptexist(370731277, "Meep City [Snowball All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Meep%20City%20Snowball%20All.lua")
+
+addscriptexist(5233268561, "A Bizarre Journey [Auto Pickup All Items]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/A%20Bizarre%20Journey%20Auto%20Pickup%20All%20Items.lua")
