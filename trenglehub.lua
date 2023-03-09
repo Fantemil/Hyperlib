30e0cfbe53d17dfa6b7fbfa2874b8c192ce87a14
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:53:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:53:24"
+lastupdate = "GMT +1: 09.03.2023 13:53:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16210,3 +16210,5 @@ addscriptexist(5956785391, "Project Slayers [Teleport to Bosses]", "pulled from 
 addscriptexist(5956785391, "Project Slayers [Lag Server]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Lag%20Server.lua")
 
 addscript(8473958692, "Current Game","The Trials [PHASE 2] [Overpowered GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Trials%20PHASE%20%20Overpowered%20GUI.lua")
+
+addscriptexist(7336302630, "Project Delta [Teleport Bypass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Delta%20Teleport%20Bypass.lua")
