2534a0ec910aae60d791bbdc31f135aa45e3f0c4
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:19:11 +0100

    Updated automatically!

--- Scripts/Autoadd/Destruction Simulator INF money.lua
+++ Scripts/Autoadd/Destruction Simulator INF money.lua
@@ -1,5 +1,1 @@
-while wait() do
-    while wait() do
-             game:GetService("ReplicatedStorage").Remotes.generateBoost:FireServer("Coins", 480, 99999999)
-     end
-end
+game:GetService("ReplicatedStorage").Remotes.generateBoost:FireServer("Coins", 480, 99999999)
