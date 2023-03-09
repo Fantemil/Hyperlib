11eb3fe9b75055470eaff8b47326c491b71cffab
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:44:31 +0100

    Updated automatically!

--- Scripts/Autoadd/Planet Simulator Auto Farm.lua
+++ Scripts/Autoadd/Planet Simulator Auto Farm.lua
@@ -1,1 +1,5 @@
-while wait() do game:GetService("ReplicatedStorage").ChangePlayerStat:FireServer("Mass",math.huge) end
+while wait() do
+   for i = 1,20 do
+game:GetService("ReplicatedStorage").ChangePlayerStat:FireServer("Mass",math.huge)
+end
+end
