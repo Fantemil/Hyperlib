0e11895ba4373edb3facd364413a8aa6d9dd3aa5
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:20:02 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:19:57"
+lastupdate = "GMT +1: 09.03.2023 15:20:02"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18420,3 +18420,5 @@ addscriptexist(4996049426, "All Star Tower Defense [Star Pass XP]", "pulled from
 addscriptexist(6878438976, "Developer Inc [10r Cash in 30 sec]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Developer%20Inc%20r%20Cash%20in%20%20sec.lua")
 
 addscriptexist(1458767429, "ABA [Automatic black flash Itadori]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ABA%20Automatic%20black%20flash%20Itadori.lua")
+
+addscriptexist(2569453732, "Rise of Nations [Trade GUI - Open Source]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rise%20of%20Nations%20Trade%20GUI%20%20Open%20Source.lua")
