786e801c3d3a78feabc2a5df1ce4557e05c37ac6
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:54:08 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 12:54:04"
+lastupdate = "GMT +1: 09.03.2023 12:54:08"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15248,3 +15248,5 @@ addscriptexist(142823291, "Murder Mystery 2 [Fly, BTools]", "pulled from rbxscri
 addscriptexist(9666226803, "Motel Simulator [Auto Farm, Walkspeed]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Motel%20Simulator%20Auto%20Farm%20Walkspeed.lua")
 
 addscriptexist(8818978508, "Space Simulator Reborn [Auto Sell, Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Space%20Simulator%20Reborn%20Auto%20Sell%20Auto%20Farm.lua")
+
+addscriptexist(8657766101, "Marvellous Playground [Gamepass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Marvellous%20Playground%20Gamepass.lua")
