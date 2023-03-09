bc0cc554b4f41bbcd197170e6ed8cd96fa31e675
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:58:00 +0100

    Updated automatically!

--- Scripts/Autoadd/NOSCOPE ARCADE Silent Aim ESP.lua
+++ Scripts/Autoadd/NOSCOPE ARCADE Silent Aim ESP.lua
@@ -1,8 +1,1 @@
-local E = "RevertSucks"
-local x = "ReptilainClubBoyz"
-local x_ = "main"
-local e = "no_scope_arena"
-local n = "lua"
-local Exxen = "https://raw.githubusercontent.com"..("/%s/%s/%s/%s.%s"):format(E,x,x_,e,n)
-
-loadstring(game:HttpGet(Exxen))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Drifter0507/GUIS/main/NOSCOPEARCADE", true))();
