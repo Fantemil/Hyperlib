9b9b2799d267d8b4b43e20d8534e134ecbc042ac
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:41:53 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Mining Simulator  Chests Serverhop.lua
@@ -0,0 +1,10 @@
+--// Settings \\--
+
+getgenv().Chests = {
+   'Legendary';
+   'Epic';
+}
+
+--// Main \\--
+
+loadstring(game:HttpGet'https://github.com/RunDTM/miningsim2/raw/main/serverhop.lua')()
