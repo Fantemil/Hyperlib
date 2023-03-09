f7d29cebfef5f2f38ecfbeb3bd894da3444ea057
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:32:55 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:32:51"
+lastupdate = "GMT +1: 09.03.2023 15:32:55"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18740,3 +18740,5 @@ addscriptexist(606849621, "Jailbreak [Instant tp base]", "pulled from rbxscript.
 addscriptexist(5780309044, "Stands Awakening [Stand Farm, Npc Teleport, Place Teleport]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Stands%20Awakening%20Stand%20Farm%20Npc%20Teleport%20Place%20Teleport.lua")
 
 addscriptexist(6679274937, "Vans World [Auto Collect Coins, Auto Xp Gain, Complete All guests]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Vans%20World%20Auto%20Collect%20Coins%20Auto%20Xp%20Gain%20Complete%20All%20guests.lua")
+
+addscriptexist(258258996, "Miner's Haven [Auto Rebirth, Rebirth farm, Anti Afk]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Miners%20Haven%20Auto%20Rebirth%20Rebirth%20farm%20Anti%20Afk.lua")
