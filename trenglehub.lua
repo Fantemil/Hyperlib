0631cb5b355c315ece1dc48b2716d4779870ca5b
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:34:05 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:34:00"
+lastupdate = "GMT +1: 09.03.2023 15:34:05"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18768,3 +18768,5 @@ addscriptexist(8540168650, "Stand Upright Rebooted [Item Grabber]", "pulled from
 addscriptexist(5787425661, "A Lucky Day [Destroy Game]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/A%20Lucky%20Day%20Destroy%20Game.lua")
 
 addscriptexist(1224212277, "Mad City [Full Invisible]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mad%20City%20Full%20Invisible.lua")
+
+addscriptexist(7603178367, "Chipotle Burrito Builder [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Chipotle%20Burrito%20Builder%20Auto%20Farm.lua")
