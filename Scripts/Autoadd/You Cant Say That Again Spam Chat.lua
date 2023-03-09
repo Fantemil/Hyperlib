717876e7859f857334dbde879e4d4c0df18520a4
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:34:59 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/You Cant Say That Again Spam Chat.lua
@@ -0,0 +1,9 @@
+_G.Settings = {
+    ["AmountOfStrings"] = 45,
+    ["StringLengthMin"] = 2,
+    ["StringLengthMax"] = 3,
+    ["Delay"] = 1.25,
+    ["Letters"] = [[abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+={}|[]\;',./<>?:`~-]]
+}
+
+loadstring(game:HttpGet('https://gist.githubusercontent.com/iiDk-the-actual/3b33970b5c4966e4c0b429c02518b52d/raw/58f1c43e1c02fd0f5f730285b9ee49bbc87f6d71/noskidding'))()
