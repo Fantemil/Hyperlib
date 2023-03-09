c1d724fdfefeec715b696c0a4663f9b4647d93e8
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:23:16 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:23:11"
+lastupdate = "GMT +1: 09.03.2023 14:23:16"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16982,3 +16982,5 @@ addscriptexist(4459593483, "FORCE [Sell Trinkets]", "pulled from rbxscript.com",
 addscriptexist(9772878203, "Raise A Floppa 2 [GUI - Auto Click Floppa, Auto Collect & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Raise%20A%20Floppa%20%20GUI%20%20Auto%20Click%20Floppa%20Auto%20Collect%20%20More.lua")
 
 addscriptexist(9568982793, "Da Hood Battlegrounds [Instant Kill]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Battlegrounds%20Instant%20Kill.lua")
+
+addscriptexist(606849621, "Jailbreak [Keycard, Unlock Doors]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jailbreak%20Keycard%20Unlock%20Doors.lua")
