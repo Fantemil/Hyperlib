fb3b25d235c1e53253779e84d1ac8453bad177d6
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:19:29 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:19:23"
+lastupdate = "GMT +1: 09.03.2023 14:19:29"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16886,3 +16886,5 @@ addscript(9845369668, "Current Game","Goofy Ahh Spider-Man [GUI - Give All Skins
 addscriptexist(8123652942, "Legend Piece [Fruit Changer]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legend%20Piece%20Fruit%20Changer.lua")
 
 addscript(9031522337, "Current Game","RPG Legends [GUI - God Mode, Invisible Name & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RPG%20Legends%20GUI%20%20God%20Mode%20Invisible%20Name%20%20More.lua")
+
+addscriptexist(6766156863, "Strongman [Wimbledon Event Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Strongman%20Wimbledon%20Event%20Auto%20Farm.lua")
