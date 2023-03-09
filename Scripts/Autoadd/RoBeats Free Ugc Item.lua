24bc724f21c93e09e96bdd4fa8c5d1485f69806f
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:59:20 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/RoBeats Free Ugc Item.lua
@@ -0,0 +1,3 @@
+game:GetService("ReplicatedStorage").KKShared.lib.Network.TRAIN_ENTER:FireServer()
+wait(10)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Workspace.KKPopupShop.Areas.Backwall.Part.CFrame 
