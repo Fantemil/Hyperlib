dd2ce2c12fdae140147f636c1a8370b3ea0dc5d5
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:56:28 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:56:24"
+lastupdate = "GMT +1: 09.03.2023 13:56:28"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16290,3 +16290,5 @@ addscriptexist(5956785391, "Project Slayers [Infinite Stamina]", "pulled from rb
 addscriptexist(920587237, "Adopt Me! [Spam Vehicles To People + More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Adopt%20Me%20Spam%20Vehicles%20To%20People%20%20More.lua")
 
 addscript(9530846958, "Current Game","EPSILON LINEAGE 2 [Infinite Mana & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/EPSILON%20LINEAGE%20%20Infinite%20Mana%20%20More.lua")
+
+addscriptexist(537413528, "Build A Boat For Treasure [Auto Farm, Tp to closed places, Anti-afk]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Farm%20Tp%20to%20closed%20places%20Antiafk.lua")
