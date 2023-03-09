9cf8274a41a67ac8db48f5a2d72c9ab547e135fc
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:36:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:36:33"
+lastupdate = "GMT +1: 09.03.2023 14:36:38"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17326,3 +17326,5 @@ addscriptexist(8603729161, "Walk 50k studs for nothing [Get Badges]", "pulled fr
 addscriptexist(6788434697, "AniPhobia [Infinite Ammo & Much More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AniPhobia%20Infinite%20Ammo%20%20Much%20More.lua")
 
 addscriptexist(9551640993, "Mining Simulator 2 [Utilities]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simulator%20%20Utilities.lua")
+
+addscriptexist(3101667897, "Legends Of Speed [Steps Farm, Gems Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legends%20Of%20Speed%20Steps%20Farm%20Gems%20Farm.lua")
