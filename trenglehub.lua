e3f5a77d754672593054e3d02e444e2002540b78
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:52:54 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 12:52:48"
+lastupdate = "GMT +1: 09.03.2023 12:52:54"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15216,3 +15216,5 @@ addscriptexist(8811271345, "Balanced Craftwars Overhaul [Auto Equip, Teleport]",
 addscriptexist(4863541388, "Scythe Masters [Auto click]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Scythe%20Masters%20Auto%20click.lua")
 
 addscriptexist(10447882390, "Zombie Research Tycoon [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Zombie%20Research%20Tycoon%20Auto%20Farm.lua")
+
+addscriptexist(260606376, "Realistic Roleplay 2 [Cash Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Realistic%20Roleplay%20%20Cash%20Auto%20Farm.lua")
