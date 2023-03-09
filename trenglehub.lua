d799a14ba3d0744313a1e1af814abe2463b770ca
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:54:56 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:54:52"
+lastupdate = "GMT +1: 09.03.2023 14:54:56"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17790,3 +17790,5 @@ addscript(9336657396, "Current Game","A Beatdown Neighborhood [Infinite Cash & M
 addscript(5062619730, "Current Game","War Simulator [Kill All - Open Source]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/War%20Simulator%20Kill%20All%20%20Open%20Source.lua")
 
 addscript(6717117731, "Current Game","Subway Surfers [Steal All Coins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Subway%20Surfers%20Steal%20All%20Coins.lua")
+
+addscript(7681451450, "Current Game","Anime Simulator X [GUI - Farm, Auto Open Egg & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Simulator%20X%20GUI%20%20Farm%20Auto%20Open%20Egg%20%20More.lua")
