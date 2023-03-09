e12694d72937c0e0a8e2bd888f4223f3d018f98b
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:05:34 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:05:30"
+lastupdate = "GMT +1: 09.03.2023 14:05:34"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16528,3 +16528,5 @@ addscript(6822455054, "Current Game","Fruit Piece [Stat Changer GUI]", "pulled f
 addscriptexist(10001513103, "Limited Words [GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Limited%20Words%20GUI.lua")
 
 addscript(7056117669, "Current Game","get chased by a rocket [Kick Player]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/get%20chased%20by%20a%20rocket%20Kick%20Player.lua")
+
+addscriptexist(7056117669, "get chased by a rocket [Kick All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/get%20chased%20by%20a%20rocket%20Kick%20All.lua")
