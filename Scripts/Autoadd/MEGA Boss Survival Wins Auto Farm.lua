a1a20d1f0d080c2612caa6b349731a6a114f9b35
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:33:14 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/MEGA Boss Survival Wins Auto Farm.lua
@@ -0,0 +1,3 @@
+local player = game.Players.LocalPlayer.Character.HumanoidRootPart
+local cframe = CFrame.new(-2.14748365e+09, 2.14748365e+09, -2.14748365e+09, -0.636765003, 0.770433605, 0.0310244858, -0.0467191339, 0.00161135197, -0.998906791, -0.769641399, -0.637518227, 0.0349678993)
+player.CFrame = cframe
