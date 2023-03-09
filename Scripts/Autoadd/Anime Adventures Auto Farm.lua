b1c28fccc11f9b4fca81ed5a26c5d044e80383e5
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:59:45 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Anime Adventures Auto Farm.lua
@@ -0,0 +1,6 @@
+getgenv().id = "" -- PUT PET ID HERE EG {123456d-123c-1234-123456789}
+getgenv().unit = "erwin" -- YOU CAN CHANGE THIS TO OTHER UNITS EX SANJI
+getgenv().hidename = false -- hides name
+getgenv().dif = "Normal" -- Normal = less hard,exp. Hard = more exp,more harder
+getgenv().act = "namek_level_6" --Lower if it is too hard
+loadstring(game:HttpGet("https://rawscripts.net/raw/RELEASE-Anime-Adventures-Lobby-3936"))()
