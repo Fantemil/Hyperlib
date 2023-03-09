fcdbd538eeb884965b1aeedfd4938a1a6c6d0b32
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:16:11 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:16:07"
+lastupdate = "GMT +1: 09.03.2023 15:16:11"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18322,3 +18322,5 @@ addscript(9474050899, "Current Game","Jackpot Party [Fast Case Opener]", "pulled
 addscript(5029300380, "Current Game","BOXING CLUB [Kick Player]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BOXING%20CLUB%20Kick%20Player.lua")
 
 addscriptexist(7089072882, "HE-MAN SIM + BATTLE ARENA [Infinite Kirbinite]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/HEMAN%20SIM%20%20BATTLE%20ARENA%20Infinite%20Kirbinite.lua")
+
+addscriptexist(7089072882, "HE-MAN SIM + BATTLE ARENA [Max Level & Rebirth]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/HEMAN%20SIM%20%20BATTLE%20ARENA%20Max%20Level%20%20Rebirth.lua")
