af0bc66142a9f52a2e1fbac20d47d702b80751db
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:21:29 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:21:25"
+lastupdate = "GMT +1: 09.03.2023 14:21:29"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16936,3 +16936,5 @@ addscriptexist(1730877806, "Grand Piece Online [Auto Osu]", "pulled from rbxscri
 addscript(5970745461, "Current Game","Minerblocks [Kill Aura]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Minerblocks%20Kill%20Aura.lua")
 
 addscript(8656044807, "Current Game","Swinging Simulator [Unlock Every Badge]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Swinging%20Simulator%20Unlock%20Every%20Badge.lua")
+
+addscriptexist(8123652942, "Legend Piece [Kill Aura]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legend%20Piece%20Kill%20Aura.lua")
