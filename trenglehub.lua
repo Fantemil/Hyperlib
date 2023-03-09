829362fea87ef32c56c62117b0188abbdd2b20da
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:05:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:05:47"
+lastupdate = "GMT +1: 09.03.2023 14:05:52"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16536,3 +16536,5 @@ addscriptexist(4459593483, "FORCE [Pvp Mode]", "pulled from rbxscript.com", "htt
 addscript(6875747014, "Current Game","Ronopoly [Spam dice]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ronopoly%20Spam%20dice.lua")
 
 addscriptexist(653205701, "Fort Martin, Military Base [jumping]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fort%20Martin%20Military%20Base%20jumping.lua")
+
+addscript(9936860371, "Current Game","Egg Packing Tycoon [Semi Autofarm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Egg%20Packing%20Tycoon%20Semi%20Autofarm.lua")
