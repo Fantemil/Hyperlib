52f47ba3eabb0749005280bbeda7fd0b2487f138
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:59:52 +0100

    Updated automatically!

--- Scripts/Autoadd/Pls Donate Auto Farm.lua
+++ Scripts/Autoadd/Pls Donate Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/public-scripts/main/Pls%20Donate%20Eazvy%20Hub.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/tzechco/roblox-scripts/main/PLS%20DONATE/autofarm.lua'))()
