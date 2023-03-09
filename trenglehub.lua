0abcea277507220e5802bde88806e915d607dae6
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:35:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:35:09"
+lastupdate = "GMT +1: 09.03.2023 14:35:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17290,3 +17290,5 @@ addscriptexist(4522347649, "Free Admin [Unlock Gamepass]", "pulled from rbxscrip
 addscriptexist(8069117419, "Demon Soul Simulator [AutoFarm, Teleports, AutoChest]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Demon%20Soul%20Simulator%20AutoFarm%20Teleports%20AutoChest.lua")
 
 addscriptexist(1962086868, "Tower of Hell [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20of%20Hell%20Auto%20Farm.lua")
+
+addscriptexist(9551640993, "Mining Simulator 2 [Auto Farm Gems]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simulator%20%20Auto%20Farm%20Gems.lua")
