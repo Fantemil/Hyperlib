aa4dc1483785453bd0b3974cb5aea076586beed8
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:55:04 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:54:59"
+lastupdate = "GMT +1: 09.03.2023 13:55:03"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16252,3 +16252,5 @@ addscriptexist(4651779470, "RECOIL [GUI- NoSprea, NoRecoil & More!]", "pulled fr
 addscriptexist(3840352284, "Volleyball 4.2 [No Line-Fault]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Volleyball%20%20No%20LineFault.lua")
 
 addscript(9601849832, "Current Game","Jetpack Battle [Insta-kill]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jetpack%20Battle%20Instakill.lua")
+
+addscriptexist(1962086868, "Tower of hell GUI [ UltraJump, Ultraspeed, Admin ]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20of%20hell%20GUI%20%20UltraJump%20Ultraspeed%20Admin%20.lua")
