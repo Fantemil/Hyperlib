eabf6b0a4bbbb9810c11c72c69a2251c3a78d806
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 16:02:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 16:02:42"
+lastupdate = "GMT +1: 09.03.2023 16:02:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19480,3 +19480,5 @@ addscriptexist(5108587039, "Palm Ridge Beta [Auto Farm]", "pulled from rbxscript
 addscriptexist(292439477, "Phantom Forces [Snipe Head]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Snipe%20Head.lua")
 
 addscriptexist(6125809531, "Untitled Attack On Titan [Auto FArm, Kill Aura Inf Gas]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Untitled%20Attack%20On%20Titan%20Auto%20FArm%20Kill%20Aura%20Inf%20Gas.lua")
+
+addscriptexist(6284583030, "Pet Simulator X [Auto-Enchant]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20AutoEnchant.lua")
