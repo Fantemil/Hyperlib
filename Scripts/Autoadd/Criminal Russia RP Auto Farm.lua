b3e9613d994a04e93a1d5cb296bb092a48005de1
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:49:53 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Criminal Russia RP Auto Farm.lua
@@ -0,0 +1,22 @@
+-- Path: game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+
+local A_1 = "Script was made by ez 8507"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+
+
+local A_1 = "ÐÑÑÐ·ÑÐ¸Ðº"
+local Event = game:GetService("ReplicatedStorage").TeamChange
+Event:FireServer(A_1)
+wait(3)
+--ÐÐ¾ÑÐ¾Ð±ÐºÐ¸(ÐÑÑÐ·ÑÐ¸Ðº)
+while true do
+    wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(249, 7, -477)
+wait(1)
+fireclickdetector(game.Workspace.B0xM0del.BOXGIVE.ClickDetector, 3)
+wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(256, 7, -525)
+wait(2)
+end
