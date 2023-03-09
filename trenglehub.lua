9fe45cbc316d2fe036a2d1fd3e4984b6b12f75cd
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:02:56 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:02:52"
+lastupdate = "GMT +1: 09.03.2023 14:02:56"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16462,3 +16462,5 @@ addscriptexist(10118559731, "Nico’s Nextbots [Bots Esp - Teleport To Safezone]
 addscriptexist(5307215810, "Randomizer [Esp, Aimbot, WalkSPeed]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Randomizer%20Esp%20Aimbot%20WalkSPeed.lua")
 
 addscript(9597319358, "Current Game","Actors Tycoon [AutoFarm Buttons, Auto Upgrade Workers, Working on mobile]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Actors%20Tycoon%20AutoFarm%20Buttons%20Auto%20Upgrade%20Workers%20Working%20on%20mobile.lua")
+
+addscript(9845529187, "Current Game","Tidewater [Annoy All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tidewater%20Annoy%20All.lua")
