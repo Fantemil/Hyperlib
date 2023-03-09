b01a2e99f0410c2d3060bfe693203a723a3ae67d
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:31:41 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:31:36"
+lastupdate = "GMT +1: 09.03.2023 15:31:41"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18710,3 +18710,5 @@ addscriptexist(6297822481, "Titan Warfare [Inf Gas]", "pulled from rbxscript.com
 addscriptexist(9203864304, "raise a floppa [Auto Click Floppa, Auto Collect money, Auto Buy]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/raise%20a%20floppa%20Auto%20Click%20Floppa%20Auto%20Collect%20money%20Auto%20Buy.lua")
 
 addscriptexist(4566572536, "Vehicle Legends [Speed Boost, Rainbow color]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Vehicle%20Legends%20Speed%20Boost%20Rainbow%20color.lua")
+
+addscriptexist(8540346411, "Rebirth Champions X [Auto Open Eggs, Unlock All Areas, Teleport To Any Area]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rebirth%20Champions%20X%20Auto%20Open%20Eggs%20Unlock%20All%20Areas%20Teleport%20To%20Any%20Area.lua")
