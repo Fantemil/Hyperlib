d779574e02664f877b5c79c5f0c37757154b6764
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 16:04:04 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 16:03:59"
+lastupdate = "GMT +1: 09.03.2023 16:04:04"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19510,3 +19510,5 @@ addscriptexist(6297822481, "Titan Warfare [Enhanced Damage]", "pulled from rbxsc
 addscriptexist(4580204640, "Survive the Killer [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Survive%20the%20Killer%20Auto%20Farm.lua")
 
 addscriptexist(4996049426, "All Star Tower Defense [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/All%20Star%20Tower%20Defense%20Auto%20Farm.lua")
+
+addscriptexist(4616652839, "Shindo Life [Auto Spin - Auto Rejoin]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shindo%20Life%20Auto%20Spin%20%20Auto%20Rejoin.lua")
