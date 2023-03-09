892da2549d724191c0ea5a6342fe51ebeeb358ca
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:36:16 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Taxi Boss Auto Farm.lua
@@ -0,0 +1,4 @@
+getgenv().RatingTarget  = 2; -- It will targets that have your set amount or above
+getgenv().HighestRatingTarget = 3.3; -- The cap of rating target (so you aren't targetting rating 8+ or something)
+
+loadstring(game:HttpGet'https://raw.githubusercontent.com/samuelLovesPie/verm_releases/main/taxiboss.lua')();
