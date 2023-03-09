85b444703f8a03f5023a6d68d10aa9a9ea8a26a4
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:41:24 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Heroes Daydream Auto Farm.lua
@@ -0,0 +1,12 @@
+wait(5) --5 is suggested, however change this to your roblox loading time
+_G.FocusedStat = 'Physic' -- Physic, Magic, Health
+_G.FPSBooster = true -- Removes environment to boost FPS
+_G.PickedDungeon = 'The Stone Mines' --put name as exactly in game, dont use with best dungeon
+_G.Difficulty = 'Easy' --pick from Easy, Medium, to Hard
+_G.IsHardcore = true --decide if you want hardcore mode or not
+_G.InfWaves = false --decide if you want inf waves mode or not
+_G.BestDungeon = false -- Go to highest dungeon you can (turn off if you want to farm item at a specific dungeon?)
+_G.SafeMethod = false -- Not needed unless normal tp gets detected
+_G.SafeMethodSpeed = 5 -- If SafeMethod is getting detected make this number higher
+
+loadstring(game:HttpGet('https://pastebin.com/raw/26BTJTvg))()
