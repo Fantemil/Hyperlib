2e2394363aa5a9b2f3eda26b6ba39dd79ed71de2
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:14:21 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Life Sentence Auto Farm  Auto Collect Points  More.lua
@@ -0,0 +1,11 @@
+_G.RedGui = true
+_G.Theme = "Dark" -- Must disable or remove _G.RedGUI to use
+--[[Themes:
+     Light
+     Dark
+     Mocha
+     Aqua
+     Jester
+]]
+_G.V3rmillion = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vfvo/scripts/main/lifesentence.lua"))()
