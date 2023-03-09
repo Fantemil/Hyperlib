30ffaf0df229b63f6e567fb493d9031486c654cc
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:57:10 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:57:06"
+lastupdate = "GMT +1: 09.03.2023 14:57:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17850,3 +17850,5 @@ addscriptexist(5777099015, "Cheese Escape [Unlock Finished Game Door & More!]", 
 addscriptexist(6342469457, "Heavens Arena [GUI - Auto Click, Auto Skill & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Heavens%20Arena%20GUI%20%20Auto%20Click%20Auto%20Skill%20%20More.lua")
 
 addscriptexist(537413528, "Build A Boat For Treasure [GUI - Auto Farm, Auto Buy]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20GUI%20%20Auto%20Farm%20Auto%20Buy.lua")
+
+addscriptexist(28382335, "Survival Zombie Tycoon [GUI - Auto Click, Auto Collect & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Survival%20Zombie%20Tycoon%20GUI%20%20Auto%20Click%20Auto%20Collect%20%20More.lua")
