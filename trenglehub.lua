46936362ee44a9962d1236619b1ef180fd6cf8f9
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:31:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:31:11"
+lastupdate = "GMT +1: 09.03.2023 14:31:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17186,3 +17186,5 @@ addscriptexist(4733278992, "Sword Blox Online: Rebirth [Kill Aura, Infinite Stam
 addscriptexist(606849621, "Jailbreak [Full Auto, Get Weapons & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jailbreak%20Full%20Auto%20Get%20Weapons%20%20More.lua")
 
 addscript(7776498299, "Current Game","Sea Cleaning Simulator [Auto Farm - GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sea%20Cleaning%20Simulator%20Auto%20Farm%20%20GUI.lua")
+
+addscript(28382335, "Current Game","Zombie Tycoon [Silent Aim - GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Zombie%20Tycoon%20Silent%20Aim%20%20GUI.lua")
