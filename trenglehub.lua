f391fee49df65c096798beb722c2c5d31bd4a343
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:52:44 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 12:52:40"
+lastupdate = "GMT +1: 09.03.2023 12:52:44"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15212,3 +15212,5 @@ addscriptexist(2537430692, "Jenga [Auto Win]", "pulled from rbxscript.com", "htt
 addscriptexist(10118559731, "nico's nextbots [bloodmoon] [ESP, Door Spam]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/nicos%20nextbots%20bloodmoon%20ESP%20Door%20Spam.lua")
 
 addscriptexist(8811271345, "Balanced Craftwars Overhaul [Auto Equip, Teleport]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Balanced%20Craftwars%20Overhaul%20Auto%20Equip%20Teleport.lua")
+
+addscriptexist(4863541388, "Scythe Masters [Auto click]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Scythe%20Masters%20Auto%20click.lua")
