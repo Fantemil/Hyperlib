d87ba50d7db6e4c2a8fb5e69e95ea990ebf7b78c
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:42:03 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Da Hood Auto Money Drop Collect.lua
@@ -0,0 +1,22 @@
+--da hood
+local localPlayer = game:GetService("Players").LocalPlayer
+
+local function booga()
+   local closestPlayer = nil
+ local shortestDistance = math.huge
+   for i, v in pairs(game:GetService("Workspace").Ignored.Drop:GetChildren()) do
+       if v:FindFirstChild("ClickDetector") then
+           local magnitude = (v.Position - localPlayer.Character.Head.Position).magnitude
+
+           if magnitude < shortestDistance then
+               closestPlayer = v
+               shortestDistance = magnitude
+           end
+       end
+   end
+
+   return closestPlayer
+end
+game:GetService"RunService".RenderStepped:Connect(function()
+fireclickdetector(booga():FindFirstChild("ClickDetector"))
+end)
