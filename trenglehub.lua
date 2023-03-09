5c389093abfb0fd4c855970bc8687d3dc5c798f0
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:13:12 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:13:08"
+lastupdate = "GMT +1: 09.03.2023 15:13:12"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18244,3 +18244,5 @@ addscript(9346039031, "Current Game","My Hello Kitty Cafe [Semi Auto Farm]", "pu
 addhub("Catix Hub V2 [Supported 252 Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Catix%20Hub%20V%20Supported%20%20Games.lua")
 
 addhub("Monkey Hub [Supported 8 Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Monkey%20Hub%20Supported%20%20Games.lua")
+
+addhub("Owl Hub [Supported 20+ Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Owl%20Hub%20Supported%20%20Games.lua")
