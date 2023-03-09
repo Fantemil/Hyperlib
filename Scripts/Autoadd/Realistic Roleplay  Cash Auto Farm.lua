f7669f737f8b2c367401681b3312dc1ea3795721
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:52:53 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Realistic Roleplay  Cash Auto Farm.lua
@@ -0,0 +1,6 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-51.7764664, 28.1612186, 21.0545406, -0.951058924, -4.81336677e-08, 0.309009582, -4.44289583e-08, 1, 1.90256646e-08, -0.309009582, 4.36555414e-09, -0.951058924)
+
+while true do
+workspace.Megamart.Cashier.CashRegister.Job.JobEvent:FireServer()
+wait(16)
+end
