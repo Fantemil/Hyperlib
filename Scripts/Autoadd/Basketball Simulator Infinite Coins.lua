09e68206634efe0de209990b5b68e878c2b3ee9c
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:12:43 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Basketball Simulator Infinite Coins.lua
@@ -0,0 +1,20 @@
+local args = {
+   [1] = "range",
+   [2] = -501
+}
+
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.2").knit.Services.LevelService.RF.LevelAbility:InvokeServer(unpack(args))
+wait(0.2)
+local args = {
+   [1] = "range",
+   [2] = "1"
+}
+
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.2").knit.Services.LevelService.RF.LevelAbility:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+   [1] = "range",
+   [2] = 501
+}
+
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.2").knit.Services.LevelService.RF.LevelAbility:InvokeServer(unpack(args))
