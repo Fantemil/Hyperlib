7ad3aebf00181e724ed23a2ac49bcde93f7fbac8
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:19:17 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Legend Piece Fruit Changer.lua
@@ -0,0 +1,7 @@
+local args = {
+   [1] = game.Players.LocalPlayer.Backpack.Combat,
+   [2] = "None",
+   [3] = "Magma" -- Change This, Light, Magma, Quake, Flame, Ice, Paw, Sand, Invisible
+}
+
+game:GetService("ReplicatedStorage").Remotes.Eat:FireServer(unpack(args))
