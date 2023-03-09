7853ca393b546e1cd2751a7f5bbe0f022f6c5c53
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:54:45 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ninja Tycoon Auto Click.lua
@@ -0,0 +1,3 @@
+game:GetService("RunService").Heartbeat:Connect(function()
+  fireclickdetector(game:GetService("Workspace")["Zednov's Tycoon Kit"].Tycoons["YOUR CLAN"].PurchasedObjects.Mine.Clicker.ClickDetector)
+end)
