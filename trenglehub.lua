a956def0b3e776a5e6ae2bd80b5a92d2494989cb
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:56:49 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:56:45"
+lastupdate = "GMT +1: 09.03.2023 14:56:49"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17840,3 +17840,5 @@ addscriptexist(6299805723, "Anime Fighters Simulator [GUI - Auto Farm]", "pulled
 addscriptexist(4787629450, "BOOGA BOOGA [GUI - Kill Aura, Auto Heal & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BOOGA%20BOOGA%20GUI%20%20Kill%20Aura%20Auto%20Heal%20%20More.lua")
 
 addscriptexist(6969185078, "Elemental Awakening [GUI - Auto Level - TP To Location]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Elemental%20Awakening%20GUI%20%20Auto%20Level%20%20TP%20To%20Location.lua")
+
+addscriptexist(9573746878, "Bleach - Senko [Shinigami]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bleach%20%20Senko%20Shinigami.lua")
