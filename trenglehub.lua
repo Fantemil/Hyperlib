b4a5e578abde095dd82ebb6530d300946890e5e4
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:14:18 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 12:14:00"
+lastupdate = "GMT +1: 09.03.2023 12:14:18"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15127,3 +15127,4 @@ addscriptexist(11118216071, "New OP Pet Trading Card Simulator GUI", "pulled fro
 addscriptexist(4872321990, "Item drop Notifier Webhook for ISLANDS", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Item%20drop%20Notifier%20Webhook%20for%20ISLANDS.lua")
 addscriptexist(11523066819, "Best Op Script For Tower Merge Simulator!", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Best%20Op%20Script%20For%20Tower%20Merge%20Simulator.lua")
 addscriptexist(6708206173, "Trivia Script - RATE MY AVATAR", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Trivia%20Script%20%20RATE%20MY%20AVATAR.lua")
+addscriptuniversal("Collision Enabler Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Collision%20Enabler%20Script.lua")
