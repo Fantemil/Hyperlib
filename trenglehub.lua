75437d795a6deb9463b02be0064321e2279c9b32
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:37:28 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:37:21"
+lastupdate = "GMT +1: 09.03.2023 14:37:28"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17346,3 +17346,5 @@ addscriptexist(6728870912, "World of Stands Demo [Auto Farm - Auto Skill - Auto 
 addscript(112420803, "Current Game","Kohls Admin House [GUI - Commands]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Kohls%20Admin%20House%20GUI%20%20Commands.lua")
 
 addscriptexist(9249776514, "Givenchy Beauty House [Finish The Event Quest]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Givenchy%20Beauty%20House%20Finish%20The%20Event%20Quest.lua")
+
+addscriptexist(1382113806, "Silent Assassin [Assassin ESP]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Silent%20Assassin%20Assassin%20ESP.lua")
