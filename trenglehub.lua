bc7ce02d2212091617b048dd78c9897a8daf80a2
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 16:05:40 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 16:05:35"
+lastupdate = "GMT +1: 09.03.2023 16:05:40"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19548,3 +19548,5 @@ addscriptexist(8232438881, "Thief Simulator [Auto Rob - Auto Sell & More!]", "pu
 addscriptexist(6494523288, "Blade Quest [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blade%20Quest%20Auto%20Farm.lua")
 
 addscript(8584121669, "Current Game","Pet Simulator X Fan Made [Infinite Gems]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Fan%20Made%20Infinite%20Gems.lua")
+
+addscriptexist(8584121669, "Pet Simulator X Fan Made [Infinite Coins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Fan%20Made%20Infinite%20Coins.lua")
