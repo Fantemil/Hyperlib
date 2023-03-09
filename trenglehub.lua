26b08b2b6e355b3169939a664981a0ebba5b2c72
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 16:06:54 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 16:06:49"
+lastupdate = "GMT +1: 09.03.2023 16:06:54"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19576,3 +19576,5 @@ addscriptexist(6284583030, "Pet Simulator X [SCAM SCRIPT - Infinite Gems]", "pul
 addscript(8697536518, "Current Game","Pet Simulator Z [Infinite Money]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20Z%20Infinite%20Money.lua")
 
 addscriptexist(8232438881, "Thief Simulator [GUI - Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Thief%20Simulator%20GUI%20%20Auto%20Farm.lua")
+
+addscriptexist(4581966615, "Anomic [Money Generator - 50K Per Sec]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anomic%20Money%20Generator%20%20K%20Per%20Sec.lua")
