545c97eb985728bcb3086e66d3761caf6285af35
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:35:37 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:35:33"
+lastupdate = "GMT +1: 09.03.2023 14:35:37"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17300,3 +17300,5 @@ addscriptexist(137877687, "RoCitizens [Nametag Color, Cape Texture Speed, Friend
 addscriptexist(1240123653, "Zombie Attack [Auto Farm, Infite Jump, God Mode]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Zombie%20Attack%20Auto%20Farm%20Infite%20Jump%20God%20Mode.lua")
 
 addscriptexist(3199109640, "Camping 2 [Get Items, Collect Wood, Get Smore]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Camping%20%20Get%20Items%20Collect%20Wood%20Get%20Smore.lua")
+
+addscriptexist(8146731988, "Ultra Power Tycoon [Give All Weapons]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultra%20Power%20Tycoon%20Give%20All%20Weapons.lua")
