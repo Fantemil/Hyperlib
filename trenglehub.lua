8e7a70d75b4afc947c005d90beab5a4b315de417
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:12:48 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:12:44"
+lastupdate = "GMT +1: 09.03.2023 14:12:48"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16712,3 +16712,5 @@ addscriptexist(9961650006, "Shockwave Racing [Auto Farm]", "pulled from rbxscrip
 addscriptexist(9508087919, "Apeirophobia [Infinite Stamina, Teleports & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apeirophobia%20Infinite%20Stamina%20Teleports%20%20More.lua")
 
 addscript(9734866408, "Current Game","Basketball Simulator [Infinite Coins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Basketball%20Simulator%20Infinite%20Coins.lua")
+
+addscriptexist(9396463375, "Mining Empire [Infinite Coins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Empire%20Infinite%20Coins.lua")
