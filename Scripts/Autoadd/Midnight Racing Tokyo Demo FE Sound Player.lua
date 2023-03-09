60a6fa8a7e645613769bdb2271eb4373eb593472
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:11:39 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Midnight Racing Tokyo Demo FE Sound Player.lua
@@ -0,0 +1,7 @@
+getgenv().Sound = "5409360995"
+getgenv().Pitch = 1
+getgenv().Volume = 10
+
+-- Loadstring <3 --
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ihaveash0rtnamefordiscord/Releases/main/MidnightFEAudio", true))(' Watermelon ! ')
