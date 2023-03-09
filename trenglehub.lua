b5d3744e9352237a4a81635e4d276d1d5a9e6324
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:45:25 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:45:21"
+lastupdate = "GMT +1: 09.03.2023 13:45:25"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15998,3 +15998,5 @@ addscriptexist(5956785391, "Project Slayers [Fish Farm]", "pulled from rbxscript
 addscriptexist(4787629450, "BOOGA BOOGA - Classic [Invisible Armor & Tools]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BOOGA%20BOOGA%20%20Classic%20Invisible%20Armor%20%20Tools.lua")
 
 addscriptexist(29812337, "Framed! [ESP]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Framed%20ESP.lua")
+
+addscriptexist(2413927524, "The Rake REMASTERED [Rake Esp, FullBirght]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Rake%20REMASTERED%20Rake%20Esp%20FullBirght.lua")
