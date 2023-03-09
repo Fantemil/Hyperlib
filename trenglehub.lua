5d9b20bdef9c2563c3406b38d52853c1ae682fd0
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:52:27 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 12:52:22"
+lastupdate = "GMT +1: 09.03.2023 12:52:27"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15204,3 +15204,5 @@ addscriptexist(9585537847, "SHADOVIS RPG [Kill Aura]", "pulled from rbxscript.co
 addscriptexist(537413528, "Build A Boat For Treasure [Auto Farm, Teleport]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Farm%20Teleport.lua")
 
 addscriptexist(189707, "Natural Disaster Survival [No Fall, Gravity]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Natural%20Disaster%20Survival%20No%20Fall%20Gravity.lua")
+
+addscriptexist(7167319176, "Chicago Remastered [Speed, Jump Power]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Chicago%20Remastered%20Speed%20Jump%20Power.lua")
