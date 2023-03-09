9293e8aa77a718960d0aee6797ec127836f64406
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:33:31 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Project Baki  Auto Farm Free.lua
@@ -0,0 +1,3 @@
+_G.Toggle = true -- true or false toggles the autofarm
+_G.Mob = 'Street Thug' -- Examples 'Karate Student' 'Street Thug' A lot more mob names on Dark Dex I don't play the game so no clue wtf are the good mobs and shit
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Reabbb/RLua/main/ProjectBaki.lua"))()
