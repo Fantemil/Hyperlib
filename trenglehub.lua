7439bc45155114287fd1d1e8a6d65b81a26e9f72
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:55:58 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:55:53"
+lastupdate = "GMT +1: 09.03.2023 14:55:58"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17818,3 +17818,5 @@ addscriptexist(7444263453, "Jetpack Jumpers [GUI - Auto Farm, Reedem Codes]", "p
 addscriptexist(680750021, "Case Clicker [GUI - Auto Farm, Auto Upgrade]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Case%20Clicker%20GUI%20%20Auto%20Farm%20Auto%20Upgrade.lua")
 
 addscript(8524876576, "Current Game","Glider Simulator [Auto Collect Coins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Glider%20Simulator%20Auto%20Collect%20Coins.lua")
+
+addscript(9488143445, "Current Game","Raise A Bloppa [Auto Farm GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Raise%20A%20Bloppa%20Auto%20Farm%20GUI.lua")
