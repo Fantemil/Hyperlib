30852e40ac1e9b0e71774799210f1d8430cb92ce
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:04:37 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Mad City Get Weekly Rewards.lua
@@ -0,0 +1,8 @@
+local A = game:GetService("ReplicatedStorage").RemoteFunction
+A:InvokeServer("WeekReward", "Week1")
+A:InvokeServer("WeekReward", "Week2")
+A:InvokeServer("WeekReward", "Week3")
+A:InvokeServer("WeekReward", "Week4")
+A:InvokeServer("WeekReward", "Week5")
+A:InvokeServer("WeekReward", "Week6")
+A:InvokeServer("WeekReward", "Season6")
