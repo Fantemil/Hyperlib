c876e62d93aa0127f46c46610680a012fc73249f
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:26:17 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:26:07"
+lastupdate = "GMT +1: 09.03.2023 13:26:17"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15498,3 +15498,5 @@ addscriptexist(155382109, "Survive and Kill the Killers in Area 51 [Infinite amm
 addscriptexist(6284583030, "Pet simulator X [Auto Farm, Auto Collect, Auto Egg]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20simulator%20X%20Auto%20Farm%20Auto%20Collect%20Auto%20Egg.lua")
 
 addscriptexist(9285238704, "Race Clicker [Infinite Speed]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Race%20Clicker%20Infinite%20Speed.lua")
+
+addscriptexist(2727067538, "World Zero V1.8 [Warlord Class]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/World%20Zero%20V%20Warlord%20Class.lua")
