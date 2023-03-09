e40c1c6be3d7c6741b888c9d11cbbd0a6c43c511
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:59:24 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Skate Park Free Ugc Items.lua
@@ -0,0 +1,6 @@
+game:GetService("ReplicatedStorage").lib.Network.RemoveRide:InvokeServer()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").BadgeAwarder.Platform.CFrame
+game:GetService("ReplicatedStorage").OSTBadge:FireServer() 
+game:GetService("ReplicatedStorage").lib.Network.RemoveRide:InvokeServer()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").BadgeAwarder.Platform.CFrame
+game:GetService("ReplicatedStorage").OSTBadge:FireServer() 
