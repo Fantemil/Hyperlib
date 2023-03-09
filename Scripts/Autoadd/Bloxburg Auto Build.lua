404011998e012025cb60cc807b8fcd800b2dc467
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:07:29 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Bloxburg Auto Build.lua
@@ -0,0 +1,2 @@
+local http_request = http_request or request or (syn and syn.request) or (http and http.request)
+loadstring(http_request({Url="https://projectillusion.dev/beta/illusion/illusion_bloxburg.lua", Method="GET"}).Body)()
