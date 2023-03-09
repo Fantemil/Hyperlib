86689cd6c12dfa6ab4ad75a69d3c71440cd9c049
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:48:22 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Vinux Universal Hub INF TOKENS FE GALE ART GRABBER.lua
@@ -0,0 +1,18 @@
+ -- Dear user! Thanks for using Vinux.
+-- Let's set up some stuff before executing script
+
+local UiTheme = "DarkTheme" -- Please, replace it with one of suggested variants below.
+
+   -- LightTheme -- VERY LIGHT THEME
+   -- DarkTheme -- One of the best themes. Looks cool and very dark.
+   -- GrapeTheme -- Looks purple. Very cool colours
+   -- BloodTheme -- wow i like it, it was used from 0.34-0.43 version
+   -- Ocean -- just a blue theme
+   -- Midnight -- that looks like blue and green lol
+   -- Sentinel -- Very nice ui. A combo of Light Green, Medium red and dark grey.
+   -- Synapse -- It looks like ui of pornhub
+   -- Serpent -- Draco Malfoy lore
+   
+  -- Now let's load in!
+   
+loadstring(game:HttpGet("https://raw.githubusercontent.com/biohzzrd/Vinux/main/Vinux"))()
