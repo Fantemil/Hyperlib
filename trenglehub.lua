aae5e4b64e96d3a2a81b62fce7a1bdf0cdc7b47a
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:31:01 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:30:57"
+lastupdate = "GMT +1: 09.03.2023 14:31:01"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17180,3 +17180,5 @@ addscriptexist(4598019433, "Generic Roleplay Gaem [Auto Farm]", "pulled from rbx
 addscriptexist(301549746, "Counter blox [Crash Server, God mode]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Counter%20blox%20Crash%20Server%20God%20mode.lua")
 
 addscriptexist(292439477, "Phantom Forces [Highlight ESP, Chams]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Highlight%20ESP%20Chams.lua")
+
+addscriptexist(4733278992, "Sword Blox Online: Rebirth [Kill Aura, Infinite Stamina, Collect Materials]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20Blox%20Online%20Rebirth%20Kill%20Aura%20Infinite%20Stamina%20Collect%20Materials.lua")
