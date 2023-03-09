2654f4adee27de61cfd24c1eafa36320d7fe86fa
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:07:10 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Hunter x Athena Auto Buy Apples.lua
@@ -0,0 +1,4 @@
+while true do
+fireclickdetector(game:GetService("Workspace").Food.Apple["Apple - 100 Jeni"].Head.ClickDetector)
+ wait(60) --seconds between apple buying(in case you dont script)
+end
