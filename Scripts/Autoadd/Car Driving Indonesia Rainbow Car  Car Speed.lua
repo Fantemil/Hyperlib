e6486948444acc8bf35b7d5653d84780f21c71fb
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:45:10 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Car Driving Indonesia Rainbow Car  Car Speed.lua
@@ -0,0 +1,7 @@
+_G.CarSpeed = {
+Enabled = true,
+Multiplier = 5
+}
+_G.RainbowCar = true
+-- MUST BE IN VEHICLE BEFORE EXECUTING
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/CarDrivingIndonesia.lua"))()
