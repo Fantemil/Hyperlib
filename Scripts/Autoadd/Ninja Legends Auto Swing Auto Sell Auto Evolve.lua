98d09d237c25fdb57279c3a582bcf6d7b746e251
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:02:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ninja Legends Auto Swing Auto Sell Auto Evolve.lua
@@ -0,0 +1,13 @@
+-- config here/might be buggy : 
+getgenv().Autofarm = false
+getgenv().AutoSell = false
+getgenv().AutoBuybelt = false
+getgenv().AutoBuysword = false
+getgenv().AutoBuyskills = false
+getgenv().AutoBuyshuriken = false
+getgenv().AutoEvlove = false
+getgenv().AutoBoss = false
+getgenv().Autohill = false
+
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Hunter1v3x/LuaForRoblox/main/Master/Hunter.lua")()
