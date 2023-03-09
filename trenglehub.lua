ec193f493bff5ad440d79a1198138257286a304b
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:15:08 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:15:04"
+lastupdate = "GMT +1: 09.03.2023 15:15:08"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18294,3 +18294,5 @@ addscriptexist(9049840490, "Sonic Speed Simulator [New Faster Auto Farm]", "pull
 addscriptexist(9049840490, "Sonic Speed Simulator [Auto Open Chests]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sonic%20Speed%20Simulator%20Auto%20Open%20Chests.lua")
 
 addscriptexist(9530846958, "Epsilon 2 [GUI V1 - Auto Trinket & More]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Epsilon%20%20GUI%20V%20%20Auto%20Trinket%20%20More.lua")
+
+addscriptexist(155382109, "Survive and kill the killers in Area 51 [GUI - Give All Guns]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Survive%20and%20kill%20the%20killers%20in%20Area%20%20GUI%20%20Give%20All%20Guns.lua")
