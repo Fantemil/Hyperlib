e737b84590f236e6a14f69321a11871f665618bc
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:59:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:59:34"
+lastupdate = "GMT +1: 09.03.2023 14:59:38"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17910,3 +17910,5 @@ addscriptexist(4087265156, "VERTIGO [Mini GUI - No Fall Damage]", "pulled from r
 addscript(7665856814, "Current Game","Insomniac World Party [Auto Mine]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Insomniac%20World%20Party%20Auto%20Mine.lua")
 
 addscriptexist(2262441883, "Electric State DarkRP [God Mode - Open Source]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Electric%20State%20DarkRP%20God%20Mode%20%20Open%20Source.lua")
+
+addscript(9139880532, "Current Game","Trollverse RP [Get Weapons, Damage Multiply]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Trollverse%20RP%20Get%20Weapons%20Damage%20Multiply.lua")
