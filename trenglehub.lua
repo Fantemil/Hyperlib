7712e4053d53d954cd480c129186d46a8130adb2
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:33:23 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:33:19"
+lastupdate = "GMT +1: 09.03.2023 15:33:23"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18752,3 +18752,5 @@ addscript(9123917180, "Current Game","Teleporter Simulator [Basic Farm]", "pulle
 addscriptexist(2000062521, "State Of Anarchy [Aimbot, Silent Aim, Esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/State%20Of%20Anarchy%20Aimbot%20Silent%20Aim%20Esp.lua")
 
 addscriptexist(1224212277, "Mad City [New XP Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mad%20City%20New%20XP%20Farm.lua")
+
+addscriptexist(9300407930, "Bot Clash [Complete All Quests]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bot%20Clash%20Complete%20All%20Quests.lua")
