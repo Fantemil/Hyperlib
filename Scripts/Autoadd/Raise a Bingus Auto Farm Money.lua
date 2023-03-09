75032260124d220477c225ff7f3e1c9965577623
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:20:32 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Raise a Bingus Auto Farm Money.lua
@@ -0,0 +1,5 @@
+game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Purchasables["Work Tier 3"].Purchasable.CFrame
+while true do
+task.wait()
+fireproximityprompt(game:GetService("Workspace").Purchasables["Work Tier 3"].Purchasable.ProximityPrompt)
+end
