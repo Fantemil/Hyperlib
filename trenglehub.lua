609cb752b375b4a9002d534f0857b083c9abe21e
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:24:03 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:23:54"
+lastupdate = "GMT +1: 09.03.2023 13:24:03"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15444,3 +15444,5 @@ addscriptexist(10386774046, "Legend Magician [1 Shot]", "pulled from rbxscript.c
 addscript(8707573053, "Current Game","nowhere [Collect Emply Bottles]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/nowhere%20Collect%20Emply%20Bottles.lua")
 
 addscript(109814071, "Current Game","Medieval Warfare Reforged [Auto Farm - GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Medieval%20Warfare%20Reforged%20Auto%20Farm%20%20GUI.lua")
+
+addscriptexist(1224212277, "Mad City Chapter 2 [Auto Arrest]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mad%20City%20Chapter%20%20Auto%20Arrest.lua")
