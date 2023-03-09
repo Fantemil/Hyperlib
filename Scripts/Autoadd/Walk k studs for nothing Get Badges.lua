f2bced6291504e5fcfc1d20421325bff5f8f058f
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:36:23 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Walk k studs for nothing Get Badges.lua
@@ -0,0 +1,7 @@
+for _,v in pairs(workspace:GetDescendants()) do
+if v:IsA("TouchTransmitter") then
+firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0) --0 is touch
+wait()
+firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1) -- 1 is untouch
+end
+end
