fa064b4f6cec8acab5e58514e94c3ca10cf665e6
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:53:12 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 12:53:08"
+lastupdate = "GMT +1: 09.03.2023 12:53:12"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15222,3 +15222,5 @@ addscriptexist(260606376, "Realistic Roleplay 2 [Cash Auto Farm]", "pulled from 
 addscriptexist(6512923934, "Ultra Clicker Simulator [Auto Click]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultra%20Clicker%20Simulator%20Auto%20Click.lua")
 
 addscriptexist(9970551995, "Touch The Grass Simulator! [Killaura, auto hatch]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Touch%20The%20Grass%20Simulator%20Killaura%20auto%20hatch.lua")
+
+addscriptexist(621129760, "Knife Ability Test [Silent Aim, Aimbot]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Knife%20Ability%20Test%20Silent%20Aim%20Aimbot.lua")
