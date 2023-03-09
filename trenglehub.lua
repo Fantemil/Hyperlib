f6526c218c554fb5edeab7450c740f7836dd41e5
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:37:37 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:37:32"
+lastupdate = "GMT +1: 09.03.2023 14:37:37"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17350,3 +17350,5 @@ addscriptexist(9249776514, "Givenchy Beauty House [Finish The Event Quest]", "pu
 addscriptexist(1382113806, "Silent Assassin [Assassin ESP]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Silent%20Assassin%20Assassin%20ESP.lua")
 
 addscript(9456941655, "Current Game","PUBG [Kill All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/PUBG%20Kill%20All.lua")
+
+addscript(9293082713, "Current Game","Prank Weapon Battles [Kill All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Prank%20Weapon%20Battles%20Kill%20All.lua")
