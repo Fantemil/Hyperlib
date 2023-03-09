ed981ef36a5ca47f9a93de5a9e3caf331b12ab78
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:41:08 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:41:03"
+lastupdate = "GMT +1: 09.03.2023 14:41:08"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17440,3 +17440,5 @@ addscriptexist(9551640993, "Mining Simulator 2 [Hatch Webhook Sender]", "pulled 
 addscriptexist(2000062521, "State of Anarchy 0.15.83.0 [Kill All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/State%20of%20Anarchy%20%20Kill%20All.lua")
 
 addscriptexist(70005410, "Blox Hunt [Prop Chams]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Hunt%20Prop%20Chams.lua")
+
+addscriptexist(370731277, "Meep City [Snowball All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Meep%20City%20Snowball%20All.lua")
