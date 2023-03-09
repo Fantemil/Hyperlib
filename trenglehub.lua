c2696385bc11900a40a620ca4b59336f00f93934
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:44:05 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:44:00"
+lastupdate = "GMT +1: 09.03.2023 15:44:05"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19022,3 +19022,5 @@ addscriptexist(6778844213, "Euphoria ragdoll testing [Ragdoll All]", "pulled fro
 addscriptexist(8916037983, "starving artists [Auto Advertise]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/starving%20artists%20Auto%20Advertise.lua")
 
 addscriptexist(2768379856, "3008 [2.7] [Simple ESP]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20%20Simple%20ESP.lua")
+
+addscriptexist(8862273695, "Another JToH RPG [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Another%20JToH%20RPG%20Auto%20Farm.lua")
