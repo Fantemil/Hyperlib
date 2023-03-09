60cc4259bdd1c75fc43f315ab05032ef1ec3e419
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:27:45 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:27:40"
+lastupdate = "GMT +1: 09.03.2023 14:27:45"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17096,3 +17096,5 @@ addscriptexist(3346617530, "Freeze Tag Extreme [Collect All Items - Tag Al]", "p
 addscriptexist(9726345231, "Duolingo Game Hub [Dev Badge - Roblox Event]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Duolingo%20Game%20Hub%20Dev%20Badge%20%20Roblox%20Event.lua")
 
 addhub("Arctic Hub [3 Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arctic%20Hub%20%20Games.lua")
+
+addscriptexist(2788229376, "Da Hood [GUI - Aimlock, KillBot & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20GUI%20%20Aimlock%20KillBot%20%20More.lua")
