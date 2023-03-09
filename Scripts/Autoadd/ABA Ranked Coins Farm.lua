22b30dc20369b3a8ca707c4252567b58f41409fa
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:49:40 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/ABA Ranked Coins Farm.lua
@@ -0,0 +1,5 @@
+getgenv().Reset = true
+getgenv().ResetDelay = 7 -- Reset after x second(s)
+getgenv().Ban = "Lemillion" -- Recommended to change this to whatever u want
+getgenv().Message = "gl hf" -- Recommended to change this to ur own message
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aknoks/script/main/RewrittenABA"))()
