b3da51a6d0751de4f1a999d50e4a599ec89bf5ac
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:38:38 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:38:34"
+lastupdate = "GMT +1: 09.03.2023 15:38:38"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18882,3 +18882,5 @@ addscriptexist(8492553855, "Project Upright [Use Any Move]", "pulled from rbxscr
 addscriptexist(7817016260, "Bloxston Mystery [Discover Everyones Roles]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bloxston%20Mystery%20Discover%20Everyones%20Roles.lua")
 
 addscriptexist(4111023553, "Deepwoken [Chest ESP, No Fog, And More]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Deepwoken%20Chest%20ESP%20No%20Fog%20And%20More.lua")
+
+addscriptexist(3501626887, "Diceboy [Bring Dicecoins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Diceboy%20Bring%20Dicecoins.lua")
