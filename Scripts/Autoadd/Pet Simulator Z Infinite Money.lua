cb73754eb8e0ade7cbd957e718642c3ae2ec315a
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 16:06:43 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Pet Simulator Z Infinite Money.lua
@@ -0,0 +1,3 @@
+game:GetService("ReplicatedStorage").BuyDoor:FireServer("Forest", -9.9999999999999999e20)
+game:GetService("ReplicatedStorage").BuyDoor:FireServer("Secret", -9.9999999999999999e20)
+game:GetService("ReplicatedStorage").BuyDoor:FireServer("TheFantasyStart", -9.9999999999999999e20)
