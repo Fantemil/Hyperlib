cafbe8306573a7cd6b80e191ee15535c25ffc682
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:32:47 +0100

    Updated automatically!

--- Scripts/Autoadd/Da Hood God mode.lua
+++ Scripts/Autoadd/Da Hood God mode.lua
@@ -1,12 +1,1 @@
-game:GetService("Players").LocalPlayer.Character.BodyEffects["K.O"]:Destroy()
-game:GetService("Players").LocalPlayer.Character.BodyEffects.Death:Destroy()
-game:GetService("Players").LocalPlayer.Character.Death:Destroy()
-local args = {
-    [1] = "colb is nice",
-    [2] = "All"
-}
-
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-
-game:GetService("Players").LocalPlayer.Character.CharacterDied:Destroy()
-game:GetService("Players").LocalPlayer.Character.Death:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cypherdh/GodMode-V3/main/script"))()
