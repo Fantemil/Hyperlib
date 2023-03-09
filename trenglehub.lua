9a4bb4c2a04f54f5f4f57235e5b2fe389ddf76da
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:46:47 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:46:43"
+lastupdate = "GMT +1: 09.03.2023 13:46:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16034,3 +16034,5 @@ addscriptexist(9167501050, "Lightsaber Arena [Kill aura, auto block, inf stamina
 addscriptexist(9377039667, "Roblox Innovation Awards Voting Hub [Teleport to secret award/badge]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Innovation%20Awards%20Voting%20Hub%20Teleport%20to%20secret%20awardbadge.lua")
 
 addscriptexist(5956785391, "Project Slayers [Speedhack]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Speedhack.lua")
+
+addscript(10041417653, "Current Game","Turbo's Murder Mystery [Esp, AutoUnBox, Sped Hack]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Turbos%20Murder%20Mystery%20Esp%20AutoUnBox%20Sped%20Hack.lua")
