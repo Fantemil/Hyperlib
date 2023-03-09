bc690dbe4f044ca41f389268bae55fea17d47df7
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:00:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:00:19"
+lastupdate = "GMT +1: 09.03.2023 13:00:31"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15398,3 +15398,5 @@ addscriptexist(4036494886, "Gun Simulator [Infinite Everything & Custom Stats]",
 addscriptexist(8689257920, "Life in Prison [GUI - Kill All & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Life%20in%20Prison%20GUI%20%20Kill%20All%20%20More.lua")
 
 addscriptexist(9498006165, "Tapping Simulator [Auto Farm - GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tapping%20Simulator%20Auto%20Farm%20%20GUI.lua")
+
+addscriptexist(7991339063, "Rainbow Friends [Coin Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rainbow%20Friends%20Coin%20Farm.lua")
