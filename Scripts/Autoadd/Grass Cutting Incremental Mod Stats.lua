c3c7c197e4603faf4c3295f676938bcaf5a86ec5
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:09:55 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Grass Cutting Incremental Mod Stats.lua
@@ -0,0 +1,8 @@
+local mod = require(game:GetService(ReplicatedStorage).Multipliers)
+
+-- Dont change the first number, when you want to get moreless then change the second number
+setconstant(mod.calculateMulti, 14, 100) -- Platinium 100%
+setconstant(mod.calculateMulti, 129, 1000000000) -- Rebirth multi
+setconstant(mod.calculateMulti, 150, 1000000000) -- Crystal Multi
+setconstant(mod.calculateMulti, 56, 2000000000000000000) -- Level Multi
+setconstant(mod.calculateMulti, 96, 100) -- Faster Spawn
