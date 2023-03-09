65ef635a2ac11d7c2d4f4a12b8445570d931b096
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:43:19 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/FORCE PREALPHA Auto Farm.lua
@@ -0,0 +1,8 @@
+_G.Autofarm = {
+   Activated = true; --[ _G.Autofarm.Activated = false ] - to Disable the autofarm
+   Type = ('All'); -- Lon FLower / All / Carami Flower / Hora Flower (Capitals apply)
+
+   Debug = false; -- Was made for myself but enable if you want to look cool I guess
+};
+
+loadstring(game:HttpGet('https://lowfihub.cc/Scripts/Force.lua', true))();
