263029e79822366299aac08719599f60e2d4b722
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:49:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:49:11"
+lastupdate = "GMT +1: 09.03.2023 15:49:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19150,3 +19150,5 @@ addscriptexist(5116869569, "Doodle World [Heal Your Team]", "pulled from rbxscri
 addscriptexist(2693023319, "Expedition Antarctica [Get 500B Coins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Expedition%20Antarctica%20Get%20B%20Coins.lua")
 
 addscriptexist(7993293100, "Tsunami Game [Auto Win - Farm Deaths & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tsunami%20Game%20Auto%20Win%20%20Farm%20Deaths%20%20More.lua")
+
+addscript(8657732325, "Current Game","World Sail [Auto Farm Enhance Level]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/World%20Sail%20Auto%20Farm%20Enhance%20Level.lua")
