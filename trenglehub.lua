ec7cb994209394a11f0b45367809d52fd6a5e37e
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:09:02 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:08:56"
+lastupdate = "GMT +1: 09.03.2023 15:09:02"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18140,3 +18140,5 @@ addscript(9426082120, "Current Game","Samsung Superstar Galaxy [Auto Collect All
 addscriptexist(4282985734, "Combat Warrior [Auto Farm Free GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Combat%20Warrior%20Auto%20Farm%20Free%20GUI.lua")
 
 addscriptexist(6284583030, "Pet Simulator X [New GUI LKHUB]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20New%20GUI%20LKHUB.lua")
+
+addscriptexist(4282985734, "Combat Warriors [CombatSpy - Modified Remote Spy]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Combat%20Warriors%20CombatSpy%20%20Modified%20Remote%20Spy.lua")
