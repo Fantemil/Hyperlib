d13b5ef238a8cbbc02d849e6073d886d6a170884
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:29:05 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Elemental Pet Battles Infinite Coins.lua
@@ -0,0 +1,6 @@
+local args = {
+   [1] = "SpawnDefault",
+   [2] = -9999999999999999
+}
+
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.3").knit.Services.EggShopService.RF.RequestBuyEggs:InvokeServer(unpack(args))
