e79f81f1a91e070bf46766a9278449514b51ea3a
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 12:58:24 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 12:58:19"
+lastupdate = "GMT +1: 09.03.2023 12:58:24"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15348,3 +15348,5 @@ addscriptexist(2474168535, "Westbound [GodMode, esp]", "pulled from rbxscript.co
 addscriptexist(2622527242, "SCP rBreach [ESP, Misc.]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SCP%20rBreach%20ESP%20Misc.lua")
 
 addscriptexist(5130598377, "A Universal Time [Auto Farm, Walkspeed]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/A%20Universal%20Time%20Auto%20Farm%20Walkspeed.lua")
+
+addscriptexist(7026828578, "Speed Simulator [Inf Score]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Speed%20Simulator%20Inf%20Score.lua")
