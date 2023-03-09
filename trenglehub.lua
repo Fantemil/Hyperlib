e7cf49203c7729b30d5d740e5249d8c65f94866a
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:55:32 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:55:26"
+lastupdate = "GMT +1: 09.03.2023 14:55:32"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17806,3 +17806,5 @@ addscript(7114796110, "Current Game","Anime Training Simulator [GUI - Auto Farm,
 addscript(7244314500, "Current Game","Fightman Simulator [GUI - Auto Farm, Teleports]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fightman%20Simulator%20GUI%20%20Auto%20Farm%20Teleports.lua")
 
 addscriptexist(142823291, "Murder Mystery 2 [ESP, Fake Knife, Invisible]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Murder%20Mystery%20%20ESP%20Fake%20Knife%20Invisible.lua")
+
+addscript(7277614831, "Current Game","Candy Eating Simulator [GUI - Auto Farm, Teleports & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Candy%20Eating%20Simulator%20GUI%20%20Auto%20Farm%20Teleports%20%20More.lua")
