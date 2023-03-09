d4853f4e6b072b09c8a8f754d6283690ac938f00
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:30:57 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Adonis Auto Bypass.lua
@@ -0,0 +1,13 @@
+local Namecall
+Namecall = hookmetamethod(game, '__namecall', function(self, ...)
+   local Caller = tostring(getcallingscript())
+   local Method = getnamecallmethod()
+   
+   if Caller == 'ClientMover' and Method == 'GetService' then
+       return
+   end
+   
+   return Namecall(self, ...)
+end)
+
+warn('[Adonis Autobypass]: this game is running with autobypass!')
