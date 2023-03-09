48af66320a5ee9f2168579b1b2dd586ff25ca6ad
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:11:48 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:11:44"
+lastupdate = "GMT +1: 09.03.2023 14:11:48"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16686,3 +16686,5 @@ addscriptexist(8739839954, "Jujutsu Tycoon [Kill All Players]", "pulled from rbx
 addscriptexist(9936860371, "Egg Packing Tycoon [GUI - Auto Collect Eggs, Auto Place Eggs]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Egg%20Packing%20Tycoon%20GUI%20%20Auto%20Collect%20Eggs%20Auto%20Place%20Eggs.lua")
 
 addscriptexist(6284583030, "Pet Simulator X [GUI - Auto Farm, Auto Collect All & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20GUI%20%20Auto%20Farm%20Auto%20Collect%20All%20%20More.lua")
+
+addscriptexist(70005410, "Blox Hunt [God Mode, Infinite Energy]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Hunt%20God%20Mode%20Infinite%20Energy.lua")
