e83f532d95ff1302771da828fc34d53f76f8b576
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:38:19 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Project Upright No Punch Cooldown.lua
@@ -0,0 +1,4 @@
+_G.Punchy = true -- Change to false to disable
+while _G.Punchy do task.wait()
+game:GetService("ReplicatedStorage","peke#7374").Ability:FireServer("Punch",{0,0,0},"peke#7374",{0,0,0},{nil,false,false})
+end
