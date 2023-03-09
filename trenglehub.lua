e99181aee6bd764a4c1216d309e2efcf5189c711
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:08:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:08:48"
+lastupdate = "GMT +1: 09.03.2023 14:08:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16612,3 +16612,5 @@ addscriptexist(8554378337, "Sword Simulator [Auto Hit]", "pulled from rbxscript.
 addhub("Roblox [Hub by Death]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Hub%20by%20Death.lua")
 
 addhub("Immigrant [ESP]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Immigrant%20ESP.lua")
+
+addscript(2688038742, "Current Game","Area - Omega [Kill All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Area%20%20Omega%20Kill%20All.lua")
