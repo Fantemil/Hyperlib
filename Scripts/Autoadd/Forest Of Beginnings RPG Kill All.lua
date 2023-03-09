b93492c2737aedfd92c126a9cb9f8fdaae0de014
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:45:07 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Forest Of Beginnings RPG Kill All.lua
@@ -0,0 +1,3 @@
+getgenv().LoopKillAll = true --Change to false to disable or true to enable and execute
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dooory/OC/main/Working/fobRPG_Kill_All.lua"))()
