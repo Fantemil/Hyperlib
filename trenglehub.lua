989c2b296350ec93dfafc890ca478583105a42c5
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:50:54 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:50:50"
+lastupdate = "GMT +1: 09.03.2023 13:50:54"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16142,3 +16142,5 @@ addscriptexist(4863541388, "Scythe Masters [Fast Strength Auto Farm]", "pulled f
 addscriptexist(7419509075, "cart ride around used car battery [Get All The Troll Items]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/cart%20ride%20around%20used%20car%20battery%20Get%20All%20The%20Troll%20Items.lua")
 
 addscriptexist(9677464361, "Basketball Pro Simulator [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Basketball%20Pro%20Simulator%20Auto%20Farm.lua")
+
+addscriptexist(8203181639, "Syrian Shenanigans [Full auto tank]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Syrian%20Shenanigans%20Full%20auto%20tank.lua")
