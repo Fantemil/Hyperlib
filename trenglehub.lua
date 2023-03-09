74c6d15585a49c1bcd6b13309e73373df5c9917c
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:37:10 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:37:06"
+lastupdate = "GMT +1: 09.03.2023 13:37:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15784,3 +15784,5 @@ addscriptexist(4809447488, "Venture Tale [KillAura, InstantKill]", "pulled from 
 addscriptexist(8962384769, "Firework Simulator [Auto-Farm, Auto-Buy Next Zone]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Firework%20Simulator%20AutoFarm%20AutoBuy%20Next%20Zone.lua")
 
 addscriptexist(6284583030, "Pet Simulator X [Mastery Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Mastery%20Auto%20Farm.lua")
+
+addscript(9377039667, "Current Game","Roblox Innovation Awards Voting Hub [All Hats & Badges]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Innovation%20Awards%20Voting%20Hub%20All%20Hats%20%20Badges.lua")
