6a623971bde70c13a11a571f89df42f1e819d890
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:39:26 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:39:22"
+lastupdate = "GMT +1: 09.03.2023 14:39:26"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17398,3 +17398,5 @@ addscriptexist(1224212277, "Mad City [Inf Nitro, Door Hack, Inf Ammo]", "pulled 
 addscriptexist(8425637426, "Write a Letter [Free Animation]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Write%20a%20Letter%20Free%20Animation.lua")
 
 addscriptexist(112420803, "Kohls Admin House NBC [Admin CMD]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Kohls%20Admin%20House%20NBC%20Admin%20CMD.lua")
+
+addscriptexist(8448735476, "Anime Rifts [Auto Attack , Auto Skills , Auto Mob]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Rifts%20Auto%20Attack%20%20Auto%20Skills%20%20Auto%20Mob.lua")
