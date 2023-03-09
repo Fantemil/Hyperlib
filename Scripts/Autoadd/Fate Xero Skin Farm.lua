e42ece0c9ec2e1846da8d3f732945a36f9d7eb3e
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:59:15 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Fate Xero Skin Farm.lua
@@ -0,0 +1,4 @@
+shared.Wanted = {"Lily","Tamaki","Aqua","Ryuko","Ayanokoji","Harribel","Zenitsu","Broly","Goku"} -- Servants/Skins you want to keep/uses string.find so if you have lily you may get any skin that has lily in it
+shared.Items = {"Gem","DragonBall","RoundTable","OrleansGrail","FuyukiGrail"} -- Items you want to use
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AstolfoHarem/Roblox-Scripts/main/FateXero%20Skin%20Farm"))()
