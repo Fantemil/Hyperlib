aac92e6cbe408636096e4179d73528b96121e1bd
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:57:36 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Raise A Bloppa Auto Collect Money Add Money Infinite Money.lua
@@ -0,0 +1,6 @@
+_G.Credits = "discord.gg/rips"
+_G.RedGUI = true
+_G.Theme = "Dark" -- Must disable or remove _G.RedGUI to use
+--Themes: Light, Dark, Mocha, Aqua and Jester
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/RaiseABloppa.lua"))()
