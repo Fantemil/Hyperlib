bda62793164c580665b6a5a468d9f97255b9a883
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:59:51 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:59:47"
+lastupdate = "GMT +1: 09.03.2023 13:59:51"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16382,3 +16382,5 @@ addscriptexist(10001513103, "Limited Words [FE Giant Spider]", "pulled from rbxs
 addscriptexist(8267733039, "Specter 2 [GUI - Infinite Stamina, Infinite Sanity & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Specter%20%20GUI%20%20Infinite%20Stamina%20Infinite%20Sanity%20%20More.lua")
 
 addscriptexist(8304191830, "Anime Adventures [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Adventures%20Auto%20Farm.lua")
+
+addscriptexist(7664413141, "Creeper Chaos [Auto Farm Wins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Creeper%20Chaos%20Auto%20Farm%20Wins.lua")
