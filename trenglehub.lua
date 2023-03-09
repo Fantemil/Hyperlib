63c11b3517cc40c551770fe6cf38905f8f4f66fa
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:53:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:53:09"
+lastupdate = "GMT +1: 09.03.2023 14:53:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17746,3 +17746,5 @@ addscriptexist(1224212277, "Mad City [GUI - Auto Arrest, Teleports & More!]", "p
 addscript(7830918930, "Current Game","Gucci Town [Collect All Letters & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Gucci%20Town%20Collect%20All%20Letters%20%20More.lua")
 
 addscriptexist(9551640993, "Mining Simulator 2 [GUI - Auto Mine, Auto Open Egg]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simulator%20%20GUI%20%20Auto%20Mine%20Auto%20Open%20Egg.lua")
+
+addscript(7065731541, "Current Game","Speedman Simulator [GUI - Auto Upgrade, Auto Farm & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Speedman%20Simulator%20GUI%20%20Auto%20Upgrade%20Auto%20Farm%20%20More.lua")
