23a2ebd826b8e3888f18d18e7eac718e7531cf64
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:50:46 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arsenal Esp.lua
@@ -0,0 +1,7 @@
+_G.FriendColor = Color3.fromRGB(0, 0, 255)
+_G.EnemyColor = Color3.fromRGB(255, 0, 0)
+_G.UseTeamColor = false
+
+--------------------------------------------------------------------
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-esp/main/esp.lua"))()
