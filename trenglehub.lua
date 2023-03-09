f67af2b156dcfc68e7f1e0f2c4b2fbc062309caf
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:40:33 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:40:29"
+lastupdate = "GMT +1: 09.03.2023 14:40:33"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17426,3 +17426,5 @@ addhub("x2SPETER HUB [5 Games]", "pulled from rbxscript.com", "https://raw.githu
 addscript(5534936306, "Current Game","A JOJO UNIVERSE [Destroy Game]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/A%20JOJO%20UNIVERSE%20Destroy%20Game.lua")
 
 addscriptexist(9049840490, "Sonic Speed Simulator [Unlock All Prizes]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sonic%20Speed%20Simulator%20Unlock%20All%20Prizes.lua")
+
+addhub("KeyBrew Hub [9 Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/KeyBrew%20Hub%20%20Games.lua")
