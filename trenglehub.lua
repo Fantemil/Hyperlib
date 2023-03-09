d2d387657648b84cf5d7707883c79e1ea01bb899
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:06:50 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:06:46"
+lastupdate = "GMT +1: 09.03.2023 14:06:50"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16562,3 +16562,5 @@ addscriptexist(8750997647, "Tapping Legends X [UPDATED AUTO FARM GUI]", "pulled 
 addscriptexist(8123652942, "Legend Piece [Give Devil Fruit]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legend%20Piece%20Give%20Devil%20Fruit.lua")
 
 addscriptexist(7560156054, "Clicker Simulator [Free Gamepasses]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Clicker%20Simulator%20Free%20Gamepasses.lua")
+
+addscriptexist(2753915549, "Blox Fruits [GUI - Auto farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20GUI%20%20Auto%20farm.lua")
