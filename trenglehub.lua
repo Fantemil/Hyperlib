34ee9166e31aa0af7ddb91b7087117dbb8b6a09f
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:52:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:52:10"
+lastupdate = "GMT +1: 09.03.2023 13:52:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16178,3 +16178,5 @@ addscriptexist(7305309231, "Taxi Boss [Remove NPC cars]", "pulled from rbxscript
 addscriptexist(4566572536, "Vehicle Legends [Car Speed]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Vehicle%20Legends%20Car%20Speed.lua")
 
 addscript(9903808883, "Current Game","Build a Market Tycoon [Money Booster]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20a%20Market%20Tycoon%20Money%20Booster.lua")
+
+addscriptexist(10041417653, "Turbo's Murder Mystery 2 [Inf Money]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Turbos%20Murder%20Mystery%20%20Inf%20Money.lua")
