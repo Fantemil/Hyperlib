a8addb1e4654c263483c96a02806031d13d1e5ec
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:30:31 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Duolingo Game Hub Free Items.lua
@@ -0,0 +1,4 @@
+game:GetService("ReplicatedStorage").CoinRemote:FireServer("addCoin", math.huge)
+wait(1)
+game:GetService("ReplicatedStorage").MerchBuyEvent:FireServer("BodySuit")
+game:GetService("ReplicatedStorage").MerchBuyEvent:FireServer("Hat")
