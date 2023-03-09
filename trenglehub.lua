619dd112f3c97e8138ce417b17d81d4cd8111e19
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:12:50 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:12:45"
+lastupdate = "GMT +1: 09.03.2023 15:12:49"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18234,3 +18234,5 @@ addscriptexist(1224212277, "Mad City [New GUI - Farm & More!]", "pulled from rbx
 addscriptexist(4588604953, "Criminality [Gun Mods, Players ESP, More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Criminality%20Gun%20Mods%20Players%20ESP%20More.lua")
 
 addhub("Neon Hub [Supported 8 Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Neon%20Hub%20Supported%20%20Games.lua")
+
+addhub("Alpha X Hub [Supported 17 Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Alpha%20X%20Hub%20Supported%20%20Games.lua")
