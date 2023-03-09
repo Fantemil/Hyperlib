7fedc634e6858b9cf4edce160138f8550bbfca61
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:44:53 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:44:48"
+lastupdate = "GMT +1: 09.03.2023 15:44:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19042,3 +19042,5 @@ addscriptexist(155382109, "Survive And Kill The Killers In Area 51 [Get An Overp
 addscript(8897454112, "Current Game","Pet Salon Tycoon [OP Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Salon%20Tycoon%20OP%20Auto%20Farm.lua")
 
 addscript(3210622855, "Current Game","Manner's Badge Walk [Get All Sky World Badges]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Manners%20Badge%20Walk%20Get%20All%20Sky%20World%20Badges.lua")
+
+addscriptexist(6778844213, "Euphoria ragdoll testing [Coin Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Euphoria%20ragdoll%20testing%20Coin%20Farm.lua")
