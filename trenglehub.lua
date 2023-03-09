14dd288971a179c5de30d98cfc7a9e39296cb1e3
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:31:49 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:31:45"
+lastupdate = "GMT +1: 09.03.2023 14:31:49"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17202,3 +17202,5 @@ addscriptexist(1345139196, "Treasure Hunt Simulator [Auto Farm]", "pulled from r
 addscript(9306912037, "Current Game","Hole In One Simulator [Auto Swing]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hole%20In%20One%20Simulator%20Auto%20Swing.lua")
 
 addscriptexist(198116126, "Tradelands [Auto Farm - Teleport - Walk On Water & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tradelands%20Auto%20Farm%20%20Teleport%20%20Walk%20On%20Water%20%20More.lua")
+
+addscriptexist(5315066937, "SuperB Surf Revised [Change Your Surfing, Bhop Speed]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SuperB%20Surf%20Revised%20Change%20Your%20Surfing%20Bhop%20Speed.lua")
