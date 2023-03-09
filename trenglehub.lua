8dee70dabd9604d2a05fc89431a8be16f2ae8164
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:27:24 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:27:19"
+lastupdate = "GMT +1: 09.03.2023 13:27:24"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15526,3 +15526,5 @@ addscript(9796315265, "Current Game","Streetz War [Anti Cheat Bypass]", "pulled 
 addscriptexist(7120576005, "Stack Ball [Auto Win - Open Source]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Stack%20Ball%20Auto%20Win%20%20Open%20Source.lua")
 
 addscriptexist(7655745946, "Dunking Simulator [Unlimited Boost]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dunking%20Simulator%20Unlimited%20Boost.lua")
+
+addscriptexist(7560156054, "Clicker Simulator [Collect Water Melons]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Clicker%20Simulator%20Collect%20Water%20Melons.lua")
