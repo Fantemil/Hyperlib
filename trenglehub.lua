c8beeb33cc78932e5e9b8b453f2691df847f11be
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:58:08 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:58:01"
+lastupdate = "GMT +1: 09.03.2023 13:58:07"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16334,3 +16334,5 @@ addscriptexist(185655149, "Bloxburg [GUI - Auto Build]", "pulled from rbxscript.
 addscriptexist(185655149, "Bloxburg [GUI - Auto Farm, Waypoint & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bloxburg%20GUI%20%20Auto%20Farm%20Waypoint%20%20More.lua")
 
 addscriptexist(5956785391, "Project Slayers [Auto Spin]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Auto%20Spin.lua")
+
+addscriptexist(4036494886, "Gun Simulator [GUI - Get Infinite Money, Set Max Weapon Damage & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Gun%20Simulator%20GUI%20%20Get%20Infinite%20Money%20Set%20Max%20Weapon%20Damage%20%20More.lua")
