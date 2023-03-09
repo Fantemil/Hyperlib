438fc700a3a13a916603dc87b1bfe68cdb65d753
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 16:00:23 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Mad City Infinite Xp  Get To Level  In  Hours.lua
@@ -0,0 +1,9 @@
+game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("SetTeam", "Police")
+wait(.70)
+game:GetService("RunService").RenderStepped:Connect(function()
+for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+  if v.Name == "Handcuffs" then v.Parent = game:GetService("Players").LocalPlayer.Character
+  end
+end
+game:GetService("ReplicatedStorage").Event:FireServer("Eject", game:GetService("Players").LocalPlayer)
+end)
