7278a52987dbd3b9a65d40a21eb0e41338024d94
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:43:49 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:43:44"
+lastupdate = "GMT +1: 09.03.2023 14:43:49"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17506,3 +17506,5 @@ addscriptexist(4566572536, "Universal Vehicle [SpeedHack]", "pulled from rbxscri
 addscriptexist(292439477, "Phantom Forces [Unlock All Guns, Admin Guns, Attachments]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Unlock%20All%20Guns%20Admin%20Guns%20Attachments.lua")
 
 addscriptexist(9551640993, "Mining Simulator 2 [Auto Mine - Auto Rebirth & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simulator%20%20Auto%20Mine%20%20Auto%20Rebirth%20%20More.lua")
+
+addscript(1365404657, "Current Game","Feather Family [Change your name into Anything]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Feather%20Family%20Change%20your%20name%20into%20Anything.lua")
