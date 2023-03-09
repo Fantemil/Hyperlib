7605edf15f8f82f896f50888fccd14dde5aab617
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:21:51 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:21:46"
+lastupdate = "GMT +1: 09.03.2023 14:21:50"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16946,3 +16946,5 @@ addscriptexist(301549746, "Counter Blox [Best Custom Skins Changer & More!]", "p
 addscriptexist(8619263259, "Critical Legends [Auto Orbs & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Critical%20Legends%20Auto%20Orbs%20%20More.lua")
 
 addscript(9492038800, "Current Game","STUMBLOX STRANGER THINGS [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/STUMBLOX%20STRANGER%20THINGS%20Auto%20Farm.lua")
+
+addscript(7089072882, "Current Game","HE-MAN SIM [Instant Max Out Character]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/HEMAN%20SIM%20Instant%20Max%20Out%20Character.lua")
