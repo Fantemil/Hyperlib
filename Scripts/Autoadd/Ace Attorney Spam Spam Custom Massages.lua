e015647586e74bc60c30d2a3c99dfb73513ccae5
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:14:38 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Ace Attorney Spam Spam Custom Massages.lua
@@ -0,0 +1,182 @@
+--âââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââ
+--âââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââ
+--âââââââ¦ââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââ
+--âââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââ
+--âââââââ¦ââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââ
+--âââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââââ
+
+local text = "Your text here"
+
+local function musicSpam()
+local args = {
+    [1] = "Trial"
+}
+
+workspace.Main.Music.Event:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "ClassroomTrial"
+}
+
+workspace.Main.Music.Event:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "Questioned"
+}
+
+workspace.Main.Music.Event:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "Caught"
+}
+
+workspace.Main.Music.Event:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "Objection"
+}
+
+workspace.Main.Music.Event:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "Cornered"
+}
+
+workspace.Main.Music.Event:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "ApolloObjection"
+}
+
+workspace.Main.Music.Event:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "ApolloCornered"
+}
+
+workspace.Main.Music.Event:FireServer(unpack(args))
+wait(0.1)
+end
+
+
+while wait() do
+local args = {
+    [1] = text,
+    [2] = "Judge",
+    [3] = "Slam",
+    [4] = "Order",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Judge",
+    [3] = "Slam",
+    [4] = "Objection",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Judge",
+    [3] = "Slam",
+    [4] = "Holdit",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Witness",
+    [3] = "Slam",
+    [4] = "Order",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Witness",
+    [3] = "Slam",
+    [4] = "Objection",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Witness",
+    [3] = "Slam",
+    [4] = "Holdit",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Prosecution",
+    [3] = "Slam",
+    [4] = "Order",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Prosecution",
+    [3] = "Slam",
+    [4] = "Objection",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Prosecution",
+    [3] = "Slam",
+    [4] = "Holdit",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Defense",
+    [3] = "Slam",
+    [4] = "Order",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Defense",
+    [3] = "Slam",
+    [4] = "Objection",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+local args = {
+    [1] = text,
+    [2] = "Defense",
+    [3] = "Slam",
+    [4] = "Holdit",
+    [5] = false
+}
+workspace.Main.General.Speak:FireServer(unpack(args))
+musicSpam()
+wait(0.1)
+end
