b7e537df11dfde96497d73e9248c119d999fe859
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:24:55 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Millionaire Empire Tycoon Infinite Cash  Auto Buy  Auto Rebirth.lua
@@ -0,0 +1,11 @@
+-- Game Name : Millionaire Empire Tycoon
+-- Game Link : https://www.roblox.com/games/6677985923/X2-CASH-Millionaire-Empire-Tycoon
+--[[Script Updates :
+   [+] Fixed
+]]
+-- Script :
+_G.RedGUI = true
+_G.Theme = "Dark" -- Must disable or remove _G.RedGUI to use
+--Themes: Light, Dark, Mocha, Aqua and Jester
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/MillionaireEmpireTycoon.lua"))()
