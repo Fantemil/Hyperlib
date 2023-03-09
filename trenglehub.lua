d155738a2521a67adad94363ea136498862fbf5c
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:30:09 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:30:05"
+lastupdate = "GMT +1: 09.03.2023 14:30:09"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17156,3 +17156,5 @@ addscriptexist(8798268679, "Crossroad Combat [God Mode]", "pulled from rbxscript
 addscript(6924952561, "Current Game","Clover City [Fly]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Clover%20City%20Fly.lua")
 
 addscriptexist(7839440834, "Sea Cleaning Simulator [Modify Boat Speed, No slow down effect, Auto Sell Trash]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sea%20Cleaning%20Simulator%20Modify%20Boat%20Speed%20No%20slow%20down%20effect%20Auto%20Sell%20Trash.lua")
+
+addscriptexist(4540286603, "NORTHWIND [Crash Server]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/NORTHWIND%20Crash%20Server.lua")
