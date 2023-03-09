4ca53d2048d753bba01235f60f8d679d65a42f45
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:22:42 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Unlimited Upgrade Simulator Inf Money.lua
@@ -0,0 +1,7 @@
+local args = {
+   [1] = "Power",
+   [2] = workspace:FindFirstChild("\230\156\168\230\161\169"):FindFirstChild("\230\156\168\230\161\1691").MeshPart,
+   [3] = 999999999999999999999999999999
+}
+
+game:GetService("ReplicatedStorage").Event.GradePromotion:FireServer(unpack(args))
