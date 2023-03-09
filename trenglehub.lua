f36be957d73b0cd1de5a546fabe3393adf7b2ffb
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:43:43 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:43:36"
+lastupdate = "GMT +1: 09.03.2023 15:43:43"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19012,3 +19012,5 @@ addscriptexist(8492553855, "Project Upright [New Auto Farm GUI]", "pulled from r
 addscriptexist(8781849572, "Star Simulator Beta [Complete Auto Farm GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Star%20Simulator%20Beta%20Complete%20Auto%20Farm%20GUI.lua")
 
 addscriptexist(8781849572, "Star Simulator Beta [Infinite Resources]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Star%20Simulator%20Beta%20Infinite%20Resources.lua")
+
+addscriptexist(3956818381, "Ninja Legends [GUI - Auto Swing, Auto Buy]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ninja%20Legends%20GUI%20%20Auto%20Swing%20Auto%20Buy.lua")
