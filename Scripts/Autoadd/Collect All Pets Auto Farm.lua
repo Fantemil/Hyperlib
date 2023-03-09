0c0ce66aa9be4d4f2eee1a3dc6f88a98c9adfc37
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:45:28 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Collect All Pets Auto Farm.lua
@@ -0,0 +1,6 @@
+_G.RedGUI = true
+_G.Theme = "Dark" -- Must disable or remove _G.RedGUI to use
+--Themes: Light, Dark, Mocha, Aqua and Jester
+_G.V3rmillion = true
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/CollectAllPets.lua"))()
