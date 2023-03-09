7e1b0d494a5d19b9bab8ebb3a875f7fc64ac4879
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:04:33 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Woman Repellant Women will not run away from you.lua
@@ -0,0 +1,5 @@
+if game.Players.LocalPlayer.Name ~= "gigaboui" then
+game.Players.LocalPlayer.Character.WomanRepellant:Destroy()
+elseif game.Players.LocalPlayer.Name == "gigaboui" then
+game.Players.LocalPlayer:Kick("Sorry, there's no help for you :)")--fuck you
+end
