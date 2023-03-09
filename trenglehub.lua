e0e175b80d63a5e67e67d2dd0e71f269101a345a
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 13:43:05 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 13:43:01"
+lastupdate = "GMT +1: 09.03.2023 13:43:05"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -15936,3 +15936,5 @@ addscriptexist(6711562581, "Eight Driver Car Racing [Infinite Money]", "pulled f
 addscriptexist(10053187005, "Counter Blox [Godmode, Aimbot, Inf Jump]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Counter%20Blox%20Godmode%20Aimbot%20Inf%20Jump.lua")
 
 addscriptexist(6677985923, "Millionaire Empire Tycoon [Infinite Cash]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Millionaire%20Empire%20Tycoon%20Infinite%20Cash.lua")
+
+addscriptexist(5993942214, "Rush Point [Silent Aim & No Recoil]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rush%20Point%20Silent%20Aim%20%20No%20Recoil.lua")
