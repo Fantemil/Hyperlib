941934d3330bd841b2c77a03a144ac451aa6da54
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 15:34:15 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 15:34:11"
+lastupdate = "GMT +1: 09.03.2023 15:34:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18772,3 +18772,5 @@ addscriptexist(1224212277, "Mad City [Full Invisible]", "pulled from rbxscript.c
 addscriptexist(7603178367, "Chipotle Burrito Builder [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Chipotle%20Burrito%20Builder%20Auto%20Farm.lua")
 
 addscriptexist(537413528, "Build A Boat For Treasure [Auto Buy, Auto Farm, Get All Items Free]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20A%20Boat%20For%20Treasure%20Auto%20Buy%20Auto%20Farm%20Get%20All%20Items%20Free.lua")
+
+addscriptexist(5116869569, "Doodle World [Auto Find]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Doodle%20World%20Auto%20Find.lua")
