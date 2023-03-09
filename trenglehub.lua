6abfd545a53f0c35a4a7076680e7f901a0a7d867
Emil Toth <emiltoth1870@gmail.com>
2023-03-09 14:34:56 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 09.03.2023 14:34:52"
+lastupdate = "GMT +1: 09.03.2023 14:34:56"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17282,3 +17282,5 @@ addscriptexist(3652625463, "Lifting Simulator [Auto Muscle, WalkSpeed, TP Safe Z
 addscriptexist(2727067538, "World // Zero [Kill Aura]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/World%20%20Zero%20Kill%20Aura.lua")
 
 addscriptexist(5827078246, "Coins Hero Simulator [Auto Coin, Auto Sell, AutoEgg]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Coins%20Hero%20Simulator%20Auto%20Coin%20Auto%20Sell%20AutoEgg.lua")
+
+addscript(574407221, "Current Game","Super Hero Tycoon [Crate Farm, Auto Cash, Auto Click]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Super%20Hero%20Tycoon%20Crate%20Farm%20Auto%20Cash%20Auto%20Click.lua")
