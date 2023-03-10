a81a86372673ba21d7b787dea49b4115845df207
Emil Toth <emiltoth1870@gmail.com>
2023-03-10 23:23:39 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 10.03.2023 08:57:23"
+lastupdate = "GMT +1: 10.03.2023 23:23:39"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19594,3 +19594,4 @@ addscriptexist(6516141723, "DOORS: ESP, Walkspeed, Get Crucifix", "pulled from r
 addscriptuniversal("Universal Lag Switch", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20Lag%20Switch.lua")
 
 addscriptexist(2753915549, "Void Hub Rain Fruits Visual", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Void%20Hub%20Rain%20Fruits%20Visual.lua")
+addscriptexist(12336036424, "Slasher Blade: Get Lots Of Gems/coins", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Slasher%20Blade%20Get%20Lots%20Of%20Gemscoins.lua")
