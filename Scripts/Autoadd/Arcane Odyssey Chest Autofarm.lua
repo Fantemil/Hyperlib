be22bc4701ce6f191c14b5b4abc6d78794707209
Emil Toth <emiltoth1870@gmail.com>
2023-03-10 23:29:16 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Arcane Odyssey Chest Autofarm.lua
@@ -0,0 +1,4 @@
+getgenv().toggle = true
+getgenv().whitelist = {"Elite Armor Chest","Elite Sailor Chest","Great Sailor Chest", "Golden Chest","Silver Chest","Food Chest" }; -- these are the chest that will be grabbed:)
+getgenv().speed = 100  --the higher the number the faster you go,increasing may lead too bans!
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Disttt/-Arcane-Odyssey/main/Autochest.lua"))()
