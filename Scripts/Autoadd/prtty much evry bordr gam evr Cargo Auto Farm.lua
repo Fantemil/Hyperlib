85407360fe8ae2060c8ac709a41f757f8f394481
Emil Toth <emiltoth1870@gmail.com>
2023-03-11 12:24:00 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/prtty much evry bordr gam evr Cargo Auto Farm.lua
@@ -0,0 +1,3 @@
+getgenv().on = not on -- So you can just execute to toggle rather than needing to change getgenv().on before execution
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zntly/pmebge/main/cargo.lua"))() -- The actual script. Source: https://raw.githubusercontent.com/zntly/pmebge/main/cargo.lua
