958a7bec4200254738321ba9e1231897d5b7e67a
Emil Toth <emiltoth1870@gmail.com>
2023-03-11 12:23:30 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 11.03.2023 12:23:24"
+lastupdate = "GMT +1: 11.03.2023 12:23:30"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19636,3 +19636,5 @@ addscriptexist(10427037408, "Life Sentence: Aimlock, Silent Guns & More", "pulle
 addscriptexist(1554960397, "Car Dealership Tycoon: Auto Race, Click TP, Walkspeed", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Car%20Dealership%20Tycoon%20Auto%20Race%20Click%20TP%20Walkspeed.lua")
 
 addscriptexist(2788229376, "Da Hood: Anti Slow, Anti Stomp & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Anti%20Slow%20Anti%20Stomp%20%20More.lua")
+
+addscriptexist(6284583030, "Pet Simulator X: Anti Stealer", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Simulator%20X%20Anti%20Stealer.lua")
