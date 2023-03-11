862d4e4c9159d02321537930f4851cd417c3946b
Emil Toth <emiltoth1870@gmail.com>
2023-03-11 12:27:10 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 11.03.2023 12:24:24"
+lastupdate = "GMT +1: 11.03.2023 12:27:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19662,3 +19662,5 @@ addscriptexist(6872265039, "BedWars: Elder Tree Orb Esp", "pulled from rbxscript
 addscript(12413160778, "Current Game","Ez Dev: Anti Cheat Bypass, Auto Farm", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ez%20Dev%20Anti%20Cheat%20Bypass%20Auto%20Farm.lua")
 
 addscriptexist(8540346411, "Rebirth Champions X: Auto Farm, Auto Open Eggs, Auto Upgrade Pets", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rebirth%20Champions%20X%20Auto%20Farm%20Auto%20Open%20Eggs%20Auto%20Upgrade%20Pets.lua")
+
+addscriptexist(8884433153, "[ kHub ] Collect All Pets Script [Working - 2023] | Auto-Farm", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20kHub%20%20Collect%20All%20Pets%20Script%20Working%20%20%20%20AutoFarm.lua")
