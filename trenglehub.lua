5af6c66c2bde56e93d1288e38cbf49a73593fff1
Emil Toth <emiltoth1870@gmail.com>
2023-03-12 16:04:11 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 12.03.2023 16:03:58"
+lastupdate = "GMT +1: 12.03.2023 16:04:11"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19689,3 +19689,4 @@ addhub("Gaming Hub: 61 Games", "pulled from rbxscript.com", "https://raw.githubu
 addscriptexist(8568266872, "Kill Monsters to Save Princess / Auto Farm", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Kill%20Monsters%20to%20Save%20Princess%20%20Auto%20Farm.lua")
 addscript(12547990726,"Current Game", "Auto Farm / by OXYGEN", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Auto%20Farm%20%20by%20OXYGEN.lua")
 addscriptexist(8500690972, "Dont Stop Obby Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dont%20Stop%20Obby%20Script.lua")
+addscriptexist(11334163219, " 2X[UPD 4🔥EVENT🍀] Anime Power Simulator Script", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20XUPD%20EVENT%20Anime%20Power%20Simulator%20Script.lua")
