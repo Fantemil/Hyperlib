1662097418044aa7cd4dae24ecb9f599efde4a44
Emil Toth <emiltoth1870@gmail.com>
2023-03-12 16:03:37 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 12.03.2023 16:00:57"
+lastupdate = "GMT +1: 12.03.2023 16:03:37"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19685,3 +19685,5 @@ addscript(12413786484, "Current Game","Anime Lost Simulator: Auto Farm, Auto Reb
 addscriptexist(5780309044, "game stand awakening: Event boss GUI", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/game%20stand%20awakening%20Event%20boss%20GUI.lua")
 
 addhub("Gaming Hub: 61 Games", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Gaming%20Hub%20%20Games.lua")
+
+addscriptexist(8568266872, "Kill Monsters to Save Princess / Auto Farm", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Kill%20Monsters%20to%20Save%20Princess%20%20Auto%20Farm.lua")
