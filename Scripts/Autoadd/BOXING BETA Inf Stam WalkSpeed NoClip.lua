e6e117149ab2d7110301df84845d213c091bf4f7
Emil Toth <emiltoth1870@gmail.com>
2023-03-13 16:42:04 +0100

    Updated automatically!

--- Scripts/Autoadd/BOXING BETA Inf Stam WalkSpeed NoClip.lua
+++ Scripts/Autoadd/BOXING BETA Inf Stam WalkSpeed NoClip.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Ihaveash0rtnamefordiscord/Releases/main/RedwoodPrison_Reworked'))(' Watermelon ? ')
+loadstring(game:HttpGet('https://eware.cc/loader.php'))()
