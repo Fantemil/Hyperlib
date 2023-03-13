0dad32164cda6e144ba5b9fe28365af3743470b1
Emil Toth <emiltoth1870@gmail.com>
2023-03-13 16:41:14 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 13.03.2023 16:41:09"
+lastupdate = "GMT +1: 13.03.2023 16:41:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19708,3 +19708,5 @@ addscriptexist(10945472407, "Strong Muscle Simulator: Auto Upgrade, Auto Egg, Au
 addscriptexist(292439477, "Phantom Forces: FOV, Aimbot, Wall Check", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20FOV%20Aimbot%20Wall%20Check.lua")
 
 addscript(12128572247, "Current Game","MOURS: Speed, God mode", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/MOURS%20Speed%20God%20mode.lua")
+
+addscriptexist(9984770240, "Obby but you're a ball: Auto Win, Teleports", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Obby%20but%20youre%20a%20ball%20Auto%20Win%20Teleports.lua")
