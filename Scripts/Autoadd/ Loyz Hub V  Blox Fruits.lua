3e3fc1cfd4596a46dbe0232183de2456c5582f7e
Emil Toth <emiltoth1870@gmail.com>
2023-03-14 14:37:47 +0100

    Updated automatically!

--- Scripts/Autoadd/ Loyz Hub V  Blox Fruits.lua
+++ Scripts/Autoadd/ Loyz Hub V  Blox Fruits.lua
@@ -1,1 +1,6 @@
+local Module = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+Module.Prompt({
+    invite = "https://discord.gg/hEQUXRdFRv",
+    name = "Loyz Hub V4 | Community",
+})
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ZoiIntra/Loyz/main/Luncher'))()
