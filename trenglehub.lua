75fabfd94d434fd6588c9747edc3c6bc0cec6b16
Emil Toth <emiltoth1870@gmail.com>
2023-03-17 14:56:30 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 17.03.2023 14:54:07"
+lastupdate = "GMT +1: 17.03.2023 14:56:30"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19780,3 +19780,5 @@ addscript(7427929301, "Current Game","Easy Parkour Obby: Auto Farm Level", "pull
 addscriptexist(6516141723, "DOORS: Notification, ESP & More", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Notification%20ESP%20%20More.lua")
 
 addscriptexist(10675066724, "Slime Tower Tycoon: Auto Merge, Auto Collect, Auto Play", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Slime%20Tower%20Tycoon%20Auto%20Merge%20Auto%20Collect%20Auto%20Play.lua")
+
+addscriptuniversal("Universal FPS Script || URIEL", "pulled from rscripts", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Universal%20FPS%20Script%20%20URIEL.lua")
