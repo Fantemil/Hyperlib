925f2f0c2c09d35bf47af4fc016f159822abc743
Emil Toth <emiltoth1870@gmail.com>
2023-03-17 14:57:27 +0100

    Updated automatically!

---
+++ Scripts/Autoadd/Selenium for Arcane Odyssey.lua
@@ -0,0 +1,10 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/synnyyy/synergy/main/selenium.lua", true))({
+ QueueOnTeleport = true -- This executes Selenium on server teleportation. If you want to enable this, set this to true. Otherwise set it to false.
+})
+
+-- Supported games are as follows:
+
+-- Rate my Avatar
+-- Pixel Piece
+-- Pilgrammed
+-- Arcane Odyssey
