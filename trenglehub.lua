236b7e733a26e2e2d06c7e523fdadcd8af122af3
Emil Toth <emiltoth1870@gmail.com>
2023-03-24 17:32:26 +0100

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 _G.gamecount = 0
 _G.scriptcount = 0
-lastupdate = "GMT +1: 24.03.2023 17:32:21"
+lastupdate = "GMT +1: 24.03.2023 17:32:26"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -19803,3 +19803,5 @@ addscriptexist(286090429, "Arsenal: Aimbot, Silent Aim, Rapid Fire", "pulled fro
 addscriptexist(621129760, "KAT: Hitbox, Silent Aim, Esp", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/KAT%20Hitbox%20Silent%20Aim%20Esp.lua")
 
 addscriptexist(2788229376, "Da Hood: Anti Aim, Silent Aim, FOV", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Anti%20Aim%20Silent%20Aim%20FOV.lua")
+
+addscript(4238077359, "Current Game","Coruscant RP: TP Exploit", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Coruscant%20RP%20TP%20Exploit.lua")
