c47460b1fba61301b933e45b37dfd15c0a72551e
Emil Toth <emiltoth1870@gmail.com>
2023-04-02 14:40:24 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/Pet Trading Card Simulator Redeem All Codes Auto Click Walkspeed.lua
@@ -0,0 +1,4 @@
+--//not open source; using mocha theme rn; other themes are light,dark,aqua,jester.
+--// ui lib is kinlei; let me know if you want other themes, i will gladly upload this script with other themes; script is unnamed.
+
+loadstring(game:HttpGet('https://pastebin.com/raw/1EKbNTYS'))()
