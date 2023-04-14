43332d068fda1368d7ace98022d4618c2a6287fa
Emil Toth <emiltoth1870@gmail.com>
2023-04-14 20:51:03 +0200

    Updated automatically!

---
+++ Scripts/Autoadd/ERLC  Infinite Fuel.lua
@@ -0,0 +1,3 @@
+--credits hrzn
+getgenv().InfFuel = true --toggle true or false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/Free%20scripts%20for%20users/InfFuel.lua", true))()
