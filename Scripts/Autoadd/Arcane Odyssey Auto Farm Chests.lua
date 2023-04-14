d9c0ef56b55a97f2a1135adcf2f91361e76d2088
Emil Toth <emiltoth1870@gmail.com>
2023-04-14 20:45:21 +0200

    Updated automatically!

--- Scripts/Autoadd/Arcane Odyssey Auto Farm Chests.lua
+++ Scripts/Autoadd/Arcane Odyssey Auto Farm Chests.lua
@@ -1,6 +1,4 @@
---this game logs how long in between you open chest, it isn't hard to get around...
-getgenv().toggle=true
-getgenv().waitbetween=0.2-- the larger of a wait the safer this is
-getgenv().speed=100-- recommended around 100 any faster may pit you at risk of a ban
-
+getgenv().toggle = true
+getgenv().whitelist = {"Elite Armor Chest","Elite Sailor Chest","Great Sailor Chest", "Golden Chest","Silver Chest","Food Chest" }; -- these are the chest that will be grabbed:)
+getgenv().speed = 100  --the higher the number the faster you go,increasing may lead too bans!
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Disttt/-Arcane-Odyssey/main/Autochest.lua"))()
