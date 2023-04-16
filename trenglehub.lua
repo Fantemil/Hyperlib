9a6fcb7db31b7e2bf4e2bc03c676ee6bb9e410ae
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:18:22 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:18:18"
+lastupdate = "GMT +1: 16.04.2023 22:18:22"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17781,3 +17781,5 @@ addscript(1858299821, "Current Game","Robloxica [Money Dupe]", "pulled from rbxs
 addscriptexist(8540168650, "Stand Upright Rebooted [GUI - Sort Tools, Items Farm & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Stand%20Upright%20Rebooted%20GUI%20%20Sort%20Tools%20Items%20Farm%20%20More.lua", "https://rbxscript.com/post/StandUprightRebootedGUISortToolsItemsFarmMore-cV0uD")
 
 addscriptexist(4459593483, "FORCE [PRE-ALPHA] [Auto Parry & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/FORCE%20PREALPHA%20Auto%20Parry%20%20More.lua", "https://rbxscript.com/post/FORCEPREALPHAAutoParryMore-4goCp")
+
+addscriptexist(2098516465, "Roblox High School 2 [Free Ugc Item]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20High%20School%20%20Free%20Ugc%20Item.lua", "https://rbxscript.com/post/RobloxHighSchool2FreeUgcItem-qaFa1")
