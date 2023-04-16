17b53d5f7a67cf3106bc05dba4c27cca90be6463
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:28:13 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:28:10"
+lastupdate = "GMT +1: 16.04.2023 21:28:13"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16367,3 +16367,5 @@ addscriptexist(1554960397, "Car Dealership Tycoon [Auto Race Dune, Auto Race Ral
 addscriptexist(417267366, "Dollhouse Roleplay [Adm Cmds]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dollhouse%20Roleplay%20Adm%20Cmds.lua", "https://rbxscript.com/post/DollhouseRoleplayAdmCmds-hLllC")
 
 addscriptexist(286090429, "Arsenal [SilentAim, KillAll]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20SilentAim%20KillAll.lua", "https://rbxscript.com/post/ArsenalSilentAimKillAll-j4IeW")
+
+addscriptexist(13822889, "Lumber Tycoon 2 [Walkspeed, AutoBuy]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Lumber%20Tycoon%20%20Walkspeed%20AutoBuy.lua", "https://rbxscript.com/post/LumberTycoon2WalkspeedAutoBuy-pmPPB")
