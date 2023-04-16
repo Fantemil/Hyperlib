812db159177e9514a7a7d9746e1c5f1f9de0c5d1
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:12:36 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:12:32"
+lastupdate = "GMT +1: 16.04.2023 22:12:36"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17613,3 +17613,5 @@ addscriptexist(3840352284, "Volleyball 4.2 [No Line-Fault]", "pulled from rbxscr
 addscript(9601849832, "Current Game","Jetpack Battle [Insta-kill]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jetpack%20Battle%20Instakill.lua", "https://rbxscript.com/post/JetpackBattleInstakill-IN9WD")
 
 addscriptexist(1962086868, "Tower of hell GUI [ UltraJump, Ultraspeed, Admin ]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tower%20of%20hell%20GUI%20%20UltraJump%20Ultraspeed%20Admin%20.lua", "https://rbxscript.com/post/TowerofhellGUIUltraJumpUltraspeedAdmin-NDzZb")
+
+addscriptexist(9103460924, "Sword Factory X [Anti Cheat Bypass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20Factory%20X%20Anti%20Cheat%20Bypass.lua", "https://rbxscript.com/post/SwordFactoryXAntiCheatBypass-40WA")
