ffeb8bbb376fb731d0e2320fce4f8c2719e0d276
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:54:08 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:54:04"
+lastupdate = "GMT +1: 16.04.2023 21:54:08"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17093,3 +17093,5 @@ addscriptexist(142823291, "Murder Mystery 2 [Unlock All Skins - Clientside]", "p
 addscriptexist(5956785391, "Project Slayers [Kill Aura - Auto Farm & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Kill%20Aura%20%20Auto%20Farm%20%20More.lua", "https://rbxscript.com/post/ProjectSlayersKillAuraAutoFarmMore-NWoPQ")
 
 addscriptexist(537413528, "Build a Boat For Treasure [Auto Farming GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20a%20Boat%20For%20Treasure%20Auto%20Farming%20GUI.lua", "https://rbxscript.com/post/BuildaBoatForTreasureAutoFarmingGUI-KNOOE")
+
+addscriptexist(3956818381, "Ninja Legends [GUI - Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ninja%20Legends%20GUI%20%20Auto%20Farm.lua", "https://rbxscript.com/post/NinjaLegendsGUIAutoFarm-mNjgV")
