2bb654dae5a369b2aef08c908f266cc8319472cd
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:46:21 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:46:17"
+lastupdate = "GMT +1: 16.04.2023 22:46:21"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18617,3 +18617,5 @@ addscriptexist(9555732501, "Nen Fighting Simulator [Strength Farm]", "pulled fro
 addscriptexist(6172932937, "Energy Assault [Aim Bot]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Energy%20Assault%20Aim%20Bot.lua", "https://rbxscript.com/post/EnergyAssaultAimBot-mlgPz")
 
 addscriptexist(4954096313, "Field Trip Z [God Mode, Remove GodMode Get Donut]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Field%20Trip%20Z%20God%20Mode%20Remove%20GodMode%20Get%20Donut.lua", "https://rbxscript.com/post/FieldTripZGodModeRemoveGodModeGetDonut-3TuC")
+
+addscript(6788434697, "Current Game","AniPhobia [Infinite Ammo, Fullbright, Kill All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AniPhobia%20Infinite%20Ammo%20Fullbright%20Kill%20All.lua", "https://rbxscript.com/post/AniPhobiaInfiniteAmmoFullbrightKillAll-qkp6z")
