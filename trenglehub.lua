f1d612fd4ceb87756f60bc91ba0a77b77a2a889b
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:47:29 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:47:26"
+lastupdate = "GMT +1: 16.04.2023 22:47:29"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18651,3 +18651,5 @@ addscriptexist(2727067538, "World // Zero [Kill Aura]", "pulled from rbxscript.c
 addscriptexist(5827078246, "Coins Hero Simulator [Auto Coin, Auto Sell, AutoEgg]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Coins%20Hero%20Simulator%20Auto%20Coin%20Auto%20Sell%20AutoEgg.lua", "https://rbxscript.com/post/CoinsHeroSimulatorAutoCoinAutoSellAutoEgg-W6N1")
 
 addscriptexist(574407221, "Super Hero Tycoon [Crate Farm, Auto Cash, Auto Click]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Super%20Hero%20Tycoon%20Crate%20Farm%20Auto%20Cash%20Auto%20Click.lua", "https://rbxscript.com/post/SuperHeroTycoonCrateFarmAutoCashAutoClick-9PzQn")
+
+addscriptexist(4522347649, "Free Admin [Unlock Gamepass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Free%20Admin%20Unlock%20Gamepass.lua", "https://rbxscript.com/post/FreeAdminUnlockGamepass-3G40e")
