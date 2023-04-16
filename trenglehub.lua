d039c5695d6273dc0251eb7b8d89f2b9b681dc99
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:31:07 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:31:03"
+lastupdate = "GMT +1: 16.04.2023 22:31:07"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18159,3 +18159,5 @@ addscriptexist(1224212277, "Mad City [Auto Job, Kill Player, Fly]", "pulled from
 addscriptexist(9551640993, "Mining Simulator 2 [Auto Farm GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simulator%20%20Auto%20Farm%20GUI.lua", "https://rbxscript.com/post/MiningSimulator2AutoFarmGUI-opnvu")
 
 addscriptexist(5766084948, "SCP The Red Lake [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SCP%20The%20Red%20Lake%20Auto%20Farm.lua", "https://rbxscript.com/post/SCPTheRedLakeAutoFarm-BoXC0")
+
+addscriptexist(9677464361, "Basketball Pro Simulator [Always Perfect Shot]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Basketball%20Pro%20Simulator%20Always%20Perfect%20Shot.lua", "https://rbxscript.com/post/BasketballProSimulatorAlwaysPerfectShot-8wR9C")
