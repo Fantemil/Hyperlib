3a50c37a9f94fcfa3afe22f7179f266a5d52a934
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:34:36 +0200

    Updated automatically!

--- Scripts/Autoadd/Speed Simulator Auto Farm.lua
+++ Scripts/Autoadd/Speed Simulator Auto Farm.lua
@@ -1,1 +1,14 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dfkx/rbx/main/Speed%20Simulator"))()
+getgenv().RebirthSpeed = math.huge -- edit this to speed u want, stay at  math.huge if u want to autofarm
+getgenv().AutoRebirth = true -- false to disable. If disabled then will give speed
+getgenv().Amount = 999999999999 -- edit this to whatever u want
+---------------------------------- made by cat ok#1337, improved auto rebirth, credits to people who made dat shit or sum
+local rs = game.ReplicatedStorage
+while getgenv().AutoRebirth == true do
+    task.wait()
+    rs.GlobalFunctions.AddPlayerSpeed:FireServer(getgenv().RebirthSpeed,getgenv().RebirthSpeed)
+    rs.GlobalFunctions.RebirthPlayerCallFromClient:FireServer()
+end
+------------------------------------------------- if auto rebirth disabled
+if getgenv().AutoRebirth == false then
+rs.GlobalFunctions.AddPlayerSpeed:FireServer(getgenv().Amount,getgenv().Amount) -- gives the speed
+end
