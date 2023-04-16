03e175bc755f727c98ee6aa7eb31e85e658afa6d
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:35:54 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:35:51"
+lastupdate = "GMT +1: 16.04.2023 22:35:54"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18303,3 +18303,5 @@ addscript(5970745461, "Current Game","Minerblocks [Kill Aura]", "pulled from rbx
 addscript(8656044807, "Current Game","Swinging Simulator [Unlock Every Badge]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Swinging%20Simulator%20Unlock%20Every%20Badge.lua", "https://rbxscript.com/post/SwingingSimulatorUnlockEveryBadge-b9bR9")
 
 addscriptexist(8123652942, "Legend Piece [Kill Aura]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legend%20Piece%20Kill%20Aura.lua", "https://rbxscript.com/post/LegendPieceKillAura-rVE4I")
+
+addscriptexist(9534337535, "Shrek in the Backrooms [GUI - ESP, Loot Stuff]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shrek%20in%20the%20Backrooms%20GUI%20%20ESP%20Loot%20Stuff.lua", "https://rbxscript.com/post/ShrekintheBackroomsGUIESPLootStuff-g1yHX")
