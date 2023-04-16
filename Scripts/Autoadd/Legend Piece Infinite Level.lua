64bae5eb14ae3140e4fe19a555bc0aa8afc53709
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:34:36 +0200

    Updated automatically!

--- Scripts/Autoadd/Legend Piece Infinite Level.lua
+++ Scripts/Autoadd/Legend Piece Infinite Level.lua
@@ -1,1 +1,3 @@
-game:GetService("ReplicatedStorage").Remotes.Stats:FireServer(game:GetService("Players").LocalPlayer.Data.Level,-9e15)
+for i = 1,10000 --[[amount of the thing your adding don't go above 10k]] do
+   game.ReplicatedStorage.Remotes.Stats:FireServer(game.Players.LocalPlayer.Data.Level --[[change level to whatever you wanna get example: Gem]], 0.5)
+end
