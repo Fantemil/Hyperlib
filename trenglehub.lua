2660291ec88a3934cfdd83b66dde82a4f5764002
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:41:56 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:41:52"
+lastupdate = "GMT +1: 16.04.2023 21:41:56"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16759,3 +16759,5 @@ addscriptexist(10118559731, "nico's nextbots [blackouts] [Bots - Powerboxs Esp -
 addhub("RBSjus HUB [17 Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RBSjus%20HUB%20%20Games.lua", "https://rbxscript.com/post/RBSjusHUB17Games-Cy7Fw")
 
 addscriptexist(6516141723, "DOORS [Legit Cheat – Esp, And More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/DOORS%20Legit%20Cheat%20%20Esp%20And%20More.lua", "https://rbxscript.com/post/DOORSLegitCheatEspAndMore-GNiaz")
+
+addscriptexist(9845465117, "AU:Reborn [Instant & Infinite Awakening", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/AUReborn%20Instant%20%20Infinite%20Awakening.lua", "https://rbxscript.com/post/AURebornInstantInfiniteAwakening-T95wL")
