2e4ce2e7f4fac00837ba80ce34aa52c999f56a45
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:22:25 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:22:21"
+lastupdate = "GMT +1: 16.04.2023 22:22:24"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17901,3 +17901,5 @@ addscript(9936860371, "Current Game","Egg Packing Tycoon [Semi Autofarm]", "pull
 addscriptexist(1730877806, "Grand Piece Online [Bounty Esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Grand%20Piece%20Online%20Bounty%20Esp.lua", "https://rbxscript.com/post/GrandPieceOnlineBountyEsp-rcWqy")
 
 addscript(9396463375, "Current Game","Mining Empire [Infinite Vouchers]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Empire%20Infinite%20Vouchers.lua", "https://rbxscript.com/post/MiningEmpireInfiniteVouchers-Zk3x2")
+
+addscriptexist(9396463375, "Mining Empire [Inf Coins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Empire%20Inf%20Coins.lua", "https://rbxscript.com/post/MiningEmpireInfCoin-enc6S")
