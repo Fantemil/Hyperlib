093af6984cc2e89da307ea1a5580502948c05da2
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:15:01 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:14:55"
+lastupdate = "GMT +1: 16.04.2023 22:15:01"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17685,3 +17685,5 @@ addscriptexist(9903808883, "Build a Market Tycoon [Fast NPC'S]", "pulled from rb
 addscriptexist(4598019433, "generic roleplay gaem [GUI - Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/generic%20roleplay%20gaem%20GUI%20%20Auto%20Farm.lua", "https://rbxscript.com/post/genericroleplaygaemGUIAutoFarm-ZXyEf")
 
 addscriptexist(863266079, "Apocalypse Rising 2 [Anti Zombie - Freeze]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apocalypse%20Rising%20%20Anti%20Zombie%20%20Freeze.lua", "https://rbxscript.com/post/ApocalypseRising2AntiZombieFreeze-ce5sy")
+
+addscriptexist(863266079, "Apocalypse Rising 2 [Teleport To Position]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apocalypse%20Rising%20%20Teleport%20To%20Position.lua", "https://rbxscript.com/post/ApocalypseRising2TeleportToPosition-BHXxY")
