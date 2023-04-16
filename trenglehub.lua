304cae1cf994cee29107beafcf79c4053f907cc9
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:51:25 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:51:20"
+lastupdate = "GMT +1: 16.04.2023 21:51:25"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17021,3 +17021,5 @@ addscriptexist(9976239895, "Gumball Factory Tycoon [Auto obby]", "pulled from rb
 addscriptexist(9976239895, "Gumball Factory Tycoon [Auto Obby]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Gumball%20Factory%20Tycoon%20Auto%20Obby.lua", "https://rbxscript.com/post/GumballFactoryTycoonAutoObby-LKGWr")
 
 addscriptexist(8962384769, "Firework Simulator [Auto Farm, WalkSpeed, Auto open pets]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Firework%20Simulator%20Auto%20Farm%20WalkSpeed%20Auto%20open%20pets.lua", "https://rbxscript.com/post/FireworkSimulatorAutoFarmWalkSpeedAutoopenpets-Un64s")
+
+addscriptexist(8425637426, "Write a Letter [A etter free animations]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Write%20a%20Letter%20A%20etter%20free%20animations.lua", "https://rbxscript.com/post/WriteaLetterAetterfreeanimations-bj3da")
