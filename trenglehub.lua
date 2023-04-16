3dab9571af1c4a57f2a48b0c2c1494edf7b017f6
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:19:00 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:18:57"
+lastupdate = "GMT +1: 16.04.2023 22:19:00"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17801,3 +17801,5 @@ addscriptexist(6938803436, "Anime Dimensions Simulator [Auto Farm]", "pulled fro
 addscriptexist(2809202155, "Your Bizarre Adventure [God Mode]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Your%20Bizarre%20Adventure%20God%20Mode.lua", "https://rbxscript.com/post/YourBizarreAdventureGodMode-zDN5x")
 
 addscriptexist(2753915549, "Blox Fruits [Unlock Abilyti]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Fruits%20Unlock%20Abilyti.lua", "https://rbxscript.com/post/BloxFruitsUnlockAbilyti-cE5r5")
+
+addscriptexist(863266079, "Apocalypse Rising 2 [Anti Zombie - Teleport]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apocalypse%20Rising%20%20Anti%20Zombie%20%20Teleport.lua", "https://rbxscript.com/post/ApocalypseRising2AntiZombieTeleport-lIZIX")
