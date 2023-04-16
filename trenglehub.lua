84417ec05edf8659cee1096fe19444355ebe7bbd
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:14:56 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:14:51"
+lastupdate = "GMT +1: 16.04.2023 22:14:55"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17683,3 +17683,5 @@ addscriptexist(863266079, "Apocalypse Rising 2 [Inf Jump, Esp, Snow Health]", "p
 addscriptexist(9903808883, "Build a Market Tycoon [Fast NPC'S]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Build%20a%20Market%20Tycoon%20Fast%20NPCS.lua", "https://rbxscript.com/post/BuildaMarketTycoonFastNPCS-mt1D0")
 
 addscriptexist(4598019433, "generic roleplay gaem [GUI - Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/generic%20roleplay%20gaem%20GUI%20%20Auto%20Farm.lua", "https://rbxscript.com/post/genericroleplaygaemGUIAutoFarm-ZXyEf")
+
+addscriptexist(863266079, "Apocalypse Rising 2 [Anti Zombie - Freeze]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apocalypse%20Rising%20%20Anti%20Zombie%20%20Freeze.lua", "https://rbxscript.com/post/ApocalypseRising2AntiZombieFreeze-ce5sy")
