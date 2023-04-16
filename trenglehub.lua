7adcc658d7f64ee73a150f10bffa61e3c8a5fab0
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:05:26 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:05:22"
+lastupdate = "GMT +1: 16.04.2023 22:05:26"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17407,3 +17407,5 @@ addscriptexist(10001513103, "Limited Words [Unlimited spins, unlimited words]", 
 addscript(6273865312, "Current Game","Supernatural Simulator [Auto Sell, Auto Lvl Up]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Supernatural%20Simulator%20Auto%20Sell%20Auto%20Lvl%20Up.lua", "https://rbxscript.com/post/SupernaturalSimulatorAutoSellAutoLvlUp-GfiEC")
 
 addscriptexist(1899149341, "Vehicle Tycoon [Collect All Gifts]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Vehicle%20Tycoon%20Collect%20All%20Gifts.lua", "https://rbxscript.com/post/VehicleTycoonCollectAllGifts-4eQCB")
+
+addscript(10191372093, "Current Game","Guillotine Tycoon [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Guillotine%20Tycoon%20Auto%20Farm.lua", "https://rbxscript.com/post/GuillotineTycoonAutoFarm-PoqBO")
