af25b6d666bd66f6ab4830672408af67a67364a3
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:47:17 +0200

    Updated automatically!

--- Scripts/Autoadd/World  Zero Kill Aura.lua
+++ Scripts/Autoadd/World  Zero Kill Aura.lua
@@ -1,1 +1,23 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sinret/rbxscript.com-scripts-reuploads-/main/wz", true))()
+getgenv().ability = true
+while getgenv().ability == true do
+task.wait(0.27) -- You can work on the timing to get it maxed out, i just did random time
+local A_1 = "MageOfLightBlastCharged"
+local A_2 = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+local Event = game:GetService("ReplicatedStorage").Shared.Combat.Attack
+Event:FireServer(A_1, A_2)
+task.wait()
+local A_1 = "MageOfLightCharged"
+local A_2 = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+local Event = game:GetService("ReplicatedStorage").Shared.Combat.Attack
+Event:FireServer(A_1, A_2)
+task.wait()
+local A_1 = "MageOfLightBlast"
+local A_2 = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+local Event = game:GetService("ReplicatedStorage").Shared.Combat.Attack
+Event:FireServer(A_1, A_2)
+task.wait()
+local A_1 = "MageOfLight"
+local A_2 = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+local Event = game:GetService("ReplicatedStorage").Shared.Combat.Attack
+Event:FireServer(A_1, A_2)
+end
