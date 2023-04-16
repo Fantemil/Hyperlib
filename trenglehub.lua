0477fd3a88937dbc44782ba35e381e23b281c3d1
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:42:04 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:42:01"
+lastupdate = "GMT +1: 16.04.2023 22:42:04"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18489,3 +18489,5 @@ addscript(9566535167, "Current Game","Diamond Reborn [Infinite Pokebooster Time]
 addscript(9808430159, "Current Game","Scan_Test.DEMO [Reveal All Parts]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ScanTestDEMO%20Reveal%20All%20Parts.lua", "https://rbxscript.com/post/ScanTestDEMORevealAllParts-lfbww")
 
 addscriptexist(4618049391, "Tang County, Hebei [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tang%20County%20Hebei%20Auto%20Farm.lua", "https://rbxscript.com/post/TangCountyHebeiAutoFarm-3hFpD")
+
+addscriptexist(9297410766, "Crossbar Challenge Simulator [Fast Auto Shoot]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Crossbar%20Challenge%20Simulator%20Fast%20Auto%20Shoot.lua", "https://rbxscript.com/post/CrossbarChallengeSimulatorFastAutoShoot-SheID")
