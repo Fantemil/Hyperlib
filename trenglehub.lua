bed460e87ed7112b0dc80aa56d3538cb23761de1
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:56:16 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:56:12"
+lastupdate = "GMT +1: 16.04.2023 22:56:15"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18905,3 +18905,5 @@ addscriptexist(1365404657, "Feather Family [Unlock All Bird - VIB Gamepass]", "p
 addscriptexist(1758401491, "Public Bathroom Simulator [Spam Sounds - FE Trolling]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Public%20Bathroom%20Simulator%20Spam%20Sounds%20%20FE%20Trolling.lua", "https://rbxscript.com/post/PublicBathroomSimulatorSpamSoundsFETrolling-2yqGU")
 
 addscriptexist(340227283, "RoBowling [Ball HBE]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RoBowling%20Ball%20HBE.lua", "https://rbxscript.com/post/RoBowlingBallHBE-262Y8")
+
+addscript(9561878567, "Current Game","Forest Of Beginnings RPG [Kill All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Forest%20Of%20Beginnings%20RPG%20Kill%20All.lua", "https://rbxscript.com/post/ForestOfBeginningsRPGKillAl-10Ctn")
