7684303a184b616b7645b05ae4b56757462a4299
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:54:45 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:54:41"
+lastupdate = "GMT +1: 16.04.2023 21:54:45"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17111,3 +17111,5 @@ addscriptexist(10370253487, "You Can't Say That Again! [Word Farmer]", "pulled f
 addscript(6488220605, "Current Game","stay alive and steal time from others [The Best GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/stay%20alive%20and%20steal%20time%20from%20others%20The%20Best%20GUI.lua", "https://rbxscript.com/post/stayaliveandstealtimefromothersTheBestGUI-D5nlb")
 
 addscriptexist(8267733039, "Specter 2 [GUI - Item Esp, Ghost Esp & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Specter%20%20GUI%20%20Item%20Esp%20Ghost%20Esp%20%20More.lua", "https://rbxscript.com/post/Specter2GUIItemEspGhostEspMore-VQT01")
+
+addscript(5876616139, "Current Game","Minus Decendance [Helper Script]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Minus%20Decendance%20Helper%20Script.lua", "https://rbxscript.com/post/MinusDecendanceHelperScript-JGQ7o")
