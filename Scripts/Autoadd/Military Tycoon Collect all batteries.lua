f48405cde1052e0e1d26e40719949bf4b3fe8a64
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:04:09 +0200

    Updated automatically!

--- Scripts/Autoadd/Military Tycoon Collect all batteries.lua
+++ Scripts/Autoadd/Military Tycoon Collect all batteries.lua
@@ -1,6 +1,6 @@
 for k,v in pairs (game:GetService("Workspace").EventsFolder.BatteryEvent:GetDescendants()) do
-    if v:IsA("BasePart") and v:GetChildren("TouchInterest") then
-       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
-       wait(0.2)
-   end
+if v:IsA("BasePart") and v:GetChildren("TouchInterest") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+wait(0.2)
+end
 end
