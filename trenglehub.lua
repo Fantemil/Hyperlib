2209c37d44f546865d48d6df18445be325efe438
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:24:05 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:24:02"
+lastupdate = "GMT +1: 16.04.2023 22:24:05"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17949,3 +17949,5 @@ addscriptexist(6677985923, "Millionaire Empire Tycoon [Auto Farm]", "pulled from
 addscriptexist(4866692557, "Age Of Heroes [Rep Farm, Orb Farm, Anti Lag]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Age%20Of%20Heroes%20Rep%20Farm%20Orb%20Farm%20Anti%20Lag.lua", "https://rbxscript.com/post/AgeOfHeroesRepFarmOrbFarmAntiLag-B1yXK")
 
 addscriptexist(292439477, "Phantom Forces [Legit Bot, Rage Bot, Esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Legit%20Bot%20Rage%20Bot%20Esp.lua", "https://rbxscript.com/post/PhantomForcesLegitBotRageBotEsp-26t8j")
+
+addscriptexist(7560156054, "Clicker Simulator [Free Gamepass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Clicker%20Simulator%20Free%20Gamepass.lua", "https://rbxscript.com/post/ClickerSimulatorFreeGamepass-1ryz2")
