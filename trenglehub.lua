535b2e54af33c1318bfa63f404da38460b99321e
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:58:57 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:58:53"
+lastupdate = "GMT +1: 16.04.2023 21:58:57"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17227,3 +17227,5 @@ addscriptexist(9103460924, "Sword Factory X [Auto Reroll Enchants]", "pulled fro
 addscriptexist(5683833663, "Ragdoll Engine [Loop Push All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ragdoll%20Engine%20Loop%20Push%20All.lua", "https://rbxscript.com/post/RagdollEngineLoopPushAll-VSFad")
 
 addscriptexist(6468323505, "Spellbound Magic RPG [Inf Money]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Spellbound%20Magic%20RPG%20Inf%20Money.lua", "https://rbxscript.com/post/SpellboundMagicRPGInfMoney-eBd9j")
+
+addscriptexist(5956785391, "Project Slayers [Semi-no cooldown for skills]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Semino%20cooldown%20for%20skills.lua", "https://rbxscript.com/post/ProjectSlayersSeminocooldownforskills-ArWbn")
