cb770bb54c4d824ad90c3c88e5eef7fdc9143184
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:44:20 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:44:13"
+lastupdate = "GMT +1: 16.04.2023 21:44:20"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16821,3 +16821,5 @@ addscriptexist(9285238704, "Race Clicker [Auto Farm]", "pulled from rbxscript.co
 addscript(6983932919, "Current Game","osu!RoVer [Auto Click Circles - Time Warp & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/osuRoVer%20Auto%20Click%20Circles%20%20Time%20Warp%20%20More.lua", "https://rbxscript.com/post/osuRoVerAutoClickCirclesTimeWarpMore-H8ZkV")
 
 addscript(9605515605, "Current Game","Sword Lands Simulator - Alpha [Inf Money]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20Lands%20Simulator%20%20Alpha%20Inf%20Money.lua", "https://rbxscript.com/post/SwordLandsSimulatorAlphaInfMoney-JJHdZ")
+
+addscriptexist(7952502098, "Impossible Glass Bridge Obby - [AUTO FARMER]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Impossible%20Glass%20Bridge%20Obby%20%20AUTO%20FARMER.lua", "https://rbxscript.com/post/ImpossibleGlassBridgeObbyAUTOFARMER-vRJq3")
