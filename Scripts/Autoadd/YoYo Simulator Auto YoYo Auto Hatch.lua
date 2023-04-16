05674b1894c3328de3ad40d3bc23a2c1ecd6ec78
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:29:09 +0200

    Updated automatically!

--- Scripts/Autoadd/YoYo Simulator Auto YoYo Auto Hatch.lua
+++ Scripts/Autoadd/YoYo Simulator Auto YoYo Auto Hatch.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JellyBagBeans/CelestialHub/main/GameLoader"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GDScripts/VGvAlEGPdRPWNibbJtYI/main/CKinvaPuhPsVMVrXMrEP_YOYO"))()
