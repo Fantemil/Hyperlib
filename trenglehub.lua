794bc25bcf6566b7612db6b899900f7999470324
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:05:57 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:05:53"
+lastupdate = "GMT +1: 16.04.2023 22:05:57"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17423,3 +17423,5 @@ addhub("Roblox [Scum Hub v1.4 ]", "pulled from rbxscript.com", "https://raw.gith
 addscript(8528736393, "Current Game","Beatland [Free UGC Items GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Beatland%20Free%20UGC%20Items%20GUI.lua", "https://rbxscript.com/post/BeatlandFreeUGCItemsGUI-8CVvc")
 
 addscript(9603033881, "Current Game","Bid Battles [Auto Queue Auction, Auto Bid, Auto Sell]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bid%20Battles%20Auto%20Queue%20Auction%20Auto%20Bid%20Auto%20Sell.lua", "https://rbxscript.com/post/BidBattlesAutoQueueAuctionAutoBidAutoSell-xeiFR")
+
+addscriptexist(9940051005, "Blox Legends [Infinite Money]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blox%20Legends%20Infinite%20Money.lua", "https://rbxscript.com/post/BloxLegendsInfiniteMoney-ynkYw")
