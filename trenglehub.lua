c80ac9f0bcd76931d6e954ea85fa93ff972499af
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:43:00 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:42:56"
+lastupdate = "GMT +1: 16.04.2023 21:42:59"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16789,3 +16789,5 @@ addscriptexist(147536429, "Field Of Battle [Kill General]", "pulled from rbxscri
 addscriptexist(21532277, "Notoriety [GUI - Teleports]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Notoriety%20GUI%20%20Teleports.lua", "https://rbxscript.com/post/NotorietyGUITeleports-AeUwo")
 
 addscript(6720959312, "Current Game","LootQuest [BETA] [Anti Afk & Auto Fuse]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LootQuest%20BETA%20Anti%20Afk%20%20Auto%20Fuse.lua", "https://rbxscript.com/post/LootQuestBETAAntiAfkAutoFuse-XsrH3")
+
+addscriptexist(9285238704, "Race Clicker [Auto Race GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Race%20Clicker%20Auto%20Race%20GUI.lua", "https://rbxscript.com/post/RaceClickerAutoRaceGUI-7FB8Z")
