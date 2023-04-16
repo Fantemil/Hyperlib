f69e5d6949e0cbe0a8190d0e147f1e99e6e629b5
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:32:52 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:32:48"
+lastupdate = "GMT +1: 16.04.2023 22:32:52"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18213,3 +18213,5 @@ addscript(4443032907, "Current Game","Sword Simulator [GUI - Auto Mobs & More!]"
 addscriptexist(6755746130, "Fruit Juice Tycoon Refreshed [Simple Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fruit%20Juice%20Tycoon%20Refreshed%20Simple%20Auto%20Farm.lua", "https://rbxscript.com/post/FruitJuiceTycoonRefreshedSimpleAutoFarm-8Bh3")
 
 addscriptexist(9551640993, "Mining Simulator 2 [GUI - Auto Mine, Mine Aura & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simulator%20%20GUI%20%20Auto%20Mine%20Mine%20Aura%20%20More.lua", "https://rbxscript.com/post/MiningSimulator2GUIAutoMineMineAuraMore-BvAYo")
+
+addscript(9611595239, "Current Game","Rift Royale [Esp + Aimbot]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rift%20Royale%20Esp%20%20Aimbot.lua", "https://rbxscript.com/post/RiftRoyaleEspAimbot-y8la1")
