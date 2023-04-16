bcec8a1faa201a4baca9bd924846092a6e2654dc
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:34:04 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:34:00"
+lastupdate = "GMT +1: 16.04.2023 22:34:04"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18249,3 +18249,5 @@ addscript(5944009947, "Current Game","FORCE [TESTING-ZONE] [Trinket Auto Farm & 
 addscript(9845369668, "Current Game","Goofy Ahh Spider-Man [GUI - Give All Skins]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Goofy%20Ahh%20SpiderMan%20GUI%20%20Give%20All%20Skins.lua", "https://rbxscript.com/post/GoofyAhhSpiderManGUIGiveAllSkins-2OQT")
 
 addscriptexist(8123652942, "Legend Piece [Fruit Changer]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legend%20Piece%20Fruit%20Changer.lua", "https://rbxscript.com/post/LegendPieceFruitChanger-dJNhZ")
+
+addscriptexist(9031522337, "RPG Legends [GUI - God Mode, Invisible Name & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RPG%20Legends%20GUI%20%20God%20Mode%20Invisible%20Name%20%20More.lua", "https://rbxscript.com/post/RPGLegendsGUIGodModeInvisibleNameMore-WdCpW")
