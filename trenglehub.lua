1ecdaca0d61ae3216845e4cfe00345f2e674cb06
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:30:28 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:30:25"
+lastupdate = "GMT +1: 16.04.2023 22:30:28"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18139,3 +18139,5 @@ addscriptexist(6938803436, "Anime Dimensions Simulator [GUI - Auto Level, Farm R
 addscript(8635207352, "Current Game","Anime Merge Simulator [Fast Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Merge%20Simulator%20Fast%20Auto%20Farm.lua", "https://rbxscript.com/post/AnimeMergeSimulatorFastAutoFarm-Vefva")
 
 addscriptexist(6206893534, "Cobra Kai Karate [GUI - Auto Farm Bags, Hide Name & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cobra%20Kai%20Karate%20GUI%20%20Auto%20Farm%20Bags%20Hide%20Name%20%20More.lua", "https://rbxscript.com/post/CobraKaiKarateGUIAutoFarmBagsHideNameMore-bjEVm")
+
+addscriptexist(4991214437, "town [Kill All, Gun Mods, Esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/town%20Kill%20All%20Gun%20Mods%20Esp.lua", "https://rbxscript.com/post/townKillAllGunModsEsp-1UsqV")
