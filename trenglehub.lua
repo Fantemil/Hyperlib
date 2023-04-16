c253aa8acb7f0f43b4e1087654e5f54f6cfe3c67
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:42:01 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:41:57"
+lastupdate = "GMT +1: 16.04.2023 22:42:01"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18487,3 +18487,5 @@ addscriptexist(1962086868, "Tower of Hell [TP To End & More!]", "pulled from rbx
 addscript(9566535167, "Current Game","Diamond Reborn [Infinite Pokebooster Time]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Diamond%20Reborn%20Infinite%20Pokebooster%20Time.lua", "https://rbxscript.com/post/DiamondRebornInfinitePokeboosterTime-HPD1z")
 
 addscript(9808430159, "Current Game","Scan_Test.DEMO [Reveal All Parts]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ScanTestDEMO%20Reveal%20All%20Parts.lua", "https://rbxscript.com/post/ScanTestDEMORevealAllParts-lfbww")
+
+addscriptexist(4618049391, "Tang County, Hebei [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tang%20County%20Hebei%20Auto%20Farm.lua", "https://rbxscript.com/post/TangCountyHebeiAutoFarm-3hFpD")
