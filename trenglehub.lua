d8091dadef8fdee00147c77c405fd3d5529e1f26
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:40:53 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:40:49"
+lastupdate = "GMT +1: 16.04.2023 21:40:53"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16729,3 +16729,5 @@ addscriptexist(10524502174, "One Punch Fighters [Auto Farm, auto eggs]", "pulled
 addscriptexist(286090429, "Arsenal [Auto farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arsenal%20Auto%20farm.lua", "https://rbxscript.com/post/ArsenalAutofarm-37UZz")
 
 addscript(2685347741, "Current Game","Ghost Simulator [Auto Farm, Auto Sell]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ghost%20Simulator%20Auto%20Farm%20Auto%20Sell.lua", "https://rbxscript.com/post/GhostSimulatorAutoFarmautosell-lHlXk")
+
+addscriptexist(292439477, "Phantom Forces [ESP, Aimbot]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20ESP%20Aimbot.lua", "https://rbxscript.com/post/PhantomForcesESPaimbot-mBfrI")
