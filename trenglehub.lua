1c57f784971a9051991678fa8c4c11de3ae6fa1c
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:41:30 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:41:26"
+lastupdate = "GMT +1: 16.04.2023 22:41:30"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18473,3 +18473,5 @@ addscript(8743251051, "Current Game","Avalanche [God Mode]", "pulled from rbxscr
 addscriptexist(621129760, "Knife Ability Test [God Mode]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Knife%20Ability%20Test%20God%20Mode.lua", "https://rbxscript.com/post/KnifeAbilityTestGodMode-brdN0")
 
 addscriptexist(5620664339, "RoStreets [Auto Collect]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RoStreets%20Auto%20Collect.lua", "https://rbxscript.com/post/RoStreetsAutoCollect-liwBT")
+
+addscriptexist(292439477, "Phantom Forces [Hinglight Esp,Chams]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Hinglight%20EspChams.lua", "https://rbxscript.com/post/PhantomForcesHinglightEspChams-DQXIw")
