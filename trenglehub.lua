e49e90c42b128f2abb2e6b3c0c02119cd20dc115
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:53:02 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:52:58"
+lastupdate = "GMT +1: 16.04.2023 21:53:02"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17069,3 +17069,5 @@ addscriptexist(4733278992, "Sword Blox Online: Rebirth [Kill Aura]", "pulled fro
 addscriptexist(6584731191, "Ragdoll Engine [FE Fling All Players]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ragdoll%20Engine%20FE%20Fling%20All%20Players.lua", "https://rbxscript.com/post/RagdollEngineFEFlingAllPlayers-eRNdk")
 
 addscriptexist(10192063645, "Thy Hood [Fly]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Thy%20Hood%20Fly.lua", "https://rbxscript.com/post/ThyHoodFly-KNTP0")
+
+addscriptexist(8916037983, "Starving Artist [Steal Art]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Starving%20Artist%20Steal%20Art.lua", "https://rbxscript.com/post/StarvingArtistStealArt-bk4hi")
