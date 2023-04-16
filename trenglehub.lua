432d52c45d7d9f676c1182ad9a23b5f77b77f9bd
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:13:00 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:12:56"
+lastupdate = "GMT +1: 16.04.2023 22:13:00"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17625,3 +17625,5 @@ addscriptexist(9551640993, "Mining Simulator 2 [Pet Guesser]", "pulled from rbxs
 addscript(2043040358, "Current Game","Vyyrahk City [Auto Farm GUI - Inf Stamina & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Vyyrahk%20City%20Auto%20Farm%20GUI%20%20Inf%20Stamina%20%20More.lua", "https://rbxscript.com/post/VyyrahkCityAutoFarmGUIInfStaminaMore-qqVkp")
 
 addscriptexist(8962384769, "Fireworks Simulator [Auto Fireworks & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Fireworks%20Simulator%20Auto%20Fireworks%20%20More.lua", "https://rbxscript.com/post/FireworksSimulatorAutoFireworksMore-LxaJu")
+
+addscriptexist(4036494886, "Gun Simulator [Infinite Ammo - Infinite Stars & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Gun%20Simulator%20Infinite%20Ammo%20%20Infinite%20Stars%20%20More.lua", "https://rbxscript.com/post/GunSimulatorInfiniteAmmoInfiniteStarsMore-4HAsu")
