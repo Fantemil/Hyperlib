49e748adc9235987f6824cdfb3ebc06886622f8c
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:53:41 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:53:37"
+lastupdate = "GMT +1: 16.04.2023 22:53:41"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18833,3 +18833,5 @@ addscriptexist(7799826347, "Sandbox Tycoon [Save Base, Build Floor, Update Build
 addscriptexist(2788229376, "Da Hood [Auto Money Drop Collect]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Auto%20Money%20Drop%20Collect.lua", "https://rbxscript.com/post/DaHoodAutoMoneyDropCollect-zYwE")
 
 addscript(9604622906, "Current Game","Diamond Hood [Auto Shoot - Kill All & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Diamond%20Hood%20Auto%20Shoot%20%20Kill%20All%20%20More.lua", "https://rbxscript.com/post/DiamondHoodAutoShootKillAllMore-aeLU9")
+
+addscriptexist(6728870912, "World of Stands Demo [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/World%20of%20Stands%20Demo%20Auto%20Farm.lua", "https://rbxscript.com/post/WorldofStandsDemoAutoFarm-vqBHn")
