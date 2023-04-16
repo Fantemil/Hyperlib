6f36e49466bc2c8f25e5a953e6661e946e414f85
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:00:28 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:00:24"
+lastupdate = "GMT +1: 16.04.2023 22:00:28"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17269,3 +17269,5 @@ addscriptexist(6299805723, "Anime Warriors [Simple Auto Farm]", "pulled from rbx
 addscriptexist(3177438863, "Dragon Blox [Auto Farm , Auto Quest GUI]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dragon%20Blox%20Auto%20Farm%20%20Auto%20Quest%20GUI.lua", "https://rbxscript.com/post/DragonBloxAutoFarmAutoQuestGUI-T3q3x")
 
 addscriptexist(7991339063, "Rainbow Friends [ESP]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rainbow%20Friends%20ESP.lua", "https://rbxscript.com/post/RainbowFriendsESP-DOp4n")
+
+addscript(10057963710, "Current Game","George Ezra’s Gold Rush Kid Experience [Collect All Puzzle Pieces]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/George%20Ezras%20Gold%20Rush%20Kid%20Experience%20Collect%20All%20Puzzle%20Pieces.lua", "https://rbxscript.com/post/GeorgeEzrasGoldRushKidExperienceCollectAllPuzzlePieces-A1dYV")
