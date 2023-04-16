127dfa5aa884a445db8073d30e3f089bf243d32e
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:10:22 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:10:17"
+lastupdate = "GMT +1: 16.04.2023 22:10:22"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17547,3 +17547,5 @@ addscriptexist(5956785391, "Project Slayers [GUI - Auto Farm Rice/Fish & More!]"
 addscriptexist(1730877806, "Grand Piece Online [you can't attack and you can't die]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Grand%20Piece%20Online%20you%20cant%20attack%20and%20you%20cant%20die.lua", "https://rbxscript.com/post/GrandPieceOnlineyoucan-vSY8b")
 
 addscriptexist(10118559731, "nico's nextbots [Serv lagger]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/nicos%20nextbots%20Serv%20lagger.lua", "https://rbxscript.com/post/nicosnextbotsServlagge-u0IzB")
+
+addscriptexist(4616652839, "Shindo life [Auto Farm, Auto Farm Boss, Auto Rank]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shindo%20life%20Auto%20Farm%20Auto%20Farm%20Boss%20Auto%20Rank.lua", "https://rbxscript.com/post/ShindolifeAutoFarmAutoFarmBossAutoRank-5PuW")
