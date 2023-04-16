2ac6e980485b364b24cab2f89dead035bc1690f1
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:54:50 +0200

    Updated automatically!

--- Scripts/Autoadd/REALISTIC HOOD Infinite Money.lua
+++ Scripts/Autoadd/REALISTIC HOOD Infinite Money.lua
@@ -1,6 +1,1 @@
---[[
-Made by: Al3xander
-Any doubts feel free to dm me [Al3xander#3748]
-]]--
-_G.Enabled = true --false to toggle it off
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FOXTROXHACKS/Roblox-Scripts/main/Realistic-Hood-Inf-Cash"))()
+game:GetService("ReplicatedStorage")["ACS_Engine"].ViewModels.Refil:FireServer(game.Players.LocalPlayer.leaderstats.Cash,-999999999)
