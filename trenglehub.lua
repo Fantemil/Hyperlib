44f846690cae195230b0e9d6cfd09e1a8ccaf89f
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:18:36 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:18:33"
+lastupdate = "GMT +1: 16.04.2023 22:18:36"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17789,3 +17789,5 @@ addscriptexist(8123652942, "Legend Piece [GUI - Teleports, Esp & More!]", "pulle
 addscriptexist(7167319176, "Chicago Remastered [GUI - Money, Teleports & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Chicago%20Remastered%20GUI%20%20Money%20Teleports%20%20More.lua", "https://rbxscript.com/post/ChicagoRemasteredGUIMoneyTeleportsMore-X2IuQ")
 
 addscriptexist(5910449407, "Project Menacing [GUI - Mob Farm, Stand Farm & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Menacing%20GUI%20%20Mob%20Farm%20Stand%20Farm%20%20More.lua", "https://rbxscript.com/post/ProjectMenacingGUIMobFarmStandFarmMore-hRKXS")
+
+addhub("GURA HUB [6 Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/GURA%20HUB%20%20Games.lua", "https://rbxscript.com/post/GURAHUB6Games-ej7es")
