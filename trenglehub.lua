43e8d96fb2cf2ad08bf2291d6a7efcd758b9cec9
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:26:04 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:26:00"
+lastupdate = "GMT +1: 16.04.2023 21:26:03"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16307,3 +16307,5 @@ addscript(3199109640, "Current Game","Camping 2 [TP, Walkspeed]", "pulled from r
 addscriptexist(3237168, "One Piece: Legendary [Auto Farm, Auto Attack]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/One%20Piece%20Legendary%20Auto%20Farm%20Auto%20Attack.lua", "https://rbxscript.com/post/OnePieceLegendaryAutoFarmAutoAttack-uuHFP")
 
 addscriptexist(10108131074, "Mow The Lawn! [Auto Farm, Serverhop]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mow%20The%20Lawn%20Auto%20Farm%20Serverhop.lua", "https://rbxscript.com/post/MowTheLawnAutoFarmServerhop-E4Z7I")
+
+addscriptexist(4003872968, "Giant Survival! [Inf Money, Best Gun]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Giant%20Survival%20Inf%20Money%20Best%20Gun.lua", "https://rbxscript.com/post/GiantSurvivalInfMoneyBestGun-V8GRF")
