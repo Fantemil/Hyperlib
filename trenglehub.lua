b643f2ec0944d8a2c14292dd566c020b2afa97b1
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:03:24 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:03:21"
+lastupdate = "GMT +1: 16.04.2023 22:03:24"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17351,3 +17351,5 @@ addscriptexist(8054462345, "Michael`s Zombies [Anti Knockdown]", "pulled from rb
 addscriptexist(8054462345, "Michael`s Zombies [Always Headshot - Instant Reload]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Michaels%20Zombies%20Always%20Headshot%20%20Instant%20Reload.lua", "https://rbxscript.com/post/MichaelsZombiesAlwaysHeadshotInstantReload-vts6E")
 
 addscript(6606507295, "Current Game","Turbo's Murder Mystery 2 [Get Free Paid Weapons]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Turbos%20Murder%20Mystery%20%20Get%20Free%20Paid%20Weapons.lua", "https://rbxscript.com/post/TurbosMurderMystery2GetFreePaidWeapons-AKbLy")
+
+addscriptexist(5956785391, "Project Slayers [Fish Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Fish%20Farm.lua", "https://rbxscript.com/post/ProjectSlayersFishFarm-6rv3v")
