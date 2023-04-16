1054e26c69bb5d94c16c666f15bbe6cb08cbe949
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:03:10 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:03:05"
+lastupdate = "GMT +1: 16.04.2023 22:03:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17343,3 +17343,5 @@ addscriptexist(6298464951, "Roblox Is Unbreakable [Infinite Cash]", "pulled from
 addscriptexist(6298464951, "Roblox Is Unbreakable [Infinite Stats]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Roblox%20Is%20Unbreakable%20Infinite%20Stats.lua", "https://rbxscript.com/post/RobloxIsUnbreakableInfiniteStats_JEUeB")
 
 addscriptexist(9551640993, "Mining Simualtor 2 [Auto Farm, Auto Mine]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mining%20Simualtor%20%20Auto%20Farm%20Auto%20Mine.lua", "https://rbxscript.com/post/MiningSimualtor2AutoFarmAutoMine-jk5LA")
+
+addscriptexist(5201039691, "Tatakai V.2 [Money Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tatakai%20V%20Money%20Farm.lua", "https://rbxscript.com/post/TatakaiV2MoneyFarm-fWuzw")
