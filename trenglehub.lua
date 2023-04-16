0e855c10f81b19f78ec30e43dbe002154ba97584
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:21:10 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:21:07"
+lastupdate = "GMT +1: 16.04.2023 22:21:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17863,3 +17863,5 @@ addscriptexist(263761432, "Horrific Housing [GUI - Destroy Meteor & More!]", "pu
 addscriptexist(9601565001, "Car Crash System [Spawn Any Paid Car]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Car%20Crash%20System%20Spawn%20Any%20Paid%20Car.lua", "https://rbxscript.com/post/CarCrashSystemSpawnAnyPaidCar-v1zD7")
 
 addscript(9578901194, "Current Game","Woman Repellant [Women will not run away from you]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Woman%20Repellant%20Women%20will%20not%20run%20away%20from%20you.lua", "https://rbxscript.com/post/WomanRepellantWomenwillnotrunawayfromyou-Xue2A")
+
+addscriptexist(1224212277, "Mad City [Get Weekly Rewards]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mad%20City%20Get%20Weekly%20Rewards.lua", "https://rbxscript.com/post/MadCityGetWeeklyReward-ciRs")
