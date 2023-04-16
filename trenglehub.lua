69d833c122f0c63517ac7e18ee3dd65ad6a1d854
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:40:16 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:40:12"
+lastupdate = "GMT +1: 16.04.2023 22:40:16"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18433,3 +18433,5 @@ addscriptexist(6755746130, "Fruit Juice Tycoon Refreshed [Auto Farm]", "pulled f
 addscriptexist(4195834406, "Dragon Ball Online Generations [Auto Teleport, Kill PLayer Aura, Esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Dragon%20Ball%20Online%20Generations%20Auto%20Teleport%20Kill%20PLayer%20Aura%20Esp.lua", "https://rbxscript.com/post/DragonBallOnlineGenerationsAutoTeleportKillPLayerAuraEsp-UIj9k")
 
 addscriptexist(606849621, "Jailbreak [Keycard / Unlock doors]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jailbreak%20Keycard%20%20Unlock%20doors.lua", "https://rbxscript.com/post/JailbreakKeycardUnlockdoors-iFqRd")
+
+addscriptexist(9786213478, "Treat Simulator [Isntant bite]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Treat%20Simulator%20Isntant%20bite.lua", "https://rbxscript.com/post/TreatSimulatorIsntantbite-s6YgH")
