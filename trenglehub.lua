bf59cb1dcb318ffe47704aa5f698849aa60b3926
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:28:03 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:27:59"
+lastupdate = "GMT +1: 16.04.2023 22:28:03"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18067,3 +18067,5 @@ addscriptexist(1730877806, "Grand Piece Online [Bounty ESP]", "pulled from rbxsc
 addscriptexist(6678877691, "ZO ぞ SAMURAI [Teleport Aura]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ZO%20%E3%81%9E%20SAMURAI%20Teleport%20Aura.lua", "https://rbxscript.com/post/ZO%E3%81%9ESAMURAITeleportAura-STuJz")
 
 addscript(447452406, "Current Game","Robloxian High School [Trash Bag Spam]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Robloxian%20High%20School%20Trash%20Bag%20Spam.lua", "https://rbxscript.com/post/RobloxianHighSchoolTrashBagSpam-SsJyh")
+
+addscriptexist(606849621, "Jailbreak [GUI - Vehicles Settings & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Jailbreak%20GUI%20%20Vehicles%20Settings%20%20More.lua", "https://rbxscript.com/post/JailbreakGUIVehiclesSettingsMore-09ZgG")
