0d8e14b2e110cfdc85e3e8acc2d1e6b6d80bb631
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:31:59 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:31:53"
+lastupdate = "GMT +1: 16.04.2023 22:31:59"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18185,3 +18185,5 @@ addscriptexist(180037022, "Sonic Ultimate RPG [Get All Rings]", "pulled from rbx
 addscriptexist(180037022, "Sonic Ultimate RPG [Get All Emeralds]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sonic%20Ultimate%20RPG%20Get%20All%20Emeralds.lua", "https://rbxscript.com/post/SonicUltimateRPGGetAllEmeralds-woS9x")
 
 addhub("ZEPHYR Hub [20 Games]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ZEPHYR%20Hub%20%20Games.lua", "https://rbxscript.com/post/ZEPHYRHub20Games-Q8WXL")
+
+addscriptexist(2906554815, "S.W.A.T Simulator [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SWAT%20Simulator%20Auto%20Farm.lua", "https://rbxscript.com/post/SWATSimulatorAutoFarm-2Bf0c")
