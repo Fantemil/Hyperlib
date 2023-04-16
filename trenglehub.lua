e032fea7d6234223c1439c14728e60844b4a539d
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:32:57 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:32:50"
+lastupdate = "GMT +1: 16.04.2023 21:32:57"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16499,3 +16499,5 @@ addscriptexist(10315830663, "Cant Say The Letter [Give Tools]", "pulled from rbx
 addscriptexist(10315830663, "Cant Say The Letter [Can says Letter]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cant%20Say%20The%20Letter%20Can%20says%20Letter.lua", "https://rbxscript.com/post/CantSayTheLetterCansaysLetter-1iaqO")
 
 addscript(10011532704, "Current Game","Anime Brawl Simulator [Inf Stars]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anime%20Brawl%20Simulator%20Inf%20Stars.lua", "https://rbxscript.com/post/AnimeBrawlSimulatorInfStars-84B6m")
+
+addscriptexist(2653064683, "Word Bomb [Auto Guess]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Word%20Bomb%20Auto%20Guess.lua", "https://rbxscript.com/post/WordBombAutoGuess-v1kDc")
