4fb89d2ed2c2e9527512215eadb2b65f1ed1de3a
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:04:58 +0200

    Updated automatically!

--- Scripts/Autoadd/Mad City Silent Aim.lua
+++ Scripts/Autoadd/Mad City Silent Aim.lua
@@ -1,18 +1,3 @@
-local uis = game:GetService("UserInputService")
-
-uis.InputBegan:Connect(function(asd)
-if asd.UserInputType == Enum.UserInputType.MouseButton1 then
-    for i,v in pairs(game.Workspace:GetChildren()) do
-            if v:FindFirstChild("Head") then
-    for i = 1,5 do
-local args = {
-    [1] = "VR",
-    [2] = v.Head,
-}
-
-game:GetService("ReplicatedStorage").Event:FireServer(unpack(args))
-end
-end
-end
-end
-end)
+getgenv() = 260 -- Field of View: The silent aim is only targetted at the target inside the fov's radius.
+getgenv = "Head" -- Targetting: "Head", "Torso". For example: Using "Head" will only deal headshots.
+loadstring(game:HttpGet('https://raw.githubusercontent.com/my-scripts/main/SAMCH2', true))()
