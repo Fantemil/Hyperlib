347c69f02384b59aeb85cde0c779c80922344c41
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:51:08 +0200

    Updated automatically!

--- Scripts/Autoadd/Gumball Factory Tycoon Auto obby.lua
+++ Scripts/Autoadd/Gumball Factory Tycoon Auto obby.lua
@@ -2,7 +2,7 @@ while true do
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-187.66159057617188, 2.9999988079071045, 322.68902587890625) -- easy obby
 wait(2.5)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(162.76344299316406, 3.999915361404419, 338.3460388183594) -- hard obby
-wait(150)
+wait(300)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-187.66159057617188, 2.9999988079071045, 322.68902587890625)
-wait(150)
+wait(300)
 end
