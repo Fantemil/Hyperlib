c9eab626ff53909858a3a0e2758dfe03641def10
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:26:01 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:25:58"
+lastupdate = "GMT +1: 16.04.2023 22:26:01"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18007,3 +18007,5 @@ addscriptexist(9875254583, "Juice Pirates [Infinite Gems]", "pulled from rbxscri
 addscript(9568982793, "Current Game","Da Hood Battlegrounds [Rapid Fire Mod]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Battlegrounds%20Rapid%20Fire%20Mod.lua", "https://rbxscript.com/post/DaHoodBattlegroundsRapidFireMod-Yheop")
 
 addscriptexist(8750997647, "Tapping Legends X [GUI - Auto Tap, Auto Rebirth & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Tapping%20Legends%20X%20GUI%20%20Auto%20Tap%20Auto%20Rebirth%20%20More.lua", "https://rbxscript.com/post/TappingLegendsXGUIAutoTapAutoRebirthMore-5ILee")
+
+addscriptexist(8303902695, "Pet Posse [GUI - Auto Farm Selected Zone, Unlock All Gamepasses & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Pet%20Posse%20GUI%20%20Auto%20Farm%20Selected%20Zone%20Unlock%20All%20Gamepasses%20%20More.lua", "https://rbxscript.com/post/PetPosseGUIAutoFarmSelectedZone-hB2ke")
