e7f7cfca8edd6e60879073476c93180fac4d948b
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:25:30 +0200

    Updated automatically!

--- Scripts/Autoadd/Crewmates Kill All.lua
+++ Scripts/Autoadd/Crewmates Kill All.lua
@@ -1,8 +1,5 @@
-for i, v in pairs(game:GetService("Players"):GetChildren()) do
-   if v.Name ~= game:GetService("Players").LocalPlayer.Name then
-       local String = "killPlayer"
-       local Player = v
-       local Vector = Vector3.new(-891.18792724609, -891.18792724609, -891.18792724609)
-       game:GetService("ReplicatedStorage").RemoteEvent:FireServer(String, Player, Vector)
-   end
+for i,v in pairs(game.Players:GetChildren()) do task.wait()
+if v ~= game.Players.LocalPlayer then
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer("killPlayer",v,v.Character.HumanoidRootPart.CFrame)
+end
 end
