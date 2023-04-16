3d2a4003302c6b664cc040cbe74d3e1c9f874182
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:51:29 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:51:25"
+lastupdate = "GMT +1: 16.04.2023 21:51:29"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17023,3 +17023,5 @@ addscriptexist(9976239895, "Gumball Factory Tycoon [Auto Obby]", "pulled from rb
 addscriptexist(8962384769, "Firework Simulator [Auto Farm, WalkSpeed, Auto open pets]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Firework%20Simulator%20Auto%20Farm%20WalkSpeed%20Auto%20open%20pets.lua", "https://rbxscript.com/post/FireworkSimulatorAutoFarmWalkSpeedAutoopenpets-Un64s")
 
 addscriptexist(8425637426, "Write a Letter [A etter free animations]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Write%20a%20Letter%20A%20etter%20free%20animations.lua", "https://rbxscript.com/post/WriteaLetterAetterfreeanimations-bj3da")
+
+addscriptexist(10386774046, "Legend Magician [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Legend%20Magician%20Auto%20Farm.lua", "https://rbxscript.com/post/LegendMagicianAutoFarm-CFm2d")
