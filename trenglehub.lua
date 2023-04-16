c2e8aea5bec5b5825659f5c89fa5196efca2caf9
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:23:50 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:23:46"
+lastupdate = "GMT +1: 16.04.2023 22:23:50"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17945,3 +17945,5 @@ addscriptexist(2753915549, "Blox Fruits [GUI - Auto Farm Level, Auto Farm]", "pu
 addscriptexist(185655149, "Bloxburg [Auto Build]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Bloxburg%20Auto%20Build.lua", "https://rbxscript.com/post/BloxburgAutoBuild-knVwE")
 
 addscriptexist(6677985923, "Millionaire Empire Tycoon [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Millionaire%20Empire%20Tycoon%20Auto%20Farm.lua", "https://rbxscript.com/post/MillionaireEmpireTycoon-ew3y2")
+
+addscriptexist(4866692557, "Age Of Heroes [Rep Farm, Orb Farm, Anti Lag]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Age%20Of%20Heroes%20Rep%20Farm%20Orb%20Farm%20Anti%20Lag.lua", "https://rbxscript.com/post/AgeOfHeroesRepFarmOrbFarmAntiLag-B1yXK")
