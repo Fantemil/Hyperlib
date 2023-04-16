2e25f81508c52228c4bf07bd0f10c633db625291
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:57:40 +0200

    Updated automatically!

--- Scripts/Autoadd/Arsenal God mode.lua
+++ Scripts/Autoadd/Arsenal God mode.lua
@@ -1,7 +1,1 @@
---[[
-please join our discord
-discord.gg/outliershub
-made by .gg#1780 / sno_
-]]
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/ArsenalGodmode.lua"))()
