18b61feba8580e0325f300ed109f2966983e398d
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:40:10 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:40:06"
+lastupdate = "GMT +1: 16.04.2023 21:40:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16709,3 +16709,5 @@ addscriptexist(7026828578, "Speed Simulator [Inf Score]", "pulled from rbxscript
 addscriptexist(7991339063, "Rainbow Friends [GodMode, AutoWin]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Rainbow%20Friends%20GodMode%20AutoWin.lua", "https://rbxscript.com/post/RainbowFriendsGodModeAutoWin-qKjRF")
 
 addscript(5201688943, "Current Game","Da hood [ESP]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20hood%20ESP.lua", "https://rbxscript.com/post/DahoodESP-6eCwk")
+
+addscriptexist(6939849554, "Cart Ride Tycoon thru a Cave 2 [Auto farm cash]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cart%20Ride%20Tycoon%20thru%20a%20Cave%20%20Auto%20farm%20cash.lua", "https://rbxscript.com/post/CartRideTycoonthruaCave2Autofarmcash-eQWuc")
