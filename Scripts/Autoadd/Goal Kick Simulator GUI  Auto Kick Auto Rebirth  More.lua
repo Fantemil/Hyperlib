d9a1116364d7f89d8b86f33861e7f2f3d9a68ed3
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:29:06 +0200

    Updated automatically!

--- Scripts/Autoadd/Goal Kick Simulator GUI  Auto Kick Auto Rebirth  More.lua
+++ Scripts/Autoadd/Goal Kick Simulator GUI  Auto Kick Auto Rebirth  More.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ExtremeAntonis/KeySystemUI/main/KeySystemUI-Obfuscated.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyTool_Key/main/Loader.lua"))()
