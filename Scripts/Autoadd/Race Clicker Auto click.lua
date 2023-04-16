1eda3fe09e7dbdba846ef9650c056c279beb2cbe
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:39:14 +0200

    Updated automatically!

--- Scripts/Autoadd/Race Clicker Auto click.lua
+++ Scripts/Autoadd/Race Clicker Auto click.lua
@@ -1,1 +1,14 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AverageRobloxUser1/MuscleRedeemCodes/main/MuscleRedeemCodes1",true))();loadstring(game:HttpGet("https://raw.githubusercontent.com/AverageRobloxUser1/AutoRaceClicker/main/AutoRaceClicker1",true))();
+bruh = nil
+for i,v in pairs(getgc()) do
+   if type(v) == 'function' then
+       if debug.getinfo(v).name == "Click" then
+           bruh = v
+       end
+   end
+end
+task.wait(1)
+while task.wait() do
+for i = 1,1000 do
+bruh(UDim2.new(0,0,0),1)
+end
+end
