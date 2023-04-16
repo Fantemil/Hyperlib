b2d30de9cde9e15c68b66488a333ad322adf6dfb
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:15:55 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:15:51"
+lastupdate = "GMT +1: 16.04.2023 22:15:55"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17709,3 +17709,5 @@ addscriptexist(5956785391, "Project Slayers [Muzan TP]", "pulled from rbxscript.
 addscriptexist(4598019433, "generic roleplay gaem [AFK Rice Autofarm, Auto Eat, Name hide]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/generic%20roleplay%20gaem%20AFK%20Rice%20Autofarm%20Auto%20Eat%20Name%20hide.lua", "https://rbxscript.com/post/genericroleplaygaemAFKRiceAutofarmAutoEatNamehide-HfKKX")
 
 addscriptexist(5956785391, "Project Slayers [Data wipe again]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20Data%20wipe%20again.lua", "https://rbxscript.com/post/ProjectSlayersDatawipeagain-7NcBf")
+
+addscriptexist(7140186081, "GunGame [Kill All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/GunGame%20Kill%20All.lua", "https://rbxscript.com/post/GunGameKillAll-u90H3")
