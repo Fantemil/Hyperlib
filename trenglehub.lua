1aa6d5740eef8664b466ecdef26b8a66ca5dcb40
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:22:44 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:22:41"
+lastupdate = "GMT +1: 16.04.2023 22:22:44"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17911,3 +17911,5 @@ addscriptexist(292439477, "Phantom Forces [Gun Mods, Esp, AimBot]", "pulled from
 addscriptexist(9508087919, "Apeirophobia [All 10 Lvl, Esp Exit, Inf Stamina]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Apeirophobia%20All%20%20Lvl%20Esp%20Exit%20Inf%20Stamina.lua", "https://rbxscript.com/post/ApeirophobiaAll10LvlEspExitInfStamina-eHdZG")
 
 addscriptexist(8619263259, "Critical Legends [Collect all chests, Tp to every statue]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Critical%20Legends%20Collect%20all%20chests%20Tp%20to%20every%20statue.lua", "https://rbxscript.com/post/CriticalLegendsCollectallchestsTptoeverystatue-K75hm")
+
+addscriptexist(2788229376, "Da Hood [Auto Farm, God mode, Kll Bot]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Auto%20Farm%20God%20mode%20Kll%20Bot.lua", "https://rbxscript.com/post/DaHoodAutoFarmGodmodeKllBot-R5DQb")
