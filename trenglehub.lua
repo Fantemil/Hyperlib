c628b2156d31fcb0fb855c3dd5a3d1958fbf9092
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:02:15 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:02:11"
+lastupdate = "GMT +1: 16.04.2023 22:02:14"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17317,3 +17317,5 @@ addscript(9397663086, "Current Game","Ninja Training Simulator [Auto Farm & More
 addscriptexist(9167501050, "Lightsaber Arena [Kill Aura - Auto Block & Infinite Stamina]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Lightsaber%20Arena%20Kill%20Aura%20%20Auto%20Block%20%20Infinite%20Stamina.lua", "https://rbxscript.com/post/LightsaberArenaKillAuraAutoBlockInfiniteStamina-3ejf")
 
 addscript(596894229, "Current Game","Elemental Adventure [Level Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Elemental%20Adventure%20Level%20Farm.lua", "https://rbxscript.com/post/ElementalAdventureLevelFarm-8pKXZ")
+
+addscriptexist(5130598377, "A Universal Time [Meteor Farm - Chest Farm - More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/A%20Universal%20Time%20Meteor%20Farm%20%20Chest%20Farm%20%20More.lua", "https://rbxscript.com/post/AUniversalTimeMeteorFarmChestFarmMore-HtRVk")
