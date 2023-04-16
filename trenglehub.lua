f0403c6c91a1903c59a846754fefa95207c51d4c
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:46:07 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:46:03"
+lastupdate = "GMT +1: 16.04.2023 21:46:06"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16871,3 +16871,5 @@ addscriptexist(47324, "Sword Fights on the Heights IV [Tp Safe Zone, Hitboxes, G
 addscript(10322949476, "Current Game","Limited Jumps [Unlimited Jumps]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Limited%20Jumps%20Unlimited%20Jumps.lua", "https://rbxscript.com/post/LimitedJumpsUnlimitedJumps-C2Kfl")
 
 addscript(151670365, "Current Game","The Quarry [Infinite Cash]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Quarry%20Infinite%20Cash.lua", "https://rbxscript.com/post/TheQuarryInfiniteCash-jGtB7")
+
+addscriptexist(4809447488, "Venture Tales [Kill Aura - Insta Kill]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Venture%20Tales%20Kill%20Aura%20%20Insta%20Kill.lua", "https://rbxscript.com/post/VentureTalesKillAuraInstaKill-yikqN")
