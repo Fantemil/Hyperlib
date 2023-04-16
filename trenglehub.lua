658775a6401b93bafa4bdb693ec8856743550adf
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:24:10 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:24:05"
+lastupdate = "GMT +1: 16.04.2023 22:24:10"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17951,3 +17951,5 @@ addscriptexist(4866692557, "Age Of Heroes [Rep Farm, Orb Farm, Anti Lag]", "pull
 addscriptexist(292439477, "Phantom Forces [Legit Bot, Rage Bot, Esp]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Phantom%20Forces%20Legit%20Bot%20Rage%20Bot%20Esp.lua", "https://rbxscript.com/post/PhantomForcesLegitBotRageBotEsp-26t8j")
 
 addscriptexist(7560156054, "Clicker Simulator [Free Gamepass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Clicker%20Simulator%20Free%20Gamepass.lua", "https://rbxscript.com/post/ClickerSimulatorFreeGamepass-1ryz2")
+
+addscriptexist(5130598377, "A Universal Time [Items Farm - God Mode & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/A%20Universal%20Time%20Items%20Farm%20%20God%20Mode%20%20More.lua", "https://rbxscript.com/post/AUniversalTimeItemsFarmGodModeMore-SxjsG")
