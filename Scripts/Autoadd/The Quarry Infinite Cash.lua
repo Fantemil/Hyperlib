eb685165fa05710b2b20baba6b1e713f24d252ce
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:46:02 +0200

    Updated automatically!

--- Scripts/Autoadd/The Quarry Infinite Cash.lua
+++ Scripts/Autoadd/The Quarry Infinite Cash.lua
@@ -4,3 +4,4 @@ local A_3 = 0.6
 local A_4 = "PickDirt"
 local Event = game:GetService("Workspace").Generation.Purchase
 Event:InvokeServer(A_1, A_2, A_3, A_4)
+-- Make sure you modify the 'local A_1' value to something legit or you may get banned
