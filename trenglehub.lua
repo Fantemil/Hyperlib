af0467db266851730399793c05a78e7818cf11b5
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:06:47 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:06:41"
+lastupdate = "GMT +1: 16.04.2023 22:06:47"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17445,3 +17445,5 @@ addscriptexist(8278412720, "CS Prison Life [AntiCheat Bypass]", "pulled from rbx
 addscriptexist(8278412720, "CS Prison Life [Auto Arrest All]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/CS%20Prison%20Life%20Auto%20Arrest%20All.lua", "https://rbxscript.com/post/CSPrisonLifeAutoArrestAll-6ytDf")
 
 addscriptexist(6512923934, "Ultra Clicker Simulator [AutoTap, UnlimtedGems, UnlimtedFloppa]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Ultra%20Clicker%20Simulator%20AutoTap%20UnlimtedGems%20UnlimtedFloppa.lua", "https://rbxscript.com/post/UltraClickerSimulatorAutoTapUnlimtedGemsUnlimtedFloppa-A9Y9g")
+
+addscriptexist(2788229376, "Da hood [Auto arrest]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20hood%20Auto%20arrest.lua", "https://rbxscript.com/post/DahoodAutoarrest-tOvDj")
