fde9c5e89778e00c5235911998b2e00c2e47d7b8
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:08:27 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:08:18"
+lastupdate = "GMT +1: 16.04.2023 22:08:27"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17493,3 +17493,5 @@ addscriptexist(2686500207, "A Bizarre Day [Loop Kill ]", "pulled from rbxscript.
 addscriptexist(2546155523, "RoVille [Auto Farm Jobs]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/RoVille%20Auto%20Farm%20Jobs.lua", "https://rbxscript.com/post/RoVilleAutoFarmJobs-OfvO2")
 
 addscriptexist(5956785391, "Project Slayers [GUI - Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Slayers%20GUI%20%20Auto%20Farm.lua", "https://rbxscript.com/post/ProjectSlayersGUIAutoFarm-ktnVH")
+
+addscriptexist(2768379856, "3008 [Auto collect food, medkits]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20Auto%20collect%20food%20medkits.lua", "https://rbxscript.com/post/Autocollectfoodmedkits-q5zxu")
