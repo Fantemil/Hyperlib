a5ae0aaaaae083a7297a62d2445f21dc5497045f
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:10:33 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:10:26"
+lastupdate = "GMT +1: 16.04.2023 22:10:33"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17551,3 +17551,5 @@ addscriptexist(10118559731, "nico's nextbots [Serv lagger]", "pulled from rbxscr
 addscriptexist(4616652839, "Shindo life [Auto Farm, Auto Farm Boss, Auto Rank]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shindo%20life%20Auto%20Farm%20Auto%20Farm%20Boss%20Auto%20Rank.lua", "https://rbxscript.com/post/ShindolifeAutoFarmAutoFarmBossAutoRank-5PuW")
 
 addscript(4838460619, "Current Game","War Simulator 2 [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/War%20Simulator%20%20Auto%20Farm.lua", "https://rbxscript.com/post/WarSimulator2AutoFarm-PFF7P")
+
+addscriptexist(6678877691, "ZO ぞ SAMURAI [Anti Cheat Bypass]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/ZO%20%E3%81%9E%20SAMURAI%20Anti%20Cheat%20Bypass.lua", "https://rbxscript.com/post/ZOSAMURAIAntiCheatBypass-mSjty")
