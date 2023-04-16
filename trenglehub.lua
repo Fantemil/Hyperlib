a206affbd385cf2e9aa5a0850bc59cf4b7e01fca
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:33:05 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:33:01"
+lastupdate = "GMT +1: 16.04.2023 21:33:05"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16503,3 +16503,5 @@ addscript(10011532704, "Current Game","Anime Brawl Simulator [Inf Stars]", "pull
 addscriptexist(2653064683, "Word Bomb [Auto Guess]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Word%20Bomb%20Auto%20Guess.lua", "https://rbxscript.com/post/WordBombAutoGuess-v1kDc")
 
 addscriptexist(9285238704, "Race Clicker [Auto Win Race]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Race%20Clicker%20Auto%20Win%20Race.lua", "https://rbxscript.com/post/RaceClickerAutoWinRace-dyeHF")
+
+addscriptexist(2788229376, "Da Hood [Jojo Stand]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Da%20Hood%20Jojo%20Stand.lua", "https://rbxscript.com/post/DaHoodJojoStand-fWLoy")
