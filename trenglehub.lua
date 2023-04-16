b12363125a89d26615111810a6c4c6fd3fc0a469
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:35:18 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:35:14"
+lastupdate = "GMT +1: 16.04.2023 21:35:18"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -16569,3 +16569,5 @@ addscriptexist(10118559731, "nico's nextbots [bloodmoon] [ESP, Door Spam]", "pul
 addscriptexist(8811271345, "Balanced Craftwars Overhaul [Auto Equip, Teleport]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Balanced%20Craftwars%20Overhaul%20Auto%20Equip%20Teleport.lua", "https://rbxscript.com/post/BalancedCraftwarsOverhaulAutoEquipTeleport-rU3jg")
 
 addscriptexist(4863541388, "Scythe Masters [Auto click]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Scythe%20Masters%20Auto%20click.lua", "https://rbxscript.com/post/ScytheMastersAutoclick-6emjw")
+
+addscriptexist(10447882390, "Zombie Research Tycoon [Auto Farm]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Zombie%20Research%20Tycoon%20Auto%20Farm.lua", "https://rbxscript.com/post/ZombieResearchTycoonAutoFarm-XKk2f")
