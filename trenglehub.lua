4c664df7e24e01184d210c8883d5fa635e6bff9f
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:07:20 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:07:16"
+lastupdate = "GMT +1: 16.04.2023 22:07:20"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17461,3 +17461,5 @@ addscriptexist(5993942214, "Rush Point [Silent Aim]", "pulled from rbxscript.com
 addscriptexist(205224386, "Hide And Seek Extreme [Collect all coins, SpeedHack etc.]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hide%20And%20Seek%20Extreme%20Collect%20all%20coins%20SpeedHack%20etc.lua", "https://rbxscript.com/post/HideAndSeekExtremeCollectallcoinsSpeedHacket-KqHNt")
 
 addscriptexist(443406476, "Project Lazarus [Auto Farm, Bring Heads, Gun Mod]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Project%20Lazarus%20Auto%20Farm%20Bring%20Heads%20Gun%20Mod.lua", "https://rbxscript.com/post/ProjectLazarusAutoFarmBringHeadsGunMod-25wOC")
+
+addscriptexist(2809202155, "Your Bizarre Adventure [Teleports,AutoFarm etc]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Your%20Bizarre%20Adventure%20TeleportsAutoFarm%20etc.lua", "https://rbxscript.com/post/YourBizarreAdventureTeleportsAutoFarmetc-J5yek")
