57b4bf0a724f7eee48d1322f49f29c42067a9eac
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:54:30 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 21:54:24"
+lastupdate = "GMT +1: 16.04.2023 21:54:30"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -17103,3 +17103,5 @@ addhub("RBXjus Hub [Games 13]", "pulled from rbxscript.com", "https://raw.github
 addscriptexist(8962384769, "Firework Simulator [GUI - Auto Farm, Auto Buy]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Firework%20Simulator%20GUI%20%20Auto%20Farm%20Auto%20Buy.lua", "https://rbxscript.com/post/FireworkSimulatorGUIAutoFarmAutoBuy-qK9H1")
 
 addscript(9705460681, "Current Game","The Ants Undergrounnd Kingdom [Free Cash]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/The%20Ants%20Undergrounnd%20Kingdom%20Free%20Cash.lua", "https://rbxscript.com/post/TheAntsUndergrounndKingdomFreeCash-eln1J")
+
+addscriptexist(9894044804, "Blade Simulator [GUI - Auto Swing, Auto Sell & More!]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Blade%20Simulator%20GUI%20%20Auto%20Swing%20Auto%20Sell%20%20More.lua", "https://rbxscript.com/post/BladeSimulatorGUIAutoSwingAutoSellMore-BaQea")
