6192450c993cc4e08649c4b82f8256f2e45e34d8
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 21:59:26 +0200

    Updated automatically!

--- Scripts/Autoadd/Pet Simulator X Auto Farm.lua
+++ Scripts/Autoadd/Pet Simulator X Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MV192/pet-sim-x-script/main/Pet%20Simulator%20X"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZaRdoOx/Ultra-Hub/main/Main"))()
