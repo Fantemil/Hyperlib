bfbd3936f918819429825a555e352a38510f9d7c
Emil Toth <emiltoth1870@gmail.com>
2023-04-16 22:45:26 +0200

    Updated automatically!

--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 getgenv().gamecount = 0
 getgenv().scriptcount = 0
-lastupdate = "GMT +1: 16.04.2023 22:45:22"
+lastupdate = "GMT +1: 16.04.2023 22:45:25"
 function getLocalPlayerName()
     local player = game:GetService("Players").LocalPlayer
     return player.Name
@@ -18589,3 +18589,5 @@ addscriptexist(6590798593, "Waterloo at home [Kill All]", "pulled from rbxscript
 addscript(8605603204, "Current Game","Mecha Simulator [Infinite Levels, Coins & Gems]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mecha%20Simulator%20Infinite%20Levels%20Coins%20%20Gems.lua", "https://rbxscript.com/post/MechaSimulatorInfiniteLevelsCoinsGems-JgC8")
 
 addscriptexist(9555732501, "Nen Fighting Simulator [Auto Strength - Auto Durability]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Nen%20Fighting%20Simulator%20Auto%20Strength%20%20Auto%20Durability.lua", "https://rbxscript.com/post/NenFightingSimulatorAutoStrengthAutoDurability-Ol2nE")
+
+addscript(7733392089, "Current Game","LuoBu Transformation Night [Auto Dance]", "pulled from rbxscript.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/LuoBu%20Transformation%20Night%20Auto%20Dance.lua", "https://rbxscript.com/post/LuoBuTransformationNightAutoDance-aQqIU")
