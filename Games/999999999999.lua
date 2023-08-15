209e2a33c2dd7ddb911f6b9401a1c44b75a74ccb
Fantemil <emiltoth1870@gmail.com>
2023-08-15 19:03:37 +0000

    Added Test1

---
+++ Games/999999999999.lua
@@ -0,0 +1,2 @@
+
+addscript(999999999999, "Current Game","Test1", "Test1", "https%3A//raw.githubusercontent.com/Fantemil/Hyperlib/main/Scripts/Autoadd/Test1166607.lua", "https://discord.gg/k6cjJhX5gZ")
