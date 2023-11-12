460ae019c02a0793980efd6dc5d99795e8367a08
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2023-11-12 17:00:34 +0000

    Scriptblox autocrawl Update

--- Games/9961650006.lua
+++ Games/9961650006.lua
@@ -2,3 +2,5 @@
 addscript(9961650006, "Current Game","script instant win", "Uploaded by brucerandy06 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/script%20instant%20win929.lua", "https://scriptblox.com/script/Shockwave-Racing-script-instant-win-6201")
 addscriptexist(9961650006, "Autofarm Boomerang rounds", "Uploaded by Frenchfryguy2011 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Autofarm%20Boomerang%20rounds972.lua", "https://scriptblox.com/script/Shockwave-Racing-Autofarm-Boomerang-rounds-7953")
                                 
+    addscriptexist(9961650006, "Shock wave", "Uploaded by sun_12y on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Shock%20wave8.lua", "https://scriptblox.com/script/Shockwave-Racing-Shock-wave-9240")
+                                    
