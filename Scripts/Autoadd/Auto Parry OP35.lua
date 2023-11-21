836d22b2447653d478fe56a5352945b2fa7b2b31
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2023-11-21 19:19:57 +0000

    Scriptblox autocrawl Update

---
+++ Scripts/Autoadd/Auto Parry OP35.lua
@@ -0,0 +1,6 @@
+-- better notifications, cool looking
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lluadev/Notifications/main/ScriptPopUp"))()
+
+-- auto parry, credits to 1f0yt
+getgenv().visualizer = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/RedCircleBlock"))()
