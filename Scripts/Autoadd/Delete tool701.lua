d1987a6bcdc5713caf6c4ebc3e790f1a7e7376ac
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2023-11-26 06:34:04 +0000

    Scriptblox autocrawl Update

---
+++ Scripts/Autoadd/Delete tool701.lua
@@ -0,0 +1,18 @@
+--credits to thatsnotmatt#2602
+getgenv().keytoclick = "Delete"
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = keytoclick
+tool.Activated:connect(function()
+    local vim = game:service("VirtualInputManager")
+vim:SendKeyEvent(true, keytoclick, false, game)
+end)
+tool.Parent = game.Players.LocalPlayer.Backpack
+wait(0.2)
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
+Notify({
+Description = "thatsnotmatt#2602";
+Title = "zzz#0001";
+Duration = 10;
+});
