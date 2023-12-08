bfee36867195a55384fc7713f5e6beca8300ebd5
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2023-12-08 03:10:28 +0000

    Scriptblox autocrawl Update

---
+++ Scripts/Autoadd/Creator ID Spoofer228.lua
@@ -0,0 +1,11 @@
+function() 
+        game.Players.LocalPlayer.Character.Head:Destroy()
+        if game.CreatorType == Enum.CreatorType.User then
+            game.Players.LocalPlayer.UserId = game.CreatorId
+            end
+            
+            if game.CreatorType == Enum.CreatorType.Group then
+            game.Players.LocalPlayer.UserId = game:GetService("GroupService"):GetGroupInfoAsync(game.CreatorId).Owner.Id
+            end
+    
+    end
