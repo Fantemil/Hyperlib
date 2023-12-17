eaf6b7cc1cf92deee401f460cfd67223c328dce9
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2023-12-17 13:38:52 +0000

    Scriptblox autocrawl Update

--- Games/5561268850.lua
+++ Games/5561268850.lua
@@ -1,2 +1,4 @@
 
 addscript(5561268850, "Current Game","the D roid Defeater", "Uploaded by Tabletopjiver on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/the%20D%20roid%20Defeater333.lua", "https://scriptblox.com/script/Randomly-Generated-Droids-the-D-roid-Defeater-6985")
+    addscriptexist(5561268850, "Autofarm n autowin", "Uploaded by grrrrrr on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Autofarm%20n%20autowin565.lua", "https://scriptblox.com/script/UPDATE-or-Randomly-Generated-Droids-Autofarm-n-autowin-9579")
+                                    
