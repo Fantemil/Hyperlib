7a65230e8fd04f90cbe92c048bcdd9a1f3464915
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2023-12-19 10:44:16 +0000

    Scriptblox autocrawl Update

--- Games/4598019433.lua
+++ Games/4598019433.lua
@@ -2,3 +2,5 @@
 addscript(4598019433, "Current Game","OP AutoFarm", "Uploaded by stonifam on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/OP%20AutoFarm216.lua", "https://scriptblox.com/script/generic-roleplay-gaem.-OP-AutoFarm-4290")
     addscriptexist(4598019433, "generic script only for synapse", "Uploaded by SlOodd0 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/generic%20script%20only%20for%20synapse977.lua", "https://scriptblox.com/script/generic-roleplay-gaem.-gaem-script-3566")
                                 
+    addscriptexist(4598019433, "grg hub", "Uploaded by x2p3m8 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/grg%20hub733.lua", "https://scriptblox.com/script/SALE-generic-roleplay-gaem.-grg-hub-9611")
+                                    
