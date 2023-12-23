9643ff07ea89349f38bf8a158b6cf52604f37576
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2023-12-23 22:57:05 +0000

    Scriptblox autocrawl Update

--- Games/5732973455.lua
+++ Games/5732973455.lua
@@ -2,3 +2,5 @@
 addscript(5732973455, "Current Game","Hours Script Unlock all you need to rejoin", "Uploaded by johanbpinagod on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Hours%20Script%20Unlock%20all%20you%20need%20to%20rejoin168.lua", "https://scriptblox.com/script/NEW-UPDATE-HOURS-Hours-Script-Unlock-all-you-need-to-rejoin-5941")
 addscriptexist(5732973455, "Stat Editor", "Uploaded by Tabletopjiver on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Stat%20Editor995.lua", "https://scriptblox.com/script/HOURS-Stat-Editor-6987")
                                 
+    addscriptexist(5732973455, "Free gamepass", "Uploaded by epic123 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Free%20gamepass870.lua", "https://scriptblox.com/script/HOURS-Free-gamepass-9619")
+                                    
