2d9f3e8f1e3200b46c8f37a5bb58ea0d9de5103f
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2023-12-26 00:03:33 +0000

    Scriptblox autocrawl Update

--- Games/3351674303.lua
+++ Games/3351674303.lua
@@ -28,3 +28,5 @@ addscriptexist(3351674303, "Driving Empire Overpowered Autofarm Car Mods Arrest 
                                     
     addscriptexist(3351674303, "Event script for Candy", "Uploaded by shresthasachin710 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Event%20script%20for%20Candy82.lua", "https://scriptblox.com/script/NEW-BRAND-Driving-Empire-Car-Racing-Event-script-for-Candy-9660")
                                     
+    addscriptexist(3351674303, "WINTERFEST AUTOFARM", "Uploaded by zerticzy on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/WINTERFEST%20AUTOFARM782.lua", "https://scriptblox.com/script/NEW-BRAND-Driving-Empire-Car-Racing-WINTERFEST-AUTOFARM-9662")
+                                    
---
+++ Scripts/Autoadd/WINTERFEST AUTOFARM782.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/cool83birdcarfly02six/DrivingEmpireEvents/main/README.md'),true))()
