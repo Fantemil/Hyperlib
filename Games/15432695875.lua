5ac8d35a20afc0d36b762681c4cd4c3abc2cdb12
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-01 08:23:27 +0000

    Scriptblox autocrawl Update

--- Games/15432695875.lua
+++ Games/15432695875.lua
@@ -2,3 +2,11 @@
     addscript(15432695875, "Current Game","Auto Dig Auto Fish", "Uploaded by escobarecristopher on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Auto%20Dig%20Auto%20Fish764.lua", "https://scriptblox.com/script/Pet-Simulator-99!-Auto-Dig-Auto-Fish-9741")
     addscriptexist(15432695875, "Redz Hub", "Uploaded by chedream1 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Redz%20Hub558.lua", "https://scriptblox.com/script/Pet-Simulator-99!-Redz-Hub-9762")
                                     
+    addscriptexist(15432695875, "Auto Stairway To Heaven", "Uploaded by chedream1 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Auto%20Stairway%20To%20Heaven332.lua", "https://scriptblox.com/script/Pet-Simulator-99!-Auto-Stairway-To-Heaven-9763")
+                                    
+    addscriptexist(15432695875, "Admin GUI", "Uploaded by chedream1 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Admin%20GUI651.lua", "https://scriptblox.com/script/Pet-Simulator-99!-Admin-GUI-9762")
+                                    
+    addscriptexist(15432695875, "Auto Fish Auto Mine", "Uploaded by chedream1 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Auto%20Fish%20Auto%20Mine218.lua", "https://scriptblox.com/script/Pet-Simulator-99!-Auto-Fish-Auto-Mine-9764")
+                                    
+    addscriptexist(15432695875, "Booth Sniper", "Uploaded by chedream1 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Booth%20Sniper574.lua", "https://scriptblox.com/script/Pet-Simulator-99!-Booth-Sniper-9765")
+                                    
---
+++ Scripts/Autoadd/Admin GUI651.lua
@@ -0,0 +1,2 @@
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AKid14/Pet-Sim-99/main/Pet%20Sim%2099%20Admin%20Panel"))()
