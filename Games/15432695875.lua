6f243648fe0a4eeeacf547c86ae937f9ddaaf3f9
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-01 07:23:16 +0000

    Scriptblox autocrawl Update

--- Games/15432695875.lua
+++ Games/15432695875.lua
@@ -1,2 +1,4 @@
 
     addscript(15432695875, "Current Game","Auto Dig Auto Fish", "Uploaded by escobarecristopher on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Auto%20Dig%20Auto%20Fish764.lua", "https://scriptblox.com/script/Pet-Simulator-99!-Auto-Dig-Auto-Fish-9741")
+    addscriptexist(15432695875, "Redz Hub", "Uploaded by chedream1 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Redz%20Hub558.lua", "https://scriptblox.com/script/Pet-Simulator-99!-Redz-Hub-9762")
+                                    
