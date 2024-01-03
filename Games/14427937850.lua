7e3b8eb11acc1094b9042ecb9caf46fef8290b85
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-03 19:31:32 +0000

    Scriptblox autocrawl Update

--- Games/14427937850.lua
+++ Games/14427937850.lua
@@ -1,2 +1,4 @@
 
 addscript(14427937850, "Current Game","script vert funni", "Uploaded by a_random_tf2_enjoyer on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/script%20vert%20funni697.lua", "https://scriptblox.com/script/Troll-Cart-Ride-script-vert-funni-7745")
+    addscriptexist(14427937850, "Very OP", "Uploaded by RobloxScripterOntop on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Very%20OP567.lua", "https://scriptblox.com/script/Troll-Cart-Ride-Very-OP-9787")
+                                    
---
+++ Scripts/Autoadd/Very OP567.lua
@@ -0,0 +1,5 @@
+while true do
+wait (0.0000000000000001)
+game:GetService("ReplicatedStorage").GR:FireServer()
+end
+ 
