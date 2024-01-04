c73dc1d4ef53e72aaf2f12ca6280efb6ee488d82
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-04 09:33:32 +0000

    Scriptblox autocrawl Update

--- Games/15367026228.lua
+++ Games/15367026228.lua
@@ -1,2 +1,4 @@
 
     addscript(15367026228, "Current Game","Auto dungeon Auto Farm and other Good feature", "Uploaded by DinoZ on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Auto%20dungeon%20Auto%20Farm%20and%20other%20Good%20feature762.lua", "https://scriptblox.com/script/UPD-1.5-Anime-Souls-Simulator-X-Auto-dungeon-Auto-Farm-and-other-Good-feature-9686")
+    addscriptexist(15367026228, "Simple Open Source Auto Farm GUI", "Uploaded by Breadido on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Simple%20Open%20Source%20Auto%20Farm%20GUI748.lua", "https://scriptblox.com/script/AURAS-Anime-Souls-Simulator-X-Simple-Open-Source-Auto-Farm-GUI-9810")
+                                    
