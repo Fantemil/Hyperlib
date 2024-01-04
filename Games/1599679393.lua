6dd25f32471b26fae910331bc1100b9df6db33b7
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-04 07:33:17 +0000

    Scriptblox autocrawl Update

--- Games/1599679393.lua
+++ Games/1599679393.lua
@@ -2,3 +2,5 @@
 addscript(1599679393, "Current Game","Auto Farm n Unlock Teleport and more", "Uploaded by KhSaeed90 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Auto%20Farm%20n%20Unlock%20Teleport%20and%20more694.lua", "https://scriptblox.com/script/Pet-Simulator!-Auto-Farm-n-Unlock-Teleport-and-more-7300")
     addscriptexist(1599679393, "Areceus Hub", "Uploaded by blugg11211 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Areceus%20Hub599.lua", "https://scriptblox.com/script/Pet-Simulator!-Areceus-Hub-9802")
                                     
+    addscriptexist(1599679393, "Arceushub", "Uploaded by blugg11211 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Arceushub473.lua", "https://scriptblox.com/script/Pet-Simulator!-Arceushub-9810")
+                                    
---
+++ Scripts/Autoadd/Arceushub473.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BluGG11/ArecusHubZ/main/ArceusHub.lua", true))()
