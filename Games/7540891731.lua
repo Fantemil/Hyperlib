5a60b0951b8bd3866bb45aa30edc33a43822022a
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-08 20:48:12 +0000

    Scriptblox autocrawl Update

--- Games/7540891731.lua
+++ Games/7540891731.lua
@@ -1,2 +1,4 @@
 
 addscript(7540891731, "Current Game"," Light  Light OutlawsHub OP Script", "Uploaded by c00dy on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/%20Light%20%20Light%20OutlawsHub%20OP%20Script376.lua", "https://scriptblox.com/script/Red-Light-Green-Light-OutlawsHub-OP-Script-3805")
+    addscriptexist(7540891731, "Squid Game", "Uploaded by MikeRobloxYTMike on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Squid%20Game435.lua", "https://scriptblox.com/script/Red-Light-Green-Light-(FIX)-Squid-Game-9837")
+                                    
