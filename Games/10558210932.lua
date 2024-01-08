772e265c3df1d2e2ccc56c80bd778d6b8f586b6d
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-08 17:47:49 +0000

    Scriptblox autocrawl Update

--- Games/10558210932.lua
+++ Games/10558210932.lua
@@ -1,2 +1,4 @@
 
 addscript(10558210932, "Current Game","Win All Minigames and Get Gun Script", "Uploaded by r3d_108 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Win%20All%20Minigames%20and%20Get%20Gun%20Script649.lua", "https://scriptblox.com/script/The-Test-Alpha-Win-All-Minigames-and-Get-Gun-Script-5735")
+    addscriptexist(10558210932, "Mike", "Uploaded by MikeRobloxYTMike on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Mike427.lua", "https://scriptblox.com/script/The-Test-Alpha-Mike-9834")
+                                    
