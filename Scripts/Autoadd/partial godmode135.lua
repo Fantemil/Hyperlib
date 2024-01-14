89a3a56182d03d839f68eaf8abc6b85d62ad7c49
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-14 22:07:57 +0000

    Scriptblox autocrawl Update

---
+++ Scripts/Autoadd/partial godmode135.lua
@@ -0,0 +1,5 @@
+while wait() do
+if game:GetService("Players").LocalPlayer.Character.Humanoid.Health < 15 then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-417, 167, 66)
+end
+end
