bbf4b051473da2a48a407831539f4c77ec72b175
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-15 09:09:15 +0000

    Scriptblox autocrawl Update

--- Games/15535115259.lua
+++ Games/15535115259.lua
@@ -1,2 +1,4 @@
 
     addscript(15535115259, "Current Game","Auto Farm", "Uploaded by StellarHub on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Auto%20Farm813.lua", "https://scriptblox.com/script/Launch-Into-Space-Simulator-Auto-Farm-9856")
+    addscriptexist(15535115259, "Requested script", "Uploaded by FahriScripter on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Requested%20script881.lua", "https://scriptblox.com/script/Launch-Into-Space-Simulator-Requested-script-9968")
+                                    
---
+++ Scripts/Autoadd/Requested script881.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/launch-into-space-simulator/main/.lua"))()
