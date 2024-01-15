ba28fcd08f7e0f72a3180fdb3467df7b45520f85
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-15 23:11:02 +0000

    Scriptblox autocrawl Update

---
+++ Scripts/Autoadd/hydroxide for mobile945.lua
@@ -0,0 +1,9 @@
+local owner = "Hosvile"
+local branch = "revision"
+
+local function webImport(file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/MC-Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+end
+
+webImport("init")
+webImport("ui/main")
