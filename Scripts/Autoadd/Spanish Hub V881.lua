7f583337542edd5248b815ecfb53e4aee913635c
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-15 20:10:38 +0000

    Scriptblox autocrawl Update

---
+++ Scripts/Autoadd/Spanish Hub V881.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet(("https://paste.myconan.net/488773.txt"),true))()
+-- It takes about 5-7 seconds to load
---
+++ Scripts/Autoadd/Spanish Hub V9350.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet(('https://paste.myconan.net/488849.txt')))() 
+
+-- Tarda ente 5 - 7 segundos en cargar
