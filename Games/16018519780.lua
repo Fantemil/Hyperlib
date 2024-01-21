35bbb94e56cd09dce6bd1bfa7d003d20a8788652
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-21 12:30:15 +0000

    Scriptblox autocrawl Update

--- Games/16018519780.lua
+++ Games/16018519780.lua
@@ -2,3 +2,5 @@
     addscript(16018519780, "Current Game","Redzhub", "Uploaded by antoneiegamer0 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Redzhub619.lua", "https://scriptblox.com/script/Pet-Simulator-99!-UPDATE-1-Redzhub-10079")
     addscriptexist(16018519780, "Inf Charm", "Uploaded by antoneiegamer0 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Inf%20Charm885.lua", "https://scriptblox.com/script/Pet-Simulator-99!-UPDATE-1-Inf-Charm-10089")
                                     
+    addscriptexist(16018519780, "Anti Stealer TESTED", "Uploaded by antoneiegamer0 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Anti%20Stealer%20TESTED720.lua", "https://scriptblox.com/script/Pet-Simulator-99!-UPDATE-1-Anti-Stealer-TESTED-10089")
+                                    
