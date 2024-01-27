9dd348635ff7fabe45cc7cc79107f526eca2053e
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-01-27 09:49:35 +0000

    Scriptblox autocrawl Update

--- Games/7697611744.lua
+++ Games/7697611744.lua
@@ -2,3 +2,5 @@
 addscript(7697611744, "Current Game","UGc ez", "Uploaded by mightyscripter on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/UGc%20ez322.lua", "https://scriptblox.com/script/Elevator-of-Fun-FREE-UGC!-UGc-ez-8070")
     addscriptexist(7697611744, "Cola spawner", "Uploaded by Minkasig on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Cola%20spawner24.lua", "https://scriptblox.com/script/Elevator-of-Fun-Cola-spawner-9679")
                                     
+    addscriptexist(7697611744, "Garbage spawner", "Uploaded by Minkasig on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Garbage%20spawner333.lua", "https://scriptblox.com/script/Elevator-of-Fun-UPDATE-Garbage-spawner-10205")
+                                    
