188fbfc981649671fe0d553e80292bedf5c828c9
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-02-01 07:04:24 +0000

    Scriptblox autocrawl Update

--- Games/14115013144.lua
+++ Games/14115013144.lua
@@ -2,3 +2,5 @@
     addscript(14115013144, "Current Game","BEST AUTO FARM SCRIPT LATEST", "Uploaded by premscripts on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BEST%20AUTO%20FARM%20SCRIPT%20LATEST873.lua", "https://scriptblox.com/script/Grimoires-Era-RELEASE-BEST-AUTO-FARM-SCRIPT-LATEST-10281")
     addscriptexist(14115013144, "BEST SCRIPT", "Uploaded by OmgHub on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/BEST%20SCRIPT827.lua", "https://scriptblox.com/script/Grimoires-Era-RELEASE-BEST-SCRIPT-10293")
                                     
+    addscriptexist(14115013144, "Demonic HUB V2", "Uploaded by Damech on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Demonic%20HUB%20V2714.lua", "https://scriptblox.com/script/Grimoires-Era-RELEASE-Demonic-HUB-V2-10301")
+                                    
