85bc0484ca35649ccbc0529a86ed3066bf3e40fa
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-09-27 01:49:16 +0000

    Scriptblox autocrawl Update

---
+++ Scripts/Autoadd/Nightmare ACS234.lua
@@ -0,0 +1,19 @@
+_ = {
+    Credits = {
+        Creator = [[ Nightmare Paid was created by 7rab, the best alt control developer on this planet ]],
+        Server = [[ Our discord server is dsc.gg/getnightmare ( That is not discord, that is dsc.gg ) ]]
+    },
+
+    Settings = {
+        Key = "Freemium-OneMonth", -- DO NOT CHANGE THIS / FREEMIUM KEY CHANGE IF U HAVE A LIFETIME KEY
+        Controller = "MainUsername", -- Insert your Controller's username, etc Roblox
+        GUI = true -- Toggle Nightmare GUI, false / true
+    }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/z7rab/nightmare.cc/main/nightmare.cc"))()
+
+--[[
+    Very sorry for the bad placement of commands, but heres the doc site:
+        https://nightmare-4.gitbook.io/nightmare.cc
+]]
--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 
 lastupdate = "GMT +0: 02.02.2024 19:08:52"
-lastupdate = "GMT +0: 27.09.2024 00:48:47"
+lastupdate = "GMT +0: 27.09.2024 01:49:13"
 
 
 
