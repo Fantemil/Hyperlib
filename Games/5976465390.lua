70cbe9f0bee6a574225fa50b4e8fafb3df387184
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-09-28 08:56:54 +0000

    Scriptblox autocrawl Update

--- Games/5976465390.lua
+++ Games/5976465390.lua
@@ -1,2 +1,4 @@
 
     addscript(5976465390, "Current Game","auto abuse", "Uploaded by Orbs on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/auto%20abuse474.lua", "https://scriptblox.com/script/Obby-For-Actual-Owner-Admin!-auto-abuse-18538")
+    addscriptexist(5976465390, "HeadAdmin Script", "Uploaded by D1amond on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/HeadAdmin%20Script20.lua", "https://scriptblox.com/script/Obby-For-Actual-Owner-Admin!-HeadAdmin-Script-19151")
+                                    
--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 
 lastupdate = "GMT +0: 02.02.2024 19:08:52"
-lastupdate = "GMT +0: 28.09.2024 06:56:33"
+lastupdate = "GMT +0: 28.09.2024 08:56:50"
 
 
 
