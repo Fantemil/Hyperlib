40b987bc0370b3022ef1654b14f22805befe8be3
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-02 05:18:06 +0000

    Scriptblox autocrawl Update

--- Games/10449761463.lua
+++ Games/10449761463.lua
@@ -388,3 +388,5 @@ addscriptexist(10449761463, "TSB", "Uploaded by collorem on scriptblox.com", "ht
                                     
     addscriptexist(10449761463, "SUKUNA moveset Not mine", "Uploaded by LPHFN44 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SUKUNA%20moveset%20Not%20mine516.lua", "https://scriptblox.com/script/The-Strongest-Battlegrounds-SUKUNA-moveset-Not-mine-19424")
                                     
+    addscriptexist(10449761463, "GOJO Moveset", "Uploaded by allowed on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/GOJO%20Moveset587.lua", "https://scriptblox.com/script/The-Strongest-Battlegrounds-GOJO-Moveset-19427")
+                                    
--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 
 lastupdate = "GMT +0: 02.02.2024 19:08:52"
-lastupdate = "GMT +0: 02.10.2024 04:17:51"
+lastupdate = "GMT +0: 02.10.2024 05:18:03"
 
 
 
