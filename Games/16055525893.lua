160eb004bcd65f09d873591ca2aadbbfd8d973fe
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-03 22:26:29 +0000

    Scriptblox autocrawl Update

--- Games/16055525893.lua
+++ Games/16055525893.lua
@@ -1,2 +1,4 @@
 
     addscript(16055525893, "Current Game","Auto farm", "Uploaded by kalas on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Auto%20farm824.lua", "https://scriptblox.com/script/RELEASE-RNG-Odyssey-Auto-farm-19410")
+    addscriptexist(16055525893, "Best script", "Uploaded by Rinn on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Best%20script304.lua", "https://scriptblox.com/script/RELEASE-RNG-Odyssey-Best-script-19476")
+                                    
