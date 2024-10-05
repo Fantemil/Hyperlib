66f6269531a603577a7e8f1b72766140639fb472
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-05 06:33:49 +0000

    Scriptblox autocrawl Update

---
+++ Scripts/Autoadd/Nanobypasser784.lua
@@ -0,0 +1,21 @@
+--[[
+    WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+--[[
+    ####################################################################
+
+                         
+   / | / /     /  )       
+  /  |/ /  /  /  /   / / / /  /  / / /  / /
+ / /|  / // / / / / // / // / // / // / // (  |  )  / /
+// |/,// ////, / ./,///_//
+                              /__//
+
+                       // by @_9xr and @kwixx \
+                      // discord.gg/nanobypasser \
+                           // FREE VERSION \
+
+    ####################################################################
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RACISTGY11/robloxscript/refs/heads/main/NanoBypasserFree"))()
