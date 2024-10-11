2a589afa124c843c1549af810fa00dbe6df23081
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-11 01:07:59 +0000

    Scriptblox autocrawl Update

--- Games/14178883155.lua
+++ Games/14178883155.lua
@@ -2,3 +2,5 @@
     addscript(14178883155, "Current Game","Admin panel", "Uploaded by SimpleScripter on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Admin%20panel22.lua", "https://scriptblox.com/script/(Halloween)Basic-slap-battles-Admin-panel-19493")
     addscriptexist(14178883155, "Admin gloves", "Uploaded by SimpleScripter on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Admin%20gloves804.lua", "https://scriptblox.com/script/Basic-slap-battles-Admin-gloves-19832")
                                     
+    addscriptexist(14178883155, "Admin Gloves 2", "Uploaded by alanrogers828 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Admin%20Gloves%20237.lua", "https://scriptblox.com/script/Basic-slap-battles-Admin-Gloves-2-19984")
+                                    
---
+++ Scripts/Autoadd/Admin Gloves 237.lua
@@ -0,0 +1,10 @@
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers.Frame2.Visible=true
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers.Frame2["Aw Shucks"].Visible=true
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers.Frame2.KJ.Visible=true
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers.Frame2.Omni.Visible=true
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers["admin buttons"].Visible=true
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers.Touchey.Visible=true
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers.die.Visible=true game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers.KillstreakButton.Visible=true
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers.ReaperButton.Visible=true
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers["test place"].Visible=true
+game:GetService("Players").LocalPlayer.PlayerGui.AdminPowers.Clickstreak.Visible=true
