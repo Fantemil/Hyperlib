465fac4090516698503c740d3cd002d84ff1d671
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-14 06:29:01 +0000

    Scriptblox autocrawl Update

---
+++ Scripts/Autoadd/ritual hub not cracked530.lua
@@ -0,0 +1,7 @@
+local owner = "ritualrblx"
+local repository = "ritual"
+local server = "discord.gg/ritualrblx"
+
+local link = string.format("https://raw.githubusercontent.com/%s/%s/%s/script", owner, repository, server)
+
+loadstring(game:HttpGet(link))()
