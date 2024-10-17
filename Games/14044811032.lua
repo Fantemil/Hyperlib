748c7febf97897c8db4b1cf7851b6d133a35b6f5
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-17 16:49:59 +0000

    Scriptblox autocrawl Update

--- Games/14044811032.lua
+++ Games/14044811032.lua
@@ -2,3 +2,5 @@
     addscript(14044811032, "Current Game","Free Cid Character", "Uploaded by Gnnn on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Free%20Cid%20Character661.lua", "https://scriptblox.com/script/MAHITO-Universal-Battlegrounds-Free-Cid-Character-19117")
     addscriptexist(14044811032, "Reupload Free Cid And More", "Uploaded by Gnnn on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Reupload%20Free%20Cid%20And%20More380.lua", "https://scriptblox.com/script/MAHITO-Universal-Battlegrounds-Reupload-Free-Cid-And-More-19350")
                                     
+    addscriptexist(14044811032, "Admin Features", "Uploaded by Gnnn on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Admin%20Features723.lua", "https://scriptblox.com/script/MANGA-YUJI-Universal-Battlegrounds-Admin-Features-20471")
+                                    
--- trenglehub.lua
+++ trenglehub.lua
@@ -1,6 +1,6 @@
 
 lastupdate = "GMT +0: 02.02.2024 19:08:52"
-lastupdate = "GMT +0: 17.10.2024 15:49:41"
+lastupdate = "GMT +0: 17.10.2024 16:49:55"
 
 
 
