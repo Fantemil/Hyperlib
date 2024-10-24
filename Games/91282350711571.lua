127e0c3fd69003190a56203b65439d43d18c0aed
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-24 17:30:43 +0000

    Scriptblox autocrawl Update

--- Games/91282350711571.lua
+++ Games/91282350711571.lua
@@ -1,2 +1,4 @@
 
     addscript(91282350711571, "Current Game","SCRIPT REQUEST", "Uploaded by tommerlemm on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/SCRIPT%20REQUEST518.lua", "https://scriptblox.com/script/Mad-City:-Chapter-1-SCRIPT-REQUEST-20605")
+    addscriptexist(91282350711571, "Script hub", "Uploaded by yuragab960 on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Script%20hub230.lua", "https://scriptblox.com/script/Mad-City:-Chapter-1-Script-hub-20971")
+                                    
---
+++ Scripts/Autoadd/Script hub230.lua
@@ -0,0 +1,3 @@
+-- Please use this script with autoexec for the autorob works
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Tech-187/Obfuscated-art/main/Mad%20City%20Chapter%201%20Main%20Script'),true))()
