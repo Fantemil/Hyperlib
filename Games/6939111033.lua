51f998d23b06b867c760d6662668c7a43e4d6b53
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-24 09:28:48 +0000

    Scriptblox autocrawl Update

--- Games/6939111033.lua
+++ Games/6939111033.lua
@@ -2,3 +2,5 @@
     addscript(6939111033, "Current Game","Sword can kill players", "Uploaded by Orbs on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Sword%20can%20kill%20players269.lua", "https://scriptblox.com/script/Escape-Papa-Pizza's-Pizzeria!-(SCARY-OBBY)-Sword-can-kill-players-20497")
     addscriptexist(6939111033, "Get all items", "Uploaded by Zous on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Get%20all%20items11.lua", "https://scriptblox.com/script/Escape-Papa-Pizza's-Pizzeria!-(SCARY-OBBY)-Get-all-items-20504")
                                     
+    addscriptexist(6939111033, "Get All Tools", "Uploaded by ExploitFin on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Get%20All%20Tools510.lua", "https://scriptblox.com/script/Escape-Papa-Pizza's-Pizzeria!-(SCARY-OBBY)-Get-All-Tools-20947")
+                                    
