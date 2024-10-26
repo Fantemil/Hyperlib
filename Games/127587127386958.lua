7b69aaa6a7766ed9b6be4687e177020039669475
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-26 07:40:48 +0000

    Scriptblox autocrawl Update

--- Games/127587127386958.lua
+++ Games/127587127386958.lua
@@ -1,2 +1,4 @@
 
     addscript(127587127386958, "Current Game","Script request", "Uploaded by sosoez on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Script%20request564.lua", "https://scriptblox.com/script/MATCHMAKING-Vision-Script-request-20168")
+    addscriptexist(127587127386958, "Request a script", "Uploaded by itoo on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Request%20a%20script459.lua", "https://scriptblox.com/script/Public-Beta-Vision-Request-a-script-21075")
+                                    
