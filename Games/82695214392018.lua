bf18030de5364e6f2148cb51bbe108c568d5f107
hyperlibbot <stuermisch_hoecker_0n@icloud.com>
2024-10-26 01:39:22 +0000

    Scriptblox autocrawl Update

--- Games/82695214392018.lua
+++ Games/82695214392018.lua
@@ -2,3 +2,5 @@
     addscript(82695214392018, "Current Game","get lantern and badge", "Uploaded by barcon on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/get%20lantern%20and%20badge620.lua", "https://scriptblox.com/script/The-Haunt-get-lantern-and-badge-21010")
     addscriptexist(82695214392018, "Get Shovel Dig All Anti AFK", "Uploaded by r4mpage on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Get%20Shovel%20Dig%20All%20Anti%20AFK761.lua", "https://scriptblox.com/script/The-Haunt-Get-Shovel-Dig-All-Anti-AFK-20987")
                                     
+    addscriptexist(82695214392018, "Submit custom images to  event", "Uploaded by kylosillyy on scriptblox.com", "https://raw.githubusercontent.com/Fantemil/Trenglehub/main/Scripts/Autoadd/Submit%20custom%20images%20to%20%20event499.lua", "https://scriptblox.com/script/The-Haunt-Submit-custom-images-to-event-21065")
+                                    
